8733fab5e326f90783e7f5d892a19b4cf481f2db
Jeremy Faden <jbfaden@users.noreply.github.com>
2003-09-22 18:22:48 +0000

    TimeUtil implements the time methods of pwDate and TimeDatum. Both pwDate and TimeDatumare on thier way out.
    svn path=/dasCore/trunk/; revision=29

--- dasCore/src/edu/uiowa/physics/pw/das/datum/TimeDatum.java
+++ dasCore/src/edu/uiowa/physics/pw/das/datum/TimeDatum.java
@@ -38,59 +38,13 @@ import java.util.TimeZone;
 public class TimeDatum extends Datum {
     
     /** Creates a new instance of TimeDatum */
-    public TimeDatum(double value, Units units) {
+    private TimeDatum(double value, Units units) {
         super(value,units);
     }
     
     public String toString() {
         return edu.uiowa.physics.pw.das.util.DasDate.create(this).toString();
     }    
-    
-    public static TimeDatum create(edu.uiowa.physics.pw.das.util.DasDate date) {
-        edu.uiowa.physics.pw.das.util.DasDate base2000= new edu.uiowa.physics.pw.das.util.DasDate("01/01/2000 00:00");
-        return new TimeDatum(date.subtract(base2000)*1000000,Units.us2000);
-    }
-            
-    public static TimeDatum create(String s) {
-        return TimeDatum.create(new edu.uiowa.physics.pw.das.util.DasDate(s));
-    }
-    
-    public static double convert( int year, int month, int day, int hour, int minute, double second, TimeLocationUnits units ) {
-        // if month==0, then day is doy (day of year).
-        int jd;
-        if ( month>0 ) {
-            jd = 367 * year - 7 * (year + (month + 9) / 12) / 4 -
-            3 * ((year + (month - 9) / 7) / 100 + 1) / 4 +
-            275 * month / 9 + day + 1721029;
-        } else {
-            int month1= 1;            
-            int day1= 1;
-            jd = 367 * year - 7 * (year + (month1 + 9) / 12) / 4 -
-            3 * ((year + (month1 - 9) / 7) / 100 + 1) / 4 +
-            275 * month1 / 9 + day1 + 1721029;
-            jd+= ( day - 1 );
-        }
-            
-        second+= hour*3600.0 + minute*60.0;
-        
-        if ( units==Units.us2000 ) {
-            return (jd-2451545)*86400000000. + second * 1000000;
-        } else {
-            throw new IllegalArgumentException("Not implemented for units: "+units.toString());
-        }        
-    }
-    
-    public static Datum now() {
-        Calendar cal= new GregorianCalendar();
-        cal.setTimeZone(TimeZone.getTimeZone("GMT"));
-        int hour= cal.get(Calendar.HOUR_OF_DAY);
-        int minute= cal.get(Calendar.MINUTE);
-        int second= cal.get(Calendar.SECOND);
-        int year= cal.get(Calendar.YEAR);
-        int doy= cal.get(Calendar.DAY_OF_YEAR);
-        
-        return TimeDatum.create(year+"//"+doy+" "+hour+":"+minute+":"+second);
-    }
     
     public DasFormatter getFormatter( Datum datum2, int nsteps ) {
         TimeDatum datum1= this;
@@ -102,30 +56,10 @@ public class TimeDatum extends Datum {
         return tf;
     }
     
-    public double getSecondsSinceMidnight() {
-        Datum x= this.convertTo(Units.t2000);
-        if (x.getValue()<0) {
-            double xx= ( x.getValue() % 86400 );
-            if (xx==0) {
-                return 0;
-            } else {
-                return 86400-xx;
-            }
-        } else {
-            return x.getValue() % 86400;
-        }
-    }
-   
     public static void main(String[] args) {
-        System.out.println(now());
-        System.out.println( convert(2000,1,1, 0, 0, 0, Units.us2000 ));
-        System.out.println( convert(2000,1,2, 0, 0, 0, Units.us2000 ));
-    }
-    
-    public TimeDatum nextMonth() {
-        DasDate pwdate= DasDate.create(this);
-        pwdate= pwdate.next(DasDate.MONTH);
-        return TimeDatum.create(pwdate);
+        System.out.println( TimeUtil.now() );
+        System.out.println( TimeUtil.convert(2000,1,1, 0, 0, 0, Units.us2000 ));
+        System.out.println( TimeUtil.convert(2000,1,2, 0, 0, 0, Units.us2000 ));
     }
     
 }
