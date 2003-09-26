daf630c4b11b12acde9de78fd1f780f01de83be1
Edward West <edward-west@users.noreply.github.com>
2003-09-26 16:25:32 +0000

    General utility methods for datums.
    svn path=/dasCore/trunk/; revision=54

---
+++ dasCore/src/edu/uiowa/physics/pw/das/datum/DatumUtil.java
@@ -0,0 +1,98 @@
+/* File: DatumUtil.java
+ * Copyright (C) 2002-2003 The University of Iowa
+ *
+ * Created on September 25, 2003, 2:45 PM
+ *      by Edward West <eew@space.physics.uiowa.edu>
+ *
+ * This file is part of the das2 library.
+ *
+ * das2 is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+
+package edu.uiowa.physics.pw.das.datum;
+
+//import edu.uiowa.physics.pw.das.datum.format.*;
+import edu.uiowa.physics.pw.das.util.DasMath;
+
+/**
+ *
+ * @author  Edward West
+ */
+public final class DatumUtil {
+    
+    /** Creates a new instance of DatumUtil */
+    private DatumUtil() {
+    }
+    
+/*
+    public static DatumFormatter bestFormatter(Datum minimum, Datum maximum, int nsteps) {
+        if ( minimum.getUnits() != minimum.getUnits() ) {
+            throw new IllegalArgumentException( "Units don't match!" );
+        }
+        //Swap them if they are in the wrong order.
+        if (maximum.lt(minimum)) {
+            Datum tmp = maximum;
+            maximum = minimum;
+            minimum = tmp;
+        }
+        
+        //Time units we will handle separately
+        if ( minimum.getUnits() instanceof TimeLocationUnits ) {
+            return bestTimeFormatter(minimum, maximum, nsteps);
+        } else {
+            DatumFormatterFactory factory = minimum.getUnits().getDatumFormatterFactory();
+            if (!(factory instanceof DefaultDatumFormatterFactory)) {
+                return factory.defaultFormatter();
+            }
+            double discernable= Math.abs( maximum.subtract(minimum).getValue() / nsteps );
+            int nFraction= -1 * (int)Math.floor(DasMath.log10(discernable));
+            nFraction= nFraction<0 ? 0 : nFraction;
+            String formatString = "0." + zeros(nFraction);
+            try {
+                return factory.newFormatter(formatString);
+            }
+            catch (java.text.ParseException pe) {
+                //Should not happen under normal circumstances, so bail.
+                throw new RuntimeException(pe);
+            }
+        }
+    }
+    
+    private static String zeros(int count) {
+        StringBuffer buff = new StringBuffer(count);
+        for (int index = 0; index < count; index++) {
+            buff.setCharAt(index, '0');
+        }
+        return buff.toString();
+    }
+    
+    public static DatumFormatter bestTimeFormatter(Datum minimum, Datum maximum, int nsteps) {
+        double secondsPerStep = maximum.subtract(minimum).convertTo(Units.seconds).getValue() / nsteps;
+        if (secondsPerStep < 1.) {
+            return TimeDatumFormatter.MICROSECONDS;
+        }
+        else if (secondsPerStep < 60.) {
+            return TimeDatumFormatter.SECONDS;
+        }
+        else if (secondsPerStep < 3600.) {
+            return TimeDatumFormatter.MINUTES;
+        }
+        else {
+            return TimeDatumFormatter.DAYS;
+        }
+    }
+ */
+    
+}
