8b6341a14bdbda2ec95b08384cd9156faa2f032f
Jeremy Faden <jbfaden@users.noreply.github.com>
2003-10-14 17:26:22 +0000

    implement desired function in that the digitizer can send the keystroke to the recorder as a quality flag.
    svn path=/dasCore/trunk/; revision=125

---
+++ dasCore/src/edu/uiowa/physics/pw/das/event/CommentDataPointSelectionEvent.java
@@ -0,0 +1,34 @@
+/*
+ * DataTupleSelectionEvent.java
+ *
+ * Created on October 13, 2003, 11:27 AM
+ */
+
+package edu.uiowa.physics.pw.das.event;
+
+import edu.uiowa.physics.pw.das.datum.Datum;
+import edu.uiowa.physics.pw.das.event.DataPointSelectionEvent;
+
+/**
+ *
+ * @author  jbf
+ */
+public class CommentDataPointSelectionEvent extends DataPointSelectionEvent {
+          
+    String comment;
+    
+    public static CommentDataPointSelectionEvent create( DataPointSelectionEvent e, String comment ) {
+        return new CommentDataPointSelectionEvent( e.getSource(), e.getX(), e.getY(), comment );
+    }
+    
+    /** Creates a new instance of DataTupleSelectionEvent */
+    private CommentDataPointSelectionEvent( Object source, Datum x, Datum y, String comment ) {        
+        super( source, x, y );
+        this.comment= comment;
+    }
+        
+    public String getComment() {
+        return comment;
+    }
+    
+}
