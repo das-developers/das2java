35ce5ebc8ba22f9836342e239b8039f8103d31bf
Jeremy Faden <jbfaden@users.noreply.github.com>
2003-10-22 16:42:36 +0000

    (one version back) axis ticks are drawn so that the brackets along the axis are drawn, like so: | 1992  | 1993 | 1994 |.
    svn path=/dasCore/trunk/; revision=132

--- dasCore/src/edu/uiowa/physics/pw/das/graph/DasLabelAxis.java
+++ dasCore/src/edu/uiowa/physics/pw/das/graph/DasLabelAxis.java
@@ -21,7 +21,7 @@
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  */
 package edu.uiowa.physics.pw.das.graph;
-
+ 
 import edu.uiowa.physics.pw.das.datum.*;
 import edu.uiowa.physics.pw.das.datum.format.DatumFormatter;
 import edu.uiowa.physics.pw.das.graph.DasAxis.TickVDescriptor;
