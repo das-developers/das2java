b21add230a74f38fe914cf6878e8b1e86da76e9f
Jeremy Faden <jbfaden@users.noreply.github.com>
2004-01-05 20:59:47 +0000

    added for use until we have a better fixed-width formatter.
    svn path=/dasCore/trunk/; revision=233

---
+++ dasCore/src/edu/uiowa/physics/pw/das/util/FixedWidthFormatter.java
@@ -0,0 +1,34 @@
+/*
+ * FixedWidthDecimalFormatter.java
+ *
+ * Created on January 5, 2004, 2:30 PM
+ */
+
+package edu.uiowa.physics.pw.das.util;
+
+import java.text.*;
+
+/**
+ *
+ * @author  Jeremy
+ */
+public class FixedWidthFormatter {
+    
+    public static String format( String s, int nchars ) {
+        if ( s.length() < nchars ) {
+            StringBuffer sb= new StringBuffer(nchars-s.length());            
+            for ( int i=0; i<(nchars-s.length()); i++ ) {
+                sb.append(' ');               
+            }            
+            s= sb.toString()+s;
+        } else if ( s.length() > nchars ) {
+            StringBuffer sb= new StringBuffer(nchars);            
+            for ( int i=0; i<(nchars); i++ ) {
+                sb.append('*');               
+            }    
+            s= sb.toString();
+        }       
+        return s;
+    }     
+    
+}
