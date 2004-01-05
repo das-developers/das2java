9a44db07c86b318c98343a5501ca460681c4555c
Jeremy Faden <jbfaden@users.noreply.github.com>
2004-01-05 20:21:26 +0000

    added dumpToAsciiStream, which writes a das2Stream of the data set.
    svn path=/dasCore/trunk/; revision=229

--- dasCore/src/edu/uiowa/physics/pw/das/dataset/VectorUtil.java
+++ dasCore/src/edu/uiowa/physics/pw/das/dataset/VectorUtil.java
@@ -2,6 +2,7 @@ package edu.uiowa.physics.pw.das.dataset;
 
 import edu.uiowa.physics.pw.das.datum.*;
 import java.io.*;
+import java.text.*;
 import java.util.*;
 
 /**
@@ -28,7 +29,7 @@ public class VectorUtil {
         while ( result>0 && xx[result]>x ) result--;
         if ( result<xx.length-2 ) {
             result= ( ( x-xx[result] ) / ( xx[result+1] - xx[result] ) < 0.5 ? result : result+1 );
-        }        
+        }
         return result;
     }
     
@@ -41,8 +42,58 @@ public class VectorUtil {
         return closest( xx, x );
     }
     
-    public static void dumpToStream( VectorDataSet vds, OutputStream out ) {
+    public static void dumpToAsciiStream( VectorDataSet vds, OutputStream out ) {
+        PrintStream pout= new PrintStream(out);
         
+        Datum base=null;
+        Units offsetUnits= null;                 
+        
+        pout.print("[00]");
+        pout.println("<stream start=\""+vds.getXTagDatum(0)+"\" end=\""+vds.getXTagDatum(vds.getXLength()-1)+"\" >");
+        pout.println("<comment>Stream creation date: "+TimeUtil.now().toString()+"</comment>");
+        pout.print("</stream>");
+        
+        if ( vds.getXUnits() instanceof LocationUnits ) {
+            base= vds.getXTagDatum(0);
+            offsetUnits= ((LocationUnits)base.getUnits()).getOffsetUnits();
+            if ( offsetUnits==Units.microseconds ) {
+                offsetUnits= Units.seconds;
+            }                       
+        } 
+
+        
+        pout.print("[01]<packet>\n");
+        pout.print("<x type=\"asciiTab10\" ");
+        if ( base!=null ) {
+            pout.print("base=\""+base+"\" ");
+            pout.print(" xUnits=\""+offsetUnits+"\" ");            
+        } else {
+            pout.print(" xUnits=\""+vds.getXUnits());
+        }
+        pout.println(" />");
+        
+        pout.print("<y type=\"asciiTab10\" yUnits=\""+vds.getYUnits()+"\" />");
+                                
+        NumberFormat xnf= new DecimalFormat("00000.000");
+        NumberFormat ynf= new DecimalFormat("0.000E000");                
+        
+        for (int i=0; i<vds.getXLength(); i++) {
+            pout.print(":01:");
+            double x;
+            if ( base!=null ) {
+                x= vds.getXTagDatum(i).subtract(base).doubleValue(offsetUnits);
+            } else {
+                x= vds.getXTagDouble(i,vds.getXUnits());
+            }
+            pout.print(xnf.format(x)+" ");
+            pout.print(ynf.format(vds.getDouble(i,vds.getYUnits()))+"\n");            
+        }
+        
+        pout.close();
+    }
+    
+
+    public static void dumpToStream( VectorDataSet vds, OutputStream out ) {
         PrintStream pout= new PrintStream(out);
         
         Datum base=null;
@@ -74,7 +125,7 @@ public class VectorUtil {
             }
             pout.print(""+x+tab);
             pout.print(""+vds.getDouble(i,vds.getYUnits()));
-            pout.println();            
+            pout.println();
         }
         
         pout.close();
