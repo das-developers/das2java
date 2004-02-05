21a836fd041d5bdf0eb00324f99e088a92b3e40d
Jeremy Faden <jbfaden@users.noreply.github.com>
2004-02-05 18:21:35 +0000

    added control for what should be done with the image result from each task.
    svn path=/dasCore/trunk/; revision=364

--- dasCore/src/edu/uiowa/physics/pw/das/components/BatchMaster.java
+++ dasCore/src/edu/uiowa/physics/pw/das/components/BatchMaster.java
@@ -10,11 +10,13 @@ import edu.uiowa.physics.pw.das.datum.*;
 import edu.uiowa.physics.pw.das.event.*;
 import edu.uiowa.physics.pw.das.graph.*;
 import edu.uiowa.physics.pw.das.util.*;
+import java.awt.*;
+import java.awt.image.*;
 import java.io.*;
 import java.text.*;
 import java.util.*;
+import java.util.List;
 import javax.swing.*;
-/*import testNew.*;*/
 
 /**
  *
@@ -22,10 +24,11 @@ import javax.swing.*;
  */
 public class BatchMaster {
     
-    List taskList;
+    java.util.List taskList;
     int itask;
     DasCanvas canvas;
-        
+    TaskOutputDescriptor tod;
+    
     public static class Timer {
         long t0= System.currentTimeMillis();;
         DecimalFormat df= new DecimalFormat( "00000.000" );
@@ -38,26 +41,53 @@ public class BatchMaster {
     
     public static Timer timer= new Timer();
     
-    public static BatchMaster create( DasCanvas canvas, File specFile ) throws ParseException, IOException {
+    public interface TaskOutputDescriptor {
+        public void completeTask( Image i, Datum begin, Datum end );
+    }
+    
+    public static TaskOutputDescriptor createPngsTaskOutputDescriptor( final String pngFilenameTemplate ) {
+        return new TaskOutputDescriptor() {
+            public void completeTask( Image image, Datum begin, Datum end ) {
+                String s= pngFilenameTemplate
+                .replaceAll( "BEGIN", begin.toString().replaceAll(":","-") )
+                .replaceAll( "END", end.toString().replaceAll(":","-") );
+                try {
+                    OutputStream out= new FileOutputStream( s );
+                    try {
+                        DasPNGEncoder encoder = new DasPNGEncoder();
+                        encoder.addText(DasPNGConstants.KEYWORD_CREATION_TIME, new Date().toString());
+                        encoder.write((BufferedImage)image, out);
+                    } finally {
+                        out.close();
+                    }
+                } catch ( IOException e ) {
+                    DasExceptionHandler.handle(e);
+                }
+            }
+        };
+    }
+    
+    public static BatchMaster create( DasCanvas canvas, File specFile, String pngFilenameTemplate ) throws ParseException, IOException {
         List taskList= new ArrayList();
         BufferedReader r= new BufferedReader( new FileReader( specFile ) );
         String s= r.readLine();
-        BatchMaster result= new BatchMaster(canvas);
+        BatchMaster result= new BatchMaster(canvas, createPngsTaskOutputDescriptor(pngFilenameTemplate) );
         while ( s!=null ) {
             String[] s1= s.split(" ");
             Datum begin= TimeUtil.create(s1[0]);
-            Datum end= TimeUtil.create(s1[1]);            
+            Datum end= TimeUtil.create(s1[1]);
             result.addTask( begin, end );
             s= r.readLine();
-        }        
+        }
         return result;
     }
     
     /** Creates a new instance of BatchMaster */
-    public BatchMaster( DasCanvas canvas ) {
+    public BatchMaster( DasCanvas canvas, TaskOutputDescriptor tod ) {
         this.canvas= canvas;
         taskList= new ArrayList();
         itask= 0;
+        this.tod= tod;
     }
     
     public void start() {
@@ -66,15 +96,7 @@ public class BatchMaster {
     
     void addTask( Datum begin, Datum end ) {
         taskList.add( new DataRangeSelectionEvent( this, begin, end ) );
-    }    
-    
-    /*void submitFirstTask() {
-        SwingUtilities.invokeLater(new Runnable() {
-            public void run() {
-                submitNextTask();
-            }
-        });
-    }*/
+    }
     
     void submitNextTask() {
         Thread thread= new Thread( new Runnable() {
@@ -83,13 +105,10 @@ public class BatchMaster {
                     DasExceptionHandler.handle( new RuntimeException( "done!" ) );
                 } else {
                     System.out.println( "itask="+taskList.get(itask) );
-                    fireDataRangeSelectionListenerDataRangeSelected( (DataRangeSelectionEvent) taskList.get(itask++) );                    
-                    try {
-                        timer.reportTime("call to writePng");
-                        canvas.writeToPng("J:/temp/"+itask+".png");
-                    } catch ( IOException e ) {
-                        DasExceptionHandler.handle(e);
-                    }
+                    DataRangeSelectionEvent ev=  (DataRangeSelectionEvent) taskList.get(itask++);
+                    fireDataRangeSelectionListenerDataRangeSelected( ev );
+                    Image image= canvas.getImage(canvas.getWidth(),canvas.getHeight());
+                    tod.completeTask( image, ev.getMinimum(), ev.getMaximum() );
                     submitNextTask();
                 }
             }
@@ -100,7 +119,7 @@ public class BatchMaster {
     /** Utility field used by event firing mechanism. */
     private javax.swing.event.EventListenerList listenerList =  null;
     
-        /** Registers DataRangeSelectionListener to receive events.
+    /** Registers DataRangeSelectionListener to receive events.
      * @param listener The listener to register.
      */
     public synchronized void addDataRangeSelectionListener(edu.uiowa.physics.pw.das.event.DataRangeSelectionListener listener) {
@@ -131,18 +150,6 @@ public class BatchMaster {
             }
         }
     }
-
-    /**
-     * @param args the command line arguments
-     */
-    /*
-    public static void main(String[] args) throws Exception {
-        TimeAxisDemo t= new TimeAxisDemo();
-        t.getAxis().setAnimated(false);
-        BatchMaster bm= create( t.getAxis().getCanvas(), new File( "J:/das/working/batchMasterTest.txt" ) );        
-        bm.addDataRangeSelectionListener( t.getAxis() );
-        bm.start();
-    }
-     */
+    
     
 }
