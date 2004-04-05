92e32abd4ea6bf28f533c32bb8df0eb29affe018
Edward West <edward-west@users.noreply.github.com>
2004-04-05 16:00:38 +0000

    Produces valid DAS2 stream for testing purposes
    svn path=/dasCore/trunk/; revision=652

--- dasCore/src/edu/uiowa/physics/pw/das/stream/test/SineWaveStream.java
+++ dasCore/src/edu/uiowa/physics/pw/das/stream/test/SineWaveStream.java
@@ -9,6 +9,9 @@ package edu.uiowa.physics.pw.das.stream.test;
 import edu.uiowa.physics.pw.das.dataset.*;
 import edu.uiowa.physics.pw.das.dataset.test.*;
 import edu.uiowa.physics.pw.das.datum.*;
+import edu.uiowa.physics.pw.das.stream.*;
+import java.nio.channels.*;
+import java.util.*;
 
 /**
  *
@@ -16,23 +19,59 @@ import edu.uiowa.physics.pw.das.datum.*;
  */
 
 /* dumps out a sine wave */
-public class SineWaveStream {          
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) throws Exception {        
-        Datum start, end, resolution;
-        
-        //if ( args.length!=3 ) {
-            start= TimeUtil.createValid( "1979-3-4" );
-            end= TimeUtil.createValid( "1979-3-4T04:00" );
-            resolution= Datum.create(1.,Units.seconds);
-        //}   
-            
-        DataSetDescriptor dsd= new SineWaveDataSetDescriptor( Datum.create(1), Units.seconds.createDatum(120) );
-        DataSet ds= dsd.getDataSet(start, end, resolution, null );                
-        
-        VectorUtil.dumpToAsciiStream((VectorDataSet)ds, System.out );
+public class SineWaveStream {
+    
+    private double frequency = 0.1;
+    private boolean compress;
+    
+    public SineWaveStream(boolean compress) {
+        this.compress = compress;
     }
     
+    public void produceSineWaveStream(WritableByteChannel out) {
+        try {
+            StreamProducer producer = new StreamProducer(out);
+            StreamDescriptor sd = new StreamDescriptor();
+            if (compress) { sd.setCompression("deflate"); }
+            producer.streamDescriptor(sd);
+
+            int nx=3600;
+
+            StreamXDescriptor xDescriptor = new StreamXDescriptor();
+            xDescriptor.setDataTransferType(DataTransferType.getByName("ascii24"));
+            xDescriptor.setUnits(Units.seconds);
+            xDescriptor.setBase(TimeUtil.createValid("2000-001"));
+            StreamMultiYDescriptor y = new StreamMultiYDescriptor();
+            y.setDataTransferType(DataTransferType.getByName("ascii10"));
+            PacketDescriptor pd = new PacketDescriptor();
+            pd.setXDescriptor(xDescriptor);
+            pd.addYDescriptor(y);
+
+            producer.packetDescriptor(pd);
+
+            double[] z = new double[1];
+            DatumVector[] yValues = new DatumVector[1];
+            for (int i = 0; i < nx; i++) {
+                Datum xTag = Datum.create((double)i, Units.seconds);
+                z[0] = Math.sin(Math.PI * frequency * i );
+                yValues[0] = DatumVector.newDatumVector(z, Units.dimensionless);
+                producer.packet(pd, xTag, yValues);
+            }
+
+            producer.streamClosed(sd);
+        }
+        catch (StreamException se) {
+            se.printStackTrace();
+            System.exit(-1);
+        }
+    }
+    
+    public static void main(String[] args) {
+        List argList = Arrays.asList(args);
+        boolean c = argList.contains("-c");
+        WritableByteChannel out = Channels.newChannel(System.out);
+        SineWaveStream rs = new SineWaveStream(c);
+        rs.produceSineWaveStream(out);
+    }
+
 }
