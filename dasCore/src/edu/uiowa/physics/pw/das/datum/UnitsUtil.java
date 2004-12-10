9e306ddce756360b5dda821b7699287b5b605aac
Jeremy Faden <jbfaden@users.noreply.github.com>
2004-12-10 22:52:58 +0000

    useful methods go here to keep the rest of the API clean
    svn path=/dasCore/trunk/; revision=1296

---
+++ dasCore/src/edu/uiowa/physics/pw/das/datum/UnitsUtil.java
@@ -0,0 +1,25 @@
+/*
+ * UnitsUtil.java
+ *
+ * Created on December 1, 2004, 10:25 PM
+ */
+
+package edu.uiowa.physics.pw.das.datum;
+
+/**
+ *
+ * @author  Jeremy
+ */
+public class UnitsUtil {
+    
+    public static Units getInverseUnit( Units unit ) {
+        if ( unit==Units.seconds ) {
+            return Units.hertz;
+        } else if ( unit==Units.dimensionless ) {
+            return Units.dimensionless;
+        } else {
+            throw new IllegalArgumentException( "units not supported: "+unit );
+        }
+    }
+    
+}
