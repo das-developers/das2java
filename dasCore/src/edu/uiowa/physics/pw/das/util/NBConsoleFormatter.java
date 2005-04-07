af0507707456d0234505dcd4c69f714a66feb92c
Jeremy Faden <jbfaden@users.noreply.github.com>
2005-04-07 17:25:23 +0000

    A separate class is needed for the formatter so that it can be referred   to in the logging properties file.
    svn path=/dasCore/trunk/; revision=1554

---
+++ dasCore/src/edu/uiowa/physics/pw/das/util/NBConsoleFormatter.java
@@ -0,0 +1,26 @@
+/*
+ * NBConsoleFormatter.java
+ *
+ * Created on April 7, 2005, 12:13 PM
+ */
+
+package edu.uiowa.physics.pw.das.util;
+
+import java.util.logging.Formatter;
+import java.util.logging.LogRecord;
+
+/**
+ *
+ * @author Jeremy
+ */
+public class NBConsoleFormatter extends Formatter {
+    
+    public String format( LogRecord rec ) {
+        StackTraceElement[] st= new Throwable().getStackTrace();
+        return rec.getLoggerName()+"\n"+rec.getLevel().getLocalizedName()+": "+rec.getMessage()+"\n\tat "+st[7]+"\n\tat "+st[8]+"\n";
+    }
+        
+    public NBConsoleFormatter() {
+    }
+    
+}
