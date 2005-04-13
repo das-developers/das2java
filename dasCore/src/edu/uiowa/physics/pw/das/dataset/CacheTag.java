718d17b93d6029d0935def71a3e893b46907b026
Jeremy Faden <jbfaden@users.noreply.github.com>
2005-04-13 13:45:25 +0000

    Use DatumRange internally. toString method improves readability contains implements the caching logic, that "contains" means within   and having equal or lower resolution
    svn path=/dasCore/trunk/; revision=1570

--- dasCore/src/edu/uiowa/physics/pw/das/dataset/CacheTag.java
+++ dasCore/src/edu/uiowa/physics/pw/das/dataset/CacheTag.java
@@ -4,20 +4,31 @@ import edu.uiowa.physics.pw.das.datum.*;
 
 public class CacheTag {
     
-    Datum start;
-    
-    Datum end;
-    
+    DatumRange range;    
     Datum resolution;
     
     public CacheTag(Datum start, Datum end, Datum resolution) {
-        this.start= start;
-        this.end= end;
+        this( new DatumRange( start, end ), resolution );
+    }
+    
+    public CacheTag( DatumRange range, Datum resolution) {
+        this.range= range;
         this.resolution= resolution;
     }
     
     public String toString() {
-        return start + " " + end + " @ " + DatumUtil.asOrderOneUnits(resolution);
+        return range + " @ " + ( resolution==null ? "intrisic" : ""+DatumUtil.asOrderOneUnits(resolution) );
+    }
+    
+    /**
+     * @returns true if the tag has a lower resolution and its bounds are within
+     * this.
+     */
+    public boolean contains( CacheTag tag ) {
+        return ( this.range.contains( tag.range ) 
+          && ( this.resolution==null 
+              || ( tag.resolution!=null && this.resolution.le( tag.resolution ) ) 
+             ) );            
     }
 }
 
