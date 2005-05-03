1bb0651f7245eec4ebc54d17b019c96165d04534
Jeremy Faden <jbfaden@users.noreply.github.com>
2005-05-03 17:52:07 +0000

    This is used to unify the logic when drawing general curves x(t),y(t)   and curves where (y(x(t)).  Currently, the SymbolLineRenderer doesn't   use the general code because you can't easily optimize.
    svn path=/dasCore/trunk/; revision=1622

---
+++ dasCore/src/edu/uiowa/physics/pw/das/dataset/XTagsVectorDataSet.java
@@ -0,0 +1,77 @@
+/*
+ * XTagsVectorDataSet.java
+ *
+ * Created on December 4, 2004, 11:11 AM
+ */
+
+package edu.uiowa.physics.pw.das.dataset;
+
+import edu.uiowa.physics.pw.das.datum.Datum;
+import edu.uiowa.physics.pw.das.datum.Units;
+
+/**
+ * Create a VectorDataSet that is the X Tags of another DataSet.  This is useful
+ * for unifying the CurveRenderer and SymbolLineRenderer codes.
+ *
+ * @author  Jeremy
+ */
+public class XTagsVectorDataSet implements VectorDataSet {
+    DataSet dataset;
+    /** Creates a new instance of XTagsVectorDataSet */
+    public XTagsVectorDataSet( DataSet dataset ) {
+        this.dataset= dataset;
+    }
+    
+    public Datum getDatum(int i) {
+        return dataset.getXTagDatum(i);
+    }
+    
+    public double getDouble(int i, Units units) {
+        return dataset.getXTagDouble(i,units);
+    }
+    
+    public int getInt(int i, Units units) {
+        return dataset.getXTagInt(i,units);
+    }
+    
+    public DataSet getPlanarView(String planeID) {
+        return dataset.getPlanarView(planeID);
+    }
+    
+    public String[] getPlaneIds() {
+        return dataset.getPlaneIds();
+    }
+    
+    public Object getProperty(String name) {
+        return dataset.getProperty(name);
+    }
+        
+    public java.util.Map getProperties() {
+        return dataset.getProperties();
+    }
+    
+    public int getXLength() {
+        return dataset.getXLength();
+    }
+    
+    public Datum getXTagDatum(int i) {
+        return dataset.getXTagDatum(i);
+    }
+    
+    public double getXTagDouble(int i, Units units) {
+        return dataset.getXTagDouble(i,units);
+    }
+    
+    public int getXTagInt(int i, Units units) {
+        return dataset.getXTagInt(i,units);
+    }
+    
+    public Units getXUnits() {
+        return dataset.getXUnits();
+    }
+    
+    public Units getYUnits() {
+        return dataset.getXUnits();
+    }
+    
+}
