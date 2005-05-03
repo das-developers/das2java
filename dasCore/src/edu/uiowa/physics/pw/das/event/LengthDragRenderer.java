a0e12ba871b1e49246c5116de58be82bd880be2a
Jeremy Faden <jbfaden@users.noreply.github.com>
2005-05-03 17:27:46 +0000

    offset units used to properly, before would crash because of the     mishandling. if rise and run are convertable, then go ahead and display the radial     distance as well.
    svn path=/dasCore/trunk/; revision=1619

--- dasCore/src/edu/uiowa/physics/pw/das/event/LengthDragRenderer.java
+++ dasCore/src/edu/uiowa/physics/pw/das/event/LengthDragRenderer.java
@@ -21,32 +21,57 @@ public class LengthDragRenderer extends LabelDragRenderer {
     DasAxis xaxis, yaxis;
     
     NumberFormat nf;
-  
+    
     /** Creates a new instance of PointSlopeDragRenderer */
     public LengthDragRenderer(DasCanvasComponent parent, DasAxis xaxis, DasAxis yaxis) {
-	super( parent );
+        super( parent );
         this.xaxis= xaxis;
-        this.yaxis= yaxis;    
-        dirtyBounds= new Rectangle();         
+        this.yaxis= yaxis;
+        dirtyBounds= new Rectangle();
     }
     
- 
+    
     public void renderDrag(java.awt.Graphics g1, java.awt.Point p1, java.awt.Point p2) {
-        Graphics2D g= ( Graphics2D ) g1;        
+        Graphics2D g= ( Graphics2D ) g1;
         g1.drawLine( p1.x, p1.y, p2.x, p2.y );
         g1.drawOval(p1.x-1, p1.y-1, 3, 3 ) ;
         
-        dirtyBounds.setRect( p1.x-2, p1.y-2, 5, 5 );        
+        dirtyBounds.setRect( p1.x-2, p1.y-2, 5, 5 );
         dirtyBounds.add(p2.x-2,p2.y-2);
         dirtyBounds.add(p2.x+2,p2.y+2);
         
-        Datum x0= xaxis.invTransform(p2.x+parent.getX());
-        Datum run= x0.subtract(xaxis.invTransform(p1.x+parent.getX()));        
-        Datum y0= yaxis.invTransform(p2.y+parent.getY());
-        Datum rise= y0.subtract(yaxis.invTransform(p1.y+parent.getY()));
+        if ( !p1.equals(p2) ) {
+            Datum x0= xaxis.invTransform(p2.x+parent.getX());
+            Datum run= x0.subtract(xaxis.invTransform(p1.x+parent.getX()));
+            run= DatumUtil.asOrderOneUnits(run);
+            String runString;
+            if ( x0.getUnits()==run.getUnits() ) {
+                runString= x0.getFormatter().format(run) + " " +run.getUnits(); //TODO: when Datum.toString() prints units, this will break
+            } else {
+                runString= run.toString() + " " +run.getUnits() ;
+            }
             
-        if ( !p1.equals(p2) ) {            
-            setLabel( "\u0394x: "+x0.getFormatter().format(run)+" \u0394y: "+y0.getFormatter().format(rise) );            
+            Datum y0= yaxis.invTransform(p2.y+parent.getY());
+            Datum rise= y0.subtract(yaxis.invTransform(p1.y+parent.getY()));
+            String riseString;
+            if ( y0.getUnits()==rise.getUnits() ) {
+                riseString= y0.getFormatter().format(rise) + " " +rise.getUnits();
+            } else {
+                riseString= rise.toString() + " " +rise.getUnits();
+            }
+            
+            String radString;
+            if ( rise.getUnits().isConvertableTo(run.getUnits()) ) {
+                Units u= run.getUnits();
+                double rised= rise.doubleValue(u);
+                double rund= run.doubleValue(u);
+                double rad= Math.sqrt( rised * rised + rund * rund );
+                radString= " " + Datum.create( rad, u ).toString();
+            } else {
+                radString= "";
+            }
+            
+            setLabel( "\u0394x: " + runString + " \u0394y: " + riseString + radString );
         } else {
             setLabel( "" );
         }
