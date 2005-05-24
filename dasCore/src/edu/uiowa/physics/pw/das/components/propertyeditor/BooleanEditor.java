5d70619fd7c9563a07bebe1ceb3b6770182a3ce4
Edward West <edward-west@users.noreply.github.com>
2005-05-24 20:45:47 +0000

    Completely reworked. Modelled after EnumerationEditor
    svn path=/dasCore/trunk/; revision=1726

--- dasCore/src/edu/uiowa/physics/pw/das/components/propertyeditor/BooleanEditor.java
+++ dasCore/src/edu/uiowa/physics/pw/das/components/propertyeditor/BooleanEditor.java
@@ -1,7 +1,7 @@
 /*
  * BooleanEditor.java
  *
- * Created on May 4, 2005, 9:09 AM
+ * Created on April 14, 2005, 9:18 AM
  */
 
 package edu.uiowa.physics.pw.das.components.propertyeditor;
@@ -11,13 +11,23 @@ import java.awt.Graphics;
 import java.awt.Rectangle;
 import java.beans.PropertyChangeListener;
 import java.beans.PropertyChangeSupport;
-import java.beans.PropertyEditor;
+import java.lang.reflect.Field;
+import java.lang.reflect.Modifier;
 import java.util.EventObject;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.IdentityHashMap;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Set;
+import javax.swing.DefaultButtonModel;
+import javax.swing.DefaultListCellRenderer;
 import javax.swing.JCheckBox;
+import javax.swing.JList;
 import javax.swing.JTable;
+import javax.swing.JToggleButton;
 import javax.swing.event.CellEditorListener;
 import javax.swing.event.ChangeEvent;
-import javax.swing.event.ChangeListener;
 import javax.swing.event.EventListenerList;
 import javax.swing.table.TableCellEditor;
 
@@ -25,121 +35,119 @@ import javax.swing.table.TableCellEditor;
  *
  * @author eew
  */
-public class BooleanEditor implements PropertyEditor, TableCellEditor {
+public class BooleanEditor implements java.beans.PropertyEditor, TableCellEditor {
     
     private JCheckBox editor;
+    private Model model;
+    private boolean selected;
+    private Class type;
     private PropertyChangeSupport pcSupport;
-    private Boolean value = Boolean.FALSE;
     private EventListenerList listeners = new EventListenerList();
     
     /** Creates a new instance of BooleanEditor */
     public BooleanEditor() {
-        editor = new JCheckBox();
-        editor.addChangeListener(new ChangeListener() {
-            public void stateChanged(ChangeEvent e) {
-                setValue(Boolean.valueOf(editor.isSelected()));
-                editor.setText(value.toString());
-                fireEditingStopped();
-            }
-        });
         pcSupport = new PropertyChangeSupport(this);
     }
+    
+    private void initEditor() {
+        if (editor == null) {
+            model = new Model();
+            editor = new JCheckBox();
+            editor.setModel(model);
+        }
+    }
+    
+    public String getAsText() {
+        return String.valueOf(selected);
+    }
+
+    public Object getValue() {
+        return selected ? Boolean.TRUE : Boolean.FALSE;
+    }
+
+    public void setAsText(String str) throws IllegalArgumentException {
+        Boolean value;
+        if ("true".equalsIgnoreCase(str)) {
+            value = Boolean.TRUE;
+        }
+        else if ("false".equalsIgnoreCase(str)) {
+            value = Boolean.FALSE;
+        }
+        else {
+            throw new IllegalArgumentException(str);
+        }
+        setValue(value);
+    }
+
+    public void setValue(Object obj) {
+        Boolean oldValue;
+        Boolean value = (Boolean)obj;
+        
+        if (selected ^ value.booleanValue()) {
+            oldValue = selected ? Boolean.TRUE : Boolean.FALSE;
+            selected = value.booleanValue();
+            pcSupport.firePropertyChange("value", oldValue, value);
+        }
+    }
+    
+    public boolean supportsCustomEditor() {
+        return true;
+    }
+    
+    public Component getCustomEditor() {
+        initEditor();
+        return editor;
+    }
+    
+    public String getJavaInitializationString() { return "???"; }
+
+    public String[] getTags() { return null; }
+
+    public boolean isPaintable() { return false; }
+
+    public void paintValue(Graphics g, Rectangle r) {}
 
     public void addPropertyChangeListener(PropertyChangeListener l) {
         pcSupport.addPropertyChangeListener(l);
     }
 
-    public String getAsText() {
-        return value.toString();
-    }
-
-    public Component getCustomEditor() {
-        return editor;
-    }
-
-    public String getJavaInitializationString() {
-        return value.toString();
-    }
-
-    public String[] getTags() {
-        return null;
-    }
-
-    public Object getValue() {
-        return value;
-    }
-
-    public boolean isPaintable() {
-        return false;
-    }
-
-    public void paintValue(Graphics g, Rectangle r) {
-    }
-
     public void removePropertyChangeListener(PropertyChangeListener l) {
         pcSupport.removePropertyChangeListener(l);
     }
-
-    public void setAsText(String str) throws IllegalArgumentException {
-        if ("true".equalsIgnoreCase(str)) {
-            setValue(Boolean.TRUE);
-        }
-        else if ("false".equalsIgnoreCase(str)) {
-            setValue(Boolean.FALSE);
-        }
-        else {
-            throw new IllegalArgumentException(str);
-        }
-    }
-
-    public void setValue(Object obj) {
-        if (!value.equals(obj)) {
-            Object oldValue = value;
-            value = (Boolean)obj;
-            editor.setSelected(value.booleanValue());
-            pcSupport.firePropertyChange("value", oldValue, value);
-        }
-    }
-
-    public boolean supportsCustomEditor() {
-        return true;
-    }
-
-    public void addCellEditorListener(CellEditorListener l) {
-        listeners.add(CellEditorListener.class, l);
-    }
-
-    public void cancelCellEditing() {
-        fireEditingCanceled();
-    }
-
+    
+    /*TableCellEditor stuff*/
+    
     public Object getCellEditorValue() {
-        return value;
+        return selected ? Boolean.TRUE : Boolean.FALSE;
     }
-
+    
     public Component getTableCellEditorComponent(JTable table, Object value, boolean isSelected, int row, int column) {
-        editor.setForeground(isSelected ? table.getSelectionForeground() : table.getForeground());
-        editor.setBackground(isSelected ? table.getSelectionBackground() : table.getBackground());
-        editor.setFont(table.getFont());
+        initEditor();
+        editor.setForeground(table.getForeground());
+        editor.setBackground(table.getBackground());
         setValue(value);
         return editor;
     }
-
-    public boolean isCellEditable(EventObject e) {
-        return true;
-    }
-
-    public void removeCellEditorListener(CellEditorListener l) {
-        listeners.remove(CellEditorListener.class, l);
-    }
-
-    public boolean shouldSelectCell(EventObject e) {
-        return true;
-    }
-
-    public boolean stopCellEditing() {
+    
+    public boolean isCellEditable(EventObject evt) { return true; }
+    
+    public boolean shouldSelectCell(EventObject evt) { return true; }
+    
+    public boolean stopCellEditing() { 
         fireEditingStopped();
         return true;
+    }
+    
+    public void cancelCellEditing() {
+        fireEditingCanceled();
+    }
+    
+    public void addCellEditorListener(CellEditorListener l) {
+        listeners.add(CellEditorListener.class, l);
+    }
+            
+    public void removeCellEditorListener(CellEditorListener l) {
+        listeners.add(CellEditorListener.class, l);
     }
     
     private ChangeEvent evt;
@@ -165,4 +173,16 @@ public class BooleanEditor implements PropertyEditor, TableCellEditor {
             }
         }
     }
+    
+    private class Model extends JToggleButton.ToggleButtonModel {
+        private Model() {}
+        public void setSelected(boolean b) {
+            setValue(b ? Boolean.TRUE : Boolean.FALSE);
+            fireEditingStopped();
+        }
+        public boolean isSelected() {
+            return ((Boolean)getValue()).booleanValue();
+        }
+    }
+
 }
