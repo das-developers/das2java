6d7117434767af5b86dcfdb1649bd5ce3859bf30
Jeremy Faden <jbfaden@users.noreply.github.com>
2005-06-01 16:11:11 +0000

    icons are put in droplist for Enumeration  implementations.
    svn path=/dasCore/trunk/; revision=1760

--- dasCore/src/edu/uiowa/physics/pw/das/components/propertyeditor/EnumerationEditor.java
+++ dasCore/src/edu/uiowa/physics/pw/das/components/propertyeditor/EnumerationEditor.java
@@ -6,6 +6,7 @@
 
 package edu.uiowa.physics.pw.das.components.propertyeditor;
 
+import java.awt.*;
 import java.awt.Component;
 import java.awt.Dimension;
 import java.awt.Graphics;
@@ -24,6 +25,7 @@ import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
+import javax.swing.*;
 import javax.swing.AbstractListModel;
 import javax.swing.ComboBoxModel;
 import javax.swing.DefaultListCellRenderer;
@@ -244,11 +246,16 @@ public class EnumerationEditor implements java.beans.PropertyEditor, TableCellEd
     }
     
     private class Renderer extends DefaultListCellRenderer {
-        public Component getListCellRendererComponent(JList list, Object value, int index, boolean isSelected, boolean hasFocus) {
-            String s = (String)valueMap.get(value);
-            return super.getListCellRendererComponent(list, s, index, isSelected, hasFocus);
+        public Component getListCellRendererComponent(JList list, Object value, int index, boolean isSelected, boolean hasFocus) {                        
+            if ( value instanceof Enumeration ) {
+                JLabel result= new JLabel( value.toString() );
+                result.setIcon(((Enumeration)value).getListIcon());
+                return result;
+            } else {
+                String s = (String)valueMap.get(value);
+                return super.getListCellRendererComponent(list, s, index, isSelected, hasFocus);
+            }            
         }
-        
     }
     
     private class Model extends AbstractListModel implements ComboBoxModel {
