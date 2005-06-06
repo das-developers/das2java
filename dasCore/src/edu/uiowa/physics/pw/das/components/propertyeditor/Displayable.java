d16de2ed3c2eb48eaf625774236be03c82f7a0cb
Jeremy Faden <jbfaden@users.noreply.github.com>
2005-06-06 19:18:58 +0000

    tweak documentation
    svn path=/dasCore/trunk/; revision=1782

---
+++ dasCore/src/edu/uiowa/physics/pw/das/components/propertyeditor/Displayable.java
@@ -0,0 +1,26 @@
+package edu.uiowa.physics.pw.das.components.propertyeditor;
+
+import javax.swing.Icon;
+
+/** Type-safe enumerations that are used as property types
+ * that are editable with a PropertyEditor should
+ * implement this interface.
+ *
+ */
+public interface Displayable {
+    
+    /** return a <code>String</code> that will help the user
+     * identify this item when choosing from a list.
+     */
+    String getListLabel();
+    
+    /** An icon can be provided that will be shown in a list
+     * along with the textual description of the element.
+     * This method should return <code>null</code> if there
+     * is no icon available.
+     *
+     */
+    Icon getListIcon();
+    
+}
+
