670c6ce78a3e06232a3329da3e1fcb1553c49784
Jeremy Faden <jbfaden@users.noreply.github.com>
2005-06-07 21:32:49 +0000

    *** empty log message ***
    svn path=/dasCore/trunk/; revision=1813

---
+++ dasCore/src/edu/uiowa/physics/pw/das/beans/DasAnnotationBeanInfo.java
@@ -0,0 +1,45 @@
+/* File: DasPlotBeanInfo.java
+ * Copyright (C) 2002-2003 The University of Iowa
+ * Created by: Jeremy Faden <jbf@space.physics.uiowa.edu>
+ *             Jessica Swanner <jessica@space.physics.uiowa.edu>
+ *             Edward E. West <eew@space.physics.uiowa.edu>
+ *
+ * This file is part of the das2 library.
+ *
+ * das2 is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+
+package edu.uiowa.physics.pw.das.beans;
+
+import java.beans.BeanInfo;
+
+public class DasAnnotationBeanInfo extends AccessLevelBeanInfo {
+    
+    private static final Property[] properties = {
+        new Property("text", AccessLevel.DASML, "getText", "setText", null),
+    };
+    
+    public DasAnnotationBeanInfo() {
+        super(properties, edu.uiowa.physics.pw.das.graph.DasAnnotation.class);
+    }
+    
+    public java.beans.BeanInfo[] getAdditionalBeanInfo() {
+        BeanInfo[] additional = {
+            new DasCanvasComponentBeanInfo()
+        };
+        return additional;
+    }
+    
+}
