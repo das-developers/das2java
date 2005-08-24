e9547026ee37a10d6f5e6119c0a6884a0f870b86
Jeremy Faden <jbfaden@users.noreply.github.com>
2005-08-24 20:40:35 +0000

    added append method, which combines two cacheTags, when possible.
    svn path=/dasCore/trunk/; revision=1924

--- dasCore/src/edu/uiowa/physics/pw/das/dataset/CacheTag.java
+++ dasCore/src/edu/uiowa/physics/pw/das/dataset/CacheTag.java
@@ -7,6 +7,40 @@ public class CacheTag {
     DatumRange range;    
     Datum resolution;
     
+    public static CacheTag append( CacheTag tag1, CacheTag tag2 ) {
+        Datum res;
+        if ( tag1.resolution==null && tag2.resolution==null ) {
+            res= null;
+        } else {
+            if ( tag1.resolution!=null && tag2.resolution!=null ) {
+                res= tag1.resolution.gt( tag2.resolution ) ? tag1.resolution : tag2.resolution;                
+            } else {
+                res= tag1.resolution==null ? tag2.resolution : tag1.resolution;
+            }
+        }        
+        
+        Datum min;
+        Datum max;
+        if ( !tag1.range.intersects(tag2.range) ) {
+            if ( tag1.range.min().lt( tag2.range.min() ) ) {
+                CacheTag temp= tag1;
+                tag1= tag2;
+                tag2= temp;
+            }
+            if ( tag1.range.max().lt(tag2.range.min()) ) {
+                throw new IllegalArgumentException("cache tags cannot be appended, they are not adjacent");
+            }
+            min= tag1.range.min();
+            max= tag1.range.max();
+        } else {
+            min= tag1.range.min().lt( tag2.range.min() ) ? tag1.range.min() : tag2.range.min();
+            max= tag1.range.max().gt( tag2.range.max() ) ? tag1.range.max() : tag2.range.max();
+        }
+        
+        DatumRange range= new DatumRange( min, max );
+        return new CacheTag( range, res );
+    }
+    
     public CacheTag(Datum start, Datum end, Datum resolution) {
         this( new DatumRange( start, end ), resolution );
     }
