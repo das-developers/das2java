66c48273c4e013777b01213ea0a0aaeb60cba081
Jeremy Faden <jbfaden@users.noreply.github.com>
2005-08-25 22:24:25 +0000

    refactoring to include unified ftp and http filesystems progress monitoring, lazy download of files
    svn path=/dasCore/trunk/; revision=1959

---
+++ dasCore/src/edu/uiowa/physics/pw/das/util/fileSystem/WebFileObject.java
@@ -0,0 +1,164 @@
+/*
+ * WebFile.java
+ *
+ * Created on May 14, 2004, 10:06 AM
+ */
+
+package edu.uiowa.physics.pw.das.util.fileSystem;
+
+import edu.uiowa.physics.pw.das.util.DasProgressMonitor;
+import java.io.*;
+import java.util.*;
+
+/**
+ *
+ * @author  Jeremy
+ *
+ * This is a refactoring of the HttpFileObject, generalized for use with FTP and HTTP file objects.  Note that
+ * the HttpFileObject has not been refactored to use this.
+ *
+ */
+public class WebFileObject extends FileObject {
+    
+    WebFileSystem wfs;
+    String pathname;
+    File localFile;
+    Date modifiedDate;
+    
+    boolean isRoot;
+    boolean isFolder;
+    
+    public boolean canRead() {
+        return true;
+    }
+    
+    public FileObject[] getChildren() {
+        if ( !isFolder ) {
+            throw new IllegalArgumentException(toString()+"is not a folder");
+        }
+        String[] list= wfs.listDirectory( pathname );
+        FileObject[] result= new FileObject[list.length];
+        for ( int i=0; i<result.length; i++ ) {
+            result[i]= new WebFileObject( wfs, list[i], new Date(System.currentTimeMillis() ) );
+        }
+        return result;
+    }
+    
+    public InputStream getInputStream( DasProgressMonitor monitor ) throws FileNotFoundException {
+        if ( isFolder ) {
+            throw new IllegalArgumentException( "is a folder" );
+        }
+        if ( !localFile.exists() ) {
+            try {
+                wfs.transferFile( pathname,localFile,monitor );
+            } catch ( FileNotFoundException e ) {
+                throw e;
+            } catch ( IOException e ) {
+                wfs.handleException(e);
+            }
+        }
+        return new FileInputStream( localFile );
+    }
+    
+    public FileObject getParent() {
+        return new WebFileObject( wfs, wfs.getLocalName( localFile.getParentFile() ), new Date(System.currentTimeMillis()) );
+    }
+    
+    public long getSize() {
+        if ( isFolder ) {
+            throw new IllegalArgumentException("is a folder");
+        }
+        return localFile.length();
+    }
+    
+    public boolean isData() {
+        return !this.isFolder;
+    }
+    
+    public boolean isFolder() {
+        return this.isFolder;
+    }
+    
+    public boolean isReadOnly() {
+        return true;
+    }
+    
+    public boolean isRoot() {
+        return this.isRoot;
+    }
+    
+    public java.util.Date lastModified() {
+        return new java.util.Date(System.currentTimeMillis());
+    }
+    
+    protected File getLocalFile() {
+        return this.localFile;
+    }
+    
+    public boolean exists() {
+        if ( localFile.exists() ) {
+            return true;
+        } else {
+            try {
+                System.err.println("This implementation is not optimal");
+                wfs.transferFile( pathname,localFile, DasProgressMonitor.NULL );
+                return localFile.exists();
+            } catch ( FileNotFoundException e ) {
+                return false;
+            } catch ( IOException e ) {
+                wfs.handleException(e);
+                return false;
+            }
+        }
+    }
+    
+    protected WebFileObject( WebFileSystem wfs, String pathname, Date modifiedDate ) {
+        this.localFile= new File( wfs.getLocalRoot(), pathname );
+        
+        // until we can check for file dates on the server, don't keep cached copies around
+        this.localFile.deleteOnExit();
+        
+        this.wfs= wfs;
+        this.pathname= pathname;
+        if ( !localFile.canRead() ) {
+            if ( wfs.isDirectory( pathname ) ) {
+                localFile.mkdirs();
+                this.isFolder= true;
+                if ( "".equals(pathname) ) {
+                    this.isRoot= true;
+                }
+            } else {
+                this.isFolder= false;
+            }
+        } else {
+            this.isFolder= localFile.isDirectory();
+        }
+        
+    }
+    
+    public String toString() {
+        return "["+wfs+"]"+getNameExt();
+    }
+    
+    public String getNameExt() {
+        return pathname;
+    }
+    
+    public java.nio.channels.Channel getChannel( DasProgressMonitor monitor ) throws FileNotFoundException {
+        return ((FileInputStream)getInputStream( monitor )).getChannel();
+    }
+    
+    public File getFile( DasProgressMonitor monitor ) throws FileNotFoundException {
+        if ( !localFile.exists() ) {
+            try {
+                wfs.transferFile( pathname,localFile, monitor );
+            } catch ( FileNotFoundException e ) {
+                throw e;
+            } catch ( IOException e ) {
+                wfs.handleException(e);
+            }
+        }
+        return localFile;
+    }
+        
+}
---
+++ dasCore/src/edu/uiowa/physics/pw/das/util/fileSystem/WebFileSystem.java
@@ -0,0 +1,126 @@
+/*
+ * WebFileSystem.java
+ *
+ * Created on May 13, 2004, 1:22 PM
+ *
+ * A WebFileSystem allows web files to be opened just as if they were
+ * local files, since it manages the transfer of the file to a local
+ * file system.
+ */
+
+package edu.uiowa.physics.pw.das.util.fileSystem;
+
+import edu.uiowa.physics.pw.das.*;
+import edu.uiowa.physics.pw.das.util.*;
+import java.io.*;
+import java.net.*;
+import java.util.*;
+import java.util.regex.*;
+import javax.swing.*;
+
+/**
+ *
+ * @author  Jeremy
+ */
+public abstract class WebFileSystem extends FileSystem {
+    
+    final File localRoot;
+    
+    /** Creates a new instance of WebFileSystem */
+    protected WebFileSystem(URL root, File localRoot) {
+        super( root );        
+        this.localRoot= localRoot;
+    }
+    
+    static protected File localRoot( URL root ) {
+        File local;
+        if ( System.getProperty("user.name").equals("Web") ) {
+            local= new File("/tmp");
+        } else {
+            local= new File( System.getProperty("user.home") );
+        }
+        local= new File( local, ".das2/fileSystemCache/WebFileSystem/" );
+        
+        local= new File( "c:/webFS/" );
+        local= new File( local, root.getProtocol() );
+        local= new File( local, root.getHost() );
+        local= new File( local, root.getFile() );
+        local.mkdirs();
+        return local;
+    }
+    
+    abstract void transferFile( String filename, File f, DasProgressMonitor monitor ) throws IOException;
+    
+    protected File getLocalRoot() {
+        return this.localRoot;
+    }
+    
+    abstract public boolean isDirectory( String filename );
+    
+    abstract public String[] listDirectory( String directory );
+    
+    public String[] listDirectory( String directory, String regex ) {
+        String[] names= listDirectory( directory );
+        Pattern pattern= Pattern.compile(regex);
+        ArrayList result= new ArrayList();
+        for ( int i=0; i<names.length; i++ ) {                        
+            if ( pattern.matcher(names[i]).matches() ) {
+                result.add(names[i]);
+            }
+        }
+        return (String[])result.toArray(new String[result.size()]);
+    }
+    
+    public URL getURL( String filename ) {
+        try {
+            filename= FileSystem.toCanonicalFilename(filename);
+            return new URL( root+filename.substring(1) );
+        } catch ( MalformedURLException e ) {
+            throw new RuntimeException(e);
+        }
+    }
+    
+    public String getLocalName( File file ) {
+        if ( !file.toString().startsWith(localRoot.toString() ) ) {
+            throw new IllegalArgumentException( "file \""+file+"\"is not of this web file system" );
+        }
+        String filename= file.toString().substring(localRoot.toString().length() );
+        filename= filename.replaceAll( "\\\\", "/" );
+        return filename;
+    }
+    
+    public String getLocalName( URL url ) {
+        if ( !url.toString().startsWith(root.toString() ) ) {
+            throw new IllegalArgumentException( "url \""+url+"\"is not of this web file system" );
+        }
+        String filename= FileSystem.toCanonicalFilename( url.toString().substring(root.toString().length() ) );
+        return filename;
+    }
+    
+    
+    public FileObject getFile( String filename ) {        
+        WebFileObject f= new WebFileObject( this, filename, new Date(System.currentTimeMillis()) );
+        return f;
+    }
+    
+    /**
+     * copies data from in to out, sending the number of bytesTransferred to the monitor.
+     */
+    protected void copyStream( InputStream is, OutputStream out, DasProgressMonitor monitor ) throws IOException {        
+        byte[] buffer= new byte[2048];
+        int bytesRead= is.read( buffer, 0, 2048 );
+        long totalBytesRead= bytesRead;
+        while ( bytesRead>-1 ) {
+            if ( monitor.isCancelled() ) throw new IOException( "operation cancelled" );
+            monitor.setTaskProgress( totalBytesRead );            
+            out.write( buffer, 0, bytesRead );            
+            bytesRead= is.read( buffer, 0, 2048 );         
+            totalBytesRead+= bytesRead;
+        }
+    }
+    
+    public String toString() {
+        return "wfs "+root;
+    }
+    
+}
