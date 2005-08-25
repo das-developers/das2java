e7925b2d4067f6f0ecc40735c38bc4fb06c12f2b
Jeremy Faden <jbfaden@users.noreply.github.com>
2005-08-25 19:33:14 +0000

    support for save and load, but it's hidden until further development
    svn path=/dasCore/trunk/; revision=1932

--- dasCore/src/edu/uiowa/physics/pw/das/components/propertyeditor/PropertyEditor.java
+++ dasCore/src/edu/uiowa/physics/pw/das/components/propertyeditor/PropertyEditor.java
@@ -24,11 +24,11 @@
 package edu.uiowa.physics.pw.das.components.propertyeditor;
 
 import edu.uiowa.physics.pw.das.beans.BeansUtil;
-import edu.uiowa.physics.pw.das.components.DatumEditor;
 import edu.uiowa.physics.pw.das.components.treetable.TreeTableCellRenderer;
 import edu.uiowa.physics.pw.das.components.treetable.TreeTableModel;
+import edu.uiowa.physics.pw.das.dasml.SerializeUtil;
 import edu.uiowa.physics.pw.das.datum.Datum;
-import edu.uiowa.physics.pw.das.graph.PsymConnector;
+import edu.uiowa.physics.pw.das.graph.DasCanvas;
 import edu.uiowa.physics.pw.das.util.DasExceptionHandler;
 import java.awt.*;
 import java.awt.event.ActionEvent;
@@ -37,13 +37,31 @@ import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
-import java.beans.PropertyEditorManager;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.io.StringWriter;
 import java.lang.reflect.InvocationTargetException;
 import java.util.Collections;
 import java.util.HashSet;
 import java.util.Set;
 import javax.swing.*;
 import javax.swing.tree.DefaultTreeModel;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import org.apache.xml.serialize.OutputFormat;
+import org.apache.xml.serialize.XMLSerializer;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.xml.sax.ErrorHandler;
+import org.xml.sax.InputSource;
+import org.xml.sax.SAXException;
 
 
 
@@ -89,8 +107,11 @@ public class PropertyEditor extends JComponent {
     
     private JDialog dialog;
     
-    public PropertyEditor(Object bean) {
+    private Object bean;
+    
+    public PropertyEditor( Object bean ) {
         setLayout(new BorderLayout());
+        this.bean= bean;
         PropertyTreeNode root = new PropertyTreeNode(bean);
         DefaultTreeModel treeModel = new DefaultTreeModel(root, true);
         TreeTableCellRenderer tree = new TreeTableCellRenderer(treeModel);
@@ -99,9 +120,9 @@ public class PropertyEditor extends JComponent {
         TreeTableModel model = new TreeTableModel(root, tree);
         table = new JTable(model);
         add(new JScrollPane(table, JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED, JScrollPane.HORIZONTAL_SCROLLBAR_AS_NEEDED), BorderLayout.CENTER);
-
-        initButtonPanel();
-
+        
+        initButtonPanel( bean instanceof DasCanvas );
+        
         PropertyCellRenderer valueRenderer = new PropertyCellRenderer();
         //PropertyCellEditor editor = new PropertyCellEditor(tree);
         PropertyEditorAdapter editor = new PropertyEditorAdapter();
@@ -115,16 +136,94 @@ public class PropertyEditor extends JComponent {
         table.addMouseListener(new PropertyTableMouseListener());
     }
     
-    private void initButtonPanel() {
-        JPanel buttonPanel = new JPanel(new FlowLayout(FlowLayout.RIGHT));
+    private Action createSaveAction( final Object bean ) {
+        return new AbstractAction("Save") {
+            public void actionPerformed( ActionEvent ev ) {
+                try {
+                    JFileChooser chooser = new JFileChooser();
+                    chooser.setFileFilter( new javax.swing.filechooser.FileFilter() {
+                        public boolean accept(File f ) { return f.toString().matches(".*\\.das2PropertySheet"); }
+                        public String getDescription() { return "*.das2PropertySheet"; }
+                    });
+                    chooser.setSelectedFile( new File( "default.das2PropertySheet" ) );
+                    int result = chooser.showSaveDialog(PropertyEditor.this);
+                    if (result == JFileChooser.APPROVE_OPTION) {
+                        Document document= DocumentBuilderFactory.newInstance().newDocumentBuilder().newDocument();
+                        Element element= SerializeUtil.getDOMElement(document, bean );
+                        document.appendChild( element );
+                        OutputStream out= new FileOutputStream( chooser.getSelectedFile() );
+                        
+                        StringWriter writer = new StringWriter();
+                        OutputFormat format = new OutputFormat(org.apache.xml.serialize.Method.XML, "UTF-8", true);
+                        XMLSerializer serializer = new XMLSerializer( new OutputStreamWriter(out), format);
+                        serializer.serialize(document);
+                        out.close();
+                    }
+                } catch ( Exception e ) {
+                    throw new RuntimeException(e);
+                }
+            }
+        };
+    }
+    
+    private static Document readDocument( File file ) throws IOException, ParserConfigurationException, SAXException {
+        InputStream in= new FileInputStream(file);
+        InputSource source = new InputSource();
+        source.setCharacterStream(new InputStreamReader(in));
+        DocumentBuilder builder;
+        ErrorHandler eh= null;
+        DocumentBuilderFactory domFactory= DocumentBuilderFactory.newInstance();
+        builder = domFactory.newDocumentBuilder();
+        builder.setErrorHandler(eh);
+        Document document= builder.parse(source);
+        return document;
+    }
+       
+    private Action createLoadAction( final Object bean ) {
+        return new AbstractAction("Load") {
+            public void actionPerformed( ActionEvent ev ) {
+                try {
+                    JFileChooser chooser = new JFileChooser();
+                    chooser.setFileFilter( new javax.swing.filechooser.FileFilter() {
+                        public boolean accept(File f ) { return f.toString().matches(".*\\.das2PropertySheet"); }
+                        public String getDescription() { return "*.das2PropertySheet"; }
+                    });
+                    int result = chooser.showOpenDialog(PropertyEditor.this);
+                    if (result == JFileChooser.APPROVE_OPTION) {
+                        try {
+                            Document document= readDocument( chooser.getSelectedFile() );
+                            Element element= document.getDocumentElement();
+                            SerializeUtil.processElement(element,bean );
+                        } catch ( IOException e ) {
+                            throw new RuntimeException(e);
+                        } catch ( ParserConfigurationException e ) {
+                            throw new RuntimeException(e);
+                        } catch ( SAXException e ) {
+                            throw new RuntimeException(e);
+                        }
+                        
+                    }
+                } catch ( Exception e ) {
+                    throw new RuntimeException(e);
+                }
+            }
+        };
+    }
+    private void initButtonPanel( boolean saveLoadButton ) {
+        JPanel buttonPanel = new JPanel(new FlowLayout(FlowLayout.RIGHT));        
+        if ( false ) { //saveLoadButton ) {
+            JButton saveButton=new JButton(createSaveAction(this.bean));
+            buttonPanel.add(saveButton);
+            JButton loadButton=new JButton(createLoadAction(this.bean));
+            buttonPanel.add(loadButton);            
+        }
         final JButton apply = new JButton("Apply Changes");
         closeButton = new JButton("Dismiss");
         ActionListener al = new ActionListener() {
             public void actionPerformed(ActionEvent e) {
                 if (e.getSource() == apply) {
                     globalApplyChanges();
-                }
-                else if (e.getSource() == closeButton) {
+                } else if (e.getSource() == closeButton) {
                     dismissDialog();
                 }
             }
@@ -141,8 +240,7 @@ public class PropertyEditor extends JComponent {
         PropertyTreeNode root = (PropertyTreeNode)model.getRoot();
         try {
             root.flush();
-        }
-        catch (InvocationTargetException ite) {
+        } catch (InvocationTargetException ite) {
             DasExceptionHandler.handle(ite.getCause());
         }
     }
@@ -152,7 +250,7 @@ public class PropertyEditor extends JComponent {
         if (root.isDirty()) {
             String[] message = new String[] {
                 "You have unsaved changes",
-                "Would you like to apply them?"
+                        "Would you like to apply them?"
             };
             int result = JOptionPane.showConfirmDialog(this, message, "", JOptionPane.YES_NO_CANCEL_OPTION, JOptionPane.QUESTION_MESSAGE);
             if (result == JOptionPane.CANCEL_OPTION) {
@@ -171,11 +269,9 @@ public class PropertyEditor extends JComponent {
             Container top = ( c == null ? null : SwingUtilities.getAncestorOfClass(Window.class, c));
             if (top instanceof JFrame) {
                 dialog = new JDialog((JFrame)top);
-            }
-            else if (top instanceof JDialog) {
+            } else if (top instanceof JDialog) {
                 dialog = new JDialog((JDialog)top);
-            }
-            else { 
+            } else {
                 dialog = new JDialog();
             }
             
@@ -197,8 +293,7 @@ public class PropertyEditor extends JComponent {
     public void doLayout() {
         if (SwingUtilities.isDescendingFrom(this, dialog)) {
             closeButton.setVisible(true);
-        }
-        else {
+        } else {
             closeButton.setVisible(false);
         }
         super.doLayout();
