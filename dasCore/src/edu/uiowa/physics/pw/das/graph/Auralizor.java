8ec9a045d6040ac007dd72414eeb6e9d181deeb5
Jeremy Faden <jbfaden@users.noreply.github.com>
2005-09-30 22:07:11 +0000

    Auralizes (sonifies, if you prefer) a vector data set with Units.seconds for the X axis, and Units.dimensionless for the Y axis, which might need to be prescaled...
    svn path=/dasCore/trunk/; revision=2121

---
+++ dasCore/src/edu/uiowa/physics/pw/das/graph/Auralizor.java
@@ -0,0 +1,97 @@
+/*
+ * Auralizor.java
+ *
+ * Created on April 2, 2004, 3:31 PM
+ */
+
+package edu.uiowa.physics.pw.das.graph;
+
+import edu.uiowa.physics.pw.das.*;
+import edu.uiowa.physics.pw.das.dataset.*;
+import edu.uiowa.physics.pw.das.dataset.test.*;
+import edu.uiowa.physics.pw.das.datum.*;
+import edu.uiowa.physics.pw.das.event.*;
+import edu.uiowa.physics.pw.das.graph.*;
+import edu.uiowa.physics.pw.das.system.DasLogger;
+import java.io.*;
+import javax.sound.sampled.*;
+import javax.swing.*;
+import testNew.graph.*;
+
+/**
+ *
+ * @author  Owner
+ */
+public class Auralizor {
+    
+    private static final int	EXTERNAL_BUFFER_SIZE = 1000;
+    byte[] buffer;
+    SourceDataLine line = null;
+    int bufferInputIndex;
+    double min;
+    double max;
+    Units yUnits;
+    
+    VectorDataSet ds;
+    
+    void setDataSet( VectorDataSet ds ) {
+        this.ds= ds;
+    }
+    
+    public void playSound() {
+        float sampleRate=   (float) ( 1. / ds.getXTagDatum(1).subtract(ds.getXTagDatum(0)).doubleValue(Units.seconds)  ) ;
+        DasLogger.getLogger(DasLogger.GRAPHICS_LOG).fine("sampleRate= "+sampleRate);
+        AudioFormat audioFormat= new AudioFormat( sampleRate, 8, 1, true, false );
+        
+        DataLine.Info info = new DataLine.Info(SourceDataLine.class,  audioFormat);
+        try {
+            line = (SourceDataLine) AudioSystem.getLine(info);
+            line.open(audioFormat);
+            line.addLineListener(getLineListener());
+        }
+        catch (Exception e) {
+            throw new RuntimeException(e);            
+        }
+        buffer = new byte[EXTERNAL_BUFFER_SIZE];
+        bufferInputIndex= 0;
+        line.start();
+        
+        int i=0;
+        int ibuf=0;
+        while ( i<ds.getXLength() ) {
+            double d= ds.getDouble(i++,yUnits);
+            int b= (int) ( 256 * ( d - min ) / ( max-min ) );
+            buffer[ibuf++]= (byte)b;
+            if ( ibuf==EXTERNAL_BUFFER_SIZE ) {
+                line.write(buffer, 0, ibuf );
+                ibuf=0;                
+            }
+        }
+        line.write(buffer, 0, ibuf );
+        ibuf=0;
+        
+        line.drain();
+        line.close();
+        
+    }
+    
+    LineListener getLineListener( ) {
+        return new LineListener() {
+            public void update( LineEvent e ) {
+                if ( e.getType().equals( LineEvent.Type.CLOSE ) ) {
+                    
+                }
+            }
+        };
+    }
+    
+    public Auralizor( VectorDataSet ds ) {
+        min= -1;
+        max= 1;
+        yUnits= Units.dimensionless;
+        this.ds= ds;
+    }
+    
+
+    
+}
