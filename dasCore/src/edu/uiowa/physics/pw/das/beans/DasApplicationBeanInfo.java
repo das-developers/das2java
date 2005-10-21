3938d6ac2b425faf857d3fad1265e705bd015dde
Jeremy Faden <jbfaden@users.noreply.github.com>
2005-10-21 21:14:32 +0000

    added dataSetCache property
    svn path=/dasCore/trunk/; revision=2179

--- dasCore/src/edu/uiowa/physics/pw/das/beans/DasApplicationBeanInfo.java
+++ dasCore/src/edu/uiowa/physics/pw/das/beans/DasApplicationBeanInfo.java
@@ -36,6 +36,7 @@ public class DasApplicationBeanInfo extends AccessLevelBeanInfo {
         new Property("reloadLoggingProperties", AccessLevel.DASML, "isReloadLoggingProperties", "setReloadLoggingProperties", null),
         new Property("headless", AccessLevel.DASML, "isHeadless", null, null),
         new Property("applet", AccessLevel.DASML, "isApplet", null, null),
+        new Property("dataSetCache", AccessLevel.DASML, "getDataSetCache", null, null),                        
         new Property("das2Version", AccessLevel.DASML, "getDas2Version", null, null),        
     };
     
