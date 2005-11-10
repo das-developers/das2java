3e4a6419e07c0567da8dbddf0e2b4a55df2c2882
Jeremy Faden <jbfaden@users.noreply.github.com>
2005-11-10 21:43:36 +0000

    deprecated
    svn path=/dasCore/trunk/; revision=2275

--- dasCore/src/edu/uiowa/physics/pw/das/event/BoxRangeSelectorMouseModule.java
+++ dasCore/src/edu/uiowa/physics/pw/das/event/BoxRangeSelectorMouseModule.java
@@ -31,7 +31,7 @@ import edu.uiowa.physics.pw.das.graph.DasPlot;
 import javax.swing.event.EventListenerList;
 
 /**
- *
+ * @depricated  used BoxSelectorMouseModule 
  * @author  jbf
  */
 public class BoxRangeSelectorMouseModule extends MouseModule {
