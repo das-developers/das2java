68287818ffb6f427ed887f213363e5170e2c29c2
Jeremy Faden <jbfaden@users.noreply.github.com>
2006-01-03 22:42:52 +0000

    now implemented with a DasPlot and EventsRenderer
    svn path=/dasCore/trunk/; revision=2350

--- dasCore/src/edu/uiowa/physics/pw/das/graph/DasEventsIndicator.java
+++ dasCore/src/edu/uiowa/physics/pw/das/graph/DasEventsIndicator.java
@@ -7,6 +7,7 @@
 package edu.uiowa.physics.pw.das.graph;
 
 import edu.uiowa.physics.pw.das.*;
+import edu.uiowa.physics.pw.das.components.DasProgressPanel;
 import edu.uiowa.physics.pw.das.dataset.*;
 import edu.uiowa.physics.pw.das.datum.*;
 import edu.uiowa.physics.pw.das.graph.*;
@@ -25,161 +26,36 @@ import javax.swing.event.*;
  * "xTagWidth."  This plane should consist of Datums with the same Units as the xAxis offset Units.
  * The y values of the DataSet will be toString'ed and this value will be the tooltip of the bar.
  */
-public class DasEventsIndicator extends DasCanvasComponent implements DataSetUpdateListener {
-    DasAxis axis;
-    DataSetDescriptor dsd;
-    VectorDataSet vds;
-    String planeId; /* name of the plane to get the event type */
-    int[] eventMap;
+public class DasEventsIndicator extends DasPlot implements DataSetUpdateListener {
+    EventsRenderer renderer;
     
     /** Creates a new instance of DasEventsIndicator */
-    public DasEventsIndicator( DataSetDescriptor dsd, DasAxis axis, String planeId ) {
-        if ( !axis.isHorizontal() ) {
-            throw new IllegalArgumentException( "Axis must be horizontal");
-        }
-        this.axis= axis;
-        this.dsd= dsd;
-        
-        if ( dsd!=null ) dsd.addDataSetUpdateListener(this);
-        
-        axis.addPropertyChangeListener("dataMinimum", getPropertyChangeListener());
-        axis.addPropertyChangeListener("dataMaximum", getPropertyChangeListener());
-        axis.addPropertyChangeListener("log", getPropertyChangeListener());
-        MouseInputAdapter ma= new MyMouseAdapter(this,planeId);
-        addMouseListener( ma );
-        addMouseMotionListener( ma );
+    public DasEventsIndicator( DataSetDescriptor dsd, DasAxis axis, DasAxis yAxis, String planeId ) {
+        super( axis, yAxis );
+        renderer= new EventsRenderer( dsd ) ;
+        addRenderer( renderer );
     }
     
-    private PropertyChangeListener getPropertyChangeListener() {
-        return new PropertyChangeListener() {
-            public void propertyChange(java.beans.PropertyChangeEvent e) {
-                markDirty();
-                update();
-            }
-        };
+    /**
+     * This method replaces the old constructor.  This is unavoidable
+     */
+    public static DasEventsIndicator create( DataSetDescriptor dsd, DasAxis axis, String planeId ) {
+        DasAxis yAxis= new DasAxis( new DatumRange( 0,1, Units.dimensionless ), DasAxis.VERTICAL );
+        yAxis.setVisible(false);
+        return new DasEventsIndicator( dsd, axis, yAxis, planeId );
     }
     
-    class MyMouseAdapter extends MouseInputAdapter {
-        DasEventsIndicator parent;
-        String planeId;
-        
-        MyMouseAdapter( DasEventsIndicator parent, String planeId ) {
-            this.parent= parent;
-            this.planeId= planeId;
-        }
-        
-        public void mouseClicked(MouseEvent e) {
-        }
-        
-        public void mouseMoved(MouseEvent e) {
-            
-            if ( parent.vds==null ) return;
-            if ( parent.vds.getXLength()==0 ) return;
-            
-            int ix= e.getX()+parent.getX();
-            Datum x= parent.axis.invTransform(ix);
-            
-            int i= eventMap[e.getX()];
-            if ( i>=0 ) {
-                Datum sx= parent.vds.getXTagDatum(i);
-                Datum sz= parent.vds.getDatum(i);
-                VectorDataSet widthsDs= (VectorDataSet)vds.getPlanarView("xTagWidth");
-                Datum sy= widthsDs.getDatum(i);
-                parent.setToolTipText(""+sx+" "+sy+" "+sz);
-                
-            } else {
-                parent.setToolTipText(null);
-            }
-        }
-        
-    }
-    
-    public void paintComponent(java.awt.Graphics g1) {
-        
-        if ( dsd==null ) {     
-            DasApplication.getDefaultApplication().getLogger(DasApplication.GRAPHICS_LOG).info("null events data set descriptor");
-            return;
-        }
-        
-        Graphics2D g= ( Graphics2D ) g1.create();
-        g.translate(-getX(), -getY());
-        g.setColor(Color.white);
-        g.fill(getBounds());
-        g.setColor(Color.DARK_GRAY);
-        g.draw(new Rectangle(getX(),getY(),getWidth()-1,getHeight()-1));
-        
-        g.setColor(new Color(100,100,100,180));
-        
-        eventMap= new int[getWidth()];        
-        
-        try {
-            vds= (VectorDataSet)dsd.getDataSet( axis.getDataMinimum(), axis.getDataMaximum(), null, null );
-            if ( vds.getPlanarView("xTagWidth")==null ) {
-                throw new IllegalArgumentException("no xTagWidth plane found.");
-            }
-            VectorDataSet widthsDs= (VectorDataSet)vds.getPlanarView("xTagWidth");
-            for ( int k=0; k<eventMap.length; k++ ) eventMap[k]= -1;
-            if ( vds.getXLength()>0 ) {
-                UnitsConverter uc=  UnitsConverter.getConverter( widthsDs.getYUnits(), axis.getUnits().getOffsetUnits() );
-                
-                int ivds0= 0;
-                int ivds1= vds.getXLength();
-                for ( int i=ivds0; i<ivds1; i++ ) {
-                    Datum x= vds.getXTagDatum(i);
-                    int ix= (int)axis.transform(x);
-                    int iwidth;
-                    if ( uc!=null ) {
-                        Datum y= widthsDs.getDatum(i);
-                        iwidth= (int)axis.transform( x.add( y ) ) - ix;
-                    } else {
-                        iwidth= 1;
-                    }
-                    if ( getColumn().getDMinimum() < ( ix+iwidth ) || 
-                        getColumn().getDMaximum() > (ix) ) {
-                        if ( iwidth==0 ) iwidth=1;
-                        g.fill( new Rectangle( ix, getY(), iwidth, getHeight() ) );
-                        int im= ix-getX();
-                        int em0= im-1;
-                        int em1= im+iwidth+1;
-                        for ( int k=em0; k<em1; k++ ) {
-                            if ( k>=0 && k<eventMap.length ) eventMap[k]= i;
-                        }
-                    }
-                }
-                for ( int k1=1; k1<=2; k1++ ) { /* add fuzziness using Larry's algorithm */
-                    for ( int k2=-1; k2<=1; k2+=2 ) {
-                        int em0= ( k2==1 ) ? 0 : eventMap.length-1;
-                        int em1= ( k2==1 ) ? eventMap.length-k1 : k1;
-                        for ( int k=em0; k!=em1; k+=k2) {
-                            if ( eventMap[k]==-1 ) eventMap[k]= eventMap[k+k2];
-                        }
-                    }
-                }
-            }
-            
-        } catch ( DasException e ) {
-            g.drawString( "exception: "+e.getMessage(), getX(), getY()+getHeight() );
-        } finally {
-            g.dispose();
-            getMouseAdapter().paint(g1);
-        }
-    }
     
     public void setDataSetDescriptor( DataSetDescriptor dsd ) {
-        if ( this.dsd!=null ) this.dsd.removeDataSetUpdateListener(this);
-        this.dsd= dsd;
-        if ( dsd!=null ) this.dsd.addDataSetUpdateListener(this);
-        markDirty();
-        update();
+        renderer.setDataSetDescriptor(dsd);
     }
     
     public DataSetDescriptor getDataSetDescriptor( ) {
-        return this.dsd;
+        return renderer.getDataSetDescriptor();
     }
     
     public void dataSetUpdated(DataSetUpdateEvent e) {
-        markDirty();
-        update();
+        ((XAxisDataLoader)renderer.getDataLoader()).dataSetUpdated(e);
     }
     
 }
