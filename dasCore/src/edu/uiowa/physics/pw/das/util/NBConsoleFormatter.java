fe81b82b75a732516f988d44f7f9b39a1773bd8a
Jeremy Faden <jbfaden@users.noreply.github.com>
2006-01-04 21:00:01 +0000

    coalesce repeated messages.
    svn path=/dasCore/trunk/; revision=2359

--- dasCore/src/edu/uiowa/physics/pw/das/util/NBConsoleFormatter.java
+++ dasCore/src/edu/uiowa/physics/pw/das/util/NBConsoleFormatter.java
@@ -14,11 +14,24 @@ import java.util.logging.LogRecord;
  * @author Jeremy
  */
 public class NBConsoleFormatter extends Formatter {
+    boolean coalesce=true;
+    String lastMessage=null;
+    int coalesceHits=0;
     
     public String format( LogRecord rec ) {
-        StackTraceElement[] st= new Throwable().getStackTrace();
-        String result= rec.getLoggerName()+"\n"+rec.getLevel().getLocalizedName()+": "+rec.getMessage()+"\n\tat "+st[7]+"\n\tat "+st[8]+"\n";
-        return result;
+        if ( coalesce && lastMessage!=null && lastMessage.equals(rec.getMessage()) ) {
+            coalesceHits++;
+            return "";
+        } else {
+            StackTraceElement[] st= new Throwable().getStackTrace();
+            String result= rec.getLoggerName()+"\n"+rec.getLevel().getLocalizedName()+": "+rec.getMessage()+"\n\tat "+st[7]+"\n\tat "+st[8]+"\n";
+            if ( coalesceHits>0 ) {
+                result= "(Last message repeats "+(coalesceHits+1)+" times)\n"+result;
+            }
+            coalesceHits= 0;
+            lastMessage= rec.getMessage();
+            return result;
+        }
     }
         
     public NBConsoleFormatter() {
