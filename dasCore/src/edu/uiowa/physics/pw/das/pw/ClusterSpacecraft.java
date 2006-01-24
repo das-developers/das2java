d6a24791188eb219361f16b1a99391078dbf6e71
Jeremy Faden <jbfaden@users.noreply.github.com>
2006-01-24 21:37:15 +0000

    Classes specific to the Plasma Wave group
    svn path=/dasCore/trunk/; revision=2399

---
+++ dasCore/src/edu/uiowa/physics/pw/das/pw/ClusterSpacecraft.java
@@ -0,0 +1,59 @@
+package edu.uiowa.physics.pw.das.pw;
+
+import java.awt.*;
+
+public class ClusterSpacecraft extends Spacecraft.GroupSpacecraft {        
+    
+    protected ClusterSpacecraft(String id, int number) {
+        super( "Cluster2", id, number );
+    }
+    
+    /**
+     * returns the Esa Number 1,2,3 or 4 for the Cluster Spacecraft 
+     */
+    public int getEsaNumber() {
+        return number;
+    }
+    
+    
+    /**
+     * returns the Cluster Spacecraft for the Esa Number 1,2,3 or 4 
+     */
+    public static ClusterSpacecraft getByEsaNumber(int number) {
+        return (ClusterSpacecraft)getByNumber("Cluster2",number);
+    }
+    
+    /**
+     * returns the Cluster Spacecraft for the wideband status byteEsa 4,5,6,7     
+     */
+    public static ClusterSpacecraft getByWbdStatusByte( int number ) {      
+        if (true) throw new IllegalArgumentException("looks like there's a bug here");
+        final int[] esaFromStatus= new int[] { 0, 0, 0, 0, 2, 3, 4, 1 };        
+        return getByEsaNumber( esaFromStatus[number] );
+    }
+    
+    public int getInstrumentNumber() {
+        final int instFromEsa[] = new int[] { 0, 9, 6, 7, 8 };
+        return instFromEsa[ getEsaNumber() ];
+    }
+        
+    public int getDsnNumber() {
+        final int dsnFromEsa[] = new int[] { 0, 183, 185, 194, 196 };
+        return dsnFromEsa[ getEsaNumber() ];
+    }
+    
+    public String getName() {
+        final String[] nameFromEsa= new String[] { "", "Rumba", "Salsa", "Samba", "Tango" };
+        return nameFromEsa[ getEsaNumber() ];
+    }
+    
+    public Color getColor() {
+        final Color[] colorFromEsa= new Color[] { null, Color.BLACK, Color.RED, Color.GREEN, Color.MAGENTA };
+        return colorFromEsa[ getEsaNumber() ];
+    }
+        
+    public String toString() {
+        return "Cluster " + getEsaNumber();
+    }
+}
+
