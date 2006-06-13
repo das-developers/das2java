cd8f08c29430df4f32ecabf01085e007fbf6c848
Jeremy Faden <jbfaden@users.noreply.github.com>
2006-06-13 16:17:17 +0000

    uses Logger instead of System.out for messages.
    svn path=/dasCore/trunk/; revision=2675

--- dasCore/src/edu/uiowa/physics/pw/das/util/awt/LoggingEventQueue.java
+++ dasCore/src/edu/uiowa/physics/pw/das/util/awt/LoggingEventQueue.java
@@ -8,24 +8,66 @@
 
 package edu.uiowa.physics.pw.das.util.awt;
 
+import edu.uiowa.physics.pw.das.system.DasLogger;
+import java.awt.AWTEvent;
 import java.awt.EventQueue;
 import java.awt.event.InvocationEvent;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.logging.Logger;
 
 /**
- * Tool for debugging event queue stuff.  This can be used to log the event 
+ * Tool for debugging event queue stuff.  This can be used to log the event
  * queue, or insert breakpoints, etc.
  * Toolkit.getDefaultToolkit().getSystemEventQueue().push(new LoggingEventQueue());
  *
  * @author Jeremy
- * 
+ *
  */
 public class LoggingEventQueue extends EventQueue {
+    
+    final static Logger logger= DasLogger.getLogger(DasLogger.GUI_LOG);
+    
+    Object lockObject= new String("lockObjectLEQ");
+    
+    private LoggingEventQueue() {
+        
+    }
+    
     public void postEvent(java.awt.AWTEvent theEvent) {
         if (theEvent instanceof InvocationEvent) {
-            System.out.print("XXX");
+         //   logger.info("XXX: "+theEvent);
+        } else {
+        //    logger.info(""+theEvent);
         }
-        System.out.println(""+theEvent);
         super.postEvent(theEvent);
+    }
+    
+    private static LoggingEventQueue instance;
+    public static LoggingEventQueue getInstance() {
+        if ( instance==null ) {
+            instance= new LoggingEventQueue();
+        }
+        return instance;
+    }
+    
+    public static synchronized void dumpPendingEvents() {
+        System.err.println("---------------------------------------------------------------");
+        Queue queue= new LinkedList();
+        AWTEvent evt;
+        while ( instance.peekEvent()!=null ) {
+            try {
+                evt= instance.getNextEvent();
+            } catch (InterruptedException ex) {
+                throw new RuntimeException(ex);
+            }
+            System.err.println("[ "+evt);
+            queue.add(evt);
+        }
+        System.err.println("-----e--n--d-----------------------------------------------------");
+        while ( queue.size() > 0 ) {
+            instance.postEvent((AWTEvent)queue.remove());
+        }
     }
     
 }
