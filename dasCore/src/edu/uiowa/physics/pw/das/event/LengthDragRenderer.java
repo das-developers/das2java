798920ee2dfd553ff76e2157a9f2dc6202969afd
Jeremy Faden <jbfaden@users.noreply.github.com>
2006-10-02 21:24:17 +0000

    support install new axis in DasPlot by allowing xaxis and yaxis to be null, indicating that plot.getXAxis() and plot.getYAxis() should be used.
    svn path=/dasCore/trunk/; revision=2797

--- dasCore/src/edu/uiowa/physics/pw/das/event/LengthDragRenderer.java
+++ dasCore/src/edu/uiowa/physics/pw/das/event/LengthDragRenderer.java
@@ -20,17 +20,27 @@ import java.text.*;
  */
 public class LengthDragRenderer extends LabelDragRenderer {
     
-    DasAxis xaxis, yaxis;
+    private DasAxis xaxis, yaxis;
+    private DasPlot plot;
     
     NumberFormat nf;
     
     /** Creates a new instance of PointSlopeDragRenderer */
     public LengthDragRenderer(DasCanvasComponent parent, DasAxis xaxis, DasAxis yaxis) {
         super( parent );
+        this.plot= (DasPlot)parent;
         this.xaxis= xaxis;
-        this.yaxis= yaxis;        
+        this.yaxis= yaxis;
     }
     
+    // kludge to handle the fact that datums are not always displayed with their units!!!
+    private String datumString( Datum d ) {
+        String result= d.toString();
+        /*if ( d.getUnits()!= Units.dimensionless && !d.getUnits().isConvertableTo(Units.us2000) ) {
+            result+= " "+d.getUnits();
+        }*/
+        return result;
+    }
     
     public Rectangle[] renderDrag(java.awt.Graphics g1, java.awt.Point p1, java.awt.Point p2) {
         Graphics2D g= ( Graphics2D ) g1;
@@ -46,21 +56,25 @@ public class LengthDragRenderer extends LabelDragRenderer {
         myDirtyBounds.add(p2.x-2,p2.y-2);
         myDirtyBounds.add(p2.x+2,p2.y+2);
         
+        DasAxis xa= xaxis == null ? plot.getXAxis() : xaxis;
+        DasAxis ya= yaxis == null ? plot.getYAxis() : yaxis;
+        
         if ( !p1.equals(p2) ) {
-            Datum x0= xaxis.invTransform(p2.x+parent.getX());
-            Datum run= x0.subtract(xaxis.invTransform(p1.x+parent.getX()));
+            Datum x0= xa.invTransform(p2.x+parent.getX());
+            Datum run= x0.subtract(xa.invTransform(p1.x+parent.getX()));
             run= DatumUtil.asOrderOneUnits(run);
             String runString;
             if ( x0.getUnits()==run.getUnits() ) {
-                runString= x0.getFormatter().format(run); 
+                runString= x0.getFormatter().format(run);
             } else {
-                runString= run.toString();
+                runString= datumString(run);
             }
             
-            Datum y0= yaxis.invTransform(p2.y+parent.getY());
-            Datum rise= y0.subtract(yaxis.invTransform(p1.y+parent.getY()));
+            Datum y0= ya.invTransform(p2.y+parent.getY());
+            Datum rise= y0.subtract(ya.invTransform(p1.y+parent.getY()));
+
             String riseString;
-            riseString= rise.toString();
+            riseString= datumString(rise);
             
             String radString;
             if ( rise.getUnits().isConvertableTo(run.getUnits()) ) {
@@ -70,7 +84,7 @@ public class LengthDragRenderer extends LabelDragRenderer {
                 double rad= Math.sqrt( rised * rised + rund * rund );
                 double srised= rise.getResolution(u);
                 double srund= run.getResolution(u);
-                double res= rad * Math.sqrt( Math.pow( srised / Math.max(  Math.abs(rised), srised ), 2 )  + 
+                double res= rad * Math.sqrt( Math.pow( srised / Math.max(  Math.abs(rised), srised ), 2 )  +
                         Math.pow( srund / Math.max( Math.abs( rund ), srund ), 2 ) );
                 Datum radDatum= Datum.create( rad, u, res );
                 
@@ -82,9 +96,9 @@ public class LengthDragRenderer extends LabelDragRenderer {
             String label= "\u0394x: " + runString + " \u0394y: " + riseString + radString ;
             
             if ( showSlope ) {
-                  label += "!c m: "+ UnitsUtil.divideToString( rise, run );
+                label += "!c m: "+ UnitsUtil.divideToString( rise, run );
             }
-                
+            
             setLabel( label );
         } else {
             setLabel( "" );
@@ -92,12 +106,12 @@ public class LengthDragRenderer extends LabelDragRenderer {
         super.renderDrag( g, p1, p2 );
         return new Rectangle[] { dirtyBounds, myDirtyBounds };
     }
-
+    
     /**
      * Holds value of property showSlope.
      */
     private boolean showSlope= false;
-
+    
     /**
      * Getter for property showSlope.
      * @return Value of property showSlope.
@@ -105,7 +119,7 @@ public class LengthDragRenderer extends LabelDragRenderer {
     public boolean isShowSlope() {
         return this.showSlope;
     }
-
+    
     /**
      * Setter for property showSlope.
      * @param showSlope New value of property showSlope.
