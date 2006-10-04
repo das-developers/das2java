0c712a37a4d9a1eac1e917a11b38cd9b7fe63337
Jeremy Faden <jbfaden@users.noreply.github.com>
2006-10-04 21:29:36 +0000

    1. improve javadoc. 2. introduce isRatiometric and isTimeLocation. 3. bugfix in divideToString. Bug was not reachable, so it had no impact.
    svn path=/dasCore/trunk/; revision=2808

--- dasCore/src/edu/uiowa/physics/pw/das/datum/UnitsUtil.java
+++ dasCore/src/edu/uiowa/physics/pw/das/datum/UnitsUtil.java
@@ -11,10 +11,33 @@ package edu.uiowa.physics.pw.das.datum;
  * @author  Jeremy
  */
 public class UnitsUtil {
-
+    
+    /**
+     * returns true if the unit is used to measure distance in a logarithmic
+     * space, such as decibels or percent increase.  Note Units.dimensionless
+     * are not considered ratiometric.  (Of course, all ratiometic
+     * units are dimensionless...)
+     */
+    public static final boolean isRatiometric( Units unit ) {
+        return unit!=Units.dimensionless && unit.isConvertableTo(Units.logERatio);
+    }
+    
+    /**
+     * returns true if the unit describes a location in time, as in us2000.
+     */
+    public static final boolean isTimeLocation( Units unit ) {
+        return unit.isConvertableTo(Units.us2000);
+    }
+    
+    /**
+     * returns the unit whose product with the parameter unit is
+     * unity.
+     */
     public static Units getInverseUnit( Units unit ) {
         if ( unit==Units.seconds ) {
             return Units.hertz;
+        } else if ( unit==Units.hertz ) {
+            return Units.seconds;
         } else if ( unit==Units.dimensionless ) {
             return Units.dimensionless;
         } else if ( unit==Units.microseconds ) {
@@ -23,7 +46,12 @@ public class UnitsUtil {
             throw new IllegalArgumentException( "units not supported: "+unit );
         }
     }
-
+    
+    /**
+     * Special division operation that either does the Datum division if
+     * possible, or returns the division of the magitude parts of the
+     * Datums plus the unit names "A/B", suitable for human consumption.
+     */
     public static String divideToString( Datum aDatum, Datum bDatum ) {
         try {
             Datum result= divide( aDatum, bDatum );
@@ -36,37 +64,34 @@ public class UnitsUtil {
             return ""+(a/b)+" "+aUnits+" / " +bUnits;
         }
     }
-
+    
+    /**
+     * attempt to perform the division of two Datums by looking for
+     * convertable units or dimensionless.
+     */
     public static Datum divide( Datum aDatum, Datum bDatum ) {
         Units bUnits= bDatum.getUnits();
         Units aUnits= aDatum.getUnits();
-
+        
         Units bInvUnits;
         try{
             bInvUnits= getInverseUnit(bUnits);
         } catch ( IllegalArgumentException e ) {
             bInvUnits= null;
         }
-
+        
         double a= aDatum.doubleValue(aUnits);
         double b= bDatum.doubleValue(bUnits);
-
+        
         if ( bUnits==Units.dimensionless ) {
             return aUnits.createDatum( a/b );
+        } else if ( aUnits==Units.dimensionless ) {
+            return bInvUnits.createDatum(a/b);
         } else {
-            UnitsConverter uc= bUnits.getConverter(aUnits);
-            if ( uc==null ) {
-                if ( bInvUnits!=null ) {
-                    UnitsConverter uc2= bInvUnits.getConverter(aUnits);
-                    if ( uc2!=null ) {
-                        return aUnits.createDatum( a * uc2.convert( 1./b )  );
-                    } else {
-                        throw new IllegalArgumentException("unable to calculate, b inverse units not convertable to a");
-                    }
-                } else {
-                    throw new IllegalArgumentException("unable to calculate, b inversion not known");
-                }
+            if ( !bUnits.isConvertableTo(aUnits) ) {
+                throw new IllegalArgumentException("unable to calculate, b units not convertable to a");
             } else {
+                UnitsConverter uc= bUnits.getConverter(aUnits);
                 return Units.dimensionless.createDatum( a / uc.convert(b) );
             }
         }
