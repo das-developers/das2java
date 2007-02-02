6a24db7865ae3cdcd06c40b8a0633c57f642f7ae
Jeremy Faden <jbfaden@users.noreply.github.com>
2007-02-02 22:42:14 +0000

    javadoc improvements
    svn path=/dasCore/trunk/; revision=3138

--- dasCore/src/edu/uiowa/physics/pw/das/dataset/CacheTag.java
+++ dasCore/src/edu/uiowa/physics/pw/das/dataset/CacheTag.java
@@ -2,14 +2,21 @@ package edu.uiowa.physics.pw.das.dataset;
 
 import edu.uiowa.physics.pw.das.datum.*;
 
+/**
+ * CacheTags are used to represent the coverage of datasets stored in a cache, and are
+ * the reference used to decide if a cache entry is capable of satisfying a data request.
+ * 
+ */
 public class CacheTag {
     
     DatumRange range;    
     Datum resolution;
     
     /**
-     * appends two CacheTags, when possible.  New range covers the ranges, and the resolution is the lower of the two.
-     * @throws IllegalArgumentException when the tags are not adjecent or overlapping.
+     * Appends two CacheTags, when possible.  The new range covers the ranges, and the resolution is the lower of the two.
+     * @param tag1 a CacheTag
+     * @param tag2 a CacheTag that is adjacent to tag1
+     * @return a CacheTag that covers both CacheTags precisely.
      */
     public static CacheTag append( CacheTag tag1, CacheTag tag2 ) {
         Datum res;
@@ -45,22 +52,41 @@ public class CacheTag {
         return new CacheTag( range, res );
     }
     
+    /**
+     * Constructs a new CacheTag.
+     * @param start the beginning of the interval.
+     * @param end the end of the interval.
+     * @param resolution the highest resolution request that can be provided.
+     */
     public CacheTag(Datum start, Datum end, Datum resolution) {
         this( new DatumRange( start, end ), resolution );
     }
     
+    /**
+     * Constucts a new CacheTag.
+     * @param range the interval covered by the CacheTag.
+     * @param resolution  the highest resolution request that can be provided.
+     */
     public CacheTag( DatumRange range, Datum resolution) {
         this.range= range;
         this.resolution= resolution;
     }
     
+    /**
+     * Returns a string representation of the object.
+     * @return a human consumable representation of the cache tag.  This should fit on
+     * one line as it is used to list cache contents.
+     */
     public String toString() {
         return range + " @ " + ( resolution==null ? "intrisic" : ""+DatumUtil.asOrderOneUnits(resolution) );
     }
     
     /**
+     * Indicates if the cache tag the the capability of satifying the given cache tag.  If the tag has a lower (courser) resolution and its bounds are within
+     * this CacheTag.
      * @return true if the tag has a lower resolution and its bounds are within
      * this CacheTag.
+     * @param tag a CacheTag to test.
      */
     public boolean contains( CacheTag tag ) {
         return ( this.range.contains( tag.range ) 
