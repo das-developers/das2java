c224e4673647e3f6bda62162ac1f63143950886f
Jeremy Faden <jbfaden@users.noreply.github.com>
2007-02-20 21:53:05 +0000

    factor out static method for drawing an arrow.  This is used by ArrowDragRenderer
    svn path=/dasCore/trunk/; revision=3182

--- dasCore/src/edu/uiowa/physics/pw/das/graph/Arrow.java
+++ dasCore/src/edu/uiowa/physics/pw/das/graph/Arrow.java
@@ -16,8 +16,8 @@ import java.awt.geom.*;
 public class Arrow extends DasCanvasComponent {
     
     Point head, tail;
-    Stroke stroke;  
-    double em=24; //pixels;   
+    Stroke stroke;
+    double em=24; //pixels;
     
     public Arrow( DasCanvas c, Point head, Point tail ) {
         this.head= head;
@@ -29,28 +29,24 @@ public class Arrow extends DasCanvasComponent {
     public void resize() {
         Rectangle bounds= new Rectangle();
         bounds.add( head.x - em, head.y-em ); // account for stroke width
-        bounds.add( tail.x + em, tail.y+em );        
+        bounds.add( tail.x + em, tail.y+em );
         setBounds(bounds);
-    }    
-  
-    protected void paintComponent(Graphics g1) {
-        Graphics2D g= (Graphics2D) g1.create();
+    }
+    
+    public static void paintArrow( Graphics2D g, Point head, Point tail, double em ) {
         g.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON );
         
-        g.translate(-getX(),-getY());
-        
-        double em=4; //pixels;   
         Line2D line= new Line2D.Double( head, tail );
         double dx= - ( head.getX() - tail.getX() );
         double dy= - ( head.getY() - tail.getY() );
         double dd= Math.sqrt( dx*dx + dy*dy );
-        dx= dx * em / dd;
-        dy= dy * em / dd;
-                
+        dx= dx * em / 4 / dd;
+        dy= dy * em / 4 / dd;
+        
         double hx= head.getX();
         double hy= head.getY();
         
-        g.setStroke( new BasicStroke( (float)(em/2), BasicStroke.CAP_ROUND, BasicStroke.JOIN_ROUND ) );
+        g.setStroke( new BasicStroke( (float)(em/8), BasicStroke.CAP_ROUND, BasicStroke.JOIN_ROUND ) );
         
         g.draw( line );
         
@@ -61,13 +57,19 @@ public class Arrow extends DasCanvasComponent {
         p.lineTo( (float)(hx+3*dx-dy), (float)(hy+3*dy+dx) );
         p.lineTo( (float)(hx+2*dx-0.5*dy), (float)(hy+2*dy+0.5*dx) );
         p.lineTo( (float)hx, (float)hy );
-        g.fill( p );             
-                
+        g.fill( p );
+        
         g.draw( p );
         g.dispose();
         
+    }
+    
+    protected void paintComponent(Graphics g1) {
+        Graphics2D g= (Graphics2D) g1.create();
+        g.translate(-getX(),-getY());
+        paintArrow( g, head, tail, em );
         getMouseAdapter().paint(g1);
-    }    
- 
+    }
+    
     
 }
