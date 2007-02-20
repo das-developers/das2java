760f6a706a805d5d23d6e5cae5853e954267d16c
Jeremy Faden <jbfaden@users.noreply.github.com>
2007-02-20 21:59:11 +0000

    allows pointAt to be added, by dragging off the annotation
    svn path=/dasCore/trunk/; revision=3190

--- dasCore/src/edu/uiowa/physics/pw/das/event/AnnotatorMouseModule.java
+++ dasCore/src/edu/uiowa/physics/pw/das/event/AnnotatorMouseModule.java
@@ -6,7 +6,10 @@
 
 package edu.uiowa.physics.pw.das.event;
 
+import edu.uiowa.physics.pw.das.datum.Datum;
 import edu.uiowa.physics.pw.das.graph.*;
+import java.awt.Point;
+import java.awt.event.MouseEvent;
         
 /**
  *
@@ -22,8 +25,45 @@ public class AnnotatorMouseModule extends MouseModule {
         this.canvas= (DasCanvas)parent.getParent();
     }
 
+    public DasCanvas getCanvas() {
+        return canvas;
+    }
+
+    
+    private MouseModule createArrowToMouseModule( final DasAnnotation anno ) {
+        return new MouseModule( parent, new ArrowDragRenderer(), "Point At" ) {
+            Point head;
+            Point tail;
+            
+            public void mousePressed(MouseEvent e) {
+                super.mousePressed(e);
+                tail= e.getPoint();
+            }
+
+            public void mouseReleased(MouseEvent e) {
+                super.mouseReleased(e);
+                head= e.getPoint();
+                head.translate( anno.getX(), anno.getY() );
+                DasCanvasComponent c= parent.getCanvas().getCanvasComponentAt( head.x, head.y );
+                if ( c instanceof DasPlot ) {
+                    final DasPlot p= (DasPlot) c;
+                    final Datum x= p.getXAxis().invTransform( head.x );
+                    final Datum y= p.getYAxis().invTransform( head.y );
+                    anno.setPointAt( new DasAnnotation.PointDescriptor() {
+                        public Point getPoint() {
+                            int ix= (int)(p.getXAxis().transform(x));
+                            int iy= (int)(p.getYAxis().transform(y));
+                            return new Point(ix,iy);
+                        }
+                    } );
+                }
+            }
+        };
+    }
+    
     public void mouseRangeSelected(MouseDragEvent e) {    
         super.mouseRangeSelected(e);
+        
         System.out.println(e);
         MouseBoxEvent me= (MouseBoxEvent) e;
                 
@@ -34,7 +74,11 @@ public class AnnotatorMouseModule extends MouseModule {
         n= canvas.getWidth();        
         DasColumn col= new DasColumn( canvas, me.getXMinimum()/n, me.getXMaximum()/n );
         
-        DasAnnotation anno= new DasAnnotation("hello there");
+        DasAnnotation anno= new DasAnnotation("right click");
+        MouseModule mm= createArrowToMouseModule( anno );
+        anno.addMouseModule( mm );
+        anno.getMouseAdapter().setPrimaryModule( mm );
+        
         canvas.add( anno, row, col );                
         canvas.revalidate();
     }
