3b44c31a1a47f1eb1477e3d3ba61045a620d4e22
Jeremy Faden <jbfaden@users.noreply.github.com>
2007-04-14 04:00:27 +0000

    1. introduce guessFieldCount method. 2. check for number format errors.
    svn path=/dasCore/trunk/; revision=3219

--- dasCore/src/edu/uiowa/physics/pw/das/dataset/parser/VectorDataSetParser.java
+++ dasCore/src/edu/uiowa/physics/pw/das/dataset/parser/VectorDataSetParser.java
@@ -17,21 +17,22 @@ import java.util.regex.*;
  * two Patterns: one for properties and one for records.  If a record matched,
  * then the record is matched and fields pulled out, parsed and insered a
  * VectorDataSetBuilder.  If a property is matched, then the builder property
- * is set.  Two Patterns are provided NAME_COLON_VALUE_PATTERN and 
+ * is set.  Two Patterns are provided NAME_COLON_VALUE_PATTERN and
  * NAME_EQUAL_VALUE_PATTERN for convenience.  The record pattern is currently
- * the number of fields identified with whitespace in between.  Note the X 
+ * the number of fields identified with whitespace in between.  Note the X
  * tags are just the record numbers.
- * 
+ *
  * @author  Jeremy
  */
 public class VectorDataSetParser {
     
     Pattern propertyPattern;
-    String commentRegex;    
+    String commentRegex;
     Pattern recordPattern;
     String[] fieldNames;
     
-    final static String decimalRegex= "\\S*\\d+\\S*";
+    final static String numberPart= "[\\d\\.eE\\+\\-]+";
+    final static String decimalRegex= numberPart;
     int skipLines;
     int recordCountLimit;
     int fieldCount;
@@ -45,21 +46,68 @@ public class VectorDataSetParser {
         StringBuffer regexBuf= new StringBuffer();
         regexBuf.append("\\s*");
         for ( int i=0; i<fieldCount-1; i++ ) {
-            regexBuf.append("("+decimalRegex+")\\s+");
+            regexBuf.append("("+decimalRegex+")[\\s+,+]\\s*");
         }
-        regexBuf.append("("+decimalRegex+")\\s*");        
+        regexBuf.append("("+decimalRegex+")\\s*");
         recordPattern= Pattern.compile(regexBuf.toString());
         this.recordPattern= recordPattern;
     }
     
     /**
+     * return the field count that would result in the largest number of records parsed.  The 
+     * entire file is scanned, and for each line the number of decimal fields is counted.  At the end
+     * of the scan, the fieldCount with the highest record count is returned.
+     */
+    public static int guessFieldCount( String filename ) throws FileNotFoundException, IOException {
+        
+        final int maxFieldCount=8;  // can only identify maxFieldCount - 1.
+        
+        int[] recCount= new int[maxFieldCount];
+        
+        StringBuffer regexBuf= new StringBuffer();
+        regexBuf.append("\\s*("+decimalRegex+")");
+        for ( int i=1; i<maxFieldCount; i++ ) {
+            regexBuf.append("([\\s+,+]\\s*("+decimalRegex+"))?");
+        }
+        regexBuf.append( "\\s*" );
+        
+        Pattern pat= Pattern.compile( regexBuf.toString() );
+        
+        BufferedReader reader= new LineNumberReader( new FileReader( filename ) );
+        
+        String line;
+        while ( ( line=reader.readLine() ) != null ) {
+            Matcher m= pat.matcher( line );
+            if ( m.matches() ) {
+                int j;
+                for ( j=1; j<m.groupCount(); j+=2 ) {
+                    if ( m.group(j)==null ) {
+                        recCount[ (j-1) / 2 ] ++;
+                        break;
+                    }
+                }
+            }
+        }
+        int max=0;
+        int imax=0;
+        for ( int j=1; j<maxFieldCount; j++ ) {
+            if ( recCount[j] > max ) {
+                imax=j;
+                max= recCount[j];
+            }           
+        }
+        
+        return imax;
+    }
+    
+    /**
      * creates a parser with @param fieldCount fields, named "field0,...,fieldN"
      */
-    public static VectorDataSetParser newParser( int fieldCount ) {        
+    public static VectorDataSetParser newParser( int fieldCount ) {
         String[] fieldNames= new String[ fieldCount ];
         for ( int i=0; i<fieldCount; i++ ) {
             fieldNames[i]= "field"+i;
-        }        
+        }
         return new VectorDataSetParser( fieldNames );
     }
     
@@ -108,16 +156,29 @@ public class VectorDataSetParser {
         Matcher m;
         while ( (line=reader.readLine() )!=null ) {
             //System.err.println(line);
-            if ( iline<skipLines ) break;
-            if ( (m=recordPattern.matcher(line)).matches() ) {
-                for ( int i=0; i<fieldCount; i++ ) {
-                    builder.insertY( irec, Double.parseDouble(m.group(i+1)), fieldNames[i] );
+            try {
+                if ( iline<skipLines ) break;
+                if ( (m=recordPattern.matcher(line)).matches() ) {
+                    boolean allInvalid= true;
+                    for ( int i=0; i<fieldCount; i++ ) {
+                        try {
+                            double d= Double.parseDouble(m.group(i+1)); 
+                            allInvalid= false;
+                        } catch ( NumberFormatException e ) {}
+                    }
+                    if ( ! allInvalid ) {
+                        for ( int i=0; i<fieldCount; i++ ) {
+                            builder.insertY( irec, Double.parseDouble(m.group(i+1)), fieldNames[i] );
+                        }
+                        irec++;
+                    }
+                } else if ( propertyPattern!=null && ( m=propertyPattern.matcher(line) ).matches()) {
+                    builder.setProperty( m.group(1).trim(), m.group(2).trim() );
+                } else {
+                    //System.out.println(line);
                 }
-                irec++;
-            } else if ( propertyPattern!=null && ( m=propertyPattern.matcher(line) ).matches()) {
-                builder.setProperty( m.group(1).trim(), m.group(2).trim() );
-            } else {
-                //System.out.println(line);
+            } catch ( NumberFormatException e ) {
+                e.printStackTrace();
             }
         }
         
@@ -132,10 +193,14 @@ public class VectorDataSetParser {
     }
     
     public static void main(String[] args) throws Exception {
-        VectorDataSetParser parser= VectorDataSetParser.newParser(5);        
+        
+        String file= "L:/ct/virbo/autoplot/data/2490lintest90005.dat";
+        System.err.println( VectorDataSetParser.guessFieldCount( file ) );
+        
+        VectorDataSetParser parser= VectorDataSetParser.newParser(5);
         parser.setPropertyPattern(Pattern.compile("\\s*(.+)\\s*\\:\\s*(.+)\\s*"));
         long t0= System.currentTimeMillis();
-        VectorDataSet ds= parser.readFile( "j:/ct/ncvs/sarahFFT/lintest10/2490lintest100002.raw" );        
+        VectorDataSet ds= parser.readFile( "j:/ct/ncvs/sarahFFT/lintest10/2490lintest100002.raw" );
         System.out.println(""+(System.currentTimeMillis()-t0));
         System.out.println(ds.getProperty("Frequency"));
         System.out.flush();
