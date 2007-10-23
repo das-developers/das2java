7e36c9cdac0bda2eb90f95fe36b76f3e03cbe169
Jeremy Faden <jbfaden@users.noreply.github.com>
2007-10-23 16:07:27 +0000

    NullProgressMonitor class introduced, DasProgressMonitor.NULL is desprecated.
    svn path=/dasCore/trunk/; revision=3572

---
+++ dasCore/src/edu/uiowa/physics/pw/das/util/NullProgressMonitor.java
@@ -0,0 +1,90 @@
+/*
+ * NullProgressMonitor.java
+ *
+ * Created on October 23, 2007, 10:11 AM
+ *
+ * To change this template, choose Tools | Template Manager
+ * and open the template in the editor.
+ */
+
+package edu.uiowa.physics.pw.das.util;
+
+/**
+ * This is a progress monitor to use when we don't care about the progress.
+ * This replaces DasProgressMonitor.NULL, which has the problem that it was
+ * stateless and there assume that progress monitors have state.
+ *
+ * Further, this can act as a base class for other monitor types.
+ *
+ * @author jbf
+ */
+public class NullProgressMonitor implements DasProgressMonitor {
+    
+    public NullProgressMonitor() {
+    }
+    
+    private long taskSize=-1 ;
+    
+    public void setTaskSize(long taskSize) {
+        this.taskSize= taskSize;
+    }
+    
+    public long getTaskSize( ) { 
+        return taskSize; 
+    }
+    
+    public void setProgressMessage( String message ) {} ;
+        
+    private long position=0;
+    
+    public void setTaskProgress(long position) throws IllegalArgumentException {
+        this.position= position;
+    }
+        
+    public long getTaskProgress() { 
+        return position; 
+    }
+    
+    private boolean started= false;
+    
+    public void started() {  
+        this.started= false;
+    }
+    
+    public boolean isStarted() {
+        return started;
+    }
+    
+    private boolean finished= false;
+    
+    public void finished() {
+        finished= true;
+    }
+    
+    public boolean isFinished() {
+        return finished;
+    }
+    
+    private boolean cancelled= false;
+    
+    public void cancel() {
+        cancelled= true;
+    }
+    
+    public boolean isCancelled() { 
+        return cancelled; 
+    }
+    
+    public void setAdditionalInfo(String s) { };
+    
+    private String label;
+    
+    public void setLabel( String s ) { 
+        this.label= label;
+    }
+    
+    public String getLabel() { 
+        return label; 
+    }
+    
+}
