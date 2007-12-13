ceb0be0d0b7fb2d922b4b87a2bb1d2c25e782e24
Jeremy Faden <jbfaden@users.noreply.github.com>
2007-12-13 21:46:22 +0000

    features added include bottom curtain opacity control, other controls.
    svn path=/dasCore/trunk/; revision=3636

--- dasCore/src/edu/uiowa/physics/pw/das/graph/ColumnColumnConnector.java
+++ dasCore/src/edu/uiowa/physics/pw/das/graph/ColumnColumnConnector.java
@@ -1,21 +1,25 @@
 package edu.uiowa.physics.pw.das.graph;
 
 import edu.uiowa.physics.pw.das.datum.Datum;
+import edu.uiowa.physics.pw.das.datum.DatumRange;
+import edu.uiowa.physics.pw.das.datum.DatumRangeUtil;
 import java.awt.*;
 import java.awt.geom.GeneralPath;
 import javax.swing.JLayeredPane;
 
+/**
+ * draws lines connecting two DasPlots, one on top of the other, typically used
+ * to show a zoom in above of a context below.
+ */
 public class ColumnColumnConnector extends DasCanvasComponent implements java.beans.PropertyChangeListener {
-
+    
     private DasCanvas parent;
-
+    
     private DasRow topRow;
     private DasRow bottomRow;
-
+    
     private DasPlot topPlot;
     private DasPlot bottomPlot;
-    
-    private boolean centerbottomColumn= false; // this causes a funny bug
     
     public ColumnColumnConnector( DasCanvas parent, DasPlot topPlot, DasRow topRow, DasPlot bottomPlot ) {
         super( );
@@ -32,26 +36,31 @@ public class ColumnColumnConnector extends DasCanvasComponent implements java.be
         this.parent= parent;
         topPlot.addPropertyChangeListener(this);
         topPlot.getXAxis().addPropertyChangeListener(this);
+        topPlot.getYAxis().addPropertyChangeListener(this);
         bottomPlot.addPropertyChangeListener(this);
         bottomPlot.getXAxis().addPropertyChangeListener(this);
     }
-
+    
     private Rectangle getMyBounds() {
         int ytop= topRow.getDMaximum();
         int ybottom= this.bottomCurtain ? bottomPlot.getRow().getDMaximum() : bottomPlot.getRow().getDMinimum() ;
         int xhigh= Math.max( topPlot.getColumn().getDMaximum(), bottomPlot.getColumn().getDMaximum() );
         int xlow= Math.min( topPlot.getColumn().getDMinimum(), bottomPlot.getColumn().getDMinimum() );
-
+        
         Rectangle result= new Rectangle( xlow, ytop, (xhigh-xlow)+1, (ybottom-ytop)  );
         return result;
     }
-
+    
+    public Shape getActiveRegion() {
+        return getMyBounds();
+    }
+    
     public void resize() {
         setBounds(getMyBounds());
     }
-
+    
     private Datum min( Datum d1, Datum d2 ) {
-       return d1.lt(d2) ? d1 : d2;
+        return d1.lt(d2) ? d1 : d2;
     }
     
     private Datum max( Datum d1, Datum d2 ) {
@@ -66,11 +75,11 @@ public class ColumnColumnConnector extends DasCanvasComponent implements java.be
         
         Graphics2D g= (Graphics2D)g1.create();
         g.setRenderingHint( RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON );
-
+        
         g.translate(-getX(), -getY());
-
+        
         int hlen=3;
-
+        
         int y1= topRow.getDMaximum()+hlen;
         int y2= bottomPlot.getRow().getDMinimum()-1-hlen;
         int y3= bottomPlot.getRow().getDMinimum()-1;
@@ -82,24 +91,20 @@ public class ColumnColumnConnector extends DasCanvasComponent implements java.be
         int xhigh2= (int)bottomPlot.getXAxis().transform(dhigh);
         int xlow1= (int)topPlot.getXAxis().transform(dlow);
         int xlow2= (int)bottomPlot.getXAxis().transform(dlow);
-
+        
+        //if ( xhigh1 > xhigh2 ) return;
+        
         GeneralPath gp= new GeneralPath();
         GeneralPath fillPath= new GeneralPath();
         
-        gp.moveTo( xlow1,y1-hlen );                   fillPath.moveTo( xlow1,y1-hlen );  
+        gp.moveTo( xlow1,y1-hlen );                   fillPath.moveTo( xlow1,y1-hlen );
         gp.lineTo( xlow1,y1 );                        fillPath.lineTo( xlow1,y1 );
         gp.lineTo( xlow2,y2 );                        fillPath.lineTo( xlow2,y2 );
         gp.lineTo( xlow2,y3 );                        fillPath.lineTo( xlow2,y3 );
-        if ( bottomCurtain ) {
-            gp.lineTo( xlow2, y4 );
-            gp.moveTo( xhigh2, y4 );
-            gp.lineTo( xhigh2, y3 );
-        } else {
-            gp.moveTo( xhigh2, y3 );
-        }
-                                      fillPath.lineTo( xhigh2,y3 );
+        gp.moveTo( xhigh2, y3 );
+        fillPath.lineTo( xhigh2,y3 );
         gp.lineTo( xhigh2,y2 );       fillPath.lineTo( xhigh2,y2 );
-        gp.lineTo( xhigh1,y1 );       fillPath.lineTo( xhigh1,y1 ); 
+        gp.lineTo( xhigh1,y1 );       fillPath.lineTo( xhigh1,y1 );
         gp.lineTo( xhigh1,y1-hlen );  fillPath.lineTo( xhigh1,y1-hlen );
         
         if ( fill ) {
@@ -108,36 +113,67 @@ public class ColumnColumnConnector extends DasCanvasComponent implements java.be
         }
         
         g.setColor( getForeground() );
-                
+        
         g.draw( gp );
         
-       /* g.draw(new java.awt.geom.Line2D.Double(xlow1,y1-hlen,xlow1,y1));
-        g.draw(new java.awt.geom.Line2D.Double(xlow2,y2,xlow2,y2+hlen));
-        g.draw(new java.awt.geom.Line2D.Double(xlow1,y1,xlow2,y2));
-        g.draw(new java.awt.geom.Line2D.Double(xhigh1,y1-hlen,xhigh1,y1));
-        g.draw(new java.awt.geom.Line2D.Double(xhigh2,y2,xhigh2,y2+hlen));
-        g.draw(new java.awt.geom.Line2D.Double(xhigh1,y1,xhigh2,y2));*/
+        if ( bottomCurtain && topPlot.getYAxis().getUnits().isConvertableTo( bottomPlot.getYAxis().getUnits() ) ) {
+            
+            DatumRange drtop= topPlot.getYAxis().getDatumRange();
+            DatumRange yaxisRange= bottomPlot.getYAxis().getDatumRange();
 
+            drtop= DatumRangeUtil.sloppyIntersection( yaxisRange, drtop );
+            
+            int y5,y6;
+            
+            y5= (int)bottomPlot.getYAxis().transform( drtop.max() );
+            y6= (int)bottomPlot.getYAxis().transform( drtop.min() );
+            
+            if ( curtainOpacityPercent > 0 ) {
+                int xLeft= (int)topPlot.getXAxis().getColumn().getDMinimum();
+                int xRight= (int)bottomPlot.getXAxis().getColumn().getDMaximum();
+                Color canvasColor= getCanvas().getBackground();
+                Color curtainColor= new Color( canvasColor.getRed(), canvasColor.getGreen(), canvasColor.getBlue(),
+                        curtainOpacityPercent * 255 / 100 );
+                
+                GeneralPath gpfill= new GeneralPath( DasRow.toRectangle(bottomPlot.getRow(),bottomPlot.getColumn() ) );
+                gpfill.append( new Rectangle( xlow2, y5, xhigh2-xlow2, y6-y5 ), false );
+                gpfill.setWindingRule( GeneralPath.WIND_EVEN_ODD );
+                
+                g.setColor( curtainColor );
+                g.fill( gpfill );
+                //g.fillRect( xLeft, y3+1, xlow2-xLeft, y4-y3-1 );
+                //g.fillRect( xhigh2+1, y3+1, xRight-xhigh2-1, y4-y3-1 );
+                g.setColor( getForeground() );
+            }
+            
+            if ( yaxisRange.contains(drtop.max()) )g.drawLine( xlow2, y5, xhigh2, y5 );
+            if ( yaxisRange.contains(drtop.min()) && drtop.min().gt( yaxisRange.min() ) ) g.drawLine( xlow2, y6, xhigh2, y6 );
+            g.drawLine( xlow2, y3, xlow2, y4 );
+            g.drawLine( xhigh2, y3, xhigh2, y4 );
+            
+            
+        }
+        
         g.dispose();
-
+        
         getMouseAdapter().paint(g1);
     }
-
+    
     public void propertyChange(java.beans.PropertyChangeEvent propertyChangeEvent) {
         markDirty();
         update();
     }
-
+    
     /**
      * Holds value of property fillColor.
      */
-    private Color fillColor= new Color( 240,240,240,0 );
-
+    private Color fillColor= new Color( 240,240,240,255 );
+    
     /**
      * Utility field used by bound properties.
      */
     private java.beans.PropertyChangeSupport propertyChangeSupport =  new java.beans.PropertyChangeSupport(this);
-
+    
     /**
      * Adds a PropertyChangeListener to the listener list.
      * @param l The listener to add.
@@ -145,7 +181,7 @@ public class ColumnColumnConnector extends DasCanvasComponent implements java.be
     public void addPropertyChangeListener(java.beans.PropertyChangeListener l) {
         propertyChangeSupport.addPropertyChangeListener(l);
     }
-
+    
     /**
      * Removes a PropertyChangeListener from the listener list.
      * @param l The listener to remove.
@@ -153,7 +189,7 @@ public class ColumnColumnConnector extends DasCanvasComponent implements java.be
     public void removePropertyChangeListener(java.beans.PropertyChangeListener l) {
         propertyChangeSupport.removePropertyChangeListener(l);
     }
-
+    
     /**
      * Getter for property fillColor.
      * @return Value of property fillColor.
@@ -161,7 +197,7 @@ public class ColumnColumnConnector extends DasCanvasComponent implements java.be
     public Color getFillColor() {
         return this.fillColor;
     }
-
+    
     /**
      * Setter for property fillColor.
      * @param fillColor New value of property fillColor.
@@ -169,14 +205,15 @@ public class ColumnColumnConnector extends DasCanvasComponent implements java.be
     public void setFillColor(Color fillColor) {
         Color oldFillColor = this.fillColor;
         this.fillColor = fillColor;
-        propertyChangeSupport.firePropertyChange ("fillColor", oldFillColor, fillColor);
+        repaint();
+        propertyChangeSupport.firePropertyChange("fillColor", oldFillColor, fillColor);
     }
-
+    
     /**
      * Holds value of property fill.
      */
     private boolean fill=false;
-
+    
     /**
      * Getter for property fill.
      * @return Value of property fill.
@@ -184,7 +221,7 @@ public class ColumnColumnConnector extends DasCanvasComponent implements java.be
     public boolean isFill() {
         return this.fill;
     }
-
+    
     /**
      * Setter for property fill.
      * @param fill New value of property fill.
@@ -192,14 +229,15 @@ public class ColumnColumnConnector extends DasCanvasComponent implements java.be
     public void setFill(boolean fill) {
         boolean oldFill = this.fill;
         this.fill = fill;
-        propertyChangeSupport.firePropertyChange ("fill", new Boolean (oldFill), new Boolean (fill));
+        repaint();
+        propertyChangeSupport.firePropertyChange("fill", new Boolean(oldFill), new Boolean(fill));
     }
-
+    
     /**
      * Holds value of property bottomCurtain.
      */
     private boolean bottomCurtain;
-
+    
     /**
      * Getter for property bottomCurtain.
      * @return Value of property bottomCurtain.
@@ -207,7 +245,7 @@ public class ColumnColumnConnector extends DasCanvasComponent implements java.be
     public boolean isBottomCurtain() {
         return this.bottomCurtain;
     }
-
+    
     /**
      * Setter for property bottomCurtain.
      * @param bottomCurtain New value of property bottomCurtain.
@@ -215,8 +253,33 @@ public class ColumnColumnConnector extends DasCanvasComponent implements java.be
     public void setBottomCurtain(boolean bottomCurtain) {
         boolean oldBottomCurtain = this.bottomCurtain;
         this.bottomCurtain = bottomCurtain;
-        propertyChangeSupport.firePropertyChange ("bottomCurtain", new Boolean (oldBottomCurtain), new Boolean (bottomCurtain));
+        repaint();
+        propertyChangeSupport.firePropertyChange("bottomCurtain", new Boolean(oldBottomCurtain), new Boolean(bottomCurtain));
     }
-
+    
+    /**
+     * Holds value of property curtainOpacityPercent.
+     */
+    private int curtainOpacityPercent= 40;
+    
+    /**
+     * Getter for property curtainOpacityPercent.
+     * @return Value of property curtainOpacityPercent.
+     */
+    public int getCurtainOpacityPercent() {
+        return this.curtainOpacityPercent;
+    }
+    
+    /**
+     * Setter for property curtainOpacityPercent.
+     * @param curtainOpacityPercent New value of property curtainOpacityPercent.
+     */
+    public void setCurtainOpacityPercent(int curtainOpacityPercent) {
+        int oldCurtainOpacityPercent = this.curtainOpacityPercent;
+        this.curtainOpacityPercent = Math.max( 0, Math.min( 100, curtainOpacityPercent ) );
+        repaint();
+        propertyChangeSupport.firePropertyChange("curtainOpacityPercent", new Integer(oldCurtainOpacityPercent), new Integer(curtainOpacityPercent));
+    }
+    
 }
 
