ea8bb4775dcd9b489d951c752d747db15222de49
Jeremy Faden <jbfaden@users.noreply.github.com>
2007-12-13 22:06:58 +0000

    trivial change.
    svn path=/dasCore/trunk/; revision=3643

--- dasCore/src/edu/uiowa/physics/pw/das/graph/TickCurveRenderer.java
+++ dasCore/src/edu/uiowa/physics/pw/das/graph/TickCurveRenderer.java
@@ -207,7 +207,6 @@ public class TickCurveRenderer extends Renderer {
         
     }
 
-    // TODO: it's likely this fails to meet the contract of animation interactive
     public void render(java.awt.Graphics g1, DasAxis xAxis, DasAxis yAxis, DasProgressMonitor mon) {
         
         if ( ds==null ) {
