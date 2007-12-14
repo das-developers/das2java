e36df737246f9f5be3b1c2d4b34be4d91e5fe4fe
Jeremy Faden <jbfaden@users.noreply.github.com>
2007-12-14 21:20:32 +0000

    bean info added to clean up properties sheet.
    svn path=/dasCore/trunk/; revision=3673

---
+++ dasCore/src/edu/uiowa/physics/pw/das/beans/ColumnColumnConnectorBeanInfo.java
@@ -0,0 +1,52 @@
+/* File: DasColorBarBeanInfo.java
+ * Copyright (C) 2002-2003 The University of Iowa
+ * Created by: Jeremy Faden <jbf@space.physics.uiowa.edu>
+ *             Jessica Swanner <jessica@space.physics.uiowa.edu>
+ *             Edward E. West <eew@space.physics.uiowa.edu>
+ *
+ * This file is part of the das2 library.
+ *
+ * das2 is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+
+package edu.uiowa.physics.pw.das.beans;
+
+import edu.uiowa.physics.pw.das.beans.AccessLevelBeanInfo.AccessLevel;
+import edu.uiowa.physics.pw.das.beans.AccessLevelBeanInfo.Property;
+import edu.uiowa.physics.pw.das.graph.ColumnColumnConnector;
+
+/**
+ * BeanInfo class for DasColorBar
+ *
+ * @author Edward West
+ */
+public class ColumnColumnConnectorBeanInfo extends AccessLevelBeanInfo {
+    
+    ColumnColumnConnector c;
+    
+    protected static final Property[] properties = {
+        new Property("bottomCurtain", AccessLevel.DASML, "isBottomCurtain", "setBottomCurtain", null),
+        new Property("curtainOpacityPercent", AccessLevel.DASML, "getCurtainOpacityPercent", "setCurtainOpacityPercent", null),
+        new Property("fill", AccessLevel.DASML, "isFill", "setFill", null),
+        new Property("fillColor", AccessLevel.DASML, "getFillColor", "setFillColor", null),
+        new Property("color", AccessLevel.DASML, "getForeground", "setForeground", null),
+        new Property("visible", AccessLevel.DASML, "isVisible", "setVisible", null),        
+    };
+
+    public ColumnColumnConnectorBeanInfo() {
+	super(properties, edu.uiowa.physics.pw.das.graph.ColumnColumnConnector.class);
+    }
+
+}
