f6e0783ddeb3e10ad546824a532683bee7c3be95
Jeremy Faden <jbfaden@users.noreply.github.com>
2008-02-05 21:28:23 +0000

    support for java 1.5 enums.
    svn path=/dasCore/trunk/; revision=3756

--- dasCore/src/edu/uiowa/physics/pw/das/components/propertyeditor/EnumerationEditor.java
+++ dasCore/src/edu/uiowa/physics/pw/das/components/propertyeditor/EnumerationEditor.java
@@ -3,7 +3,6 @@
  *
  * Created on April 14, 2005, 9:18 AM
  */
-
 package edu.uiowa.physics.pw.das.components.propertyeditor;
 
 import java.awt.Component;
@@ -40,7 +39,7 @@ import javax.swing.table.TableCellEditor;
  * @author eew
  */
 public class EnumerationEditor implements java.beans.PropertyEditor, TableCellEditor {
-    
+
     private JComboBox editor;
     private Model model;
     private Object selected;
@@ -48,26 +47,26 @@ public class EnumerationEditor implements java.beans.PropertyEditor, TableCellEd
     private boolean guessType;
     private PropertyChangeSupport pcSupport;
     private EventListenerList listeners = new EventListenerList();
-    
     private Map valueMap;
     private Map nameMap;
     private Map toStringMap;
-    
+
     /** Creates a new instance of EnumerationEditor */
     public EnumerationEditor() {
         guessType = true;
         pcSupport = new PropertyChangeSupport(this);
     }
-    
+
     protected EnumerationEditor(Class c) {
         setClass(c);
         guessType = false;
     }
-    
+
     private void initEditor() {
         if (editor == null) {
             model = new Model();
             editor = new JComboBox(model) {
+
                 public void setBounds(int x, int y, int width, int height) {
                     Dimension preferred = getPreferredSize();
                     super.setBounds(x, y, width, preferred.height);
@@ -76,41 +75,51 @@ public class EnumerationEditor implements java.beans.PropertyEditor, TableCellEd
             editor.setRenderer(new Renderer());
         }
     }
-    
-    private static final int PUBLIC_STATIC_FINAL
-            = Modifier.PUBLIC | Modifier.STATIC | Modifier.FINAL;
-    
+    private static final int PUBLIC_STATIC_FINAL = Modifier.PUBLIC | Modifier.STATIC | Modifier.FINAL;
+
     private void setClass(Class c) {
         type = c;
-        Field[] fields = type.getDeclaredFields();
-        Set psf = new HashSet();
-        Map valueM = new HashMap();
-        Map nameM = new IdentityHashMap();
-        Map toStringM = new HashMap();
-        for (int i = 0; i < fields.length; i++) {
-            int modifiers = fields[i].getModifiers();
-            if ((modifiers & PUBLIC_STATIC_FINAL) == PUBLIC_STATIC_FINAL) {
-                psf.add(fields[i]);
+            Map valueM = new HashMap();
+            Map nameM = new IdentityHashMap();
+            Map toStringM = new HashMap();
+
+        if (c.isEnum()) {
+            Object[] vals= c.getEnumConstants();
+            for ( Object o:vals ) {
+                Enum e = (Enum)o;
+                nameM.put(e.name(), e);
+                toStringM.put(e.toString(), e);
+                valueM.put(e, e.name());
+            }
+            
+        } else {
+
+            Field[] fields = type.getDeclaredFields();
+            Set psf = new HashSet();
+            for (int i = 0; i < fields.length; i++) {
+                int modifiers = fields[i].getModifiers();
+                if ((modifiers & PUBLIC_STATIC_FINAL) == PUBLIC_STATIC_FINAL) {
+                    psf.add(fields[i]);
+                }
+            }
+            for (Iterator i = psf.iterator(); i.hasNext();) {
+                try {
+                    Field f = (Field) i.next();
+                    String name = f.getName();
+                    Object value = f.get(null);
+                    nameM.put(name, value);
+                    toStringM.put(value.toString(), value);
+                    valueM.put(value, name);
+                } catch (IllegalAccessException iae) {
+                    IllegalAccessError err = new IllegalAccessError(iae.getMessage());
+                    err.initCause(iae);
+                    throw err;
+                }
             }
         }
-        for (Iterator i = psf.iterator(); i.hasNext();) {
-            try {
-                Field f = (Field)i.next();
-                String name = f.getName();
-                Object value = f.get(null);
-                nameM.put(name, value);
-                toStringM.put(value.toString(), value);
-                valueM.put(value, name);
-            }
-            catch (IllegalAccessException iae) {
-                IllegalAccessError err = new IllegalAccessError(iae.getMessage());
-                err.initCause(iae);
-                throw err;
-            }
-        }
-        nameMap = nameM;
-        valueMap = valueM;
-        toStringMap = toStringM;
+            nameMap = nameM;
+            valueMap = valueM;
+            toStringMap = toStringM;
     }
 
     public String getAsText() {
@@ -141,20 +150,22 @@ public class EnumerationEditor implements java.beans.PropertyEditor, TableCellEd
         Class c = getTypeClass(obj);
         if (type != c) {
             int size = 0;
-            if (model !=  null) { size = model.getSize(); }
+            if (model != null) {
+                size = model.getSize();
+            }
             setClass(c);
             if (model != null) {
                 model.fireIntervalRemoved(model, 0, size - 1);
                 model.fireIntervalAdded(model, 0, model.getSize() - 1);
             }
         }
-        Object oldValue= selected;
+        Object oldValue = selected;
         selected = obj;
-        if ( oldValue!=obj ) {
+        if (oldValue != obj) {
             pcSupport.firePropertyChange("value", oldValue, selected);
         }
     }
-    
+
     private Class getTypeClass(Object obj) {
         Class c = obj.getClass();
         String name = c.getName();
@@ -167,19 +178,26 @@ public class EnumerationEditor implements java.beans.PropertyEditor, TableCellEd
     public boolean supportsCustomEditor() {
         return true;
     }
-    
+
     public Component getCustomEditor() {
         initEditor();
         return editor;
     }
-    
-    public String getJavaInitializationString() { return "???"; }
 
-    public String[] getTags() { return null; }
+    public String getJavaInitializationString() {
+        return "???";
+    }
 
-    public boolean isPaintable() { return false; }
+    public String[] getTags() {
+        return null;
+    }
 
-    public void paintValue(Graphics g, Rectangle r) {}
+    public boolean isPaintable() {
+        return false;
+    }
+
+    public void paintValue(Graphics g, Rectangle r) {
+    }
 
     public void addPropertyChangeListener(PropertyChangeListener l) {
         pcSupport.addPropertyChangeListener(l);
@@ -188,13 +206,12 @@ public class EnumerationEditor implements java.beans.PropertyEditor, TableCellEd
     public void removePropertyChangeListener(PropertyChangeListener l) {
         pcSupport.removePropertyChangeListener(l);
     }
-    
+
     /*TableCellEditor stuff*/
-    
     public Object getCellEditorValue() {
         return selected;
     }
-    
+
     public Component getTableCellEditorComponent(JTable table, Object value, boolean isSelected, int row, int column) {
         initEditor();
         editor.setForeground(table.getForeground());
@@ -202,85 +219,94 @@ public class EnumerationEditor implements java.beans.PropertyEditor, TableCellEd
         setValue(value);
         return editor;
     }
-    
-    public boolean isCellEditable(EventObject evt) { return true; }
-    
-    public boolean shouldSelectCell(EventObject evt) { return true; }
-    
-    public boolean stopCellEditing() { 
+
+    public boolean isCellEditable(EventObject evt) {
+        return true;
+    }
+
+    public boolean shouldSelectCell(EventObject evt) {
+        return true;
+    }
+
+    public boolean stopCellEditing() {
         fireEditingStopped();
         return true;
     }
-    
+
     public void cancelCellEditing() {
         fireEditingCanceled();
     }
-    
+
     public void addCellEditorListener(CellEditorListener l) {
         listeners.add(CellEditorListener.class, l);
     }
-            
+
     public void removeCellEditorListener(CellEditorListener l) {
         listeners.add(CellEditorListener.class, l);
     }
-    
     private ChangeEvent evt;
-    
+
     private void fireEditingStopped() {
         Object[] l = listeners.getListenerList();
-        for (int i = 0; i < l.length; i+=2) {
+        for (int i = 0; i < l.length; i += 2) {
             if (l[i] == CellEditorListener.class) {
-                CellEditorListener cel = (CellEditorListener)l[i+1];
-                if (evt == null) { evt = new ChangeEvent(this); }
+                CellEditorListener cel = (CellEditorListener) l[i + 1];
+                if (evt == null) {
+                    evt = new ChangeEvent(this);
+                }
                 cel.editingStopped(evt);
             }
         }
     }
-    
+
     private void fireEditingCanceled() {
         Object[] l = listeners.getListenerList();
-        for (int i = 0; i < l.length; i+=2) {
+        for (int i = 0; i < l.length; i += 2) {
             if (l[i] == CellEditorListener.class) {
-                CellEditorListener cel = (CellEditorListener)l[i+1];
-                if (evt == null) { evt = new ChangeEvent(this); }
+                CellEditorListener cel = (CellEditorListener) l[i + 1];
+                if (evt == null) {
+                    evt = new ChangeEvent(this);
+                }
                 cel.editingCanceled(evt);
             }
         }
     }
-    
+
     private class Renderer extends DefaultListCellRenderer {
-        public Component getListCellRendererComponent(JList list, Object value, int index, boolean isSelected, boolean hasFocus) {                        
-            String s = (String)valueMap.get(value);
+
+        public Component getListCellRendererComponent(JList list, Object value, int index, boolean isSelected, boolean hasFocus) {
+            String s = (String) valueMap.get(value);
             super.getListCellRendererComponent(list, s, index, isSelected, hasFocus);
-            if ( value instanceof Enumeration ) {
+            if (value instanceof Enumeration) {
                 setText(value.toString());
-                setIcon(((Enumeration)value).getListIcon());
+                setIcon(((Enumeration) value).getListIcon());
             }
             return this;
         }
     }
-    
+
     private class Model extends AbstractListModel implements ComboBoxModel {
+
         private List list;
-        
+
         private Model() {
             list = new ArrayList(nameMap.keySet());
             Collections.sort(list, String.CASE_INSENSITIVE_ORDER);
         }
-        
+
         public Object getSelectedItem() {
             return selected;
         }
-        
+
         public void setSelectedItem(Object o) {
             setValue(o);
             stopCellEditing();
         }
-        
+
         public Object getElementAt(int index) {
             return nameMap.get(list.get(index));
         }
-        
+
         public int getSize() {
             return list.size();
         }
@@ -300,5 +326,4 @@ public class EnumerationEditor implements java.beans.PropertyEditor, TableCellEd
             super.fireContentsChanged(source, index0, index1);
         }
     }
-
 }
