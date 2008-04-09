7748ff4e2d05e6a2cacac95afa878679a3dbab79
Jeremy Faden <jbfaden@users.noreply.github.com>
2008-04-09 14:23:15 +0000

    reformat code.
    svn path=/dasCore/trunk/; revision=3847

--- dasCore/src/edu/uiowa/physics/pw/das/event/DisplayDataMouseModule.java
+++ dasCore/src/edu/uiowa/physics/pw/das/event/DisplayDataMouseModule.java
@@ -6,7 +6,6 @@
  * To change this template, choose Tools | Template Manager
  * and open the template in the editor.
  */
-
 package edu.uiowa.physics.pw.das.event;
 
 import edu.uiowa.physics.pw.das.dataset.ClippedTableDataSet;
@@ -38,178 +37,176 @@ import javax.swing.text.Document;
  * @author jbf
  */
 public class DisplayDataMouseModule extends MouseModule {
-    
-    final static String LABEL= "Display Data";
-    
+
+    final static String LABEL = "Display Data";
     DasPlot plot;
-    
     static JFrame myFrame;
     static JPanel myPanel;
     static JEditorPane myEdit;
-    
+
     /** Creates a new instance of DisplayDataMouseModule */
-    public DisplayDataMouseModule( DasPlot parent ) {
-        super( parent, new BoxRenderer(parent), LABEL );
-        this.plot= parent;
+    public DisplayDataMouseModule(DasPlot parent) {
+        super(parent, new BoxRenderer(parent), LABEL);
+        this.plot = parent;
     }
-    
+
     private void maybeCreateFrame() {
-        if ( myFrame==null ) {
-            myFrame= new JFrame( LABEL );
-            myPanel= new JPanel();
-            myPanel.setPreferredSize( new Dimension( 300, 300 ) );
-            myPanel.setLayout( new BorderLayout() );
-            myEdit= new JEditorPane();
-            myEdit.setFont( Font.decode("fixed-10" ) );
-            JScrollPane scrollPane= new JScrollPane( myEdit, JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED, JScrollPane.HORIZONTAL_SCROLLBAR_AS_NEEDED );
-            myPanel.add( scrollPane, BorderLayout.CENTER );
+        if (myFrame == null) {
+            myFrame = new JFrame(LABEL);
+            myPanel = new JPanel();
+            myPanel.setPreferredSize(new Dimension(300, 300));
+            myPanel.setLayout(new BorderLayout());
+            myEdit = new JEditorPane();
+            myEdit.setFont(Font.decode("fixed-10"));
+            JScrollPane scrollPane = new JScrollPane(myEdit, JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED, JScrollPane.HORIZONTAL_SCROLLBAR_AS_NEEDED);
+            myPanel.add(scrollPane, BorderLayout.CENTER);
             myFrame.getContentPane().add(myPanel);
             myFrame.pack();
         }
         return;
     }
-    
-    private String unitsStr( Units u ) {
-        return u==Units.dimensionless ? "" : "("+u.toString()+")";
+
+    private String unitsStr(Units u) {
+        return u == Units.dimensionless ? "" : "(" + u.toString() + ")";
     }
-    
+
     public void mouseRangeSelected(MouseDragEvent e0) {
-        
+
         maybeCreateFrame();
         myFrame.setVisible(true);
-        
-        MouseBoxEvent e= (MouseBoxEvent)e0;
-        
+
+        MouseBoxEvent e = (MouseBoxEvent) e0;
+
         DatumRange xrange;
         DatumRange yrange;
-        
-		if (plot.getXAxis().isFlipped()) {
-			xrange= new DatumRange( plot.getXAxis().invTransform(e.getXMaximum()), plot.getXAxis().invTransform(e.getXMinimum()) );
-		}
-		else {
-			xrange= new DatumRange( plot.getXAxis().invTransform(e.getXMinimum()), plot.getXAxis().invTransform(e.getXMaximum()) );
-		}
-		if (plot.getYAxis().isFlipped()) {
-			yrange= new DatumRange( plot.getYAxis().invTransform(e.getYMinimum()), plot.getYAxis().invTransform(e.getYMaximum()) );
-		}
-		else {
-			yrange= new DatumRange( plot.getYAxis().invTransform(e.getYMaximum()), plot.getYAxis().invTransform(e.getYMinimum()) );
-		}
-        
-        Renderer[] rends= plot.getRenderers();
-        
-        Document doc= myEdit.getDocument();
-        
+
+        if (plot.getXAxis().isFlipped()) {
+            xrange = new DatumRange(plot.getXAxis().invTransform(e.getXMaximum()), plot.getXAxis().invTransform(e.getXMinimum()));
+        } else {
+            xrange = new DatumRange(plot.getXAxis().invTransform(e.getXMinimum()), plot.getXAxis().invTransform(e.getXMaximum()));
+        }
+        if (plot.getYAxis().isFlipped()) {
+            yrange = new DatumRange(plot.getYAxis().invTransform(e.getYMinimum()), plot.getYAxis().invTransform(e.getYMaximum()));
+        } else {
+            yrange = new DatumRange(plot.getYAxis().invTransform(e.getYMaximum()), plot.getYAxis().invTransform(e.getYMinimum()));
+        }
+
+        Renderer[] rends = plot.getRenderers();
+
+        Document doc = myEdit.getDocument();
+
         try {
-            
-            AttributeSet attrSet= null;
-            
-            doc.remove( 0, doc.getLength() ); // erase all
-            
-            for ( int irend=0; irend<rends.length; irend++ ) {
-                
-                doc.insertString( doc.getLength(), "Renderer #"+irend+"\n", attrSet );
-                
-                DataSet ds= rends[irend].getDataSet();
-                
-                if ( ds==null ) {
-                    doc.insertString( doc.getLength(), "(no dataset)\n", attrSet );
+
+            AttributeSet attrSet = null;
+
+            doc.remove(0, doc.getLength()); // erase all
+
+            for (int irend = 0; irend < rends.length; irend++) {
+
+                doc.insertString(doc.getLength(), "Renderer #" + irend + "\n", attrSet);
+
+                DataSet ds = rends[irend].getDataSet();
+
+                if (ds == null) {
+                    doc.insertString(doc.getLength(), "(no dataset)\n", attrSet);
                 }
-                
+
                 DataSet outds;
-                if ( ds instanceof TableDataSet ) {
-                    TableDataSet tds= (TableDataSet)ds;
-                    TableDataSet toutds= new ClippedTableDataSet( tds, xrange, yrange );
-                    
-                    StringBuffer buf= new StringBuffer();
-                    
-                    Units zunits= tds.getZUnits();
-                    DatumFormatter df= tds.getDatum(0,0).getFormatter();
-                    
-                    buf.append( "TableDataSet "+toutds.getXLength()+"x"+toutds.getYLength(0)+" "+unitsStr(zunits)+"\n" );
-                    for ( int i=0; i<toutds.getXLength(); i++ ) {
-                        for ( int j=0; j<toutds.getYLength(0); j++ ) {
+                if (ds instanceof TableDataSet) {
+                    TableDataSet tds = (TableDataSet) ds;
+                    TableDataSet toutds = new ClippedTableDataSet(tds, xrange, yrange);
+
+                    StringBuffer buf = new StringBuffer();
+
+                    Units zunits = tds.getZUnits();
+                    DatumFormatter df = tds.getDatum(0, 0).getFormatter();
+
+                    buf.append("TableDataSet " + toutds.getXLength() + "x" + toutds.getYLength(0) + " " + unitsStr(zunits) + "\n");
+                    for (int i = 0; i < toutds.getXLength(); i++) {
+                        for (int j = 0; j < toutds.getYLength(0); j++) {
                             try {
-                                buf.append( df.format(toutds.getDatum(i,j),zunits) );
-                                buf.append( " " );
-                            } catch ( IndexOutOfBoundsException ex ) {
+                                buf.append(df.format(toutds.getDatum(i, j), zunits));
+                                buf.append(" ");
+                            } catch (IndexOutOfBoundsException ex) {
                                 System.err.println("here");
                             }
                         }
                         buf.append("\n");
                     }
-                    doc.insertString( doc.getLength(), buf.toString(), attrSet );
-                    
+                    doc.insertString(doc.getLength(), buf.toString(), attrSet);
+
                 } else if (ds instanceof VectorDataSet) {
-                    VectorDataSet vds= (VectorDataSet)ds;
-                    
-                    Units units= vds.getYUnits();
-                    Units xunits= vds.getXUnits();
-                    
-                    StringBuffer buf= new StringBuffer();
-                    DatumFormatter df=  vds.getDatum(0).getFormatter();
-                    DatumFormatter xdf= vds.getXTagDatum(0).getFormatter();
-                    
-                    String[] planes= vds.getPlaneIds();
-                    VectorDataSet[] vdss= new VectorDataSet[planes.length];
-                    for ( int j=0; j<planes.length; j++ ) {
-                        vdss[j]= (VectorDataSet) vds.getPlanarView(planes[j]);
+                    VectorDataSet vds = (VectorDataSet) ds;
+
+                    Units units = vds.getYUnits();
+                    Units xunits = vds.getXUnits();
+
+                    StringBuffer buf = new StringBuffer();
+                    DatumFormatter df = vds.getDatum(0).getFormatter();
+                    DatumFormatter xdf = vds.getXTagDatum(0).getFormatter();
+
+                    String[] planes = vds.getPlaneIds();
+                    VectorDataSet[] vdss = new VectorDataSet[planes.length];
+                    for (int j = 0; j < planes.length; j++) {
+                        vdss[j] = (VectorDataSet) vds.getPlanarView(planes[j]);
                     }
-                    
-                    if ( planes.length>1 ) {
-                        buf.append( "X"+unitsStr(xunits)+"\t" );
-                        buf.append( "Y"+unitsStr(units)+"\t");
-                        for ( int j=0; j<vdss.length; j++ ) {
-                            if ( !planes[j].equals("") ) buf.append( ""+ planes[j]+""+unitsStr(vdss[j].getYUnits())+"\t" );
+
+                    if (planes.length > 1) {
+                        buf.append("X" + unitsStr(xunits) + "\t");
+                        buf.append("Y" + unitsStr(units) + "\t");
+                        for (int j = 0; j < vdss.length; j++) {
+                            if (!planes[j].equals("")) {
+                                buf.append("" + planes[j] + "" + unitsStr(vdss[j].getYUnits()) + "\t");
+                            }
                         }
                         buf.append("\n");
                     }
-                    
-                    VectorDataSetBuilder builder= new VectorDataSetBuilder(vds.getXUnits(),vds.getYUnits());
-                    for ( int i=0; i<vds.getXLength(); i++ ) {
-                        if ( xrange.contains( vds.getXTagDatum(i) ) && ( !yclip || yrange.contains(vds.getDatum(i)) ) ) {
-                            buf.append( xdf.format( vds.getXTagDatum(i), xunits ) + "\t" + df.format( vds.getDatum(i), units ) );
-                            for ( int j=0; j<planes.length; j++ ) {
-                                if ( !planes[j].equals("") ) buf.append( "\t" + df.format( vdss[j].getDatum(i), vdss[j].getYUnits() ) );
+
+                    VectorDataSetBuilder builder = new VectorDataSetBuilder(vds.getXUnits(), vds.getYUnits());
+                    for (int i = 0; i < vds.getXLength(); i++) {
+                        if (xrange.contains(vds.getXTagDatum(i)) && (!yclip || yrange.contains(vds.getDatum(i)))) {
+                            buf.append(xdf.format(vds.getXTagDatum(i), xunits) + "\t" + df.format(vds.getDatum(i), units));
+                            for (int j = 0; j < planes.length; j++) {
+                                if (!planes[j].equals("")) {
+                                    buf.append("\t" + df.format(vdss[j].getDatum(i), vdss[j].getYUnits()));
+                                }
                             }
-                            
-                            buf.append( "\n" );
+
+                            buf.append("\n");
                         }
                     }
-                    doc.insertString( doc.getLength(), buf.toString(), attrSet );
-                    
+                    doc.insertString(doc.getLength(), buf.toString(), attrSet);
+
                 }
-                
+
             }
-        } catch ( BadLocationException ex) {
+        } catch (BadLocationException ex) {
             DasExceptionHandler.handle(ex);
         }
     }
-    
+
     public String getListLabel() {
         return getLabel();
     }
-    
+
     public Icon getListIcon() {
         ImageIcon icon;
-        icon= new ImageIcon( this.getClass().getResource("/images/icons/showDataMouseModule.png" ) );
+        icon = new ImageIcon(this.getClass().getResource("/images/icons/showDataMouseModule.png"));
         return icon;
     }
-    
+
     public String getLabel() {
         return LABEL;
     }
-    
     /**
      * Holds value of property yclip.
      */
-    private boolean yclip=false;
-    
+    private boolean yclip = false;
     /**
      * Utility field used by bound properties.
      */
-    private java.beans.PropertyChangeSupport propertyChangeSupport =  new java.beans.PropertyChangeSupport(this);
-    
+    private java.beans.PropertyChangeSupport propertyChangeSupport = new java.beans.PropertyChangeSupport(this);
+
     /**
      * Adds a PropertyChangeListener to the listener list.
      * @param l The listener to add.
@@ -217,7 +214,7 @@ public class DisplayDataMouseModule extends MouseModule {
     public void addPropertyChangeListener(java.beans.PropertyChangeListener l) {
         propertyChangeSupport.addPropertyChangeListener(l);
     }
-    
+
     /**
      * Removes a PropertyChangeListener from the listener list.
      * @param l The listener to remove.
@@ -225,7 +222,7 @@ public class DisplayDataMouseModule extends MouseModule {
     public void removePropertyChangeListener(java.beans.PropertyChangeListener l) {
         propertyChangeSupport.removePropertyChangeListener(l);
     }
-    
+
     /**
      * Getter for property yclip.
      * @return Value of property yclip.
@@ -233,7 +230,7 @@ public class DisplayDataMouseModule extends MouseModule {
     public boolean isYclip() {
         return this.yclip;
     }
-    
+
     /**
      * Setter for property yclip.
      * @param yclip New value of property yclip.
@@ -243,6 +240,4 @@ public class DisplayDataMouseModule extends MouseModule {
         this.yclip = yclip;
         propertyChangeSupport.firePropertyChange("yclip", new Boolean(oldYclip), new Boolean(yclip));
     }
-    
-    
 }
