b167503f1ad43469e0fa188ee9fe54e3eed9e421
Jeremy Faden <jbfaden@users.noreply.github.com>
2008-04-18 14:24:36 +0000

    more checks for runtime exceptions, probably caused by interaction with jython.
    svn path=/dasCore/trunk/; revision=3866

--- dasCore/src/edu/uiowa/physics/pw/das/util/TimerConsoleFormatter.java
+++ dasCore/src/edu/uiowa/physics/pw/das/util/TimerConsoleFormatter.java
@@ -3,7 +3,6 @@
  *
  * Created on April 7, 2005, 12:13 PM
  */
-
 package edu.uiowa.physics.pw.das.util;
 
 import java.text.DecimalFormat;
@@ -15,37 +14,39 @@ import java.util.logging.LogRecord;
  * @author Jeremy
  */
 public class TimerConsoleFormatter extends Formatter {
-    
+
     long t0;
     DecimalFormat nf;
     String lastLoggerName;
-    
-    public String format( LogRecord rec ) {
-        String resetMessage= "firing data point selection event";
-        StackTraceElement[] st= new Throwable().getStackTrace();
+
+    public String format(LogRecord rec) {
+        String resetMessage = "firing data point selection event";
+        StackTraceElement[] st = new Throwable().getStackTrace();
         final String message = rec.getMessage();
         //if ( lastLoggerName==null || lastLoggerName!=rec.getLoggerName() ) {
-        if ( message!=null && message.contains(resetMessage)) {
-            lastLoggerName= rec.getLoggerName();
-            t0=System.currentTimeMillis();
+        if (message != null && message.contains(resetMessage)) {
+            lastLoggerName = rec.getLoggerName();
+            t0 = System.currentTimeMillis();
         }
 
-        String source= String.valueOf( st[8].getClassName() );
-        if ( source.startsWith("edu.uiowa.physics.pw.das" ) ) {
-            String spaces= "                                  ";            
-            source= source.substring("edu.uiowa.physics.pw.das".length());
-            if ( source.length() < spaces.length() ) {
-                source= spaces.substring(source.length()) + source;
+        String source = "???";
+        if (st.length > 8) {
+            source = String.valueOf(st[8].getClassName());
+            if (source.startsWith("edu.uiowa.physics.pw.das")) {
+                String spaces = "                                  ";
+                source = source.substring("edu.uiowa.physics.pw.das".length());
+                if (source.length() < spaces.length()) {
+                    source = spaces.substring(source.length()) + source;
+                }
             }
         }
 
-        long t= System.currentTimeMillis() - t0;
-        return nf.format(t) + ":" + rec.getLoggerName()+": "+ source + ": " + rec.getLevel().getLocalizedName()+": "+ String.valueOf(message)+"\n";
+        long t = System.currentTimeMillis() - t0;
+        return nf.format(t) + ":" + rec.getLoggerName() + ": " + source + ": " + rec.getLevel().getLocalizedName() + ": " + String.valueOf(message) + "\n";
     }
-        
+
     public TimerConsoleFormatter() {
-        t0= System.currentTimeMillis();
-        nf= new DecimalFormat("00000");
+        t0 = System.currentTimeMillis();
+        nf = new DecimalFormat("00000");
     }
-    
 }
