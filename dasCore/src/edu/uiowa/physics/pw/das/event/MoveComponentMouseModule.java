5c92771bbbd464984cfa5f46733d6165a8fe0b36
Jeremy Faden <jbfaden@users.noreply.github.com>
2008-06-06 21:47:31 +0000

    bugfix: enlarge works now, so dirty bounds are properly reported. attempt to draw component image during draw that doesn't work commented out.
    svn path=/dasCore/trunk/; revision=3920

--- dasCore/src/edu/uiowa/physics/pw/das/event/MoveComponentMouseModule.java
+++ dasCore/src/edu/uiowa/physics/pw/das/event/MoveComponentMouseModule.java
@@ -6,7 +6,6 @@
  * To change this template, choose Tools | Template Manager
  * and open the template in the editor.
  */
-
 package edu.uiowa.physics.pw.das.event;
 
 import edu.uiowa.physics.pw.das.graph.DasCanvasComponent;
@@ -22,70 +21,84 @@ import java.awt.event.MouseEvent;
 import java.awt.geom.AffineTransform;
 import java.awt.geom.GeneralPath;
 import java.awt.geom.PathIterator;
-import javax.swing.JComponent;
+import java.awt.image.BufferedImage;
 
 /**
  *
  * @author jbf
  */
 public class MoveComponentMouseModule extends MouseModule {
-    
+
     Point p0;
-    
+
     static class MoveRenderer implements DragRenderer {
 
-        JComponent c;
-        
-        MoveRenderer( JComponent c ) {
-            this.c= c;
+        DasCanvasComponent c;
+        BufferedImage i;
+
+        MoveRenderer(DasCanvasComponent c) {
+            this.c = c;
         }
-        
-        public void setComponent( JComponent c ) {
+
+        private void refreshImage() { // this doesn't work.
+            Rectangle bounds = c.getActiveRegion().getBounds();
+            i = new BufferedImage( bounds.width, bounds.height, BufferedImage.TYPE_INT_ARGB );
+            Graphics g= i.getGraphics();
+            g.translate( c.getX(), c.getY() );
+            c.paint( g );
+
         }
-        
-        private Point center( Shape s ) {
-            long avgX=0, avgY=0;
-            double[] coords= new double[6];
-            long count= 0;
-            for ( PathIterator i= s.getPathIterator(null); !i.isDone(); i.next() ) {
-                i.currentSegment( coords );
-                avgX += coords[0];
-                avgY += coords[1];
-                count++;
+
+        private Point center(Shape s) {
+            long avgX = 0,  avgY = 0;
+            double[] coords = new double[6];
+            long count = 0;
+            for (PathIterator i = s.getPathIterator(null); !i.isDone(); i.next()) {
+                int type = i.currentSegment(coords);
+                if (type == PathIterator.SEG_LINETO) {
+                    avgX += coords[0];
+                    avgY += coords[1];
+                    count++;
+                }
             }
-            
-            avgX/= count;
-            avgY/= count;
-            
-            return new Point( (int)avgX, (int)avgY );
+
+            avgX /= count;
+            avgY /= count;
+
+            return new Point((int) avgX, (int) avgY);
         }
-        
-        private Shape enlarge( Shape s, double scale ) {
-            Point c= center(s);
-            AffineTransform at= new AffineTransform();
-            System.err.println(c);
-            at.translate( +c.x, +c.y );
-            at.scale( scale, scale );
-            GeneralPath gp= new GeneralPath(s);
-            gp.transform( at );
-            at= new AffineTransform();
-            at.translate( -c.x, -c.y );
-            gp.transform( at );
-            
+
+        private Shape enlarge(Shape s, double scale) {
+            Point c = center(s);
+            AffineTransform at = new AffineTransform();
+            System.err.println("enlarge:" + c);
+            at.translate( +c.x, +c.y);
+            at.scale(scale, scale);
+            GeneralPath gp = new GeneralPath(s);
+            gp.transform(at);
+            at = new AffineTransform();
+            at.translate(-c.x*scale, -c.y*scale);
+            gp.transform(at);
+
             return gp;
         }
-        
+
         public Rectangle[] renderDrag(Graphics g1, Point p1, Point p2) {
-            Rectangle bounds= c.getBounds();
-            bounds.translate( -c.getX(), -c.getY() );
-            bounds.translate( p2.x - p1.x, p2.y - p1.x );
-            Graphics2D g= (Graphics2D) g1;
+            Rectangle bounds = c.getActiveRegion().getBounds();
+            bounds.translate(-c.getX(), -c.getY());
+            bounds.translate(p2.x - p1.x, p2.y - p1.y);
+            Graphics2D g = (Graphics2D) g1;
+            
+            //g.drawImage( i, p2.x - p1.x, p2.y-p1.y, c );
+            
             g.setClip(null);
-            g.setColor( Color.RED );
-            g.draw( bounds );
-            g.draw( enlarge( bounds.getBounds(), 1.2 ) );
-            System.err.println( "draw "+bounds.getBounds() );
-            return new Rectangle[] { enlarge( bounds.getBounds(), 1.4 ).getBounds() };
+            g.setColor(Color.BLACK);
+            g.draw(bounds);
+            Point p = center(bounds);
+            //g.drawOval(p.x - 2, p.y - 2, 5, 5);
+            //g.draw(enlarge(bounds.getBounds(), 1.2));
+            //System.err.println("draw " + bounds.getBounds());
+            return new Rectangle[] { enlarge(bounds.getBounds(), 1.2 ).getBounds()};
         }
 
         public void clear(Graphics g) {
@@ -103,34 +116,34 @@ public class MoveComponentMouseModule extends MouseModule {
             return true;
         }
     }
-    
+
     /** Creates a new instance of MoveComponentMouseModule */
-    public MoveComponentMouseModule( DasCanvasComponent parent ) {
-        super( parent, new MoveRenderer(parent), "Move Component" );
+    public MoveComponentMouseModule(DasCanvasComponent parent) {
+        super(parent, new MoveRenderer(parent), "Move Component");
     }
 
     public void mouseReleased(MouseEvent e) {
         super.mouseReleased(e);
-        Point p= e.getPoint();
-        int dx= p.x - p0.x;
-        int dy= p.y - p0.y;
-        
-        DasRow row= this.parent.getRow();
-        row.setDPosition( row.getDMinimum() + dy, row.getDMaximum() + dy );
-        
-        DasColumn col= this.parent.getColumn();
-        col.setDPosition( col.getDMinimum() + dx, col.getDMaximum() + dx );
-        
-        p0=null;
+        Point p = e.getPoint();
+        int dx = p.x - p0.x;
+        int dy = p.y - p0.y;
+
+        DasRow row = this.parent.getRow();
+        row.setDPosition(row.getDMinimum() + dy, row.getDMaximum() + dy);
+
+        DasColumn col = this.parent.getColumn();
+        col.setDPosition(col.getDMinimum() + dx, col.getDMaximum() + dx);
+
+        p0 = null;
     }
 
     public void mousePressed(MouseEvent e) {
         super.mousePressed(e);
-        p0= e.getPoint();
+        ((MoveRenderer)this.dragRenderer).refreshImage();
+        p0 = e.getPoint();
     }
 
     public void mouseDragged(MouseEvent e) {
         super.mouseDragged(e);
     }
-    
 }
