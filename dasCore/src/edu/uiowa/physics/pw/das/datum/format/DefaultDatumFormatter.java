ac6e772970f79dfcd99368f358541dde830e04bd
Jeremy Faden <jbfaden@users.noreply.github.com>
2008-06-24 02:15:30 +0000

    introduce axisFormat.  factor out formatLimitedResolution to make code more readable.
    svn path=/dasCore/trunk/; revision=3996

--- dasCore/src/edu/uiowa/physics/pw/das/datum/format/DefaultDatumFormatter.java
+++ dasCore/src/edu/uiowa/physics/pw/das/datum/format/DefaultDatumFormatter.java
@@ -20,7 +20,6 @@
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  */
-
 package edu.uiowa.physics.pw.das.datum.format;
 
 import edu.uiowa.physics.pw.das.datum.*;
@@ -34,14 +33,14 @@ import java.util.Locale;
  * @author  Edward West
  */
 public class DefaultDatumFormatter extends DatumFormatter {
-    
+
     private String formatString;
-    
     private NumberFormat format;
-    
+
     /** Available for use by subclasses */
-    protected DefaultDatumFormatter() {}
-    
+    protected DefaultDatumFormatter() {
+    }
+
     /** Creates a new instance of DatumFormatter */
     public DefaultDatumFormatter(String formatString) throws ParseException {
         if (formatString.equals("")) {
@@ -52,87 +51,131 @@ public class DefaultDatumFormatter extends DatumFormatter {
             format = NumberFormatUtil.getDecimalFormat(formatString);
         }
     }
-    
+
     public String format(Datum datum) {
-        return format( datum, datum.getUnits() ) + " " + datum.getUnits();
+        return format(datum, datum.getUnits()) + " " + datum.getUnits();
     }
-    
-    public String format(Datum datum, Units units ) {
-        double d= datum.doubleValue(units);
-        if ( Double.isInfinite(d) || Double.isNaN(d) ) return ""+d;
-        
+
+    public String format(Datum datum, Units units) {
+        double d = datum.doubleValue(units);
+        if (Double.isInfinite(d) || Double.isNaN(d)) {
+            return "" + d;
+        }
         String result;
         if (format == null) {
-            double resolution= datum.getResolution( units.getOffsetUnits() );
-            if ( resolution==0. && Double.toString(d).length()>7 ) {
-                // make the default resolution be 0.01%.
-                resolution= d / 10000;
-            }
-            if ( resolution>0 ) {
-                // 28 -->   scale = -1
-                // 2.8 -->  scale = 0 
-                
-                int scale= (int)Math.ceil( -1 * DasMath.log10(resolution) - 0.00001 );
-                int exp;
-                if ( d != 0. ) {
-                    exp= (int)DasMath.log10( Math.abs(d) );
-                } else {
-                    exp= 0;
-                }
-                if ( scale>=0 ) {
-                    DecimalFormat f;
-                    if ( exp<=-5 || exp >=5 ) {
-                        f= NumberFormatUtil.getDecimalFormat( "0E0" );
-                        f.setMinimumFractionDigits(scale+exp-1);
-                        f.setMaximumFractionDigits(scale+exp-1);
-                    } else {
-                        f = NumberFormatUtil.getDecimalFormat("0");
-                        f.setMinimumFractionDigits(scale);
-                        f.setMaximumFractionDigits(scale);
-                    }
-                    result= f.format(d);
-                } else {
-                    double round= DasMath.exp10( -1*scale );
-                    d= Math.round( d / round ) * round;
-                    DecimalFormat f;
-                    if ( exp<=-5 || exp >=5 ) {
-                        f= NumberFormatUtil.getDecimalFormat( "0E0" );
-                        f.setMinimumFractionDigits(scale+exp+1);
-                        f.setMaximumFractionDigits(scale+exp+1);
-                    } else {
-                        f = NumberFormatUtil.getDecimalFormat("0");
-                    }
-                    result= f.format(d);
-                }
-            } else {
-                result=  Double.toString(d);
-            }
+            double resolution = datum.getResolution(units.getOffsetUnits());
+            result = formatLimitedResolution(d, resolution);
         } else {
-            result= format.format(datum.doubleValue(units));
+            result = format.format(datum.doubleValue(units));
         }
         return result;
     }
-    
-    public String grannyFormat( Datum datum, Units units ) {
-        String format= format( datum, units );
-        if ( format.indexOf("E")!=-1 ) {
-            int iE= format.indexOf("E");
-            StringBuffer granny = new StringBuffer(format.length() + 4);
-            String mant= format.substring(0,iE);
-            if (Double.parseDouble(mant)!=1.0) {
+
+    @Override
+    public String grannyFormat(Datum datum, Units units) {
+        String formt = format(datum, units);
+        if (formt.indexOf("E") != -1) {
+            int iE = formt.indexOf("E");
+            StringBuffer granny = new StringBuffer(formt.length() + 4);
+            String mant = formt.substring(0, iE);
+            if (Double.parseDouble(mant) != 1.0) {
                 granny.append(mant).append("\u00d7");
             }
-            granny.append("10").append("!A").append(format.substring(iE+1)).append("!N");
-            format = granny.toString();
+            granny.append("10").append("!A").append(formt.substring(iE + 1)).append("!N");
+            formt = granny.toString();
         }
-        return format;
+        return formt;
     }
-    
+
+    @Override
+    public String[] axisFormat(DatumVector datums, DatumRange context ) {
+        Units units= context.getUnits();
+        String[] result = new String[datums.getLength()];
+        for (int i = 0; i < result.length; i++) {
+            result[i] = format(datums.get(i), units);
+        }
+        boolean hasMant = false;
+        for (int i = 0; i < result.length; i++) {
+            String res1 = result[i];
+            if (res1.indexOf("E") != -1) {
+                int iE = res1.indexOf("E");
+                String mant = res1.substring(0, iE);
+                if (Double.parseDouble(mant) != 1.0) {
+                    hasMant = true;
+                }
+            }
+        }
+        for (int i = 0; i < result.length; i++) {
+            String res1 = result[i];
+            if (res1.indexOf("E") != -1) {
+                int iE = res1.indexOf("E");
+                StringBuffer granny = new StringBuffer(res1.length() + 4);
+                String mant = res1.substring(0, iE);
+
+                if (hasMant) {
+                    granny.append(mant).append("\u00d7");
+                }
+
+                granny.append("10").append("!A").append(res1.substring(iE + 1)).append("!N");
+                result[i] = granny.toString();
+            }
+
+        }
+        return result;
+    }
+
     public String grannyFormat(Datum datum) {
-        return grannyFormat(datum,datum.getUnits()) + " " + datum.getUnits();
+        return grannyFormat(datum, datum.getUnits()) + " " + datum.getUnits();
     }
-    
+
     public String toString() {
         return formatString;
+    }
+
+    private String formatLimitedResolution(double d, double resolution) {
+        String result;
+        if (resolution == 0. && Double.toString(d).length() > 7) {
+            // make the default resolution be 0.01%.
+            resolution = d / 10000;
+        }
+        if (resolution > 0) {
+            // 28 -->   scale = -1
+            // 2.8 -->  scale = 0
+            int scale = (int) Math.ceil(-1 * DasMath.log10(resolution) - 0.00001);
+            int exp;
+            if (d != 0.) {
+                exp = (int) DasMath.log10(Math.abs(d));
+            } else {
+                exp = 0;
+            }
+            if (scale >= 0) {
+                DecimalFormat f;
+                if (exp <= -5 || exp >= 5) {
+                    f = NumberFormatUtil.getDecimalFormat("0E0");
+                    f.setMinimumFractionDigits(scale + exp - 1);
+                    f.setMaximumFractionDigits(scale + exp - 1);
+                } else {
+                    f = NumberFormatUtil.getDecimalFormat("0");
+                    f.setMinimumFractionDigits(scale);
+                    f.setMaximumFractionDigits(scale);
+                }
+                result = f.format(d);
+            } else {
+                double round = DasMath.exp10(-1 * scale);
+                d = Math.round(d / round) * round;
+                DecimalFormat f;
+                if (exp <= -5 || exp >= 5) {
+                    f = NumberFormatUtil.getDecimalFormat("0E0");
+                    f.setMinimumFractionDigits(scale + exp + 1);
+                    f.setMaximumFractionDigits(scale + exp + 1);
+                } else {
+                    f = NumberFormatUtil.getDecimalFormat("0");
+                }
+                result = f.format(d);
+            }
+        } else {
+            result = Double.toString(d);
+        }
+        return result;
     }
 }
