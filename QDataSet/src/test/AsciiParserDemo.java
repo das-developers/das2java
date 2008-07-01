ad83512185e33f4577d696ad275139865842a5c8
Jeremy Faden <jbfaden@users.noreply.github.com>
2008-07-01 02:32:12 +0000

    demo shortcoming of String.split function, which omits the empty "" when nothing follows the regex.
    svn path=/autoplot/trunk/QDataSet/; revision=610

--- QDataSet/src/test/AsciiParserDemo.java
+++ QDataSet/src/test/AsciiParserDemo.java
@@ -10,6 +10,7 @@
 package test;
 
 import edu.uiowa.physics.pw.das.datum.Units;
+import java.net.MalformedURLException;
 import org.das2.util.monitor.NullProgressMonitor;
 import edu.uiowa.physics.pw.das.util.TimeParser;
 import java.io.IOException;
@@ -28,24 +29,36 @@ public class AsciiParserDemo {
      * @param args the command line arguments
      */
     public static void main(String[] args) throws IOException {
-        AsciiParser parser= new AsciiParser();
-        URL testFile= new URL( "file:///N:/data/examples/asciitable/omni2_1965.dat" );
-        parser.setDelimParser( new InputStreamReader( testFile.openStream() ), "\\s+" );
         
-        WritableDataSet ds= parser.readStream( new InputStreamReader( testFile.openStream() ), new NullProgressMonitor() );
+        //test1();
+        
+        System.err.println( ",hello,".split(",").length );
+        System.err.println( " hello ".split("\\s+").length );
+        
+        System.err.println( TimeParser.iso8601String("2008-09-04T00:00:22") );
+        System.err.println( TimeParser.iso8601String("2008-004 00:00") );
+        System.err.println( TimeParser.iso8601String("2008-09-04T00:00:22.000000") );
+        System.err.println( TimeParser.iso8601String("19951231T235959") );
+    }
 
-        final TimeParser p= TimeParser.create("%Y %j %H");
-        final Units u= Units.t2000;        
+    private static void test1() throws IOException, MalformedURLException {
+        AsciiParser parser = new AsciiParser();
+        URL testFile = new URL("file:///N:/data/examples/asciitable/omni2_1965.dat");
+        parser.setDelimParser(new InputStreamReader(testFile.openStream()), "\\s+");
+
+        WritableDataSet ds = parser.readStream(new InputStreamReader(testFile.openStream()), new NullProgressMonitor());
+
+        final TimeParser p = TimeParser.create("%Y %j %H");
+        final Units u = Units.t2000;
         // replace the first column with the datum time
-        for ( int i=0; i<ds.length(); i++ ) {
-            p.setDigit( 0, (int) ds.value( i,0 ) );
-            p.setDigit( 1, (int) ds.value( i,1 ) );
-            p.setDigit( 2, (int) ds.value( i,2 ) );
-            ds.putValue( i, 0, p.getTime( Units.t2000 ) );
+        for (int i = 0; i < ds.length(); i++) {
+            p.setDigit(0, (int) ds.value( i,0 ));
+            p.setDigit(1, (int) ds.value( i,1 ));
+            p.setDigit(2, (int) ds.value( i,2 ));
+            ds.putValue(i, 0, p.getTime(Units.t2000));
         }
-        
+
         System.err.println(ds);
-        
     }
     
 }
