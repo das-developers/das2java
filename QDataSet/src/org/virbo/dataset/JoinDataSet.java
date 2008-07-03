4b22a93cae4251aebc835a90a0fcb8bc574ffc66
Jeremy Faden <jbfaden@users.noreply.github.com>
2008-07-03 15:24:58 +0000

    unused JoinDataSet changed to implement the anti-slice dataset as intended.  JoinDataSet takes N QDataSets of rank M to make a new QDataSet of rank M+1 with length()=N.
    svn path=/autoplot/trunk/QDataSet/; revision=620

--- QDataSet/src/org/virbo/dataset/JoinDataSet.java
+++ QDataSet/src/org/virbo/dataset/JoinDataSet.java
@@ -10,74 +10,83 @@
 package org.virbo.dataset;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
- *
+ * create a higher rank dataset with dim 0 being a join dimension.
  * @author jbf
  */
-public class JoinDataSet implements QDataSet {
+public class JoinDataSet implements QDataSet, MutablePropertyDataSet {
     
-    ArrayList datasets;
+    ArrayList<QDataSet> datasets;
     int[] offsets= new int[20];
     int[] lengths= new int[20];
-    int count=0;
     int rank;
+    Map<String,Object> properties;
     
     /** Creates a new instance of JoinDataSet */
-    public JoinDataSet() {
-        count=0;
+    public JoinDataSet( int dataSetsRank ) {
+        this.rank= dataSetsRank;
+        datasets= new ArrayList<QDataSet>();
+        properties= new HashMap<String,Object>();
     }
     
     public void join( QDataSet ds ) {
+        if ( ds.rank()!=this.rank ) throw new IllegalArgumentException("dataset rank must be "+this.rank);
         datasets.add( ds );
-        if ( count==0 ) {
-            offsets[count]= 0;   
-            rank= ds.rank();
-        } else {
-            offsets[count]= offsets[count-1] + lengths[count-1];
-        }
-        lengths[count]= ds.length();
-        count++;
     }
 
     public int rank() {
-        return rank;
+        return rank+1;
     }
 
     public double value(int i) {
-        throw new RuntimeException("not implemented");
+        throw new IllegalArgumentException("rank error");
     }
 
     public double value(int i0, int i1) {
-        throw new RuntimeException("not implemented");
+        return datasets.get(i0).value(i1);
     }
 
     public double value(int i0, int i1, int i2) {
-        throw new RuntimeException("not implemented");
+        return datasets.get(i0).value(i1,i2);
     }
 
     public Object property(String name) {
-        throw new RuntimeException("not implemented");
+        return properties.get(name);
     }
 
-    public Object property(String name, int i) {
-        throw new RuntimeException("not implemented");
+    public Object property(String name, int i0) {
+        return datasets.get(i0).property(name);
     }
 
     public Object property(String name, int i0, int i1) {
-        throw new RuntimeException("not implemented");
+        return datasets.get(i0).property(name,i1);
     }
 
     public int length() {
-        throw new RuntimeException("not implemented");
+        return datasets.size();
     }
 
-    public int length(int i) {
-        throw new RuntimeException("not implemented");
+    public int length(int i0) {
+        return datasets.get(i0).length();
     }
 
-    public int length(int i, int j) {
-        throw new RuntimeException("not implemented");
+    public int length(int i0, int i1) {
+        return datasets.get(i0).length(i1);
+    }
+
+    public void putProperty(String name, Object value) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    public void putProperty(String name, int index, Object value) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    public void putProperty(String name, int index1, int index2, Object value) {
+        throw new UnsupportedOperationException("Not supported yet.");
     }
     
 }
