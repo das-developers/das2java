6f04f85cbdfcbcff985c03b925b77d0380803499
Jeremy Faden <jbfaden@users.noreply.github.com>
2008-07-04 02:57:06 +0000

    tree supports drill down to see some data.
    svn path=/autoplot/trunk/QDataSet/; revision=630

--- QDataSet/src/test/PropertiesTreeModelDemo.java
+++ QDataSet/src/test/PropertiesTreeModelDemo.java
@@ -12,6 +12,7 @@ import javax.swing.tree.TreeModel;
 import org.virbo.dataset.DDataSet;
 import org.virbo.dataset.DataSetUtil;
 import org.virbo.dataset.QDataSet;
+import org.virbo.dsops.Ops;
 import org.virbo.dsutil.PropertiesTreeModel;
 
 /**
@@ -22,11 +23,11 @@ public class PropertiesTreeModelDemo {
 
     public static void main(String[] args) {
         
-        DDataSet ds = DDataSet.createRank2(10, 20);
+        DDataSet ds = (DDataSet)Ops.dindgen(10, 20);
         ds.putProperty(DDataSet.NAME, "testData");
         ds.putProperty(QDataSet.UNITS, Units.meters);
         
-        ds.putProperty(DDataSet.PLANE_0, DDataSet.createRank2(10, 20) );
+        ds.putProperty(DDataSet.PLANE_0, Ops.dindgen(10, 20) );
                 
         HashMap props= (HashMap) DataSetUtil.getProperties(ds);
         
