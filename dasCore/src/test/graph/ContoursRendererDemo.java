5dbaf45f1471de009193ed1d102dffd3ce4bb852
Jeremy Faden <jbfaden@users.noreply.github.com>
2008-08-06 02:07:34 +0000

    introduce demo for contoursRenderer
    svn path=/dasCore/trunk/; revision=4099

---
+++ dasCore/src/test/graph/ContoursRendererDemo.java
@@ -0,0 +1,52 @@
+/*
+ * ContoursRendererDemo.java
+ *
+ * Created on December 7, 2007, 3:22 PM
+ *
+ * To change this template, choose Tools | Template Manager
+ * and open the template in the editor.
+ */
+
+package test.graph;
+
+import edu.uiowa.physics.pw.das.dataset.TableDataSet;
+import edu.uiowa.physics.pw.das.dataset.test.RipplesDataSet;
+import edu.uiowa.physics.pw.das.event.AnnotatorMouseModule;
+import edu.uiowa.physics.pw.das.event.MouseModule;
+import edu.uiowa.physics.pw.das.event.PointSlopeDragRenderer;
+import edu.uiowa.physics.pw.das.graph.GraphUtil;
+import edu.uiowa.physics.pw.das.graph.Renderer;
+import edu.uiowa.physics.pw.das.graph.ContoursRenderer;
+import edu.uiowa.physics.pw.das.graph.DasPlot;
+
+/**
+ *
+ * @author jbf
+ */
+public class ContoursRendererDemo  {
+    
+    /** Creates a new instance of ContoursRendererDemo */
+    public ContoursRendererDemo() {
+        super();
+        Renderer rend= new ContoursRenderer();
+        
+        TableDataSet tds= new RipplesDataSet(50,50,20,70,70,30,100,100);
+        
+        DasPlot p= GraphUtil.visualize( tds );
+        p.getXAxis().setAnimated(false);
+        p.getYAxis().setAnimated(false);
+        
+	MouseModule mm= new MouseModule( p, new PointSlopeDragRenderer(p, p.getXAxis(), p.getYAxis() ), "Slope" );
+	
+	p.addMouseModule( mm );
+	
+        rend.setDataSet( tds );
+        
+        p.addRenderer( rend );
+        
+    }
+    
+    public static void main( String[] args ) {
+        new ContoursRendererDemo();
+    }
+}
