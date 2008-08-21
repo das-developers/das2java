d4a433209497a407eb2e72ec3df4e485b4c6d100
Jeremy Faden <jbfaden@users.noreply.github.com>
2008-08-21 20:21:20 +0000

    indicate (ordinal) in toString.
    svn path=/dasCore/trunk/; revision=4117

--- dasCore/src/edu/uiowa/physics/pw/das/datum/EnumerationUnits.java
+++ dasCore/src/edu/uiowa/physics/pw/das/datum/EnumerationUnits.java
@@ -20,12 +20,11 @@
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  */
-
 package edu.uiowa.physics.pw.das.datum;
 
-import java.util.HashMap;
 import edu.uiowa.physics.pw.das.datum.format.*;
-import java.util.*;
+import java.util.HashMap;
+import java.util.Iterator;
 
 /**
  * Units class for mapping arbitary objects to Datums.  Nothing about the contract
@@ -33,140 +32,147 @@ import java.util.*;
  * assign a mapping from numbers to objects using this class.  This allows 
  * information such as "Cluster 1" or "Spin Flip" to be encoded.
  * 
+ * This is used to model ordinal or nominal data, as described in
+ * http://en.wikipedia.org/wiki/Level_of_measurement
+ * 
  * @author  Jeremy
  */
 public class EnumerationUnits extends Units {
-    
+
     private HashMap ordinals;  // maps from ordinal to Datum.Integer
     private int highestOrdinal; // highest ordinal for each Units type
     private HashMap objects;    // maps from object to Datum.Integer
     private HashMap invObjects; // maps from Datum.Integer to object
-    
     public static HashMap unitsInstances;
-    
+
     public EnumerationUnits(String id) {
-        this(id,"");
+        this(id, "");
     }
-    
-    public EnumerationUnits(String id, String description ) {        
-        super(id,description);
-        highestOrdinal=0;
-        ordinals= new HashMap();
-        objects= new HashMap();
-        invObjects= new HashMap();
+
+    public EnumerationUnits(String id, String description) {
+        super(id, description);
+        highestOrdinal = 0;
+        ordinals = new HashMap();
+        objects = new HashMap();
+        invObjects = new HashMap();
     }
-    
-    public static Datum createDatumAndUnits( Object object ) {
-        return create(object).createDatum( object );
+
+    public static Datum createDatumAndUnits(Object object) {
+        return create(object).createDatum(object);
     }
-    
-    public DatumVector createDatumVector( Object[] objects ) {
-        double[] doubles= new double[objects.length];
-        for ( int i=0; i<objects.length; i++ ) {
-            doubles[i]= createDatum(objects[i]).doubleValue(this);
+
+    public DatumVector createDatumVector(Object[] objects) {
+        double[] doubles = new double[objects.length];
+        for (int i = 0; i < objects.length; i++) {
+            doubles[i] = createDatum(objects[i]).doubleValue(this);
         }
-        return DatumVector.newDatumVector(doubles,this);
+        return DatumVector.newDatumVector(doubles, this);
     }
-    
-    public Datum createDatum( Object object ) {
-        if ( objects.containsKey(object) ) {
-            return (Datum)objects.get(object);
+
+    public synchronized Datum createDatum(Object object) {
+        if (objects.containsKey(object)) {
+            return (Datum) objects.get(object);
         } else {
             highestOrdinal++;
-            Integer ordinal= new Integer( highestOrdinal );
-            Datum result= new Datum.Double( ordinal, this );
-            ordinals.put(ordinal,result);            
-            invObjects.put(result,object);
-            objects.put(object,result);
+            Integer ordinal = new Integer(highestOrdinal);
+            Datum result = new Datum.Double(ordinal, this);
+            ordinals.put(ordinal, result);
+            invObjects.put(result, object);
+            objects.put(object, result);
             return result;
         }
-    }    
-    
-    public Datum createDatum( int value ) {
-        Integer key= new Integer(value);
-        if ( ordinals.containsKey(key) ) {
-            return (Datum)ordinals.get(key);
-        } else {
-            throw new IllegalArgumentException("No Datum exists for this ordinal: "+value);
-        }
-    }            
-    
-    public Datum createDatum(long value) {
-        return createDatum( (int)value );
     }
-    
+
+    public Datum createDatum(int value) {
+        Integer key = new Integer(value);
+        if (ordinals.containsKey(key)) {
+            return (Datum) ordinals.get(key);
+        } else {
+            throw new IllegalArgumentException("No Datum exists for this ordinal: " + value);
+        }
+    }
+
+    public Datum createDatum(long value) {
+        return createDatum((int) value);
+    }
+
     public Datum createDatum(Number value) {
         return createDatum(value.intValue());
     }
 
-    public Object getObject( Datum datum ) {        
-        if ( invObjects.containsKey(datum) ) {
+    public Object getObject(Datum datum) {
+        if (invObjects.containsKey(datum)) {
             return invObjects.get(datum);
         } else {
-            throw new IllegalArgumentException( "This Datum doesn't map back to an object!  This shouldn't happen!" );
+            throw new IllegalArgumentException("This Datum doesn't map back to an object!  This shouldn't happen!");
         }
     }
-    
-    public static EnumerationUnits create( Object o ) {
-        if (unitsInstances==null) unitsInstances= new HashMap();
-        Class c= o.getClass();
+
+    public static EnumerationUnits create(Object o) {
+        if (unitsInstances == null)
+            unitsInstances = new HashMap();
+        Class c = o.getClass();
         if (unitsInstances.containsKey(c)) {
-            return (EnumerationUnits)unitsInstances.get(c);
+            return (EnumerationUnits) unitsInstances.get(c);
         } else {
-            EnumerationUnits result= new EnumerationUnits(c.toString()+"Unit");
-            unitsInstances.put(c,result);
+            EnumerationUnits result = new EnumerationUnits(c.toString() + "Unit");
+            unitsInstances.put(c, result);
             return result;
         }
     }
-    
-    public Datum createDatum( double d ) {
-        return createDatum((int)d);
-    }    
-    
-    public Datum createDatum( double d, double resolution ) {
-        return createDatum((int)d);
+
+    public Datum createDatum(double d) {
+        return createDatum((int) d);
     }
-    
+
+    public Datum createDatum(double d, double resolution) {
+        return createDatum((int) d);
+    }
+
     public DatumFormatterFactory getDatumFormatterFactory() {
         return EnumerationDatumFormatterFactory.getInstance();
-    }            
-    
+    }
+
     public Datum subtract(Number a, Number b, Units bUnits) {
         throw new IllegalArgumentException("subtract on EnumerationUnit");
     }
-    
+
     public Datum add(Number a, Number b, Units bUnits) {
         throw new IllegalArgumentException("add on EnumerationUnit");
     }
-    
+
     public Datum divide(Number a, Number b, Units bUnits) {
         throw new IllegalArgumentException("divide on EnumerationUnit");
     }
-    
+
     public Datum multiply(Number a, Number b, Units bUnits) {
         throw new IllegalArgumentException("multiply on EnumerationUnit");
     }
-    
+
     public Datum parse(String s) throws java.text.ParseException {
-        Datum result=null;
-        for ( Iterator i= objects.keySet().iterator(); i.hasNext(); ) {
+        Datum result = null;
+        for (Iterator i = objects.keySet().iterator(); i.hasNext();) {
             Object key = i.next();
-            Object value= objects.get(key);
-            if ( key.toString().equals(s) ) { // if the look the same, they are the same
-                if ( result==null ) {
-                    result= (Datum)objects.get(key);
+            Object value = objects.get(key);
+            if (key.toString().equals(s)) { // if the look the same, they are the same
+                if (result == null) {
+                    result = (Datum) objects.get(key);
                 } else {
                     throw new IllegalStateException("Multiple Objects' string representations match");
                 }
             }
         }
-        if ( result==null ) {
-            throw new java.text.ParseException("no objects match \""+s+"\"",0);
+        if (result == null) {
+            throw new java.text.ParseException("no objects match \"" + s + "\"", 0);
         }
         return result;
-    }    
-    
+    }
+
     public int getHighestOrdinal() {
         return this.highestOrdinal;
+    }
+
+    public String toString() {
+        return this.getId() + "(ordinal)";
     }
 }
