6d52f02ad1bc8636e995072280c1dfe00d949a15
Jeremy Faden <jbfaden@users.noreply.github.com>
2008-08-21 20:23:29 +0000

    ctrl + mousewheel is now pan.
    svn path=/dasCore/trunk/; revision=4119

--- dasCore/src/edu/uiowa/physics/pw/das/event/ZoomPanMouseModule.java
+++ dasCore/src/edu/uiowa/physics/pw/das/event/ZoomPanMouseModule.java
@@ -6,7 +6,6 @@
  * To change this template, choose Tools | Template Manager
  * and open the template in the editor.
  */
-
 package edu.uiowa.physics.pw.das.event;
 
 import edu.uiowa.physics.pw.das.datum.Datum;
@@ -27,149 +26,154 @@ import java.awt.event.MouseWheelEvent;
  * @author jbf
  */
 public class ZoomPanMouseModule extends MouseModule {
-    
+
     DasAxis xAxis;
     DasAxis yAxis;
-    
     DasAxis.Lock xAxisLock;
     DasAxis.Lock yAxisLock;
-    
     Point p0;
     DatumRange xAxisRange0;
     DatumRange yAxisRange0;
-    
+
     /** Creates a new instance of ZoomPanMouseModule */
-    public ZoomPanMouseModule( DasCanvasComponent parent,DasAxis horizontalAxis, DasAxis verticalAxis  ) {
+    public ZoomPanMouseModule(DasCanvasComponent parent, DasAxis horizontalAxis, DasAxis verticalAxis) {
         super(parent);
         setLabel("Zoom Pan");
-        this.xAxis= horizontalAxis;
-        this.yAxis= verticalAxis;
+        this.xAxis = horizontalAxis;
+        this.yAxis = verticalAxis;
     }
-    
-    private boolean axisIsAdjustable( DasAxis axis ) {
-	return axis!=null && ( UnitsUtil.isIntervalMeasurement(axis.getUnits()) || UnitsUtil.isRatioMeasurement(axis.getUnits()) );
+
+    private boolean axisIsAdjustable(DasAxis axis) {
+        return axis != null && (UnitsUtil.isIntervalMeasurement(axis.getUnits()) || UnitsUtil.isRatioMeasurement(axis.getUnits()));
     }
-    
+
     public void mouseWheelMoved(MouseWheelEvent e) {
         double nmin, nmax;
-        if ( e.getWheelRotation()<0 ) {
-            nmin= 0.20;
-            nmax= 0.80;
+        if (e.isControlDown()) {
+            if (e.getWheelRotation() < 0) {
+                nmin = -0.20;
+                nmax = +0.80;
+            } else {
+                nmin = +0.20;
+                nmax = +1.20;
+            }            
         } else {
-            nmin= -0.25;
-            nmax= 1.25;
+            if (e.getWheelRotation() < 0) {
+                nmin = 0.20;
+                nmax = 0.80;
+            } else {
+                nmin = -0.25;
+                nmax = 1.25;
+            }
         }
         //int clickMag= Math.abs(e.getWheelRotation());
-        int clickMag= 1;
-        if ( axisIsAdjustable(xAxis) ) {
-            DatumRange dr= xAxis.getDatumRange();
-            for ( int i=0; i<clickMag; i++ ) {
-                if ( xAxis.isLog() ) {
-                    dr= DatumRangeUtil.rescaleLog( dr, nmin, nmax );
+        int clickMag = 1;
+        if (axisIsAdjustable(xAxis)) {
+            DatumRange dr = xAxis.getDatumRange();
+            for (int i = 0; i < clickMag; i++) {
+                if (xAxis.isLog()) {
+                    dr = DatumRangeUtil.rescaleLog(dr, nmin, nmax);
                 } else {
-                    dr= DatumRangeUtil.rescale( dr, nmin, nmax );
+                    dr = DatumRangeUtil.rescale(dr, nmin, nmax);
                 }
             }
-            xAxis.setDatumRange( dr );
+            xAxis.setDatumRange(dr);
         }
-        if ( axisIsAdjustable(yAxis) ) {
-            DatumRange dr= yAxis.getDatumRange();
-            for ( int i=0; i<clickMag; i++ ) {
-                
-                if ( yAxis.isLog() ) {
-                    dr= DatumRangeUtil.rescaleLog( dr, nmin, nmax );
+        if (axisIsAdjustable(yAxis)) {
+            DatumRange dr = yAxis.getDatumRange();
+            for (int i = 0; i < clickMag; i++) {
+
+                if (yAxis.isLog()) {
+                    dr = DatumRangeUtil.rescaleLog(dr, nmin, nmax);
                 } else {
-                    dr= DatumRangeUtil.rescale( dr, nmin, nmax );
+                    dr = DatumRangeUtil.rescale(dr, nmin, nmax);
                 }
             }
-            yAxis.setDatumRange( dr );
+            yAxis.setDatumRange(dr);
         }
-        
+
         super.mouseWheelMoved(e);
     }
-    
+
     public void mouseReleased(MouseEvent e) {
         super.mouseReleased(e);
-        if ( xAxis!=null ) {
+        if (xAxis != null) {
             xAxisLock.unlock();
-            xAxisLock=null;
+            xAxisLock = null;
         }
-        if ( yAxis!=null ) {
+        if (yAxis != null) {
             yAxisLock.unlock();
-            yAxisLock=null;
+            yAxisLock = null;
         }
-        doPan(e, false );
-	parent.getCanvas().getGlassPane().setCursor( null );
+        doPan(e, false);
+        parent.getCanvas().getGlassPane().setCursor(null);
     }
-    
+
     /**
      * round to a nice boundaries.
      */
-    private static DatumRange doRound( DatumRange dr, DasAxis axis ) {
+    private static DatumRange doRound(DatumRange dr, DasAxis axis) {
         TickVDescriptor ticks;
-        if ( dr.getUnits() instanceof TimeLocationUnits) {
-            ticks= TickVDescriptor.bestTickVTime( dr.min(), dr.max(), axis.getDLength()/2, axis.getDLength(), true );
-        } else if ( axis.isLog() ) {
-            ticks= TickVDescriptor.bestTickVLogNew( dr.min(), dr.max(), axis.getDLength()/2, axis.getDLength(), true );
+        if (dr.getUnits() instanceof TimeLocationUnits) {
+            ticks = TickVDescriptor.bestTickVTime(dr.min(), dr.max(), axis.getDLength() / 2, axis.getDLength(), true);
+        } else if (axis.isLog()) {
+            ticks = TickVDescriptor.bestTickVLogNew(dr.min(), dr.max(), axis.getDLength() / 2, axis.getDLength(), true);
         } else {
-            ticks= TickVDescriptor.bestTickVLinear( dr.min(), dr.max(), axis.getDLength()/2, axis.getDLength(), true );
+            ticks = TickVDescriptor.bestTickVLinear(dr.min(), dr.max(), axis.getDLength() / 2, axis.getDLength(), true);
         }
-        return ticks.enclosingRange( dr, true );
+        return ticks.enclosingRange(dr, true);
     }
-    
+
     private void doPan(final MouseEvent e, boolean round) {
-        Point p2= e.getPoint();
-        if ( axisIsAdjustable(xAxis) ) {
+        Point p2 = e.getPoint();
+        if (axisIsAdjustable(xAxis)) {
             DatumRange dr;
-            if ( xAxis.isLog() ) {
-                Datum delta= xAxis.invTransform( p0.getX() ).divide( xAxis.invTransform( p2.getX() ) );
-                dr= new DatumRange( xAxisRange0.min().multiply(delta), xAxisRange0.max().multiply(delta) );
+            if (xAxis.isLog()) {
+                Datum delta = xAxis.invTransform(p0.getX()).divide(xAxis.invTransform(p2.getX()));
+                dr = new DatumRange(xAxisRange0.min().multiply(delta), xAxisRange0.max().multiply(delta));
             } else {
-                Datum delta= xAxis.invTransform( p0.getX() ).subtract( xAxis.invTransform( p2.getX() ) );
-                dr= new DatumRange( xAxisRange0.min().add(delta), xAxisRange0.max().add(delta) );
+                Datum delta = xAxis.invTransform(p0.getX()).subtract(xAxis.invTransform(p2.getX()));
+                dr = new DatumRange(xAxisRange0.min().add(delta), xAxisRange0.max().add(delta));
             }
-            if ( round ) {
-                dr= doRound(dr,xAxis);
+            if (round) {
+                dr = doRound(dr, xAxis);
             }
-            xAxis.setDatumRange( dr );
+            xAxis.setDatumRange(dr);
         }
-        if ( axisIsAdjustable(yAxis) ) {
+        if (axisIsAdjustable(yAxis)) {
             DatumRange dr;
-            if ( yAxis.isLog() ) {
-                Datum ydelta= yAxis.invTransform( p0.getY() ).divide( yAxis.invTransform( p2.getY() ) );
-                dr= new DatumRange( yAxisRange0.min().multiply(ydelta), yAxisRange0.max().multiply(ydelta) );
+            if (yAxis.isLog()) {
+                Datum ydelta = yAxis.invTransform(p0.getY()).divide(yAxis.invTransform(p2.getY()));
+                dr = new DatumRange(yAxisRange0.min().multiply(ydelta), yAxisRange0.max().multiply(ydelta));
             } else {
-                Datum ydelta= yAxis.invTransform( p0.getY() ).subtract( yAxis.invTransform( p2.getY() ) );
-                dr= new DatumRange( yAxisRange0.min().add(ydelta), yAxisRange0.max().add(ydelta) );
+                Datum ydelta = yAxis.invTransform(p0.getY()).subtract(yAxis.invTransform(p2.getY()));
+                dr = new DatumRange(yAxisRange0.min().add(ydelta), yAxisRange0.max().add(ydelta));
             }
-            if ( round ) {
-                dr= doRound(dr,yAxis);
+            if (round) {
+                dr = doRound(dr, yAxis);
             }
-            yAxis.setDatumRange( dr );
+            yAxis.setDatumRange(dr);
         }
     }
-    
+
     public void mouseDragged(MouseEvent e) {
         super.mouseDragged(e);
         doPan(e, false);
     }
-    
+
     public void mousePressed(MouseEvent e) {
         super.mousePressed(e);
-        p0= e.getPoint();
-        if ( xAxis!=null ) {
-            xAxisRange0= xAxis.getDatumRange();
-            xAxisLock= xAxis.mutatorLock();
+        p0 = e.getPoint();
+        if (xAxis != null) {
+            xAxisRange0 = xAxis.getDatumRange();
+            xAxisLock = xAxis.mutatorLock();
             xAxisLock.lock();
         }
-        if ( yAxis!=null ) {
-            yAxisRange0= yAxis.getDatumRange();
-            yAxisLock= yAxis.mutatorLock();
+        if (yAxis != null) {
+            yAxisRange0 = yAxis.getDatumRange();
+            yAxisLock = yAxis.mutatorLock();
             yAxisLock.lock();
         }
-	parent.getCanvas().getGlassPane().setCursor( new Cursor( Cursor.HAND_CURSOR ) );
+        parent.getCanvas().getGlassPane().setCursor(new Cursor(Cursor.HAND_CURSOR));
     }
-    
-    
-    
 }
