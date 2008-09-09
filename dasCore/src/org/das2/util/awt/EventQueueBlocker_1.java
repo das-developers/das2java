a42a64ad6653f4dd953acd7dc8a061969c8db2dc
Jeremy Faden <jbfaden@users.noreply.github.com>
2008-09-09 20:03:07 +0000

    edu.uiowa.physics.pw.das renamed to org.das2
    svn path=/dasCore/trunk/; revision=4172

---
+++ dasCore/src/org/das2/util/awt/EventQueueBlocker_1.java
@@ -0,0 +1,95 @@
+/*
+ * EventQueueBlocker.java
+ *
+ * Created on May 25, 2006, 8:31 PM
+ *
+ * To change this template, choose Tools | Template Manager
+ * and open the template in the editor.
+ */
+
+package org.das2.util.awt;
+
+import edu.uiowa.physics.pw.das.event.DasUpdateEvent;
+import edu.uiowa.physics.pw.das.system.DasLogger;
+import java.awt.AWTEvent;
+import java.awt.EventQueue;
+import java.awt.Toolkit;
+import java.io.PrintStream;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.logging.Logger;
+import javax.swing.SwingUtilities;
+
+/**
+ *
+ * @author Jeremy
+ */
+public final class EventQueueBlocker_1 {
+    
+    private static final Logger logger= DasLogger.getLogger(DasLogger.SYSTEM_LOG);
+    
+    private static Object lockObject= new String("EQB_1");
+    
+    /** Creates a new instance of EventQueueBlocker */
+    private EventQueueBlocker_1() {
+    }
+    
+    private static Runnable clearEventQueueImmediatelyRunnable= new Runnable() {
+        public void run() {
+            clearEventQueueImmediately();
+        }
+    };
+    
+    private static void clearEventQueueImmediately() {
+        DasUpdateEvent evt;
+        evt= (DasUpdateEvent) Toolkit.getDefaultToolkit().getSystemEventQueue().peekEvent(DasUpdateEvent.DAS_UPDATE_EVENT_ID)  ;
+        if ( evt != null ) {
+            logger.finer("pending update event: "+evt);
+            EventQueue.invokeLater( clearEventQueueImmediatelyRunnable );
+        } else {
+            logger.finer("no update events found ");
+            synchronized(lockObject) {
+                lockObject.notify();
+            }
+        }
+    }
+    
+    public static void clearEventQueue() throws InterruptedException {
+        
+        if ( SwingUtilities.isEventDispatchThread() ) {
+            throw new IllegalStateException( "must not be called on the EventQueue");
+        }
+        synchronized(lockObject) {
+            if ( Toolkit.getDefaultToolkit().getSystemEventQueue().peekEvent(DasUpdateEvent.DAS_UPDATE_EVENT_ID) != null ) {
+                EventQueue.invokeLater( clearEventQueueImmediatelyRunnable );
+                logger.finer("waiting for lockObject to indicate eventQueue is clear");
+                lockObject.wait();
+            } else {
+                logger.finer("no update events found, no runnable submitted ");
+            }
+        }
+    }
+    
+    /**
+     * method for inspecting the event queue.
+     */
+    public static void dumpEventQueue( PrintStream out ) {
+        
+        EventQueue eventQueue= Toolkit.getDefaultToolkit().getSystemEventQueue();
+        
+        Queue queue= new LinkedList();
+        AWTEvent evt=null;
+        DasUpdateEvent result=null;
+        while ( eventQueue.peekEvent()!=null ) {
+            try {
+                evt= eventQueue.getNextEvent();
+                out.println(evt);
+            } catch (InterruptedException ex) {
+            }
+            queue.add(evt);
+        }
+        while ( queue.size() > 0 ) {
+            eventQueue.postEvent((AWTEvent)queue.remove());
+        }
+    }
+}
