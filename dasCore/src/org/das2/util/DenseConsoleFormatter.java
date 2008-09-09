127870b3a24091c229f981d516dacbb89da9a69d
Jeremy Faden <jbfaden@users.noreply.github.com>
2008-09-09 19:36:59 +0000

    edu.uiowa.physics.pw.das renamed to org.das2
    svn path=/dasCore/trunk/; revision=4165

---
+++ dasCore/src/org/das2/util/DenseConsoleFormatter.java
@@ -0,0 +1,26 @@
+/*
+ * NBConsoleFormatter.java
+ *
+ * Created on April 7, 2005, 12:13 PM
+ */
+
+package org.das2.util;
+
+import java.util.logging.Formatter;
+import java.util.logging.LogRecord;
+
+/**
+ *
+ * @author Jeremy
+ */
+public class DenseConsoleFormatter extends Formatter {
+    
+    public String format( LogRecord rec ) {
+        StackTraceElement[] st= new Throwable().getStackTrace();
+        return rec.getLoggerName()+": "+rec.getLevel().getLocalizedName()+": "+rec.getMessage()+"\n";
+    }
+        
+    public DenseConsoleFormatter() {
+    }
+    
+}
