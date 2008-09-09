263c50021451b8fc24f4c66499db0af2969c9dce
Jeremy Faden <jbfaden@users.noreply.github.com>
2008-09-09 19:37:46 +0000

    file utilities, including deleteFileTree
    svn path=/dasCore/trunk/; revision=4166

---
+++ dasCore/src/org/das2/util/FileUtil.java
@@ -0,0 +1,51 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package org.das2.util;
+
+import java.io.File;
+
+/**
+ * static utility methods.
+ * 
+ * introduced Jul 28, 2008.
+ * 
+ * @author jbf
+ */
+public class FileUtil {
+    
+    private FileUtil() {
+    }
+    
+    /**
+     * deletes all files and folders below root, and root, just as "rm -r" would.
+     * TODO: check links
+     * @throws IllegalArgumentException if it is unable to delete a file
+     * @return true if the operation was successful.
+     */
+    public static boolean deleteFileTree(File root) throws IllegalArgumentException {
+        if (!root.exists()) {
+            return true;
+        }
+        File[] children = root.listFiles();
+        boolean success = true;
+        for (int i = 0; i < children.length; i++) {
+            if (children[i].isDirectory()) {
+                success = success && deleteFileTree(children[i]);
+            } else {
+                success = success && children[i].delete();
+                if (!success) {
+                    throw new IllegalArgumentException("unable to delete file " + children[i]);
+                }
+            }
+        }
+        success = success && (!root.exists() || root.delete());
+        if (!success) {
+            throw new IllegalArgumentException("unable to delete folder " + root);
+        }
+        return success;
+    }
+
+}
