c9cd2c22656998f229fcad5408da0404b5362db9
Jeremy Faden <jbfaden@users.noreply.github.com>
2008-09-27 02:18:18 +0000

    bugfixes discovered by first use
    svn path=/dasCore/trunk/; revision=4209

--- dasCore/src/org/das2/system/NullPreferencesFactory.java
+++ dasCore/src/org/das2/system/NullPreferencesFactory.java
@@ -11,12 +11,19 @@ import java.util.prefs.PreferencesFactory;
 /**
  * Creates NullPreferences for use with applets.  The system property 
  * java.util.prefs.Preferences should be set to 
- * org.das2.system.NullPreferencesFactory 
- * (or soon org.das2.system.NullPreferencesFactory) to use this class.
+ * org.das2.system.NullPreferencesFactory to use this class.
+ * 
+ * System.setProperty( "java.util.prefs.Preferences", "org.das2.system.NullPreferencesFactory" ) doesn't
+ * work in applets--security exception.
+        
  * @author jbf
  */
 public class NullPreferencesFactory implements PreferencesFactory {
 
+    public NullPreferencesFactory() {
+        System.err.println("using NullPreferencesFactory");
+    }
+    
     public Preferences systemRoot() {
         return new NullPreferences();
     }
