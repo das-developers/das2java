f6fc231a3cb09a47ca6a50a494f6114c7e08c09f
Jeremy Faden <jbfaden@users.noreply.github.com>
2008-10-02 22:21:09 +0000

    add test7, which has 190000 points and 14 planes.  add source of random fake data.
    svn path=/autoplot/trunk/QStream/; revision=1133

--- QStream/src/test/SimpleStreamFormatterTest.java
+++ QStream/src/test/SimpleStreamFormatterTest.java
@@ -8,12 +8,16 @@ import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.text.ParseException;
+import java.util.Random;
 import javax.xml.parsers.ParserConfigurationException;
 import org.das2.datum.EnumerationUnits;
 import org.das2.datum.Units;
 import org.virbo.dataset.DDataSet;
+import org.virbo.dataset.FDataSet;
 import org.virbo.dataset.MutablePropertyDataSet;
 import org.virbo.dataset.QDataSet;
+import org.virbo.dataset.QubeDataSetIterator;
+import org.virbo.dataset.WritableDataSet;
 import org.virbo.dsops.Ops;
 import org.virbo.qstream.SimpleStreamFormatter;
 import org.virbo.qstream.StreamException;
@@ -24,11 +28,12 @@ import org.virbo.qstream.StreamException;
  */
 public class SimpleStreamFormatterTest {
 
-    public static void main(String[] args) throws ParseException, StreamException, FileNotFoundException, IOException, ParserConfigurationException {
-        test6();
+    public static void main(String[] args) throws ParseException, StreamException, FileNotFoundException, IOException {
+        //test6();
+        test7();
     }
 
-    private static QDataSet test1() throws ParseException, StreamException, IOException, ParserConfigurationException {
+    private static QDataSet test1() throws ParseException, StreamException, IOException {
         QDataSet ds = Ops.timegen("2003-09-09", "1 " + Units.days, 11);
         SimpleStreamFormatter format = new SimpleStreamFormatter();
 
@@ -36,7 +41,7 @@ public class SimpleStreamFormatterTest {
         return ds;
     }
 
-    private static QDataSet test1_5() throws ParseException, StreamException, IOException, ParserConfigurationException {
+    private static QDataSet test1_5() throws ParseException, StreamException, IOException {
         QDataSet ds= Ops.labels(new String[]{"B-GSM,X", "B-GSM,Y", "B-GSM,Z"});
         SimpleStreamFormatter format = new SimpleStreamFormatter();
 
@@ -44,7 +49,7 @@ public class SimpleStreamFormatterTest {
         return ds;
     }
     
-    private static QDataSet test2() throws ParseException, StreamException, IOException, ParserConfigurationException  {
+    private static QDataSet test2() throws ParseException, StreamException, IOException  {
         MutablePropertyDataSet tags = (MutablePropertyDataSet) Ops.timegen("2003-09-09", "1 " + Units.days, 11);
         tags.putProperty( QDataSet.NAME, "time");
         MutablePropertyDataSet ds = (MutablePropertyDataSet) Ops.findgen(11, 3);
@@ -63,7 +68,7 @@ public class SimpleStreamFormatterTest {
     }
     
     
-    private static QDataSet test3() throws ParseException, StreamException, IOException, ParserConfigurationException  {
+    private static QDataSet test3() throws ParseException, StreamException, IOException  {
         MutablePropertyDataSet tags = (MutablePropertyDataSet) Ops.timegen("2003-09-09", "13.86 " + Units.seconds, 11 );
         tags.putProperty( QDataSet.NAME, "time");
         
@@ -97,7 +102,7 @@ public class SimpleStreamFormatterTest {
         return ds;
     }
     
-    private static QDataSet test4_rank3() throws ParseException, StreamException, IOException, ParserConfigurationException {
+    private static QDataSet test4_rank3() throws ParseException, StreamException, IOException {
         DDataSet ds= (DDataSet) Ops.dindgen( 3, 4, 5 );
         ds.putValue( 1, 2, 3, 0.05 );
         SimpleStreamFormatter format = new SimpleStreamFormatter();
@@ -106,7 +111,7 @@ public class SimpleStreamFormatterTest {
         return ds;
     }
     
-    private static QDataSet test0_rank2()  throws ParseException, StreamException, IOException, ParserConfigurationException {
+    private static QDataSet test0_rank2()  throws ParseException, StreamException, IOException {
         DDataSet ds= (DDataSet) Ops.dindgen( 3, 4 );
         SimpleStreamFormatter format = new SimpleStreamFormatter();
 
@@ -114,7 +119,7 @@ public class SimpleStreamFormatterTest {
         return ds;
     }
     
-    private static QDataSet test5() throws StreamException, IOException, ParserConfigurationException {
+    private static QDataSet test5() throws StreamException, IOException {
         DDataSet ds= (DDataSet) Ops.dindgen( 5 );
         SimpleStreamFormatter format = new SimpleStreamFormatter();
         format.format( ds, new FileOutputStream("test5.qds"), true );
@@ -128,7 +133,7 @@ public class SimpleStreamFormatterTest {
      * @throws java.io.IOException
      * @throws javax.xml.parsers.ParserConfigurationException
      */
-    private static QDataSet test6() throws StreamException, IOException, ParserConfigurationException {
+    private static QDataSet test6() throws StreamException, IOException {
         QDataSet result= null;
         result= Ops.join( result, Ops.dindgen( 5 ) );
         result= Ops.join( result, Ops.dindgen( 5 ) );
@@ -142,4 +147,56 @@ public class SimpleStreamFormatterTest {
         return result;
     }
 
+    /**
+     * test performance of formatting  200K records with 15 planes.
+     * @return
+     */
+    private static QDataSet test7() throws StreamException, IOException {
+        int nrec= 190000;
+        
+        long t0= System.currentTimeMillis();
+        
+        
+        FDataSet result= FDataSet.createRank1( nrec );
+        funData( result, 9.2, 0.01, 0, false );
+        DDataSet dep0= DDataSet.createRank1( nrec );
+        funData( dep0, 10000, 0.01, 0, true );
+        result.putProperty( QDataSet.DEPEND_0, dep0 );
+        for ( int i=0; i<13; i++ ) {
+            FDataSet planeds= FDataSet.createRank1( nrec );
+            funData( planeds, Math.random()*100, Math.random()*10, 0, false );
+            planeds.putProperty( QDataSet.NAME, "myplane_"+i );
+            result.putProperty( "PLANE_"+i, planeds );
+        }
+        System.err.println( "generated data in  "+ ( System.currentTimeMillis()-t0) );
+        
+        t0= System.currentTimeMillis();
+        System.err.println( "formatting... " );
+        
+        SimpleStreamFormatter format = new SimpleStreamFormatter();
+        format.format( result, new FileOutputStream("test7.qds"), false );
+        
+        System.err.println( "time: "+ ( System.currentTimeMillis()-t0) );
+        
+        return result;
+    }
+    
+    private static void funData( WritableDataSet ds, double start, double res, int seed, boolean mono ) {
+        Random rand= new Random(seed);
+        if ( !mono ) {
+            QubeDataSetIterator it= new QubeDataSetIterator(ds);
+            while ( it.hasNext() ) {
+                it.next();
+                it.putValue( ds, start );
+                start+= res * ( rand.nextDouble() - 0.5 );
+            }
+        } else {
+            QubeDataSetIterator it= new QubeDataSetIterator(ds);
+            while ( it.hasNext() ) {
+                it.next();
+                it.putValue( ds, start );
+                start+= res;
+            }            
+        }
+    }
 }
