71b408f35036cb940db7645169095bd0e1b9f1f0
Jeremy Faden <jbfaden@users.noreply.github.com>
2008-10-14 16:34:12 +0000

    tests
    svn path=/autoplot/trunk/QStream/; revision=1191

--- QStream/src/test/TestQDataSetStreamHandler.java
+++ QStream/src/test/TestQDataSetStreamHandler.java
@@ -25,7 +25,8 @@ import org.virbo.qstream.StreamTool;
 public class TestQDataSetStreamHandler {
     public static void main( String[] args ) throws StreamException, IOException, org.das2.stream.StreamException, ParserConfigurationException {
         //QDataSet qds= test3_binary();
-        QDataSet qds= test0_rank2_0();
+        QDataSet qds= test0_rank2();
+        //QDataSet qds= test0_rank2_0();
         
         SimpleStreamFormatter fmt2 = new SimpleStreamFormatter();
 
@@ -33,6 +34,24 @@ public class TestQDataSetStreamHandler {
         
     }
 
+    private static QDataSet test0_rank2() throws IOException, StreamException {
+        URL url = TestQDataSetStreamHandler.class.getResource("test2.qds");
+        QDataSetStreamHandler handler = new QDataSetStreamHandler();
+        StreamTool.readStream(Channels.newChannel(url.openStream()), handler);
+        QDataSet qds = handler.getDataSet();
+        QubeDataSetIterator it= new QubeDataSetIterator(qds);
+        while ( it.hasNext() ) {
+            it.next();
+            System.err.println(" "+it+" "+it.getValue(qds) );
+        }
+        QDataSet tds= (QDataSet) qds.property(QDataSet.DEPEND_0);
+        it= new QubeDataSetIterator(tds);
+        while ( it.hasNext() ) {
+            it.next();
+            System.err.println(" "+it+" "+it.getValue(tds) );
+        }
+        return qds;
+    }    
 
     private static QDataSet test0_rank2_0() throws IOException, StreamException {
         URL url = TestQDataSetStreamHandler.class.getResource("test0_rank2_1.qds");
