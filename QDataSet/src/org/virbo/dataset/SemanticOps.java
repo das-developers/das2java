8d002a48ee2898fe413d394044fb73740430b3d0
Jeremy Faden <jbfaden@users.noreply.github.com>
2009-01-26 19:05:45 +0000

    introduce SemanticOps, which contains code isolating the application of semmantics to QDataSet.
    svn path=/autoplot/trunk/QDataSet/; revision=1581

---
+++ QDataSet/src/org/virbo/dataset/SemanticOps.java
@@ -0,0 +1,42 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package org.virbo.dataset;
+
+import org.das2.datum.Units;
+
+/**
+ * Common expressions that apply semantics to QDataSet.  Introduced
+ * to reduce a lot of repeated code, but also to make it clear where semantics
+ * are being applied.
+ * @author jbf
+ */
+public class SemanticOps {
+  
+    public final static Units getUnits( QDataSet ds ) {
+        Units u= (Units) ds.property( QDataSet.UNITS );
+        return u==null ? Units.dimensionless : u;
+    }
+    
+    /**
+     * return the labels for a dataset where DEPEND_1 is a bundle dimension.
+     * @param ds
+     * @return
+     */
+    public final static String[] getComponentLabels( QDataSet ds ) {
+        int n= ds.length(0);
+        QDataSet labels= (QDataSet) ds.property(QDataSet.DEPEND_1);
+        Units u= getUnits( labels );
+        String[] slabels= new String[n];
+        for ( int i=0; i<n; i++ ) {
+            if ( labels==null ) {       
+                slabels[i]= String.valueOf( i );
+            } else {
+                slabels[i]= String.valueOf( u.createDatum( labels.value(i) ) );
+            }
+        }
+        return slabels;
+    }    
+}
