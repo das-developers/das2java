062de0871eabfe5c5d60cf5283d712b002092ada
Jeremy Faden <jbfaden@users.noreply.github.com>
2009-02-25 16:08:22 +0000

    introduce utilities and dumpToFile
    svn path=/dasCore/trunk/; revision=4335

---
+++ dasCore/src/org/das2/util/filesystem/FileSystemUtil.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package org.das2.util.filesystem;
+
+import java.io.File;
+import java.io.InputStream;
+
+/**
+ *
+ * @author jbf
+ */
+public class FileSystemUtil {
+    public static void dumpToFile( InputStream in, File f ) {
+        throw new IllegalArgumentException("not implemented");
+    }
+}
