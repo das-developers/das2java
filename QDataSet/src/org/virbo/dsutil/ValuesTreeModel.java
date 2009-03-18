1ca1f7822bca8823c89fe785c1e570040dc2b513
Jeremy Faden <jbfaden@users.noreply.github.com>
2009-03-18 15:43:37 +0000

    introduce code that allows for treeview that scales with dataset size, by being lazy about creating nodes.  This is not yet used. introduce sizeLimit parameter, so clients can ask to have 100 values displayed.
    svn path=/autoplot/trunk/QDataSet/; revision=1789

--- QDataSet/src/org/virbo/dsutil/ValuesTreeModel.java
+++ QDataSet/src/org/virbo/dsutil/ValuesTreeModel.java
@@ -4,10 +4,12 @@
  */
 package org.virbo.dsutil;
 
+import java.util.Enumeration;
 import org.das2.datum.Units;
 import javax.swing.tree.DefaultMutableTreeNode;
 import javax.swing.tree.DefaultTreeModel;
 import javax.swing.tree.MutableTreeNode;
+import javax.swing.tree.TreeNode;
 import org.virbo.dataset.DataSetOps;
 import org.virbo.dataset.DataSetUtil;
 import org.virbo.dataset.QDataSet;
@@ -35,31 +37,100 @@ public class ValuesTreeModel extends DefaultTreeModel {
      */
     public ValuesTreeModel( String prefix, QDataSet ds ) {
         super( new DefaultMutableTreeNode((  prefix==null ? "" : prefix ) + DataSetUtil.toString(ds) ) );
-        valuesTreeNode( "HELP", mroot, ds );
+        valuesTreeNode( "HELP", mroot, ds ,10);
     }
-    
-    public static MutableTreeNode valuesTreeNode( String prefix, MutableTreeNode aroot, QDataSet ds ) {
-        
+
+    /**
+     * The valuesTreeNode implementation that uses MutableTreeNodes cannot scale to very large datasets, since all
+     * nodes are created immediately.  This implementation only queries the dataset as children are opened, and
+     * should eventually allow for exploration of any dataset.  This is not used right now in the metadata
+     * tab of Autoplot because the root node of the tree is a MutableTreeNode and all children must therefore
+     * be MutableTreeNodes.
+     */
+    public static TreeNode valuesTreeNode2( final String prefix, final TreeNode parent, final QDataSet ds ) {
+        final Units units= (Units) ds.property( QDataSet.UNITS );
+        final QDataSet wds= DataSetUtil.weightsDataSet(ds);
+        final QDataSet dep0= (QDataSet) ds.property(QDataSet.DEPEND_0);
+        final Units depu= dep0==null ? null : (Units) dep0.property(QDataSet.UNITS);
+
+        return new TreeNode() {
+
+            public TreeNode getChildAt(int i) {
+                if ( ds.rank()==1 ) {
+                    String sval= units==null ? String.valueOf(ds.value(i)) : String.valueOf(units.createDatum(ds.value(i)));
+                    if ( wds.value(i) > 0. ) sval= "fill"; //TODO: future datum class may allow for toString to return nominal data for invalid data.
+                    return new DefaultMutableTreeNode( prefix+""+i+")="+sval);
+                } else {
+                    String sdepu= dep0==null ? String.valueOf(i) : String.valueOf(depu.createDatum(dep0.value(i)));
+                    MutableTreeNode sliceNode= new DefaultMutableTreeNode( "values @ "+ sdepu );
+                    TreeNode atree= valuesTreeNode2( prefix + i+",", sliceNode, DataSetOps.slice0(ds, i) );
+                    return sliceNode;
+                }
+            }
+
+            public int getChildCount() {
+                return Math.min( ds.length(), 10 ); // TODO: "..."
+            }
+
+            public TreeNode getParent() {
+                return parent;
+            }
+
+            public int getIndex(TreeNode node) {
+                throw new UnsupportedOperationException("Not supported yet.");
+            }
+
+            public boolean getAllowsChildren() {
+                return true;
+            }
+
+            public boolean isLeaf() {
+                return false;
+            }
+
+            public Enumeration children() {
+                return new Enumeration() {
+                    int i=0;
+                    public boolean hasMoreElements() {
+                        return i<getChildCount();
+                    }
+
+                    public Object nextElement() {
+                        return getChildAt(i++);
+                    }
+                };
+            }
+        };
+    }
+
+    public static MutableTreeNode valuesTreeNode( String prefix, MutableTreeNode aroot, QDataSet ds, int sizeLimit ) {
+        QDataSet wds= DataSetUtil.weightsDataSet(ds);
+        QDataSet dep0= (QDataSet) ds.property(QDataSet.DEPEND_0);
+        Units depu= dep0==null ? Units.dimensionless : (Units) dep0.property(QDataSet.UNITS);
+        if ( depu==null ) depu= Units.dimensionless;
+
         if ( ds.rank()==1 ) {
             Units units= (Units) ds.property(QDataSet.UNITS);
             if ( units==null ) units= Units.dimensionless;
-            for ( int i=0; i<Math.min( ds.length(), 10 ); i++ ) {
-                aroot.insert(  new DefaultMutableTreeNode( prefix+""+i+")="+units.createDatum(ds.value(i))), aroot.getChildCount() ); 
+            for ( int i=0; i<Math.min( ds.length(), sizeLimit ); i++ ) {
+                //TODO: future datum class may allow for toString to return nominal data for invalid data.
+                String sval= wds.value(i) > 0. ? String.valueOf(units.createDatum(ds.value(i))) : "fill";
+                if ( dep0!=null ) sval += " @ " +String.valueOf(depu.createDatum(dep0.value(i)));
+                aroot.insert(  new DefaultMutableTreeNode( prefix+""+i+")="+sval), aroot.getChildCount() );
             }
-            if ( ds.length()>=10 ) {
+            if ( ds.length()>=sizeLimit ) {
                 aroot.insert( new DefaultMutableTreeNode( "..." ), aroot.getChildCount() );
             }
         } else {
-            QDataSet dep0= (QDataSet) ds.property(QDataSet.DEPEND_0);
-            if ( dep0==null ) dep0= Ops.dindgen(10);
-            Units depu= (Units) dep0.property(QDataSet.UNITS);
+            if ( dep0==null ) dep0= Ops.dindgen(ds.length());
             if ( depu==null ) depu= Units.dimensionless;
-            for ( int i=0; i<Math.min( ds.length(), 10 ); i++ ) {
+        
+            for ( int i=0; i<Math.min( ds.length(), sizeLimit ); i++ ) {
                 MutableTreeNode sliceNode= new DefaultMutableTreeNode( "values @ "+depu.createDatum(dep0.value(i)) ); 
-                MutableTreeNode atree= valuesTreeNode( prefix + i+",", sliceNode, DataSetOps.slice0(ds, i) );
+                MutableTreeNode atree= valuesTreeNode( prefix + i+",", sliceNode, DataSetOps.slice0(ds, i) , sizeLimit );
                 aroot.insert( sliceNode, aroot.getChildCount() );
             }
-            if ( ds.length()>=10 ) {
+            if ( ds.length()>=sizeLimit ) {
                 aroot.insert( new DefaultMutableTreeNode( "..." ), aroot.getChildCount() );
             }
         }
