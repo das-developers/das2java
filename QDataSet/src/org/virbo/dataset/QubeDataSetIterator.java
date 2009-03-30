cab3f6c9d98f9c7a713f9c1b7cc0a47286a72d73
Jeremy Faden <jbfaden@users.noreply.github.com>
2009-03-30 14:35:36 +0000

    support rank 0 datasets.
    svn path=/autoplot/trunk/QDataSet/; revision=1933

--- QDataSet/src/org/virbo/dataset/QubeDataSetIterator.java
+++ QDataSet/src/org/virbo/dataset/QubeDataSetIterator.java
@@ -19,7 +19,6 @@ public class QubeDataSetIterator implements DataSetIterator {
         int index();
 
         int length();
-        
     }
 
     public interface DimensionIteratorFactory {
@@ -35,12 +34,12 @@ public class QubeDataSetIterator implements DataSetIterator {
         int index;
         boolean all; // just for toString
 
-        public StartStopStepIterator(int start, int stop, int step, boolean all ) {
+        public StartStopStepIterator(int start, int stop, int step, boolean all) {
             this.start = start;
             this.stop = stop;
             this.step = step;
-            this.index= start-step;
-            this.all= all;
+            this.index = start - step;
+            this.all = all;
         }
 
         public boolean hasNext() {
@@ -62,9 +61,8 @@ public class QubeDataSetIterator implements DataSetIterator {
 
         @Override
         public String toString() {
-            return all ? ":" : ""+start+":"+stop+( step==1 ? "" : ":"+step ) ;
+            return all ? ":" : "" + start + ":" + stop + (step == 1 ? "" : ":" + step);
         }
-        
     }
 
     public static class StartStopStepIteratorFactory implements DimensionIteratorFactory {
@@ -83,10 +81,14 @@ public class QubeDataSetIterator implements DataSetIterator {
             int start1 = start == null ? 0 : start;
             int stop1 = stop == null ? length : stop;
             int step1 = step == null ? 1 : step;
-            if ( start1 < 0) start1 = length + start1;
-            if ( stop1 < 0) stop1 = length + stop1;
-            
-            return new StartStopStepIterator(start1, stop1, step1, start==null && stop==null && step==null );
+            if (start1 < 0) {
+                start1 = length + start1;
+            }
+            if (stop1 < 0) {
+                stop1 = length + stop1;
+            }
+
+            return new StartStopStepIterator(start1, stop1, step1, start == null && stop == null && step == null);
         }
     }
 
@@ -119,9 +121,8 @@ public class QubeDataSetIterator implements DataSetIterator {
 
         @Override
         public String toString() {
-            return "["+ds.toString()+"]";
+            return "[" + ds.toString() + "]";
         }
-        
     }
 
     public static class IndexListIteratorFactory implements DimensionIteratorFactory {
@@ -165,9 +166,8 @@ public class QubeDataSetIterator implements DataSetIterator {
 
         @Override
         public String toString() {
-            return ""+index;
+            return "" + index;
         }
-        
     }
 
     public static class SingletonIteratorFactory implements DimensionIteratorFactory {
@@ -182,14 +182,13 @@ public class QubeDataSetIterator implements DataSetIterator {
             return new SingletonIterator(index);
         }
     }
-    
     private DimensionIterator[] it = new DimensionIterator[3];
     private DimensionIteratorFactory[] fit = new DimensionIteratorFactory[3];
     private int rank;
     private int[] qube;
     private QDataSet ds;
-    private boolean allnext= true;  // we'll have to do a borrow to get started.
-    
+    private boolean allnext = true;  // we'll have to do a borrow to get started.
+
     /**
      * dataset iterator to help in implementing the complex indexing
      * types of python.  
@@ -197,31 +196,31 @@ public class QubeDataSetIterator implements DataSetIterator {
      * create a new iterator, set the index iterator factories, iterate.
      * @param ds
      */
-    public QubeDataSetIterator( QDataSet ds ) {
-        if ( Boolean.TRUE.equals(ds.property(QDataSet.QUBE)) ) {
+    public QubeDataSetIterator(QDataSet ds) {
+        if (Boolean.TRUE.equals(ds.property(QDataSet.QUBE))) {
             this.qube = DataSetUtil.qubeDims(ds);
         } else {
-            this.ds= ds;
+            this.ds = ds;
         }
-        this.rank= ds.rank();
+        this.rank = ds.rank();
         for (int i = 0; i < ds.rank(); i++) {
             fit[i] = new StartStopStepIteratorFactory(0, null, 1);
         }
         initialize();
     }
 
-    private QubeDataSetIterator( QDataSet ds, DimensionIteratorFactory[] fits ) {
-        if ( Boolean.TRUE.equals(ds.property(QDataSet.QUBE)) ) {
+    private QubeDataSetIterator(QDataSet ds, DimensionIteratorFactory[] fits) {
+        if (Boolean.TRUE.equals(ds.property(QDataSet.QUBE))) {
             this.qube = DataSetUtil.qubeDims(ds);
         } else {
-            this.ds= ds;
+            this.ds = ds;
         }
-        this.rank= ds.rank();
-        this.ds= ds;
-        this.fit= fits;
+        this.rank = ds.rank();
+        this.ds = ds;
+        this.fit = fits;
         initialize();
     }
-    
+
     /**
      * return an iterator for the slice of a dataset.  This is introduced to improve
      * performance by reducing the number of bounds checks, etc from the general
@@ -230,23 +229,25 @@ public class QubeDataSetIterator implements DataSetIterator {
      * @param sliceIndex
      * @return
      */
-    public static QubeDataSetIterator sliceIterator( QDataSet ds, int sliceIndex ) {
+    public static QubeDataSetIterator sliceIterator(QDataSet ds, int sliceIndex) {
         QubeDataSetIterator result;
-        if ( ds.rank()==1 ) {
+        if (ds.rank() == 0) {
+            throw new IllegalArgumentException("can't slice rank 0");
+        } else if (ds.rank() == 1) {
             throw new IllegalArgumentException("can't slice rank 1");
-        } else if ( ds.rank()==2 ) {
-            result= new QubeDataSetIterator( ds, new DimensionIteratorFactory[] { new SingletonIteratorFactory(sliceIndex), new StartStopStepIteratorFactory( 0, ds.length(sliceIndex), 1 ) } );
-        } else if ( ds.rank()==3 ) {
-            result= new QubeDataSetIterator( ds, 
-                    new DimensionIteratorFactory[] { new SingletonIteratorFactory(sliceIndex), 
-                    new StartStopStepIteratorFactory( 0, ds.length(sliceIndex), 1 ),
-                    new StartStopStepIteratorFactory( 0, null, 1 ),
-            } );
+        } else if (ds.rank() == 2) {
+            result = new QubeDataSetIterator(ds, new DimensionIteratorFactory[]{new SingletonIteratorFactory(sliceIndex), new StartStopStepIteratorFactory(0, ds.length(sliceIndex), 1)});
+        } else if (ds.rank() == 3) {
+            result = new QubeDataSetIterator(ds,
+                    new DimensionIteratorFactory[]{new SingletonIteratorFactory(sliceIndex),
+                        new StartStopStepIteratorFactory(0, ds.length(sliceIndex), 1),
+                        new StartStopStepIteratorFactory(0, null, 1),});
         } else {
             throw new IllegalArgumentException("rank limit");
         }
         return result;
     }
+
     /**
      * reinitializes the iterator.
      * @param dim
@@ -262,66 +263,82 @@ public class QubeDataSetIterator implements DataSetIterator {
      * begin iterating.
      */
     private void initialize() {
-        for ( int i=0; i<rank; i++ ) {
-            it[i] = fit[i].newIterator( dimLength(i) );
-        }        
+        for (int i = 0; i < rank; i++) {
+            it[i] = fit[i].newIterator(dimLength(i));
+        }
     }
-    
+
     /**
      * return the length of the dimension.  This is introduced with the intention
      * that this can support non-qube datasets as well.
      * @param idim
      * @return
      */
-    private final int dimLength( int idim ) {
-        if ( qube!=null ) {
+    private final int dimLength(int idim) {
+        if (qube != null) {
             return qube[idim];
         } else {
-            int result=0;
+            int result = 0;
             switch (idim) {
-                case 0: result= ds.length(); break;
-                case 1: result= ds.length( Math.max(0,index(0)) ); break;
-                case 2: result= ds.length( Math.max(0,index(0)), Math.max(0,index(1)) ); break;
-                default: throw new IllegalArgumentException("dimension not supported: "+idim);
+                case 0:
+                    result = ds.length();
+                    break;
+                case 1:
+                    result = ds.length(Math.max(0, index(0)));
+                    break;
+                case 2:
+                    result = ds.length(Math.max(0, index(0)), Math.max(0, index(1)));
+                    break;
+                default:
+                    throw new IllegalArgumentException("dimension not supported: " + idim);
             }
             return result;
         }
     }
-    
+
     public boolean hasNext() {
-        int i = rank - 1;
-        if (it[i].hasNext()) {
-            return true;
+        if (rank == 0) {
+            return this.allnext;
         } else {
-            if (i > 0) {
-                for (int j = i - 1; j >= 0; j--) {
-                    if ( it[j].hasNext() ) {
-                        return true;
+            int i = rank - 1;
+            if (it[i].hasNext()) {
+                return true;
+            } else {
+                if (i > 0) {
+                    for (int j = i - 1; j >= 0; j--) {
+                        if (it[j].hasNext()) {
+                            return true;
+                        }
                     }
                 }
-            }            
+            }
+            return false;
         }
-        return false;
     }
 
     public void next() {
-        
-        if ( this.allnext ) {
-            for ( int i=0; i<(rank-1); i++ ) it[i].nextIndex();
-            allnext= false;
+
+        if (this.allnext) {
+            for (int i = 0; i < (rank - 1); i++) {
+                it[i].nextIndex();
+            }
+            allnext = false;
+            if (rank == 0) {
+                return;
+            }
         }
-        
+
         // implement borrow logic
         int i = rank - 1;
-        if ( it[i].hasNext() ) {
+        if (it[i].hasNext()) {
             it[i].nextIndex();
         } else {
             if (i > 0) {
                 for (int j = i - 1; j >= 0; j--) {
-                    if ( it[j].hasNext() ) {
+                    if (it[j].hasNext()) {
                         it[j].nextIndex();
-                        for ( int k=j+1; k<=i; k++ ) {
-                            it[k]= fit[k].newIterator( dimLength(k) );
+                        for (int k = j + 1; k <= i; k++) {
+                            it[k] = fit[k].newIterator(dimLength(k));
                             it[k].nextIndex();
                         }
                         break;
@@ -347,52 +364,60 @@ public class QubeDataSetIterator implements DataSetIterator {
 
     @Override
     public String toString() {
-        String its=it[0].toString();
-        String ats=""+it[0].index();
-        for ( int i=1; i<rank; i++ ) {
-            its= its + "," + it[i].toString();
-            ats= ats + "," + it[i].index();
+        if (rank == 0) {
+            return "Iter hasNext=" + hasNext();
+        } else {
+            String its = it[0].toString();
+            String ats = "" + it[0].index();
+            for (int i = 1; i < rank; i++) {
+                its = its + "," + it[i].toString();
+                ats = ats + "," + it[i].index();
+            }
+            return "Iter [" + its + "] @ [" + ats + "] ";
         }
-        return "Iter [" + its + "] @ ["+ ats + "] ";
     }
-    
+
     /**
      * get the value from ds at the current iterator position.
      * @param ds a dataset with capatible geometry as the iterator's geometry.
      * @return the value of ds at the current iterator position.
      */
-    public final double getValue( QDataSet ds ) {
-        switch ( rank ) {
+    public final double getValue(QDataSet ds) {
+        switch (rank) {
+            case 0:
+                return ds.value();
             case 1:
-                return ds.value( index(0) );
+                return ds.value(index(0));
             case 2:
-                return ds.value( index(0), index(1) );
+                return ds.value(index(0), index(1));
             case 3:
-                return ds.value( index(0), index(1), index(2) );
+                return ds.value(index(0), index(1), index(2));
             default:
                 throw new IllegalArgumentException("rank limit");
-        }        
+        }
     }
-    
+
     /**
      * replace the value in ds at the current iterator position.
      * @param ds a writable dataset with capatible geometry as the iterator's geometry.
      * @param v the value to insert.
      */
-    public final void putValue( WritableDataSet ds, double v ) {
-        switch ( rank ) {
+    public final void putValue(WritableDataSet ds, double v) {
+        switch (rank) {
+            case 0:
+                ds.putValue(v);
+                return;
             case 1:
-                ds.putValue( index(0), v );
+                ds.putValue(index(0), v);
                 return;
             case 2:
-                ds.putValue( index(0), index(1), v );
+                ds.putValue(index(0), index(1), v);
                 return;
             case 3:
-                ds.putValue( index(0), index(1), index(2), v );
+                ds.putValue(index(0), index(1), index(2), v);
                 return;
             default:
                 throw new IllegalArgumentException("rank limit");
-        }        
+        }
     }
-    
 }
