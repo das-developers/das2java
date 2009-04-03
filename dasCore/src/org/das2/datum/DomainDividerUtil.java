868255f01c2c0f713a6bc1890626e15b69555528
Jeremy Faden <jbfaden@users.noreply.github.com>
2009-04-03 01:58:22 +0000

    add OrdinalTimeDivider. add getDatumFormatter to aid in integrating it to existing DasAxis.
    svn path=/dasCore/community/autoplot/trunk/; revision=4406

--- dasCore/src/org/das2/datum/DomainDividerUtil.java
+++ dasCore/src/org/das2/datum/DomainDividerUtil.java
@@ -1,5 +1,9 @@
 package org.das2.datum;
 
+import org.das2.datum.format.DatumFormatter;
+import org.das2.datum.format.DefaultDatumFormatter;
+import org.das2.datum.format.TimeDatumFormatter;
+
 /**
  *
  * @author Ed Jackson
@@ -9,12 +13,35 @@ public final class DomainDividerUtil {
     
     public static DomainDivider getDomainDivider(Datum min, Datum max, boolean log) {
         // Look at supplied min/max units and make a guess at the appropriate divider
-
-        //placehoder:
-        return new LinearDomainDivider();
+        if ( UnitsUtil.isTimeLocation( min.getUnits() ) ) {
+            return new OrdinalTimeDomainDivider();
+        } else {
+            return new LinearDomainDivider();
+        }
     }
 
     public static DomainDivider getDomainDivider(Datum min, Datum max) {
         return getDomainDivider(min, max, false);
+    }
+
+    /**
+     * encapsulate the kludges that allow us to identify a good formatter for the divider and
+     * a given range.
+     * @param div
+     * @param min
+     * @param max
+     * @return
+     */
+    public static DatumFormatter getDatumFormatter( DomainDivider div, DatumRange range ) {
+        if ( div instanceof OrdinalTimeDomainDivider ) {
+            return DatumUtil.bestTimeFormatter( range.min(), range.max(),
+                    (int)div.boundaryCount( range.min(), range.max() ) - 1 );
+        } else if ( div instanceof LogDomainDivider ) {
+            return DatumUtil.limitLogResolutionFormatter( range.min(), range.max(),
+                    (int)div.boundaryCount( range.min(), range.max() ) - 1 );
+        } else {
+            return DatumUtil.bestFormatter( range.min(), range.max(),
+                    (int)div.boundaryCount( range.min(), range.max() ) - 1 );
+        }
     }
 }
