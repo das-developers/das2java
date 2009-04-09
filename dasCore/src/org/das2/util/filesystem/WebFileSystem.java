1fdfefcbcdb31307c9a5921f9095d7466e3c9dbc
Jeremy Faden <jbfaden@users.noreply.github.com>
2009-04-09 16:52:21 +0000

    factor out "downloads" map from HTTP filesystem into getDownloadLock, which blocks all but one thread trying to download the same resource.
    svn path=/dasCore/community/autoplot/trunk/; revision=4423

--- dasCore/src/org/das2/util/filesystem/WebFileSystem.java
+++ dasCore/src/org/das2/util/filesystem/WebFileSystem.java
@@ -24,7 +24,6 @@
  * local files, since it manages the transfer of the file to a local
  * file system.
  */
-
 package org.das2.util.filesystem;
 
 import java.beans.PropertyChangeListener;
@@ -34,6 +33,7 @@ import java.io.*;
 import java.net.*;
 import java.util.*;
 import java.util.regex.*;
+import org.das2.system.MutatorLock;
 
 /**
  * Base class for HTTP and FTP-based filesystems.  A local cache is kept of
@@ -54,22 +54,17 @@ public abstract class WebFileSystem extends FileSystem {
 
         return local;
     }
-    
     protected final File localRoot;
-    
     /**
      * if true, then don't download to local cache.  Instead, provide inputStream
      * and getFile throws exception.
      */
     private boolean applet;
-    
     /**
      * plug-in template for implementation.  if non-null, use this.
      */
     protected WebProtocol protocol;
-
     protected boolean offline = true;
-        
     /**
      * if true, then the remote filesystem is not accessible, but local cache
      * copies may be accessed.  See FileSystemSettings.allowOffline
@@ -85,7 +80,6 @@ public abstract class WebFileSystem extends FileSystem {
         this.offline = offline;
         propertyChangeSupport.firePropertyChange(PROP_OFFLINE, oldOffline, offline);
     }
-
     private PropertyChangeSupport propertyChangeSupport = new PropertyChangeSupport(this);
 
     public void addPropertyChangeListener(PropertyChangeListener listener) {
@@ -98,27 +92,113 @@ public abstract class WebFileSystem extends FileSystem {
 
     /** Creates a new instance of WebFileSystem */
     protected WebFileSystem(URL root, File localRoot) {
-        super( root );        
-        this.localRoot= localRoot;
-        if ( localRoot==null ) {
-            if ( root.getProtocol().equals("http") ) {
-                this.protocol= new AppletHttpProtocol();
+        super(root);
+        this.localRoot = localRoot;
+        if (localRoot == null) {
+            if (root.getProtocol().equals("http")) {
+                this.protocol = new AppletHttpProtocol();
             }
         }
     }
-    
-    static protected File localRoot( URL root ) {
 
-        File local= FileSystem.settings().getLocalCacheDir();
-       
-        String s= root.getProtocol() + "/"+ root.getHost() + "/" + root.getFile();
-        
-        local= new File( local, s );
+    static protected File localRoot(URL root) {
+
+        File local = FileSystem.settings().getLocalCacheDir();
+
+        String s = root.getProtocol() + "/" + root.getHost() + "/" + root.getFile();
+
+        local = new File(local, s);
 
         local.mkdirs();
         return local;
     }
-    
+    /**
+     * Keep track of active downloads.  This handles, for example, the case
+     * where the same file is requested several times by different threads.
+     */
+    private final Map downloads = new HashMap();
+
+    /**
+     * Wait while another thread is downloading the file.
+     * @param monitor this thread's monitor.
+     * @param mon the monitor of the thread doing the download.
+     * @param filename
+     * @throws java.lang.RuntimeException
+     */
+    private void waitForDownload(ProgressMonitor monitor, ProgressMonitor mon, final String filename) throws RuntimeException {
+
+        monitor.setProgressMessage("Waiting for file to download");
+        while (mon != null) {
+            while (!mon.isStarted()) {
+                try {
+                    Thread.sleep(100);
+                } catch (InterruptedException e) {
+                }
+            }
+            monitor.setTaskSize(mon.getTaskSize());
+            monitor.started();
+
+            while (mon != null) {
+                // this monitor can tell the downloading monitor to cancel.
+                if (monitor.isCancelled()) {
+                    mon.cancel();
+                }
+
+                // echo what the download monitor is reporting.
+                monitor.setTaskProgress(mon.getTaskProgress());
+
+                try {
+                    Thread.sleep(100);
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+                mon = (ProgressMonitor) downloads.get(filename);
+            }
+            logger.finest("waiting for download");
+        }
+        monitor.finished();
+    }
+
+    /**
+     * Request lock to download file.  If you get the lock, then you should download
+     * the file.  If null is returned, then another thread has downloaded the 
+     * file this thread can return.
+     * 
+     * @param filename
+     * @param mon
+     * @return MutatorLock.  The client should call mutatorLock.unlock() when the download is complete
+     */
+    protected MutatorLock getDownloadLock(final String filename, File f, ProgressMonitor monitor) throws IOException {
+        logger.finer( "" + Thread.currentThread().getName() + " wants download lock for "+filename + " wfs impl " + this.hashCode() );
+        System.err.println( "" + Thread.currentThread().getName() + " wants download lock for "+filename + " wfs impl " + this.hashCode() );
+        synchronized (downloads) {
+            ProgressMonitor mon = (ProgressMonitor) downloads.get(filename);
+            if (mon != null) { // the webfilesystem is already loading this file, so wait.
+                logger.fine("another thread is downloading "+filename+", waiting...");
+                waitForDownload(monitor, mon, filename);
+                if (f.exists()) {
+                    return null;
+                } else {
+                    throw new FileNotFoundException("expected to find " + f);
+                }
+            } else {
+                logger.fine("this thread will download "+filename+".");
+                downloads.put(filename, monitor);
+
+                return new MutatorLock() {
+
+                    public void lock() {
+                    }
+
+                    public void unlock() {
+                        downloads.remove(filename);
+                    }
+                };
+
+            }
+        }
+    }
+
     /**
      * Transfers the file from the remote store to a local copy f.  This should only be
      * used within the class and subclasses, clients should use getFileObject( String ).getFile().
@@ -127,98 +207,101 @@ public abstract class WebFileSystem extends FileSystem {
      *
      * @param partfile the temporary file during download.
      */
-    protected abstract void downloadFile( String filename, File f, File partfile, ProgressMonitor monitor ) throws IOException;
-    
-	 /** Get the root of the local file cache 
+    protected abstract void downloadFile(String filename, File f, File partfile, ProgressMonitor monitor) throws IOException;
+
+    /** Get the root of the local file cache
      * @deprecated use getLocalRoot().getAbsolutePath()
      */
-	 public String getLocalRootAbsPath(){  return this.localRoot.getAbsolutePath();	 }
-	 
+    public String getLocalRootAbsPath() {
+        return this.localRoot.getAbsolutePath();
+    }
+
     public File getLocalRoot() {
         return this.localRoot;
     }
-    
-    abstract public boolean isDirectory( String filename ) throws IOException;
-    
-    abstract public String[] listDirectory( String directory ) throws IOException;
-    
-    public String[] listDirectory( String directory, String regex ) throws IOException {
-        String[] names= listDirectory( directory );
-        Pattern pattern= Pattern.compile(regex);
-        ArrayList result= new ArrayList();
-        for ( int i=0; i<names.length; i++ ) {   
-            if ( names[i].endsWith("/") ) names[i]= names[i].substring(0,names[i].length()-1);
-            if ( pattern.matcher(names[i]).matches() ) {
+
+    abstract public boolean isDirectory(String filename) throws IOException;
+
+    abstract public String[] listDirectory(String directory) throws IOException;
+
+    public String[] listDirectory(String directory, String regex) throws IOException {
+        String[] names = listDirectory(directory);
+        Pattern pattern = Pattern.compile(regex);
+        ArrayList result = new ArrayList();
+        for (int i = 0; i < names.length; i++) {
+            if (names[i].endsWith("/")) {
+                names[i] = names[i].substring(0, names[i].length() - 1);
+            }
+            if (pattern.matcher(names[i]).matches()) {
                 result.add(names[i]);
             }
         }
-        return (String[])result.toArray(new String[result.size()]);
+        return (String[]) result.toArray(new String[result.size()]);
     }
-    
-    public URL getURL( String filename ) {
+
+    public URL getURL(String filename) {
         try {
-            filename= FileSystem.toCanonicalFilename(filename);
-            return new URL( root+filename.substring(1) );
-        } catch ( MalformedURLException e ) {
+            filename = FileSystem.toCanonicalFilename(filename);
+            return new URL(root + filename.substring(1));
+        } catch (MalformedURLException e) {
             throw new RuntimeException(e);
         }
     }
-    
+
     /**
      * return the name of the File within the FileSystem, where File is a local
      * file within the local copy of the filesystem.
      */
-    public String getLocalName( File file ) {
-        if ( !file.toString().startsWith(localRoot.toString() ) ) {
-            throw new IllegalArgumentException( "file \""+file+"\"is not of this web file system" );
+    public String getLocalName(File file) {
+        if (!file.toString().startsWith(localRoot.toString())) {
+            throw new IllegalArgumentException("file \"" + file + "\"is not of this web file system");
         }
-        String filename= file.toString().substring(localRoot.toString().length() );
-        filename= filename.replaceAll( "\\\\", "/" );
+        String filename = file.toString().substring(localRoot.toString().length());
+        filename = filename.replaceAll("\\\\", "/");
         return filename;
     }
-    
-    public String getLocalName( URL url ) {
-        if ( !url.toString().startsWith(root.toString() ) ) {
-            throw new IllegalArgumentException( "url \""+url+"\"is not of this web file system" );
+
+    public String getLocalName(URL url) {
+        if (!url.toString().startsWith(root.toString())) {
+            throw new IllegalArgumentException("url \"" + url + "\"is not of this web file system");
         }
-        String filename= FileSystem.toCanonicalFilename( url.toString().substring(root.toString().length() ) );
+        String filename = FileSystem.toCanonicalFilename(url.toString().substring(root.toString().length()));
         return filename;
     }
-    
-    
-    public FileObject getFileObject( String filename ) {
-        WebFileObject f= new WebFileObject( this, filename, new Date(System.currentTimeMillis()) );
+
+    public FileObject getFileObject(String filename) {
+        WebFileObject f = new WebFileObject(this, filename, new Date(System.currentTimeMillis()));
         return f;
     }
-    
+
     /**
      * copies data from in to out, sending the number of bytesTransferred to the monitor.
      */
-    protected void copyStream( InputStream is, OutputStream out, ProgressMonitor monitor ) throws IOException {        
-        byte[] buffer= new byte[2048];
-        int bytesRead= is.read( buffer, 0, 2048 );
-        long totalBytesRead= bytesRead;
-        while ( bytesRead>-1 ) {
-            if ( monitor.isCancelled() ) throw new InterruptedIOException( );
-            monitor.setTaskProgress( totalBytesRead );            
-            out.write( buffer, 0, bytesRead );            
-            bytesRead= is.read( buffer, 0, 2048 );         
-            totalBytesRead+= bytesRead;
-            logger.finest( "transferring data" );
+    protected void copyStream(InputStream is, OutputStream out, ProgressMonitor monitor) throws IOException {
+        byte[] buffer = new byte[2048];
+        int bytesRead = is.read(buffer, 0, 2048);
+        long totalBytesRead = bytesRead;
+        while (bytesRead > -1) {
+            if (monitor.isCancelled()) {
+                throw new InterruptedIOException();
+            }
+            monitor.setTaskProgress(totalBytesRead);
+            out.write(buffer, 0, bytesRead);
+            bytesRead = is.read(buffer, 0, 2048);
+            totalBytesRead += bytesRead;
+            logger.finest("transferring data");
         }
     }
-    
+
     public String toString() {
-        return "wfs "+root;
+        return "wfs " + root;
     }
-    
+
     public boolean isAppletMode() {
         return applet;
     }
-    
-    public void setAppletMode( boolean applet ) {
-        this.applet= applet;
+
+    public void setAppletMode(boolean applet) {
+        this.applet = applet;
     }
-    
-    
 }
