da6f4c2d3108169820c05ab025df03512d5cc908
Jeremy Faden <jbfaden@users.noreply.github.com>
2009-05-14 15:58:08 +0000

    add getReleaseTag( Class ) which gets the release tag for the jar of the given class.
    svn path=/dasCore/community/autoplot/trunk/; revision=4468

--- dasCore/src/org/das2/util/AboutUtil.java
+++ dasCore/src/org/das2/util/AboutUtil.java
@@ -107,6 +107,22 @@ public class AboutUtil {
 
     }
 
+    public static String getReleaseTag( Class clas ) throws IOException {
+        Properties props = new Properties();
+        String clasFile= clas.getName().replaceAll("\\.","/")+".class";
+        URL url = clas.getClassLoader().getResource( clasFile );
+        if ( url!=null )  {
+            String surl= url.toString();
+            url= new URL( new URL( surl.substring(0,surl.length()-clasFile.length() ) ), "META-INF/build.txt" );
+            props.load(url.openStream());
+            String tagName = props.getProperty("build.tag");
+            if ( tagName!=null && tagName.trim().length()>0 ) {
+                return tagName;
+            }
+        }
+        return null;
+    }
+
     /**
      * Identify the release version by looking a non-null build.tag.  It's expected
      * that the build script will insert build.tag into META-INF/build.txt
