14da8deb179a80d53635e3b0371a61477a51a088
Jeremy Faden <jbfaden@users.noreply.github.com>
2009-05-22 17:55:21 +0000

    introduce implicit component labels ch_0, ch_1, ch_2, ... to support switch to and fro spectrogram and series rendertypes.
    svn path=/autoplot/trunk/QDataSet/; revision=2396

--- QDataSet/src/org/virbo/dataset/SemanticOps.java
+++ QDataSet/src/org/virbo/dataset/SemanticOps.java
@@ -2,7 +2,6 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package org.virbo.dataset;
 
 import org.das2.datum.Units;
@@ -14,29 +13,37 @@ import org.das2.datum.Units;
  * @author jbf
  */
 public class SemanticOps {
-  
-    public final static Units getUnits( QDataSet ds ) {
-        Units u= (Units) ds.property( QDataSet.UNITS );
-        return u==null ? Units.dimensionless : u;
+
+    public final static Units getUnits(QDataSet ds) {
+        Units u = (Units) ds.property(QDataSet.UNITS);
+        return u == null ? Units.dimensionless : u;
     }
-    
+
     /**
      * return the labels for a dataset where DEPEND_1 is a bundle dimension.
      * @param ds
      * @return
      */
-    public final static String[] getComponentLabels( QDataSet ds ) {
-        int n= ds.length(0);
-        QDataSet labels= (QDataSet) ds.property(QDataSet.DEPEND_1);
-        Units u= getUnits( labels );
-        String[] slabels= new String[n];
-        for ( int i=0; i<n; i++ ) {
-            if ( labels==null ) {       
-                slabels[i]= String.valueOf( i );
-            } else {
-                slabels[i]= String.valueOf( u.createDatum( labels.value(i) ) );
+    public final static String[] getComponentLabels(QDataSet ds) {
+        int n = ds.length(0);
+        QDataSet labels = (QDataSet) ds.property(QDataSet.DEPEND_1);
+        if (labels == null) {
+            String[] result = new String[n];
+            for (int i = 0; i < n; i++) {
+                result[i] = "ch_" + i;
             }
+            return result;
+        } else {
+            Units u = getUnits(labels);
+            String[] slabels = new String[n];
+            for (int i = 0; i < n; i++) {
+                if (labels == null) {
+                    slabels[i] = String.valueOf(i);
+                } else {
+                    slabels[i] = String.valueOf(u.createDatum(labels.value(i)));
+                }
+            }
+            return slabels;
         }
-        return slabels;
-    }    
+    }
 }
