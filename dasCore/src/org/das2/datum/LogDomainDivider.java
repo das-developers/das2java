d9cdb4c0e3fef340c8dc0ecd365aea6a17d33f47
Ed Jackson <ed.jackson@cottagesystems.com>
2009-06-08 19:47:53 +0000

    Removed unused and confusing support for bases other than 10 from LogDomainDivider.  If needed, new classes LnDomainDivider and/or BinaryDomainDivider can be added.
    svn path=/dasCore/community/autoplot/trunk/; revision=4487

--- dasCore/src/org/das2/datum/LogDomainDivider.java
+++ dasCore/src/org/das2/datum/LogDomainDivider.java
@@ -1,51 +1,39 @@
 package org.das2.datum;
 
-// TODO: remove support for bases other than 10.  If needed, we can later
-// add LnDomainDivider and/or BinaryDomainDivider
 /**
- * A <code>DomainDivider</code> to divide a range into logarithmically equal
- * segments.  By default, it uses base 10, but can use any integer base.  Natural
- * logs are not supported by this class.
+ * A <code>DomainDivider</code> to divide a range into (base 10) logarithmically equal
+ * segments. 
  * @author ed
  */
 public class LogDomainDivider implements DomainDivider {
-    private final int logBase;
+
     private final DomainDivider expDivider;
 
     protected LogDomainDivider() {
-        this(10);
-    }
-
-    protected LogDomainDivider(int logBase) {
-        this(logBase, new LinearDomainDivider());
+        this(new LinearDomainDivider());
     }
 
     // for use by finerDivider and coarserDivider
-    private LogDomainDivider(int logBase, DomainDivider expDivider) {
-        this.logBase = logBase;
+    private LogDomainDivider(DomainDivider expDivider) {
         this.expDivider = expDivider;
     }
-    
+
     public DomainDivider coarserDivider(boolean superset) {
-        return new LogDomainDivider(logBase, expDivider.coarserDivider(superset));
+        return new LogDomainDivider(expDivider.coarserDivider(superset));
     }
 
     public DomainDivider finerDivider(boolean superset) {
         // Should update to return a LogLinDomainDivider instead of fractional powers
-        return new LogDomainDivider(logBase, expDivider.finerDivider(superset));
+        return new LogDomainDivider(expDivider.finerDivider(superset));
     }
 
     public DatumVector boundaries(Datum min, Datum max) {
         long nb = boundaryCount(min, max);
-        if (nb > MAX_BOUNDARIES )
-            throw new IllegalArgumentException("too many divisions requested ("+ nb +")");
+        if (nb > MAX_BOUNDARIES)
+            throw new IllegalArgumentException("too many divisions requested (" + nb + ")");
 
         double logmin = Math.log10(min.doubleValue());
         double logmax = Math.log10(max.doubleValue());
-        if (logBase != 10) {
-            logmin /= Math.log10(logBase);
-            logmax /= Math.log10(logBase);
-        }
 
         Datum dmin = Datum.create(logmin);
         Datum dmax = Datum.create(logmax);
@@ -55,8 +43,9 @@ public class LogDomainDivider implements DomainDivider {
             throw new IllegalArgumentException("fatal error in LogDomainDivider");
         double[] result = new double[exponents.length];
 
-        for(int i = 0; i < nb; ++i)
-            result[i] = Math.pow(logBase, exponents[i]);
+        for (int i = 0; i < nb; ++i) {
+            result[i] = Math.pow(10, exponents[i]);
+        }
 
         return DatumVector.newDatumVector(result, min.getUnits());
     }
@@ -71,21 +60,15 @@ public class LogDomainDivider implements DomainDivider {
         double logmin = Math.log10(min.doubleValue());
         double logmax = Math.log10(max.doubleValue());
 
-        if(logBase != 10) {  //convert to appropriate base
-            // remember logX(n) = log10(n) / log10(X)
-            logmin /= Math.log10(logBase);
-            logmax /= Math.log10(logBase);
-        }
-
         return expDivider.boundaryCount(Datum.create(logmin), Datum.create(logmax));
     }
 
     public DatumRange rangeContaining(Datum v) {
         double logv = Math.log10(v.doubleValue());
-        
+
         DatumRange exponentRange = expDivider.rangeContaining(Datum.create(logv));
-        double rangeMin = Math.pow(logBase, exponentRange.min().doubleValue());
-        double rangeMax = Math.pow(logBase, exponentRange.max().doubleValue());
+        double rangeMin = Math.pow(10, exponentRange.min().doubleValue());
+        double rangeMax = Math.pow(10, exponentRange.max().doubleValue());
 
         return new DatumRange(rangeMin, rangeMax, v.getUnits());
     }
@@ -101,8 +84,6 @@ public class LogDomainDivider implements DomainDivider {
         System.err.println(div2.boundaries(dr.min(), dr.max()));
         div2 = div.finerDivider(false);
         System.err.println(div2.boundaryCount(dr.min(), dr.max()));
-        System.err.println(div2.boundaries(dr.min(), dr.max()));        
-}
-
-
+        System.err.println(div2.boundaries(dr.min(), dr.max()));
+    }
 }
