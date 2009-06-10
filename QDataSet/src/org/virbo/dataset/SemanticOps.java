8b18aa9712cb11c6eb28b41a884ac8118a8b5b4e
Jeremy Faden <jbfaden@users.noreply.github.com>
2009-06-10 13:18:40 +0000

    move lookupUnits stuff to here to make more accessible in scripting.
    svn path=/autoplot/trunk/QDataSet/; revision=2537

--- QDataSet/src/org/virbo/dataset/SemanticOps.java
+++ QDataSet/src/org/virbo/dataset/SemanticOps.java
@@ -4,7 +4,14 @@
  */
 package org.virbo.dataset;
 
+import java.text.ParseException;
+import org.das2.datum.Basis;
+import org.das2.datum.Datum;
+import org.das2.datum.NumberUnits;
+import org.das2.datum.TimeLocationUnits;
+import org.das2.datum.TimeUtil;
 import org.das2.datum.Units;
+import org.das2.datum.UnitsConverter.ScaleOffset;
 
 /**
  * Common expressions that apply semantics to QDataSet.  Introduced
@@ -44,6 +51,80 @@ public class SemanticOps {
                 }
             }
             return slabels;
+        }
+    }
+
+    /**
+     * lookupUnits canonical units object, or allocate one.
+     * @param units string identifier.
+     * @return canonical units object.
+     */
+    public static synchronized Units lookupUnits(String sunits) {
+        Units result;
+        try {
+            result= Units.getByName(sunits);
+        } catch ( IllegalArgumentException ex ) {
+            if ( sunits.equals("sec") ) {   // begin, giant table of kludges
+                result= Units.seconds;
+            } else if ( sunits.equals("msec") ) {  // CDF
+                result= Units.milliseconds;
+            } else {
+                result= new NumberUnits( sunits );
+            }
+        }
+        return result;
+    }
+
+    /**
+     * return canonical das2 unit for colloquial time.
+     * @param string
+     * @return
+     */
+    public static Units lookupTimeLengthUnit(String s) throws ParseException {
+        s= s.toLowerCase().trim();
+        if ( s.startsWith("sec") ) {
+            return Units.seconds;
+        } else if ( s.startsWith("ms") || s.startsWith("millisec") ) {
+            return Units.milliseconds;
+        } else if ( s.equals("hr") || s.startsWith("hour") ) {
+            return Units.hours;
+        } else if ( s.equals("mn") || s.startsWith("min") ) {
+            return Units.minutes;
+        } else if ( s.startsWith("us") || s.startsWith("\u00B5s" ) || s.startsWith("micros")) {
+            return Units.microseconds;
+        } else if ( s.startsWith("ns") || s.startsWith("nanos" ) ) {
+            return Units.nanoseconds;
+        } else if ( s.startsWith("d") ) {
+            return Units.days;
+        } else {
+            throw new ParseException("failed to identify unit: "+s,0);
+        }
+    }
+
+    /**
+     * lookupUnits canonical units object, or allocate one.  If one is
+     * allocated, then parse for "<unit> since <datum>" and add conversion to
+     * "microseconds since 2000-001T00:00."  Note leap seconds are ignored!
+     * @param timeUnits
+     * @return
+     */
+    public static synchronized Units lookupTimeUnits( String units ) throws ParseException {
+        Units result;
+        try {
+            result= Units.getByName(units);
+            return result;
+        } catch ( IllegalArgumentException ex ) {
+            String[] ss= units.split("since");
+            Units offsetUnits= lookupTimeLengthUnit(ss[0]);
+            Datum datum= TimeUtil.create(ss[1]);
+            String canonicalName = "" + offsetUnits + " since "+ datum;
+            Basis basis= new Basis( "since "+ datum, "since "+ datum, Basis.since2000, datum.doubleValue(Units.us2000), Units.us2000.getOffsetUnits() );
+            result= new TimeLocationUnits( canonicalName, canonicalName, offsetUnits, basis );
+            result.registerConverter( Units.us2000,
+                    new ScaleOffset(
+                    offsetUnits.convertDoubleTo(Units.microseconds, 1.0),
+                    datum.doubleValue(Units.us2000) ) );
+            return result;
         }
     }
 }
