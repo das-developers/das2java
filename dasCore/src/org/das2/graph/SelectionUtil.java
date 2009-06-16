54fdd4a421a9d6e2311ce1e17e316f2f723dd05e
Jeremy Faden <jbfaden@users.noreply.github.com>
2009-06-16 11:02:10 +0000

    introduce SelectionUtil.getSelectionArea(Object) which delegates to components or renderers.
    svn path=/dasCore/community/autoplot/trunk/; revision=4507

---
+++ dasCore/src/org/das2/graph/SelectionUtil.java
@@ -0,0 +1,33 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package org.das2.graph;
+
+import java.awt.Rectangle;
+import java.awt.Shape;
+
+/**
+ *
+ * @author jbf
+ */
+public class SelectionUtil {
+    public static Shape getSelectionArea( Renderer r ) {
+        if ( r instanceof SeriesRenderer ) {
+            Shape a= ((SeriesRenderer)r).selectionArea();
+            return a;
+        } else if ( r instanceof SpectrogramRenderer ) {
+            return ((SpectrogramRenderer)r).selectionArea();
+        } else {
+            Rectangle rect= DasDevicePosition.toRectangle( r.getParent().getRow(), r.getParent().getColumn() );
+            rect= new Rectangle( rect.x+5, rect.y+5, rect.width-10, rect.height-10 );
+            return rect;
+        }
+    }
+
+    public static Shape getSelectionArea( DasCanvasComponent cc ) {
+        return cc.getActiveRegion();
+    }
+    
+}
