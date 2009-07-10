be42a24d89fb482c7779af92256f43a076661a1e
Jeremy Faden <jbfaden@users.noreply.github.com>
2009-07-10 11:50:32 +0000

    auralize data into 16-bit stream using java.nio.  It was 8 bit and I don't think it worked because of unsigned/signed byte inconsistency.
    svn path=/dasCore/community/autoplot/trunk/; revision=4538

--- dasCore/src/org/das2/graph/Auralizor.java
+++ dasCore/src/org/das2/graph/Auralizor.java
@@ -6,10 +6,20 @@
 
 package org.das2.graph;
 
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
+import java.nio.channels.WritableByteChannel;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import org.das2.dataset.VectorDataSet;
 import org.das2.datum.Units;
 import org.das2.system.DasLogger;
 import javax.sound.sampled.*;
+import org.das2.dataset.DataSetUtil;
+import org.das2.datum.DatumRange;
 
 /**
  *
@@ -17,8 +27,9 @@ import javax.sound.sampled.*;
  */
 public class Auralizor {
     
-    private static final int	EXTERNAL_BUFFER_SIZE = 1000;
-    byte[] buffer;
+    private static final int	EXTERNAL_BUFFER_SIZE = 100000;
+    ByteBuffer buffer;
+    byte[] buf;
     SourceDataLine line = null;
     int bufferInputIndex;
     double min;
@@ -34,7 +45,12 @@ public class Auralizor {
     public void playSound() {
         float sampleRate=   (float) ( 1. / ds.getXTagDatum(1).subtract(ds.getXTagDatum(0)).doubleValue(Units.seconds)  ) ;
         DasLogger.getLogger(DasLogger.GRAPHICS_LOG).fine("sampleRate= "+sampleRate);
-        AudioFormat audioFormat= new AudioFormat( sampleRate, 8, 1, true, false );
+        AudioFormat audioFormat= new AudioFormat( sampleRate, 16, 1, true, true );
+
+        buf= new byte[EXTERNAL_BUFFER_SIZE];
+        buffer= ByteBuffer.wrap(buf);
+
+        buffer.order( ByteOrder.BIG_ENDIAN );
         
         DataLine.Info info = new DataLine.Info(SourceDataLine.class,  audioFormat);
         try {
@@ -45,22 +61,43 @@ public class Auralizor {
         catch (Exception e) {
             throw new RuntimeException(e);            
         }
-        buffer = new byte[EXTERNAL_BUFFER_SIZE];
+        
         bufferInputIndex= 0;
         line.start();
         
         int i=0;
         int ibuf=0;
+
         while ( i<ds.getXLength() ) {
             double d= ds.getDouble(i++,yUnits);
-            int b= (int) ( 256 * ( d - min ) / ( max-min ) );
-            buffer[ibuf++]= (byte)b;
+            int b= (int) ( 65536 * ( d - min ) / ( max-min ) ) - 32768;
+            try {
+                buffer.putShort( ibuf, (short)b );
+
+            } catch ( IndexOutOfBoundsException ex ) {
+                ex.printStackTrace();
+            }
+            ibuf+=2;
             if ( ibuf==EXTERNAL_BUFFER_SIZE ) {
-                line.write(buffer, 0, ibuf );
-                ibuf=0;                
+                line.write(buf, 0, ibuf );
+                try {
+                    WritableByteChannel out = new FileOutputStream("/home/jbf/tmp/foo.bin").getChannel();
+                    out.write(buffer);
+                    buffer.flip();
+                } catch (IOException ex) {
+                    Logger.getLogger(Auralizor.class.getName()).log(Level.SEVERE, null, ex);
+                }
+                ibuf=0;
             }
         }
-        line.write(buffer, 0, ibuf );
+        line.write(buf, 0, ibuf );
+        try {
+            WritableByteChannel out = new FileOutputStream("/home/jbf/tmp/foo.bin").getChannel();
+            out.write(buffer);
+            buffer.flip();
+        } catch (IOException ex) {
+            Logger.getLogger(Auralizor.class.getName()).log(Level.SEVERE, null, ex);
+        }
         ibuf=0;
         
         line.drain();
@@ -81,7 +118,10 @@ public class Auralizor {
     public Auralizor( VectorDataSet ds ) {
         min= -1;
         max= 1;
+        DatumRange yrange= DataSetUtil.yRange(ds);
         yUnits= Units.dimensionless;
+        min= yrange.min().doubleValue(yUnits);
+        max= yrange.max().doubleValue(yUnits);
         this.ds= ds;
     }
     
