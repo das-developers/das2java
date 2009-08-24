fc84172d2f00fcf138bb2082453fb6660d77464d
Jeremy Faden <jbfaden@users.noreply.github.com>
2009-08-24 22:23:07 +0000

    support rank 1, which is bundle of rank 0 datasets (and should be the slice0 of a rank 2). add BUNDLE_0/BUNDLE_1 property. rank 2 has QUBE set.
    svn path=/autoplot/trunk/QDataSet/; revision=2934

--- QDataSet/src/org/virbo/dataset/BundleDataSet.java
+++ QDataSet/src/org/virbo/dataset/BundleDataSet.java
@@ -33,39 +33,111 @@ public class BundleDataSet extends AbstractDataSet {
     /**
      * the length of each dataset.
      */
-    int len0;
-    
-    /** Creates a new instance of BundleDataSet
-     * 
+    int len0=-1;
+
+    /**
+     * return a BundleDataSet for bundling rank 0 datasets.  The result
+     * will be a rank 1 datasets with BUNDLE_0 non-null.
+     * @return a rank 1 BundleDataSet
      */
-    public BundleDataSet( ) {
-        this.rank= 2;
-        datasets= new ArrayList<QDataSet>();
+    public static BundleDataSet createRank0Bundle() {
+        return new BundleDataSet(1);
     }
 
     /**
-     * add the dataset to the bundle of datasets.  Currently this implementation only supports rank 1 datasets, but
-     * the QDataSet spec allows for qube datasets of any rank>0 to be bundled.  This limitation will be removed
+     * return a BundleDataSet for bundling rank 1 datasets.  The result
+     * will be a rank 2 datasets with BUNDLE_1 non-null.
+     * @return a rank 2 BundleDataSet
+     */
+    public static BundleDataSet createRank1Bundle() {
+        return new BundleDataSet(2);
+    }
+
+    /** Creates a new instance of BundleDataSet that accepts rank 1 datasets.
+     * (Rank 2 and up are not yet supported.)
+     */
+    public BundleDataSet( ) {
+        this(2);
+    }
+
+    /** Creates a new instance of BundleDataSet with the given rank.  Rank 1
+     * datasets can bundle rank 0 datasets, while rank 2 can only bundle
+     * rank 1 datasets with the same depend_0.
+     */
+    public BundleDataSet( int rank ) {
+        if ( rank>2 ) throw new IllegalArgumentException("only rank 1 and 2 are supported.");
+        this.rank= rank;
+        datasets= new ArrayList<QDataSet>();
+        if ( rank==2 ) {
+            putProperty( QDataSet.BUNDLE_1, new BundleDescriptor() );
+            putProperty( QDataSet.QUBE, Boolean.TRUE );
+        } else {
+            putProperty( QDataSet.BUNDLE_0, new BundleDescriptor() );
+        }
+
+    }
+    /**
+     * add the dataset to the bundle of datasets.  Currently this implementation only supports rank N-1 datasets (N is this
+     * dataset's rank), but the QDataSet spec allows for qube datasets of any rank>1 to be bundled.  This limitation will be removed
      * in a future version.
      * 
      * @param ds
      */
     public void bundle( QDataSet ds ) {
-        if ( ds.rank()!=this.rank-1 ) throw new IllegalArgumentException("only rank 1 supported for now.");
-        if ( datasets.size()>0 ) {
-            len0= ds.length();
-        } else {
-            if ( ds.length()!=len0 ) throw new IllegalArgumentException("dataset length is not consistent with the bundle.");
+        if ( ds.rank()!=this.rank-1 ) throw new IllegalArgumentException("dataset rank must be "+(this.rank-1));
+        if ( this.rank>1 ) {
+            if ( len0==-1 ) {
+                len0= ds.length();
+            } else {
+                if ( ds.length()!=len0 ) throw new IllegalArgumentException("dataset length is not consistent with the bundle.");
+            }
         }
         datasets.add( ds );
+    }
+
+    class BundleDescriptor extends AbstractDataSet {
+        public int rank() {
+            return 2;
+        }
+
+        @Override
+        public int length() {
+            return datasets.size();
+        }
+
+        @Override
+        public int length(int i) {
+            return 0;  // support bundling just rank N-1 datasets.
+        }
+
+        @Override
+        public Object property(String name, int i) {
+            return datasets.get(i).property(name);
+        }
+
+        @Override
+        public double value(int i0, int i1) {
+            // support bundling just rank N-1 datasets.  to support higher rank
+            // datasets, this should return the qube dims.
+            throw new IndexOutOfBoundsException("length=0");
+        }
+
     }
 
     public int rank() {
         return rank;
     }
 
+    public double value(int i0) {
+        return datasets.get(i0).value();
+    }
+
     public double value(int i0, int i1) {
         return datasets.get(i1).value(i0);
+    }
+
+    public Object property(String name, int i0) {
+        return datasets.get(i0).property(name);
     }
 
     public Object property(String name, int i0, int i1) {
@@ -73,7 +145,7 @@ public class BundleDataSet extends AbstractDataSet {
     }
 
     public int length() {
-        return len0;
+        return this.rank==2 ? len0 : datasets.size();
     }
 
     public int length(int i0) {
@@ -81,10 +153,14 @@ public class BundleDataSet extends AbstractDataSet {
     }
     
     public String toString() {
-        if ( datasets.size()>4 ) {
-            return "BundleDataSet["+datasets.size()+" datasets: "+ datasets.get(0)+", "+datasets.get(1)+", ...]";
+        if ( rank==1 ) {
+            return DataSetUtil.format(this);
         } else {
-            return "BundleDataSet["+datasets.size()+" datasets: "+ datasets +" ]";
+             if ( datasets.size()>4 ) {
+                return "BundleDataSet["+datasets.size()+" datasets: "+ datasets.get(0)+", "+datasets.get(1)+", ...]";
+            } else {
+                return "BundleDataSet["+datasets.size()+" datasets: "+ datasets +" ]";
+            }
         }
     }
 
