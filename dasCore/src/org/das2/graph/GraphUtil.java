07d9d4cd6911186e131aaf06e47313721269e871
Jeremy Faden <jbfaden@users.noreply.github.com>
2009-08-28 00:41:50 +0000

    reducePath returns the number of points in the new path instead of the GeneralPath, since the mutable GeneralPath is passed in.
    svn path=/dasCore/community/autoplot/trunk/; revision=4595

--- dasCore/src/org/das2/graph/GraphUtil.java
+++ dasCore/src/org/das2/graph/GraphUtil.java
@@ -338,19 +338,22 @@ public class GraphUtil {
         return result;
     }
 
-    public static GeneralPath reducePath(PathIterator it, GeneralPath result) {
+    public static int reducePath(PathIterator it, GeneralPath result) {
         return reducePath( it, result, 1 );
     }
     /**
      * Returns the input GeneralPath filled with new points which will be rendered identically to the input path,
      * but contains a minimal number of points.  We bin average the points within a cell, because descretization
      * would mess up the label orientation in contour plotting.
-     * @return a new GeneralPath which will be rendered identically to the input path,
+     *
+     * a new GeneralPath which will be rendered identically to the input path,
      * but contains a minimal number of points.
+     *
+     * @return the number of "points" (LINE_TOs) in the result.
      * @param it A path iterator with minute details that will be lost when rendering.
      * @param result A GeneralPath to put the result into.
      */
-    public static GeneralPath reducePath(PathIterator it, GeneralPath result, int res ) {
+    public static int reducePath(PathIterator it, GeneralPath result, int res ) {
 
         float[] p = new float[6];
 
@@ -444,7 +447,7 @@ public class GraphUtil {
                 throw new IllegalArgumentException("not supported");
         }
 
-        return result;
+        return points;
     }
 
     /**
