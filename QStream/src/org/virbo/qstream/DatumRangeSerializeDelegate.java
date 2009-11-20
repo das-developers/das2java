7589e410db19aa83c80e86b644d994405d005637
Jeremy Faden <jbfaden@users.noreply.github.com>
2009-11-20 22:14:10 +0000

    bugfix: units like "1/(cm^2-s-sr)" would cause parse exception because of first-character is letter test.  Now we try to parse it as a double... when formatting, prefix times with "time:" to remove ambiguity of "1990 to 2000"
    svn path=/autoplot/trunk/QStream/; revision=3768

--- QStream/src/org/virbo/qstream/DatumRangeSerializeDelegate.java
+++ QStream/src/org/virbo/qstream/DatumRangeSerializeDelegate.java
@@ -6,22 +6,36 @@
 package org.virbo.qstream;
 
 import java.text.ParseException;
+import org.das2.datum.DatumRange;
 import org.das2.datum.DatumRangeUtil;
 import org.das2.datum.Units;
+import org.das2.datum.UnitsUtil;
 import org.virbo.dataset.SemanticOps;
 
 /**
  * This serialize delegate assumes the formatted object is a time range,
- * and if it doesn't parse then it's a datum range.
+ * and if it doesn't parse then it's a datum range.  Time Ranges
+ * may be explicitly declared with "time:" prefix.
+ *
  * @author jbf
  */
 public class DatumRangeSerializeDelegate implements SerializeDelegate {
 
     public String format(Object o) {
-        return o.toString();
+        DatumRange dr= (DatumRange)o;
+        Units u= dr.getUnits();
+        if ( UnitsUtil.isTimeLocation(u) ) {
+            // because of ambiguity of "1990 to 2000"
+            return "time:"+ o;
+        } else {
+            return o.toString();
+        }
     }
 
     public Object parse(String typeId, String s) throws ParseException {
+        if ( s.startsWith("time:") ) {
+            return DatumRangeUtil.parseTimeRange(s.substring(5));
+        }
         try {
             return DatumRangeUtil.parseTimeRange(s);
         } catch ( ParseException e ) {
@@ -30,11 +44,13 @@ public class DatumRangeSerializeDelegate implements SerializeDelegate {
                 return DatumRangeUtil.parseDatumRange(s, Units.dimensionless );
             } else {
                 String sunits= s.substring(i).trim();
-                if ( Character.isLetter(sunits.charAt(0) ) ) {
+                try {
+                    // if the last thing is a double, then assume dimensionless units.
+                    Double d= Double.parseDouble(sunits);
+                    return DatumRangeUtil.parseDatumRange(s, Units.dimensionless );
+                } catch ( NumberFormatException ex ) {
                     Units u= SemanticOps.lookupUnits(sunits);
                     return DatumRangeUtil.parseDatumRange(s, u );
-                } else {
-                    return DatumRangeUtil.parseDatumRange(s, Units.dimensionless );
                 }
             }
         }
