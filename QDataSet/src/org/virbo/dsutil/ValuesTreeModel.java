cf19fadd367879771bd2b6797bd0aec41623ee78
Jeremy Faden <jbfaden@users.noreply.github.com>
2010-05-04 20:45:08 +0000

    bugfix 2996776: values in metadata tab suddenly don't display.
    svn path=/autoplot/branches/agu2009/QDataSet/; revision=4278

--- QDataSet/src/org/virbo/dsutil/ValuesTreeModel.java
+++ QDataSet/src/org/virbo/dsutil/ValuesTreeModel.java
@@ -132,7 +132,7 @@ public class ValuesTreeModel extends DefaultTreeModel {
             for ( int i=0; i<Math.min( ds.length(), sizeLimit ); i++ ) {
                 if ( dep0.rank()==1 ) { //TODO: waht should this do for rank>1?
                     MutableTreeNode sliceNode= new DefaultMutableTreeNode( "values @ "+depu.createDatum(dep0.value(i)) );
-                    //MutableTreeNode atree= valuesTreeNode( prefix + i+",", sliceNode, DataSetOps.slice0(ds, i) , sizeLimit );
+                    MutableTreeNode atree= valuesTreeNode( prefix + i+",", sliceNode, DataSetOps.slice0(ds, i) , sizeLimit );
                     aroot.insert( sliceNode, aroot.getChildCount() );
                 }
             }
