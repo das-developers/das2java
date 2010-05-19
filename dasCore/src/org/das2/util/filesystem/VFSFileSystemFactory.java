abb7facfb5297d05cbc80e5b52df4a373e95ba0e
Jeremy Faden <jbfaden@users.noreply.github.com>
2010-05-19 20:20:36 +0000

    throw UnknownHostException. https://sourceforge.net/tracker/index.php?func=detail&aid=2987760&group_id=199733&atid=970682
    svn path=/dasCore/community/autoplot/trunk/; revision=4855

--- dasCore/src/org/das2/util/filesystem/VFSFileSystemFactory.java
+++ dasCore/src/org/das2/util/filesystem/VFSFileSystemFactory.java
@@ -2,6 +2,7 @@ package org.das2.util.filesystem;
 
 import java.net.MalformedURLException;
 import java.net.URI;
+import java.net.UnknownHostException;
 import org.das2.util.filesystem.FileSystem.FileSystemOfflineException;
 
 /**
@@ -13,7 +14,7 @@ public class VFSFileSystemFactory implements FileSystemFactory {
     public VFSFileSystemFactory() {
     }
 
-    public FileSystem createFileSystem(URI root) throws FileSystemOfflineException, MalformedURLException {
+    public FileSystem createFileSystem(URI root) throws FileSystemOfflineException, UnknownHostException, MalformedURLException {
         VFSFileSystem vfs = VFSFileSystem.createVFSFileSystem(root);
         return vfs;
     }
