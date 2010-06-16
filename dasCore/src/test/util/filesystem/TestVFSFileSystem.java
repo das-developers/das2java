d891d409897a5bdc88d3d9e5188abaed37fea97e
Ed Jackson <ed.jackson@cottagesystems.com>
2010-06-16 15:32:47 +0000

    VFS file system test code
    svn path=/dasCore/community/autoplot/trunk/; revision=4871

--- dasCore/src/test/util/filesystem/TestVFSFileSystem.java
+++ dasCore/src/test/util/filesystem/TestVFSFileSystem.java
@@ -19,14 +19,20 @@ import org.das2.util.filesystem.VFSFileSystemFactory;
 public class TestVFSFileSystem {
     public static void main( String[] args ) {
         try {
-            FileSystem.registerFileSystemFactory("sftp", new VFSFileSystemFactory());
-            URI uri = new URI("sftp://USER@MY.HOST.NAME:PORT/");  // or USER:PASS
-            System.out.println("Scheme: " + uri.getScheme());
-            System.out.println("Host: " + uri.getHost());
-            System.out.println("Port: " + uri.getPort());
-            System.out.println("String: " + uri.toString());
-            
-            FileSystem fs = FileSystem.create(uri);
+//            FileSystem.registerFileSystemFactory("sftp", new VFSFileSystemFactory());
+            URI uri = new URI("ftp://anonymous:hithere@127.0.0.1/public/fakeFolder/");  // or USER:PASS
+//            System.out.println("Scheme: " + uri.getScheme());
+//            System.out.println("Host: " + uri.getHost());
+//            System.out.println("Port: " + uri.getPort());
+//            System.out.println("String: " + uri.toString());
+
+            //VFSFileSystemFactory factory = new VFSFileSystemFactory();
+            VFSFileSystem fs = VFSFileSystem.createVFSFileSystem(uri, false);
+
+            for (String s: fs.listDirectory("/")) {
+                System.out.println(s);
+            }
+            fs.close();
 
 //            FileObject fo = fs.getFileObject("/home/ed/elite.xml");
 //            System.out.println("Stats for file /home/ed/elite.xml");
@@ -39,14 +45,14 @@ public class TestVFSFileSystem {
 //            char[] buf = new char[256];
 //            while ( reader.read(buf) >= 0 ) System.out.print(buf);
 
-            VFSFileObject vfo = (VFSFileObject) fs.getFileObject("/home/ed/created_by_vfs.txt");
-            vfo.createFile();
-            PrintStream os = new PrintStream(vfo.getOutputStream(false));
-            os.println("This is a test.  I sure hope it works.");
-            os.println(new java.util.Date().toString());
-            os.close();
-            vfo.close();
-            ((VFSFileSystem)fs).close();
+//            VFSFileObject vfo = (VFSFileObject) fs.getFileObject("/home/ed/created_by_vfs.txt");
+//            vfo.createFile();
+//            PrintStream os = new PrintStream(vfo.getOutputStream(false));
+//            os.println("This is a test.  I sure hope it works.");
+//            os.println(new java.util.Date().toString());
+//            os.close();
+//            vfo.close();
+//            ((VFSFileSystem)fs).close();
 
         } catch(Exception e) {
             e.printStackTrace();
