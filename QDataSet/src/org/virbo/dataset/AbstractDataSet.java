0da13cb01091c8f3a898dad57d821e8c1607fada
Jeremy Faden <jbfaden@users.noreply.github.com>
2010-06-24 20:10:08 +0000

    bug 3020986: QDataSet property inheritance
    svn path=/autoplot/branches/agu2009/QDataSet/; revision=4591

--- QDataSet/src/org/virbo/dataset/AbstractDataSet.java
+++ QDataSet/src/org/virbo/dataset/AbstractDataSet.java
@@ -52,19 +52,35 @@ public abstract class AbstractDataSet implements QDataSet, MutablePropertyDataSe
     }
 
     public Object property(String name, int i) {
-        return properties.get(name);
+        if ( DataSetUtil.isInheritedProperty(name) ) {
+            return properties.get(name);
+        } else {
+            return null;
+        }
     }
 
     public Object property(String name, int i0, int i1) {
-        return properties.get(name);
+        if ( DataSetUtil.isInheritedProperty(name) ) {
+            return properties.get(name);
+        } else {
+            return null;
+        }
     }
 
     public Object property(String name, int i0, int i1, int i2) {
-        return properties.get(name);
+        if ( DataSetUtil.isInheritedProperty(name) ) {
+            return properties.get(name);
+        } else {
+            return null;
+        }
     }
 
     public Object property(String name, int i0, int i1, int i2, int i3) {
-        return properties.get(name);
+        if ( DataSetUtil.isInheritedProperty(name) ) {
+            return properties.get(name);
+        } else {
+            return null;
+        }
     }
 
     public void putProperty( String name, Object value ) {
