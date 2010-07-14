5f7ac5eaf87338fd97d20d3fffde45cb23c751e6
Jeremy Faden <jbfaden@users.noreply.github.com>
2010-07-14 18:41:56 +0000

    trivial: change in PitchAngleDistributionRenderer constructor.  Working towards finished implementation
    svn path=/dasCore/community/autoplot/trunk/; revision=4890

--- dasCore/src/org/das2/graph/PitchAngleDistributionRenderer.java
+++ dasCore/src/org/das2/graph/PitchAngleDistributionRenderer.java
@@ -7,7 +7,11 @@ package org.das2.graph;
 
 import java.awt.Color;
 import java.awt.Graphics;
+import java.awt.Graphics2D;
 import java.awt.Polygon;
+import java.awt.RenderingHints;
+import java.beans.PropertyChangeEvent;
+import java.beans.PropertyChangeListener;
 import org.das2.dataset.TableDataSet;
 import org.das2.datum.Units;
 import org.das2.util.monitor.ProgressMonitor;
@@ -19,6 +23,10 @@ import static java.lang.Math.*;
  */
 public class PitchAngleDistributionRenderer extends Renderer {
 
+    public PitchAngleDistributionRenderer( DasColorBar cb ) {
+        setColorBar(cb);
+    }
+    
     DasColorBar cb;
 
     protected DasColorBar colorBar = null;
@@ -28,18 +36,45 @@ public class PitchAngleDistributionRenderer extends Renderer {
         return colorBar;
     }
 
+    PropertyChangeListener rebinListener= new PropertyChangeListener() {
+        public void propertyChange(PropertyChangeEvent e) {
+            update();
+            refreshImage();
+        }
+    };
+
     public void setColorBar(DasColorBar colorBar) {
         DasColorBar oldColorBar = this.colorBar;
+        if ( this.colorBar!=null ) {
+            colorBar.removePropertyChangeListener("dataMinimum", rebinListener);
+            colorBar.removePropertyChangeListener("dataMaximum", rebinListener);
+            colorBar.removePropertyChangeListener("log", rebinListener);
+            colorBar.removePropertyChangeListener(DasColorBar.PROPERTY_TYPE, rebinListener);
+            colorBar.removePropertyChangeListener(DasColorBar.PROPERTY_FILL_COLOR, rebinListener);
+
+        }
         this.colorBar = colorBar;
+        if (this.colorBar != null) {
+            colorBar.addPropertyChangeListener("dataMinimum", rebinListener);
+            colorBar.addPropertyChangeListener("dataMaximum", rebinListener);
+            colorBar.addPropertyChangeListener("log", rebinListener);
+            colorBar.addPropertyChangeListener(DasColorBar.PROPERTY_TYPE, rebinListener);
+            colorBar.addPropertyChangeListener(DasColorBar.PROPERTY_FILL_COLOR, rebinListener);
+        }
         propertyChangeSupport.firePropertyChange(PROP_COLORBAR, oldColorBar, colorBar);
     }
 
     @Override
-    public void render(Graphics g, DasAxis xAxis, DasAxis yAxis, ProgressMonitor mon) {
+    public void render(Graphics g1, DasAxis xAxis, DasAxis yAxis, ProgressMonitor mon) {
         TableDataSet tds= (TableDataSet)ds;
+        Graphics2D g= (Graphics2D)g1;
+        g.setRenderingHint( RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON );
 
         double radMin=0;
 
+        if ( tds==null ) return;
+        if ( colorBar==null ) return;
+        
         double[][] xx= new double[ tds.getXLength()+1 ] [ tds.getYLength(0)+1 ];
         double[][] yy= new double[ tds.getXLength()+1 ] [ tds.getYLength(0)+1 ];
 
@@ -48,24 +83,32 @@ public class PitchAngleDistributionRenderer extends Renderer {
         Units yunits= Units.dimensionless;  // should be eV
         Units zunits= tds.getZUnits();
 
-        double da= tds.getXTagDouble(1,xunits) - tds.getXTagDouble(0,xunits);
+        double da= ( tds.getXTagDouble(1,xunits) - tds.getXTagDouble(0,xunits) ) / 2;
 
         double x0= xAxis.transform(0,Units.dimensionless);
         double y0= yAxis.transform(0,Units.dimensionless);
 
-        for ( int j=0; j<tds.getYLength(0); j++ ) {
+        for ( int j=0; j<tds.getYLength(0)-1; j++ ) {
             double v1= tds.getYTagDouble(0, j,yunits);
-            double dy= Math.abs( yAxis.transform(v1,yunits) - y0 );
+            double v2= tds.getYTagDouble(0, j+1,yunits);
+            double r0= y0 - ( yAxis.transform(v1,yunits) ); // in
+            double r1= y0 - ( yAxis.transform(v2,yunits) ); // out
             for ( int i=0; i<tds.getXLength(); i++ ) {
                 double a1= tds.getXTagDouble(i,xunits);
-                xx[i][j]= x0 + sin(a1-da) * dy;
-                yy[i][j]= y0 + cos(a1-da) * dy;
-                xx[i+1][j+1]= x0 + sin(a1+da) * dy;
-                yy[i+1][j+1]= y0 + cos(a1+da) * dy;
+                xx[i][j]= x0 + cos(a1-da) * r0;
+                yy[i][j]= y0 - sin(a1-da) * r0;
+                xx[i][j+1]= x0 + cos(a1-da) * r1;
+                yy[i][j+1]= y0 - sin(a1-da) * r1;
+                xx[i+1][j]= x0 + cos(a1+da) * r0;
+                yy[i+1][j]= y0 - sin(a1+da) * r0;
+                xx[i+1][j+1]= x0 + cos(a1+da) * r1;
+                yy[i+1][j+1]= y0 - sin(a1+da) * r1;
                 int zz= colorBar.indexColorTransform( tds.getDouble(i, j, zunits ), zunits );
-                int[] x= new int [] {(int) xx[i][j], (int)xx[i][j], (int)xx[i+1][j], (int)xx[i+1][j], (int)xx[i][j] };
-                int[] y= new int [] { (int)yy[i][j], (int)yy[i][j+1], (int)yy[i][j+1], (int)yy[i][j], (int)yy[i][j] };
-                Polygon p= new Polygon( x, y, 5 );
+                //int[] x= new int [] {(int) xx[i][j], (int)xx[i][j+1], (int)xx[i+1][j+1], (int)xx[i+1][j], (int)xx[i][j] };
+                //int[] y= new int [] { (int)yy[i][j], (int)yy[i][j+1], (int)yy[i+1][j+1], (int)yy[i+1][j], (int)yy[i][j] };
+                int[] x= new int [] {(int) xx[i][j], (int)xx[i][j+1], (int)xx[i+1][j+1], (int)xx[i+1][j],};
+                int[] y= new int [] { (int)yy[i][j], (int)yy[i][j+1], (int)yy[i+1][j+1], (int)yy[i+1][j]};
+                Polygon p= new Polygon( x, y, 4 );
                 g.setColor( new Color(zz) );
                 g.fillPolygon(p);
 
@@ -74,4 +117,21 @@ public class PitchAngleDistributionRenderer extends Renderer {
 
     }
 
+    protected void installRenderer() {
+        if (parent != null && parent.getCanvas() != null) {
+            if (colorBar != null) {
+                if (colorBar.getColumn() == DasColumn.NULL) {
+                    DasColumn column = parent.getColumn();
+                    colorBar.setColumn(new DasColumn(null, column, 1.0, 1.0, 1, 2, 0, 0));
+                }
+                parent.getCanvas().add(colorBar, parent.getRow(), colorBar.getColumn());
+            }
+        }
+    }
+
+    protected void uninstallRenderer() {
+        if (colorBar != null && colorBar.getCanvas() != null) {
+            colorBar.getCanvas().remove(colorBar);
+        }
+    }
 }
