fdd99e4ea6b6a91df5f66dca1e89754669f9a89d
Jeremy Faden <jbfaden@users.noreply.github.com>
2010-07-16 12:10:13 +0000

    bugfix: rfe 3020986 in DataSetOps.leafTrim caused problem where DEPEND_0 was still inherited.
    svn path=/autoplot/branches/agu2009/QDataSet/; revision=4722

--- QDataSet/src/org/virbo/dataset/DataSetOps.java
+++ QDataSet/src/org/virbo/dataset/DataSetOps.java
@@ -173,13 +173,17 @@ public class DataSetOps {
      * pull out a subset of the dataset by reducing the number of columns in the
      * last dimension.  This does not reduce rank.  This assumes the dataset has no
      * row with length>end.
+     * This is extended to support rank 4 datasets.
+     * TODO: This probably doesn't handle bundles property.
+     * TODO: slice and trim should probably be implemented here for efficiently.
+     * TODO: this should be pulled out into its own class.
      * @param start first index to include.
      * @param end last index, exclusive
      */
     public static MutablePropertyDataSet leafTrim(final QDataSet ds, final int start, final int end) {
 
-        if (ds.rank() > 3) {
-            throw new IllegalArgumentException("rank limit > 3");
+        if (ds.rank() > 4) {
+            throw new IllegalArgumentException("rank limit > 4");
         }
 
         MutablePropertyDataSet result = new AbstractDataSet() {
@@ -200,6 +204,11 @@ public class DataSetOps {
                 return ds.value(i0, i1, i2 + start);
             }
 
+            @Override
+            public double value(int i0, int i1, int i2, int i3 ) {
+                return ds.value(i0, i1, i2, i3 + start);
+            }
+
             public Object property(String name) {
                 if (properties.containsKey(name)) {
                     return properties.get(name);
@@ -209,18 +218,35 @@ public class DataSetOps {
             }
 
             public Object property(String name, int i) {
-                if (properties.containsKey(name)) {
+                if ( DataSetUtil.isInheritedProperty(name) && properties.containsKey(name) ) {
                     return properties.get(name);
                 } else {
-                    return ds.property(name);
+                    return ds.rank() == 1 ? ds.property(name,i-start): ds.property(name,i);
                 }
             }
 
             public Object property(String name, int i0, int i1) {
-                if (properties.containsKey(name)) {
+                if ( DataSetUtil.isInheritedProperty(name) && properties.containsKey(name) ) {
                     return properties.get(name);
                 } else {
-                    return ds.property(name);
+                    return ds.rank() == 2 ? ds.property(name,i0,i1-start): ds.property(name,i0,i1);
+                }
+            }
+
+            public Object property(String name, int i0, int i1, int i2 ) {
+                if ( DataSetUtil.isInheritedProperty(name) && properties.containsKey(name) ) {
+                    return properties.get(name);
+                } else {
+                    return ds.rank() == 3 ? ds.property(name,i0,i1,i2-start): ds.property(name,i0,i1,i2);
+                }
+            }
+
+            @Override
+            public Object property(String name, int i0, int i1, int i2, int i3) {
+                if ( DataSetUtil.isInheritedProperty(name) && properties.containsKey(name) ) {
+                    return properties.get(name);
+                } else {
+                    return ds.rank() == 4 ? ds.property(name,i0,i1,i2,i3-start): ds.property(name,i0,i1,i2,i3);
                 }
             }
 
@@ -235,6 +261,12 @@ public class DataSetOps {
             public int length(int i, int j) {
                 return ds.rank() == 3 ? end - start : ds.length(i, j);
             }
+
+            @Override
+            public int length(int i0, int i1, int i2 ) {
+                return ds.rank() == 4 ? end - start : ds.length(i0, i1, i2);
+            }
+
         };
 
         String depNName = "DEPEND_" + (ds.rank() - 1);
