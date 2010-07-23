2ecf75f9f812c36f7778e72c04efd63bd0522974
Jeremy Faden <jbfaden@users.noreply.github.com>
2010-07-23 19:43:32 +0000

    trivial: put in todo comment that we should use ds.slice() for slice0, for performance reasons.
    svn path=/autoplot/branches/agu2009/QDataSet/; revision=4792

--- QDataSet/src/org/virbo/dataset/DataSetOps.java
+++ QDataSet/src/org/virbo/dataset/DataSetOps.java
@@ -634,7 +634,7 @@ public class DataSetOps {
                 if ( dim==0 ) {
                     if ( idx>=fillDs.length() ) idx=fillDs.length()-1;
                     if ( idx<0 ) idx=0;
-                    fillDs= slice0(fillDs, idx);
+                    fillDs= slice0(fillDs, idx); //TODO: use fillDs.slice
                 } else if ( dim==1 ) {
                     if ( idx>=fillDs.length(0) ) idx=fillDs.length(0)-1;
                     if ( idx<0 ) idx=0;
