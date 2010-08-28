b8d26bcf70c274deefcf20b4cb9d7519d9b3733c
Jeremy Faden <jbfaden@users.noreply.github.com>
2010-08-28 12:38:41 +0000

    test for versioning support.
    svn path=/dasCore/community/autoplot/trunk/; revision=4935

---
+++ dasCore/src/test/util/filesystem/TestVersioning.java
@@ -0,0 +1,32 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package test.util.filesystem;
+
+import java.io.File;
+import org.das2.fsm.FileStorageModelNew;
+import org.das2.util.filesystem.FileSystem;
+
+/**
+ *
+ * @author jbf
+ */
+public class TestVersioning {
+    public static void main( String[] args ) throws Exception {
+        System.err.println("---");
+        FileSystem fs= FileSystem.create( new File("/tmp/").toURI() );
+        FileStorageModelNew fsm= FileStorageModelNew.create( fs, "data_$Y_$m_$d_v$(v,sep).qds" );
+        File[] ff= fsm.getBestFilesFor(null);
+        for ( File f: ff ) {
+            System.err.println(f);
+        }
+        System.err.println("---");
+        fsm= FileStorageModelNew.create( fs, "data_$Y_$m_$d_v$v.qds" );
+        ff= fsm.getBestFilesFor(null);
+        for ( File f: ff ) {
+            System.err.println(f);
+        }
+    }
+}
