ab23ee4eedc5e4e6ab35530cec82b93814e9e5e9
Jeremy Faden <jbfaden@users.noreply.github.com>
2010-09-21 16:19:28 +0000

    refactor DDataSet stuff to insert ArrayDataSet that contains the common stuff.  This reduces code, and also fixes problems where data would be truncated depending on which was called.  (This was never observed as a bug.)
    svn path=/autoplot/branches/agu2009/QDataSet/; revision=5127

---
+++ QDataSet/src/org/virbo/dataset/ArrayDataSet.java
@@ -0,0 +1,347 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package org.virbo.dataset;
+
+import java.lang.reflect.Array;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * A number of static methods were initially defined in DDataSet, then
+ * copied into FDataSet and others when they were made.  This super implementation
+ * will parent all such datasets and provide common methods.
+ * @author jbf
+ */
+public abstract class ArrayDataSet extends AbstractDataSet implements WritableDataSet {
+    int rank;
+
+    int len0;
+    int len1;
+    int len2;
+    int len3;
+
+    float fill= Float.NaN;
+    double dfill= Double.NaN;
+
+    protected static ArrayDataSet create( int rank, int len0, int len1, int len2, int len3, Object back ) {
+        Class c= back.getClass().getComponentType();
+        if ( c==double.class ) return new DDataSet( rank, len0, len1, len2, len3, (double[])back );
+        if ( c==float.class ) return new FDataSet( rank, len0, len1, len2, len3, (float[])back );
+        if ( c==long.class ) return new LDataSet( rank, len0, len1, len2, len3, (long[])back );
+        if ( c==int.class ) return new IDataSet( rank, len0, len1, len2, len3, (int[])back );
+        if ( c==short.class ) return new SDataSet( rank, len0, len1, len2, len3, (short[])back );
+        if ( c==byte.class ) return new BDataSet( rank, len0, len1, len2, len3, (byte[])back );
+        throw new IllegalArgumentException("class not supported: "+c);
+    }
+
+    public static ArrayDataSet createRank1( Class c, int len0 ) {
+        if ( c==double.class ) return new DDataSet( 1, len0, 1, 1, 1 );
+        if ( c==float.class ) return new FDataSet( 1, len0, 1, 1, 1 );
+        if ( c==long.class ) return new LDataSet( 1, len0, 1, 1, 1 );
+        if ( c==int.class ) return new IDataSet( 1, len0, 1, 1, 1 );
+        if ( c==short.class ) return new SDataSet( 1, len0, 1, 1, 1 );
+        if ( c==byte.class ) return new BDataSet( 1, len0, 1, 1, 1 );
+        throw new IllegalArgumentException("class not supported: "+c);
+    }
+
+    public static ArrayDataSet createRank2( Class c, int len0, int len1 ) {
+        if ( c==double.class ) return new DDataSet( 2, len0, len1, 1, 1 );
+        if ( c==float.class ) return new FDataSet( 2, len0, len1, 1, 1 );
+        if ( c==long.class ) return new LDataSet( 2, len0, len1, 1, 1 );
+        if ( c==int.class ) return new IDataSet( 2, len0, len1, 1, 1 );
+        if ( c==short.class ) return new SDataSet( 2, len0, len1, 1, 1 );
+        if ( c==byte.class ) return new BDataSet( 2, len0, len1, 1, 1 );
+        throw new IllegalArgumentException("class not supported: "+c);
+    }
+
+    public static ArrayDataSet createRank3( Class c, int len0, int len1, int len2 ) {
+        if ( c==double.class ) return new DDataSet( 3, len0, len1, len2, 1 );
+        if ( c==float.class ) return new FDataSet( 3, len0, len1, len2, 1 );
+        if ( c==long.class ) return new LDataSet( 3, len0, len1, len2, 1 );
+        if ( c==int.class ) return new IDataSet( 3, len0, len1, len2, 1 );
+        if ( c==short.class ) return new SDataSet( 3, len0, len1, len2, 1 );
+        if ( c==byte.class ) return new BDataSet( 3, len0, len1, len2, 1 );
+        throw new IllegalArgumentException("class not supported: "+c);
+    }
+
+    public static ArrayDataSet createRank4( Class c, int len0, int len1, int len2, int len3 ) {
+        if ( c==double.class ) return new DDataSet( 4, len0, len1, len2, len3 );
+        if ( c==float.class ) return new FDataSet( 4, len0, len1, len2, len3 );
+        if ( c==long.class ) return new LDataSet( 4, len0, len1, len2, len3 );
+        if ( c==int.class ) return new IDataSet( 4, len0, len1, len2, len3 );
+        if ( c==short.class ) return new SDataSet( 4, len0, len1, len2, len3 );
+        if ( c==byte.class ) return new BDataSet( 4, len0, len1, len2, len3 );
+        throw new IllegalArgumentException("class not supported: "+c);
+    }
+
+    public int rank() {
+        return rank;
+    }
+
+    @Override
+    public int length() {
+        return len0;
+    }
+
+    @Override
+    public int length(int i) {
+        return len1;
+    }
+
+    @Override
+    public int length( int i0, int i1 ) {
+        return len2;
+    }
+
+    @Override
+    public int length( int i0, int i1, int i2) {
+        return len3;
+    }
+
+    public Class getComponentType() {
+        return getBack().getClass().getComponentType();
+    }
+
+    /**
+     * Shorten the dataset by changing it's dim 0 length parameter.  The same backing array is used,
+     * so the element that remain will be the same.
+     * can only shorten!
+     */
+    public void putLength( int len ) {
+        if ( len>len0 ) throw new IllegalArgumentException("dataset cannot be lengthened");
+        len0= len;
+    }
+
+    /**
+     * copies the properties, copying depend datasets as well.
+     */
+    private static Map copyProperties( QDataSet ds ) {
+        Map result = new HashMap();
+        Map srcProps= DataSetUtil.getProperties(ds);
+
+        result.putAll(srcProps);
+
+        for ( int i=0; i < ds.rank(); i++) {
+            QDataSet dep = (QDataSet) ds.property("DEPEND_" + i);
+            if (dep == ds) {
+                throw new IllegalArgumentException("dataset is dependent on itsself!");
+            }
+            if (dep != null) {
+                result.put("DEPEND_" + i, copy(dep) ); // for timetags
+            }
+        }
+
+        for (int i = 0; i < QDataSet.MAX_PLANE_COUNT; i++) {
+            QDataSet plane0 = (QDataSet) ds.property("PLANE_" + i);
+            if (plane0 != null) {
+                result.put("PLANE_" + i, copy(plane0));
+            } else {
+                break;
+            }
+        }
+
+        return result;
+    }
+
+    /**
+     * Copy the dataset to a DDataSet only if the dataset is not already a DDataSet.
+     * @param ds
+     * @return
+     */
+    public static ArrayDataSet maybeCopy( QDataSet ds ) {
+        if ( ds instanceof ArrayDataSet ) {
+            return (ArrayDataSet)ds;
+        } else {
+            return copy(ds);
+        }
+    }
+
+    /**
+     * Copy the dataset to a ArrayDataSet only if the dataset is not already a
+     * particular instance of ArrayDataSet.
+     * @param ds
+     * @return
+     */
+    public static ArrayDataSet maybeCopy( Class c, QDataSet ds ) {
+        if ( ds instanceof ArrayDataSet && ((ArrayDataSet)ds).getComponentType()==c ) {
+            return (ArrayDataSet)ds;
+        } else {
+            return copy(ds);
+        }
+    }
+
+    /**
+     * provide access to the backing array.
+     * @return
+     */
+    protected abstract Object getBack();
+
+    private static ArrayDataSet ddcopy(ArrayDataSet ds) {
+        int dsLength = ds.len0 * ds.len1 * ds.len2 * ds.len3;
+
+        Object newback = Array.newInstance( ds.getBack().getClass().getComponentType(), dsLength);
+
+        System.arraycopy( ds.getBack(), 0, newback, 0, dsLength );
+
+        ArrayDataSet result = ArrayDataSet.create(ds.rank, ds.len0, ds.len1, ds.len2, ds.len3, newback);
+        result.properties.putAll(copyProperties(ds)); // TODO: problems...
+
+        return result;
+    }
+
+    /**
+     *
+     * @param c  the primitive type to use (e.g. double.class).
+     * @param ds
+     * @return
+     */
+    public static ArrayDataSet copy( Class c, QDataSet ds ) {
+
+        if ( ds instanceof ArrayDataSet && ((ArrayDataSet)ds).getBack().getClass().getComponentType()==c ) return ddcopy( (ArrayDataSet)ds );
+        
+        int rank= ds.rank();
+        ArrayDataSet result;
+
+        switch (rank) {
+            case 1:
+                result= createRank1( c, ds.length() );
+                for ( int i=0; i<ds.length(); i++ ) {
+                    result.putValue( i, ds.value(i) );
+                }
+                break;
+            case 2:
+                result= createRank2( c, ds.length(), ds.length(0) );
+                for ( int i=0; i<ds.length(); i++ ) {
+                    for ( int j=0; j<ds.length(i); j++ ) {
+                        result.putValue( i, j, ds.value(i,j) );
+                    }
+                }
+                break;
+            case 3:
+                result= createRank3( c, ds.length(), ds.length(0), ds.length(0,0) );
+                for ( int i=0; i<ds.length(); i++ ) {
+                    for ( int j=0; j<ds.length(i); j++ ) {
+                        for ( int k=0; k<ds.length(i,j); k++ ) {
+                            result.putValue( i, j, k, ds.value(i,j,k) );
+                        }
+                    }
+                }
+                break;
+            case 4:
+                result = createRank4( c, ds.length(), ds.length(0), ds.length(0,0), ds.length(0,0,0));
+                for ( int i=0; i<ds.length(); i++ )
+                    for ( int j=0; j<ds.length(i); j++ )
+                        for ( int k=0; k<ds.length(i,j); k++ )
+                            for ( int l=0; l<ds.length(i,j,k); l++ )
+                                result.putValue( i, j, k, l, ds.value(i,j,k,l));
+                break;
+
+            default: throw new IllegalArgumentException("bad rank");
+        }
+        result.properties.putAll( copyProperties(ds) );
+        result.checkFill();
+
+        return result;
+
+    }
+
+    /**
+     * copies the dataset into a writeable ArrayDataSet, and all of it's depend datasets as well.
+     */
+    public static ArrayDataSet copy( QDataSet ds ) {
+        if ( ds instanceof ArrayDataSet ) return ddcopy( (ArrayDataSet)ds );
+        return copy( double.class, ds );
+    }
+
+
+    
+    /**
+     * check for fill property and set local variable.
+     */
+    protected void checkFill() {
+        Number f= (Number) properties.get(QDataSet.FILL_VALUE);
+        if ( f!=null ) {
+            fill= f.floatValue();
+            dfill= f.doubleValue();
+        } else {
+            fill= Float.NaN;
+            dfill= Double.NaN;
+        }
+    }
+
+    /**
+     * append the second dataset onto this dataset.  Not thread safe!!!
+     * TODO: this really should return a new dataset.  Presumably this is to avoid copies, but currently it copies anyway!
+     */
+    public static ArrayDataSet append( ArrayDataSet ths, ArrayDataSet ds ) {
+        if ( ds.rank()!=ths.rank ) throw new IllegalArgumentException("rank mismatch");
+        if ( ds.len1!=ths.len1 ) throw new IllegalArgumentException("len1 mismatch");
+        if ( ds.len2!=ths.len2 ) throw new IllegalArgumentException("len2 mismatch");
+        if ( ds.len3!=ths.len3 ) throw new IllegalArgumentException("len3 mismatch");
+        if ( ths.getBack().getClass()!=ds.getBack().getClass() ) throw new IllegalArgumentException("backing type mismatch");
+
+        int myLength= ths.len0 * ths.len1 * ths.len2 * ths.len3;
+        int dsLength= ds.len0 * ds.len1 * ds.len2 * ds.len3;
+
+        Object newback= Array.newInstance( ths.getBack().getClass().getComponentType(), myLength + dsLength );
+
+        System.arraycopy( ths.getBack(), 0, newback, 0, myLength );
+        System.arraycopy( ds.getBack(), 0, newback, myLength, dsLength );
+
+        int len0= ths.len0 + ds.len0;
+        
+        ArrayDataSet result= create( ths.rank, len0, ths.len1, ths.len2, ths.len3, newback );
+
+        result.properties.putAll( joinProperties( ths, ds ) );
+        return result;
+    }
+
+
+    /**
+     * TODO: this has a number of problems including that it would reduce resolution
+     * for some types. (FDataSet was always used to join DEPEND_0 for example.)
+     * @param ds
+     */
+    protected static Map joinProperties( ArrayDataSet ths, ArrayDataSet ds ) {
+        Map result= new HashMap();
+        for ( int i=0; i<ds.rank(); i++ ) {
+            QDataSet thatDep= (QDataSet) ds.property( "DEPEND_"+i );
+            if ( thatDep!=null && ( i==0 || thatDep.rank()>1 ) ) {
+                QDataSet thisDep= (QDataSet) ths.property( "DEPEND_"+i );
+                ArrayDataSet djoin= copy( thisDep ); //TODO: reconcile types
+                ArrayDataSet ddep1= thatDep instanceof ArrayDataSet ? (ArrayDataSet) thatDep : maybeCopy( thatDep );
+                djoin= append( djoin, ddep1 );
+                result.put( "DEPEND_"+i, djoin );
+
+            } else if ( thatDep!=null && thatDep.rank()==1 ) {
+                //TODO: check properties equal.
+                result.put( "DEPEND_"+i, thatDep );
+            }
+        }
+        QDataSet dep1= (QDataSet) ds.property( QDataSet.PLANE_0 );
+        if ( dep1!=null ) {
+            QDataSet dep0= (QDataSet) ths.property( QDataSet.PLANE_0 );
+            ArrayDataSet djoin=  copy( dep0 );
+            ArrayDataSet dd1=  maybeCopy(dep1);
+            djoin.append( djoin, dd1 );
+            result.put( QDataSet.PLANE_0, djoin );
+        }
+        String[] props= DataSetUtil.dimensionProperties();
+        for ( int i=0; i<props.length; i++ ) {
+            String prop= props[i];
+            Object value= ths.property(prop);
+            if ( value!=null && value.equals(ds.property(prop) ) ) {
+                result.put( prop, ths.property(prop) );
+            }
+        }
+        return result;
+    }
+
+    @Override
+    public String toString( ) {
+        return DataSetUtil.toString( this );
+    }
+}
