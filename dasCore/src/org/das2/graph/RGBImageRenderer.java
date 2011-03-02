0e4542efa1c38e5bba41b6d5a33da288266e34a4
Jeremy Faden <jbfaden@users.noreply.github.com>
2011-03-02 23:12:19 +0000

    add RGBImageRenderer
    svn path=/dasCore/community/autoplot2011/trunk/; revision=5247

---
+++ dasCore/src/org/das2/graph/RGBImageRenderer.java
@@ -0,0 +1,132 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package org.das2.graph;
+
+import java.awt.Graphics;
+import java.awt.image.BufferedImage;
+import org.das2.datum.Units;
+import org.das2.util.monitor.ProgressMonitor;
+import org.virbo.dataset.QDataSet;
+import org.virbo.dataset.SemanticOps;
+import org.virbo.dsops.Ops;
+
+/**
+ * Renders RBG images stored in a QDataSet[m,n,3], etc.
+ * @author jbf
+ */
+public class RGBImageRenderer extends Renderer {
+
+    BufferedImage image=null;
+
+    @Override
+    public void render(Graphics g, DasAxis xAxis, DasAxis yAxis, ProgressMonitor mon) {
+        QDataSet ds= getDataSet();
+        QDataSet dep0;
+        QDataSet dep1;
+
+        dep0= (QDataSet)ds.property(QDataSet.DEPEND_0);
+        dep1= (QDataSet)ds.property(QDataSet.DEPEND_1);
+
+        if ( dep0==null ) dep0= Ops.dindgen(image.getWidth());
+        if ( dep1==null ) dep1= Ops.dindgen(image.getHeight());
+        
+        Units xunits= SemanticOps.getUnits(dep0);
+        Units yunits= SemanticOps.getUnits(dep1);
+
+        int h= image.getHeight();
+        int w= image.getWidth();
+        
+        double dx0= dep0.value(1)-dep0.value(0);
+        double dy0= dep1.value(1)-dep1.value(0);
+        int x0= (int)xAxis.transform( dep0.value(0) - dx0/2, xunits);
+        int y0= (int)yAxis.transform( dep1.value(0) - dy0/2, yunits);
+        int x1= (int)xAxis.transform( dep0.value(w-1) + dx0/2, xunits );
+        int y1= (int)yAxis.transform( dep1.value(h-1) + dy0/2, yunits );
+        g.drawImage( image, x0, y0, x1-x0, y1-y0, null );
+        
+    }
+
+    @Override
+    public void setDataSet(QDataSet ds) {
+        super.setDataSet(ds);
+        image= getImage(ds);
+    }
+
+    
+    private BufferedImage getImage(QDataSet ds) throws IllegalArgumentException {
+        // ds should be a rank 2 gray scale of Width x Height values, valued from 0-255,
+        // or a rank 3 color image 3,W,H.
+        int imageType = -19999;
+        int w = 0;
+        int h = 0;
+
+        w = ds.length();
+        h = ds.length(0);
+
+        BufferedImage im;
+        if (ds.rank() == 2) {
+            imageType = BufferedImage.TYPE_BYTE_GRAY;
+        } else if (ds.rank() == 3) {
+            if (ds.length(0,0) == 3) {
+                QDataSet dep2 = (QDataSet) ds.property(QDataSet.DEPEND_2);
+                imageType = BufferedImage.TYPE_INT_RGB; // default
+                if (dep2 != null) {
+                    String s0 = org.virbo.dataset.DataSetUtil.asDatum(dep2.slice(0)).toString().toLowerCase();
+                    String s1 = org.virbo.dataset.DataSetUtil.asDatum(dep2.slice(1)).toString().toLowerCase();
+                    String s2 = org.virbo.dataset.DataSetUtil.asDatum(dep2.slice(2)).toString().toLowerCase();
+                    if (s0.startsWith("r") && s1.startsWith("g") && s2.startsWith("b")) {
+                        imageType = BufferedImage.TYPE_INT_RGB;
+                    } else if (s0.startsWith("b") && s1.startsWith("g") && s2.startsWith("r")) {
+                        imageType = BufferedImage.TYPE_INT_BGR;
+                    }
+                }
+            } else if (ds.length(0,0) == 4) {
+                QDataSet dep2 = (QDataSet) ds.property(QDataSet.DEPEND_2);
+                imageType = BufferedImage.TYPE_INT_ARGB;
+                if (dep2 != null) {
+                    String s0 = org.virbo.dataset.DataSetUtil.asDatum(dep2.slice(0)).toString().toLowerCase();
+                    String s1 = org.virbo.dataset.DataSetUtil.asDatum(dep2.slice(1)).toString().toLowerCase();
+                    String s2 = org.virbo.dataset.DataSetUtil.asDatum(dep2.slice(2)).toString().toLowerCase();
+                    String s3 = org.virbo.dataset.DataSetUtil.asDatum(dep2.slice(3)).toString().toLowerCase();
+                    if (s0.startsWith("a") && s1.startsWith("r") && s2.startsWith("g") && s3.startsWith("b")) {
+                        imageType = BufferedImage.TYPE_INT_ARGB;
+                    } else if ( s0.startsWith("a") && s1.startsWith("b") && s2.startsWith("g") && s3.startsWith("r") ) {
+                        imageType = BufferedImage.TYPE_4BYTE_ABGR;
+                    }
+                }
+            }
+        }
+        if (imageType == -19999) {
+            throw new IllegalArgumentException("DataSet must be ds[w,h] ds[w,h,3] or ds[w,h,4] and be RGB, BGR, or ARGB.  Default is RBG");
+        } else {
+            im = new BufferedImage(w, h, imageType);
+        }
+        if (ds.rank() == 2) {
+            for (int i = 0; i < w; i++) {
+                for (int j = 0; j < h; j++) {
+                    im.setRGB(i, j, (int) ds.value(i,j));
+                }
+            }
+        } else if (ds.rank() == 3) {
+            for (int i = 0; i < w; i++) {
+                for (int j = 0; j < h; j++) {
+                    int v;
+                    if (ds.length(0,0) == 4) {
+                        v = 16777216 * (int) ds.value(i,j,0) + 65536 * (int) ds.value(i,j,1) + 256 * (int) ds.value(i,j,2) + (int) ds.value(i,j,3);
+                    } else if (ds.length(0,0) == 3) {
+                        v = 65536 * (int) ds.value(i,j,0) + 256 * (int) ds.value(i,j,1) + (int) ds.value(i,j,2);
+                    } else {
+                        throw new IllegalArgumentException("ds.length=" + ds.length());
+                    }
+                    im.setRGB(i, j, v);
+                }
+            }
+        }
+
+        return im;
+    }
+
+}
---
+++ dasCore/src/test/graph/RGBImageDemo.java
@@ -0,0 +1,88 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package test.graph;
+
+import java.awt.BorderLayout;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import org.das2.graph.RGBImageRenderer;
+import org.das2.datum.DatumRange;
+import org.das2.datum.Units;
+import org.das2.graph.DasCanvas;
+import org.das2.graph.DasPlot;
+import org.das2.graph.GraphUtil;
+import org.das2.graph.Renderer;
+import org.virbo.dataset.DataSetUtil;
+import org.virbo.dataset.MutablePropertyDataSet;
+import org.virbo.dataset.QDataSet;
+import org.virbo.dataset.SDataSet;
+import org.virbo.dsops.Ops;
+
+/**
+ *
+ * @author jbf
+ */
+public class RGBImageDemo extends PlotDemo {
+    JPanel contentPane;
+
+    private synchronized JPanel getContentPane() {
+        if (contentPane == null) {
+            contentPane = new JPanel();
+        }
+        return contentPane;
+    }
+
+    public void showFrame() {
+        JFrame frame= new JFrame( "Axis Demo");
+        frame.getContentPane().add(getContentPane());
+        frame.pack();
+        frame.setDefaultCloseOperation( JFrame.EXIT_ON_CLOSE );
+        frame.setVisible(true);
+    }
+
+    public RGBImageDemo() {
+        int width = 500;
+        int height = 500;
+
+        getContentPane().setLayout(new BorderLayout());
+
+        DasCanvas canvas = new DasCanvas(width, height);
+
+        getContentPane().add(canvas, BorderLayout.CENTER );
+
+        DatumRange xrange= DatumRange.newDatumRange(-100,400,Units.dimensionless);
+        DatumRange yrange= DatumRange.newDatumRange(-100,400,Units.dimensionless);
+
+        DasPlot plot= GraphUtil.newDasPlot(canvas, xrange, yrange);
+
+        plot.getXAxis().setUseDomainDivider(true);
+        plot.getYAxis().setUseDomainDivider(true);
+
+        Renderer r= new RGBImageRenderer();
+
+        QDataSet red= Ops.multiply( DataSetUtil.asDataSet(255), Ops.rand(200,200) );
+        QDataSet green= Ops.multiply( DataSetUtil.asDataSet(0), Ops.rand(200,200) );
+        QDataSet blue= Ops.multiply( DataSetUtil.asDataSet(0), Ops.rand(200,200) );
+
+        SDataSet rgb= SDataSet.createRank3( 200,200,3 );
+        for ( int i=0; i<rgb.length(); i++ ) {
+            for ( int j=0; j<rgb.length(0); j++ ) {
+                rgb.putValue( i, j, 0, 255 );
+                rgb.putValue( i, j, 1, i%3==0 ? 255 : 0 );
+                rgb.putValue( i, j, 2, j%5==0 ? 255 : 0 );
+            }
+        }
+        rgb.putProperty( QDataSet.DEPEND_2, Ops.labels( new String[] { "b", "g", "r" } ) );
+        r.setDataSet( rgb );
+
+        plot.addRenderer( r );
+        
+    }
+
+    public static void main( String[] args ) {
+        new RGBImageDemo().showFrame();
+    }
+}
