6bc570ce69ee1e8df693798a6944a685240cd091
Jeremy Faden <jbfaden@users.noreply.github.com>
2011-03-03 13:00:12 +0000

    revert to try and figure out error that has returned in hudson autoplot_test013.
    svn path=/autoplot/trunk/QDataSet/; revision=6023

--- QDataSet/src/org/virbo/dataset/JoinDataSet.java
+++ QDataSet/src/org/virbo/dataset/JoinDataSet.java
@@ -12,9 +12,6 @@ package org.virbo.dataset;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
-import java.util.Map.Entry;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 /**
  * Create a higher rank dataset with dim 0 being a JOIN dimension.  Join implies
@@ -45,8 +42,8 @@ public class JoinDataSet extends AbstractDataSet {
      */
     public JoinDataSet( int rank ) {
         this.rank= rank;
-        putProperty(QDataSet.JOIN_0,  DDataSet.create( new int[0] ) ); // rank 0 dataset allows properties like CACHE_TAG to be stored here.
-        //putProperty(QDataSet.JOIN_0,  "DEPEND_1" );
+        //putProperty(QDataSet.JOIN_0,  DDataSet.create( new int[0] ) ); // rank 0 dataset allows properties like CACHE_TAG to be stored here.
+        putProperty(QDataSet.JOIN_0,  "DEPEND_1" );
         datasets= new ArrayList<QDataSet>();
     }
 
