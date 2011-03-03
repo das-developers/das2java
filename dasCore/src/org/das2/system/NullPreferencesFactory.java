4136e33df97464223477cbec0d5f7c2d6bf2ffbd
Jeremy Faden <jbfaden@users.noreply.github.com>
2011-03-03 21:23:55 +0000

    javadoc updates, add link to resource explaining use.
    svn path=/dasCore/community/autoplot2011/trunk/; revision=5250

--- dasCore/src/org/das2/system/NullPreferencesFactory.java
+++ dasCore/src/org/das2/system/NullPreferencesFactory.java
@@ -10,12 +10,17 @@ import java.util.prefs.PreferencesFactory;
 
 /**
  * Creates NullPreferences for use with applets.  The system property 
- * java.util.prefs.Preferences should be set to 
+ * java.util.prefs.PreferencesFactory should be set to
  * org.das2.system.NullPreferencesFactory to use this class.
  * 
- * System.setProperty( "java.util.prefs.Preferences", "org.das2.system.NullPreferencesFactory" ) doesn't
+ * System.setProperty( "java.util.prefs.PreferencesFactory", "org.das2.system.NullPreferencesFactory" ) doesn't
  * work in applets--security exception.
-        
+ *
+ * Also this works: java  -Djava.util.prefs.PreferencesFactory=org.das2.system.NullPreferencesFactory ...
+ * 
+ * I'm not sure where I found this solution originally, but here is a nice writeup:
+ *     http://www.allaboutbalance.com/articles/disableprefs/
+ * 
  * @author jbf
  */
 public class NullPreferencesFactory implements PreferencesFactory {
