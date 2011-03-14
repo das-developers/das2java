69c55f8bb0e840b4f25946d1f05bedc50846967b
Jeremy Faden <jbfaden@users.noreply.github.com>
2011-03-14 17:16:47 +0000

    QFunction briefly had the rule that both rank 1 and rank 2 inputs needed to be supported by the value method, and I quickly found that this was overly burdensome to implementations.  Now rank 2 is supported via the values method, and we introduce AbstractQFunction, which handles values and exampleOutput.  The simplest functions will now just implement value and exampleInput as originally, and when speed is an issue, they may also implement values.
    svn path=/dasCore/community/autoplot2011/trunk/; revision=5281

--- dasCore/src/test/graph/QFunctionLarry.java
+++ dasCore/src/test/graph/QFunctionLarry.java
@@ -6,19 +6,19 @@
 package test.graph;
 
 import org.das2.datum.Units;
+import org.virbo.dataset.AbstractQFunction;
 import org.virbo.dataset.DDataSet;
 import org.virbo.dataset.DataSetOps;
 import org.virbo.dataset.DataSetUtil;
 import org.virbo.dataset.MutablePropertyDataSet;
 import org.virbo.dataset.QDataSet;
-import org.virbo.dataset.QFunction;
 import org.virbo.dsops.Ops;
 
 /**
  *
  * @author jbf
  */
-public class QFunctionLarry implements QFunction {
+public class QFunctionLarry extends AbstractQFunction {
 
 	DDataSet descriptor = DDataSet.createRank2(2,0);
 	{
@@ -34,41 +34,36 @@ public class QFunctionLarry implements QFunction {
 	@Override
 	public QDataSet value(QDataSet parm) {
 
-            //this doesn't support rank 2--don't use this as an example!
+            MutablePropertyDataSet q = (MutablePropertyDataSet) DataSetOps.unbundle(parm, 0);
+            //We're going to do this by hand for now since it's broken
 
-		MutablePropertyDataSet q = (MutablePropertyDataSet) DataSetOps.unbundle(parm, 0);
-		//We're going to do this by hand for now since it's broken
+            Units u= (Units)  parm.property(QDataSet.UNITS,0);
+            if ( u==null ) u= (Units) ((QDataSet)parm.property(QDataSet.BUNDLE_0)).property( QDataSet.UNITS,0 );
+            q.putProperty(QDataSet.UNITS,u);
 
-		Units u= (Units)  parm.property(QDataSet.UNITS,0);
-		if ( u==null ) u= (Units) ((QDataSet)parm.property(QDataSet.BUNDLE_0)).property( QDataSet.UNITS,0 );
-		q.putProperty(QDataSet.UNITS,u);
+            DDataSet ret = DDataSet.createRank1(2);
+            ret.putValue(0,98.12345678);
+            ret.putValue(1,99.12345678);
+            ret.putProperty(QDataSet.BUNDLE_0,descriptor);
 
-		DDataSet ret = DDataSet.createRank1(2);
-		ret.putValue(0,98.12345678);
-		ret.putValue(1,99.12345678);
-		ret.putProperty(QDataSet.BUNDLE_0,descriptor);
+            return ret;
 
-		return ret;
 	}
 
 
 	DDataSet inputDescriptor = DDataSet.createRank2(1,0);
 	{
-		inputDescriptor.putProperty(QDataSet.LABEL,0,"Time");
-		inputDescriptor.putProperty(QDataSet.UNITS,0,Units.t2000);
+            inputDescriptor.putProperty(QDataSet.LABEL, 0, "Time");
+            inputDescriptor.putProperty(QDataSet.UNITS, 0, Units.t2000);
 	}
 
 
 	@Override
 	public QDataSet exampleInput() {
-		QDataSet q = DataSetUtil.asDataSet(0,Units.t2000);
-		MutablePropertyDataSet ret = (MutablePropertyDataSet) Ops.bundle(null,q);
-		ret.putProperty(QDataSet.BUNDLE_0,inputDescriptor);
-		return ret;
+            QDataSet q = DataSetUtil.asDataSet(0,Units.t2000);
+            MutablePropertyDataSet ret = (MutablePropertyDataSet) Ops.bundle(null,q);
+            ret.putProperty(QDataSet.BUNDLE_0,inputDescriptor);
+            return ret;
 	}
-
-        public QDataSet exampleOutput() {
-            return value( exampleInput() );
-        }
 
 }
