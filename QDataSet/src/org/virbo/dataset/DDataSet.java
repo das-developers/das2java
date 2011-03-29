3fbc1081811cecd26af051717b280450264930fd
Jeremy Faden <jbfaden@users.noreply.github.com>
2011-03-29 22:27:16 +0000

    remove DEPEND_0 from the properties that are trivially copied, because we get a warning because it's the wrong length.
    svn path=/autoplot/trunk/QDataSet/; revision=6264

--- QDataSet/src/org/virbo/dataset/DDataSet.java
+++ QDataSet/src/org/virbo/dataset/DDataSet.java
@@ -409,7 +409,9 @@ public final class DDataSet extends ArrayDataSet {
         double[] newback = new double[noff2-noff1];
         System.arraycopy( this.back, noff1, newback, 0, noff2-noff1 );
         DDataSet result= new DDataSet( nrank, end-start, len1, len2, len3, newback );
-        DataSetUtil.putProperties( DataSetUtil.getProperties(this), result );
+        Map<String,Object> props= DataSetUtil.getProperties(this);
+        props.remove(QDataSet.DEPEND_0);
+        DataSetUtil.putProperties( props, result );
         QDataSet dep0= (QDataSet) property(QDataSet.DEPEND_0);
         if ( dep0!=null ) result.putProperty( QDataSet.DEPEND_0, dep0.trim(start, end) );
         return result;
