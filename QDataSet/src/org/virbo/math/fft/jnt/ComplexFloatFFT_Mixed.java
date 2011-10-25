9e57789409076334f635921f6c020f0db8246f4e
Jeremy Faden <jbfaden@users.noreply.github.com>
2011-10-25 03:05:18 +0000

    findbugs DLS_DEAD_LOCAL_STORE
    svn path=/autoplot/trunk/QDataSet/; revision=7845

--- QDataSet/src/org/virbo/math/fft/jnt/ComplexFloatFFT_Mixed.java
+++ QDataSet/src/org/virbo/math/fft/jnt/ComplexFloatFFT_Mixed.java
@@ -190,7 +190,7 @@ public class ComplexFloatFFT_Mixed extends ComplexFloatFFT{
         int m = n / factor;
         int q = n / product;
         int product_1 = product / factor;
-        int jump = (factor - 1) * product_1;
+
         
         float tau = (float)(sign * Math.sqrt(3.0) / 2.0);
         int di = istride * m;
@@ -261,7 +261,7 @@ public class ComplexFloatFFT_Mixed extends ComplexFloatFFT{
         int m = n / factor;
         int q = n / product;
         int p_1 = product / factor;
-        int jump = (factor - 1) * p_1;
+
         
         int i = in0, j = out0;
         int di = istride * m;
@@ -345,7 +345,7 @@ public class ComplexFloatFFT_Mixed extends ComplexFloatFFT{
         int m = n / factor;
         int q = n / product;
         int p_1 = product / factor;
-        int jump = (factor - 1) * p_1;
+
         
         float tau = (float)(Math.sqrt(5.0) / 4.0);
         float sin_2pi_by_5 =  (float)(sign * Math.sin(2.0 * PI / 5.0));
@@ -612,7 +612,7 @@ public class ComplexFloatFFT_Mixed extends ComplexFloatFFT{
         int m = n / factor;
         int q = n / product;
         int p_1 = product / factor;
-        int jump = (factor - 1) * p_1;
+
         
         float c1 = (float)Math.cos(1.0 * 2.0 * PI / 7.0);
         float c2 = (float)Math.cos(2.0 * 2.0 * PI / 7.0);
