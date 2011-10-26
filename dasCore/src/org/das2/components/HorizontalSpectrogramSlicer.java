54a0dd3df4e15f79f5a0098d1ce047ac9efad428
Jeremy Faden <jbfaden@users.noreply.github.com>
2011-10-26 16:16:59 +0000

    findbugs DLS_DEAD_LOCAL_STORE.  showPopup would have failed if called from off the event thread.
    svn path=/dasCore/community/autoplot2011/trunk/; revision=5631

--- dasCore/src/org/das2/components/HorizontalSpectrogramSlicer.java
+++ dasCore/src/org/das2/components/HorizontalSpectrogramSlicer.java
@@ -88,6 +88,7 @@ public class HorizontalSpectrogramSlicer extends DasPlot implements DataPointSel
                     showPopupImpl();
                 }
             };
+            SwingUtilities.invokeLater(r);
         }
     }
     
