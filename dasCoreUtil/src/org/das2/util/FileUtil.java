e125bbc374387af36ce2d113c9a1cceba0ebc4c2
Jeremy Faden <jbfaden@users.noreply.github.com>
2011-11-22 17:49:19 +0000

    add deleteWithinFileTree to support deleting all the .listing files within a FTP tree.
    svn path=/dasCore/community/autoplot2011/trunk/; revision=5768

--- dasCoreUtil/src/org/das2/util/FileUtil.java
+++ dasCoreUtil/src/org/das2/util/FileUtil.java
@@ -48,4 +48,30 @@ public class FileUtil {
         return success;
     }
 
+    /**
+     * deletes all files with the given name, and root, just as "find , -name name -exec rm {} \;" would.
+     * TODO: check links
+     * @throws IllegalArgumentException if it is unable to delete a file
+     * @return true if the operation was successful.
+     */
+    public static boolean deleteWithinFileTree(File root,String name) throws IllegalArgumentException {
+        if (!root.exists()) {
+            return true;
+        }
+        File[] children = root.listFiles();
+        boolean success = true;
+        for (int i = 0; i < children.length; i++) {
+            if (children[i].isDirectory()) {
+                success = success && deleteWithinFileTree(children[i],name);
+            } else {
+                if ( children[i].getName().equals(name) ) {
+                    success = success && children[i].delete();
+                    if (!success) {
+                        throw new IllegalArgumentException("unable to delete file " + children[i]);
+                    }
+                }
+            }
+        }
+        return success;
+    }
 }
