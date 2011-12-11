21e732545aea08c67205ecccecf6a0002bb4acff
Jeremy Faden <jbfaden@users.noreply.github.com>
2011-12-11 13:36:16 +0000

    add javadoc
    svn path=/dasCore/community/autoplot2011/trunk/; revision=5792

--- dasCoreUtil/src/org/das2/util/filesystem/WriteCapability.java
+++ dasCoreUtil/src/org/das2/util/filesystem/WriteCapability.java
@@ -13,7 +13,24 @@ import java.io.OutputStream;
  * @author jbf
  */
 public interface WriteCapability {
+    /**
+     * Get the output stream.
+     * @return
+     * @throws IOException
+     */
     public OutputStream getOutputStream( ) throws IOException;
+
+    /**
+     * Test to see if we can write to this file.
+     * @return
+     * @throws IOException
+     */
     public boolean canWrite() throws IOException;
+
+    /**
+     * delete the file
+     * @return
+     * @throws IOException
+     */
     public boolean delete() throws IOException;
 }
