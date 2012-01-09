50c23dfbd28eb4b5631be2a84b4fd88688ad4676
Jeremy Faden <jbfaden@users.noreply.github.com>
2012-01-09 20:49:37 +0000

    comment out new code, which doesn't work anyway because of mouseAdapter.removeMouseModule, and caused a NullPointerException for me.
    svn path=/dasCore/community/autoplot2011/trunk/; revision=5834

--- dasCore/src/org/das2/graph/SpectrogramRenderer.java
+++ dasCore/src/org/das2/graph/SpectrogramRenderer.java
@@ -680,10 +680,10 @@ public class SpectrogramRenderer extends Renderer implements TableDataSetConsume
 
         if (!"true".equals(DasApplication.getProperty("java.awt.headless", "false"))) {
             //TODO: remove slicers.  Note if two spectrograms, then we'll have problems.
-            DasMouseInputAdapter mouseAdapter = parent.mouseAdapter;
-            mouseAdapter.removeMouseModule( mouseAdapter.getModuleByLabel("Vertical Slice") );
-            mouseAdapter.removeMouseModule( mouseAdapter.getModuleByLabel("Horizontal Slice") );
-            mouseAdapter.removeMouseModule( mouseAdapter.getModuleByLabel("Horizontal Drag Range") );
+            //DasMouseInputAdapter mouseAdapter = parent.mouseAdapter;
+            //mouseAdapter.removeMouseModule( mouseAdapter.getModuleByLabel("Vertical Slice") );
+            //mouseAdapter.removeMouseModule( mouseAdapter.getModuleByLabel("Horizontal Slice") );
+            //mouseAdapter.removeMouseModule( mouseAdapter.getModuleByLabel("Horizontal Drag Range") );
 
         }
     }
