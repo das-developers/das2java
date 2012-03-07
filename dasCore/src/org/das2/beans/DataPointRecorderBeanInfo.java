120dcb81d0d3792b8a6f0173e724a03ea7dffea9
Jeremy Faden <jbfaden@users.noreply.github.com>
2012-03-07 14:55:51 +0000

    add "sorted" to bean properties of DataPointRecorder
    svn path=/dasCore/community/autoplot2011/trunk/; revision=5939

--- dasCore/src/org/das2/beans/DataPointRecorderBeanInfo.java
+++ dasCore/src/org/das2/beans/DataPointRecorderBeanInfo.java
@@ -29,7 +29,8 @@ public class DataPointRecorderBeanInfo extends AccessLevelBeanInfo {
     
     private static final Property[] properties = {   
         new Property("xTagWidth", AccessLevel.DASML, "getXTagWidth", "setXTagWidth", null),        
-        new Property("snapToGrid", AccessLevel.DASML, "isSnapToGrid", "setSnapToGrid", null),        
+        new Property("snapToGrid", AccessLevel.DASML, "isSnapToGrid", "setSnapToGrid", null),
+        new Property("sorted", AccessLevel.DASML, "isSorted", "setSorted", null),
     };
     
     public DataPointRecorderBeanInfo() {
