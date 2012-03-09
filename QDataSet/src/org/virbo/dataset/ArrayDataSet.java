4fe6f9639d02283780326264d7543f1b1b26797f
Jeremy Faden <jbfaden@users.noreply.github.com>
2012-03-09 12:45:29 +0000

    improve javadoc
    svn path=/autoplot/trunk/QDataSet/; revision=9002

--- QDataSet/src/org/virbo/dataset/ArrayDataSet.java
+++ QDataSet/src/org/virbo/dataset/ArrayDataSet.java
@@ -348,10 +348,11 @@ public abstract class ArrayDataSet extends AbstractDataSet implements WritableDa
     }
 
     /**
-     *
+     * Copy to array of specific type.  For example, copy( double.class, ds ) would return a copy
+     * in a DDataSet.
      * @param c  the primitive type to use (e.g. double.class).
-     * @param ds
-     * @return
+     * @param ds the data to copy.
+     * @return ArrayDataSet of specific type.
      */
     public static ArrayDataSet copy( Class c, QDataSet ds ) {
 
