b50d40a16469f89ff71ad2c108b801036d3808d1
Jeremy Faden <jbfaden@users.noreply.github.com>
2012-04-12 22:55:10 +0000

    demo bug half a bin was drawn for the last bin...
    svn path=/dasCore/community/autoplot2011/trunk/; revision=6016

---
+++ dasCore/src/test/graph/SeriesBreakHist.java
@@ -0,0 +1,39 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package test.graph;
+
+import org.das2.datum.Datum;
+import org.das2.graph.DasPlot;
+import org.das2.graph.GraphUtil;
+import org.das2.graph.SeriesRenderer;
+import org.virbo.dataset.ArrayDataSet;
+import org.virbo.dataset.DataSetUtil;
+import org.virbo.dataset.QDataSet;
+import org.virbo.dsops.Ops;
+
+/**
+ *
+ * @author jbf
+ */
+public class SeriesBreakHist extends PlotDemo {
+
+     public static void main( String[] args ) {
+
+        int len0= 10;
+
+        ArrayDataSet y= ArrayDataSet.maybeCopy( Ops.multiply( DataSetUtil.asDataSet(30), Ops.sin(Ops.linspace(0,5*Math.PI,len0) ) ) );
+        y.putProperty(QDataSet.VALID_MIN,-998);
+        y.putValue(len0-2,-999);
+
+        DasPlot p = GraphUtil.visualize(y);
+        SeriesRenderer r= (SeriesRenderer) p.getRenderer(0);
+
+        r.setHistogram(true);
+        //r.setFillToReference(true);
+        r.setReference( Datum.create(-100) );
+    }
+
+}
