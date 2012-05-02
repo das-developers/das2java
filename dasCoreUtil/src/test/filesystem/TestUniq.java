aa394cf31cc0da50e8be0b657429f9b353469590
Jeremy Faden <jbfaden@users.noreply.github.com>
2012-05-02 20:58:01 +0000

    make URIs canonical by adding trailing slash if it is missing.  The caching of filesystems, ensuring just one instance of any folder, was failing (harmlessly) by not identifying file:/foo and file:/foo/.
    svn path=/dasCore/community/autoplot2011/trunk/; revision=6044

---
+++ dasCoreUtil/src/test/filesystem/TestUniq.java
@@ -0,0 +1,24 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package test.filesystem;
+
+import java.net.UnknownHostException;
+import org.das2.util.filesystem.FileSystem;
+import org.das2.util.filesystem.FileSystem.FileSystemOfflineException;
+
+/**
+ *
+ * @author jbf
+ */
+public class TestUniq {
+    public static void main( String[] args ) throws FileSystemOfflineException, UnknownHostException {
+        FileSystem fs1= FileSystem.create("http://sarahandjeremy.net/~jbf/1wire/data/2012");
+        FileSystem fs2= FileSystem.create("http://sarahandjeremy.net/~jbf/1wire/data/2012/");
+
+        System.err.println( fs1==fs2 );
+
+    }
+}
