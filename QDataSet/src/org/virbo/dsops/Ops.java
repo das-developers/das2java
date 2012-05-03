2bc61517f4e890afe07536033f13760312cf2c41
Jeremy Faden <jbfaden@users.noreply.github.com>
2012-05-03 11:11:05 +0000

    add three and four argument bundle
    svn path=/autoplot/trunk/QDataSet/; revision=9497

--- QDataSet/src/org/virbo/dsops/Ops.java
+++ QDataSet/src/org/virbo/dsops/Ops.java
@@ -3712,7 +3712,7 @@ public class Ops {
     /**
      * shorthand for bundling four datasets.  This bundles on the second dimension,
      * unlike join.  This is just like bundle(ds1,ds2), in fact this just calls
-     * bundle( bundle( ds1,ds2 ), ds3 )
+     * bundle( bundle( bundle( ds1,ds2 ), ds3 ), ds4 )
      * @param ds1 rank 1 (for now) dataset or rank 2 bundle dataset
      * @param ds2 rank 1 (for now) dataset
      * @param ds3 rank 1 (for now) dataset
