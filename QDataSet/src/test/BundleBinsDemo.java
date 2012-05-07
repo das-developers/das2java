fc6cdf0b193ce221b146aeadcf11c19685eea6ab
Jeremy Faden <jbfaden@users.noreply.github.com>
2012-05-07 17:22:26 +0000

    whoops.  This was never updated after the update: http://autoplot.org/QDataSet#2011-Apr-13
    svn path=/autoplot/trunk/QDataSet/; revision=9554

--- QDataSet/src/test/BundleBinsDemo.java
+++ QDataSet/src/test/BundleBinsDemo.java
@@ -9,14 +9,19 @@ import java.util.Random;
 import org.das2.datum.Units;
 import org.virbo.dataset.AbstractDataSet;
 import org.virbo.dataset.DDataSet;
+import org.virbo.dataset.DataSetOps;
 import org.virbo.dataset.QDataSet;
 import org.virbo.dsops.Ops;
 
 /**
- *
+ * Demonstrations of the somewhat nasty BundleDataSet, which allows many data types to be encoded into a rank 2 table.
  * @author jbf
  */
 public class BundleBinsDemo {
+    /**
+     * create a bundle of [ rank 2 time bins, rank 2 B-gsm, rank 1 cmps ]
+     * @return
+     */
      public static QDataSet demo1() {
          final DDataSet result= DDataSet.createRank2(10,6);
          Random r= new Random(12345);
@@ -36,7 +41,7 @@ public class BundleBinsDemo {
 
             @Override
             public int length() {
-                return 3;
+                return 6;
             }
 
             @Override
@@ -44,24 +49,46 @@ public class BundleBinsDemo {
                 switch (i) {
                     case 0: return 1;
                     case 1: return 1;
-                    case 2: return 0;
+                    case 2: return 1;
+                    case 3: return 1;
+                    case 4: return 1;
+                    case 5: return 0;
                     default: throw new IllegalArgumentException("bad index");
                 }
             }
 
             @Override
             public Object property(String name, int i) {
-                if ( i==0 ) {
+                if ( QDataSet.NAME.equals(name) ) {
+                    switch (i) {
+                        case 0: return "TimeMin";
+                        case 1: return "TimeMax";
+                        case 2: return "BGSM_X";
+                        case 3: return "BGSM_Y";
+                        case 4: return "BGSM_Z";
+                        case 5: return "Speed";
+                    }
+                }
+                if ( i==0 || i==1 ) {
                     if ( QDataSet.BINS_1.equals(name) ) return "min,max";
                     if ( QDataSet.UNITS.equals(name) ) return Units.us2000;
-                    if ( QDataSet.NAME.equals(name) ) return "TimeBins";
-                } else if ( i==1 ) {
+                    //if ( QDataSet.NAME.equals(name) ) return "TimeBins";
+                    if ( QDataSet.ELEMENT_NAME.equals(name) ) return "TimeBins";
+                    if ( QDataSet.START_INDEX.equals(name) ) return 0;
+
+                } else if ( i==2 || i==3 || i==4 ) {
                     if ( QDataSet.DEPEND_1.equals(name) ) return Ops.labels( new String[] { "B-GSM-X","B-GSM-Y","B-GSM-Z" } );
-                    if ( QDataSet.DEPEND_0.equals(name) ) return "TimeBins";
+                    if ( QDataSet.DEPENDNAME_0.equals(name) ) return "TimeBins";
                     if ( QDataSet.UNITS.equals(name) ) return Units.meters;
-                } else if ( i==2 ) {
-                    if ( QDataSet.DEPEND_0.equals(name) ) return "TimeBins";
+                    //if ( QDataSet.NAME.equals(name) ) return "BGSM";
+                    if ( QDataSet.ELEMENT_NAME.equals(name) ) return "BGSM";
+                    if ( QDataSet.START_INDEX.equals(name) ) return 2;
+
+                } else if ( i==5 ) {
+                    if ( QDataSet.DEPENDNAME_0.equals(name) ) return "TimeBins";
                     if ( QDataSet.UNITS.equals(name) ) return Units.cmps;
+                    //if ( QDataSet.NAME.equals(name) ) return "Speed";
+
                 } else {
                     throw new IllegalArgumentException("bad index");
                 }
@@ -70,11 +97,11 @@ public class BundleBinsDemo {
 
             @Override
             public double value(int i, int i1) {
-                if ( i==0 ) {
+                if ( i==0 || i==1) {
                     return 2;
-                } else if ( i==1 ) {
+                } else if ( i==2 || i==3 || i==4 ) {
                     return 3;
-                } else if ( i==2 ) {
+                } else if ( i==5 ) {
                     throw new IndexOutOfBoundsException("bad index");
                 } else {
                     throw new IndexOutOfBoundsException("bad index");
@@ -85,5 +112,13 @@ public class BundleBinsDemo {
         result.putProperty( QDataSet.BUNDLE_1, bundleDescriptor );
 
         return result;
+     }
+
+     public static void main( String[] args ) {
+         QDataSet ds= demo1();
+         System.err.println( DataSetOps.unbundle( ds, "TimeBins" ) );
+         System.err.println( DataSetOps.unbundle( ds, "BGSM" ) );
+         System.err.println( DataSetOps.unbundle( ds, "Speed" ) );
+
      }
 }
