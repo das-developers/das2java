5ea60000163693ec756fce1e2073ca243a570b79
Jeremy Faden <jbfaden@users.noreply.github.com>
2012-05-08 17:41:42 +0000

    trivial, rearrange to clarify
    svn path=/autoplot/trunk/QStream/; revision=9573

--- QStream/src/org/virbo/qstream/PacketDescriptor.java
+++ QStream/src/org/virbo/qstream/PacketDescriptor.java
@@ -7,8 +7,6 @@ package org.virbo.qstream;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
-import org.virbo.dataset.DataSetUtil;
-import org.virbo.dataset.QDataSet;
 import org.w3c.dom.Element;
 
 /**
@@ -16,19 +14,6 @@ import org.w3c.dom.Element;
  * @author jbf
  */
 public class PacketDescriptor implements Descriptor {
-
-    List<PlaneDescriptor> planes;
-    /**
-     * If true, then slices of the dataset fill each packet.
-     */
-    boolean stream;
-    
-    /**
-     * number of dimensions being streamed.  Zero means all the data is 
-     * found in the first packet (no streaming).  One means a qube is being
-     * streamed.  Two means a this packet is one qube of a higher rank dataset.
-     */
-    int streamRank;
 
     PacketDescriptor() {
         planes = new ArrayList<PlaneDescriptor>();
@@ -42,6 +27,11 @@ public class PacketDescriptor implements Descriptor {
         this.domElement= element;
     }
 
+    boolean stream;
+
+    /**
+     * If true, then slices of the dataset fill each packet.
+     */
     public boolean isStream() {
         return stream;
     }
@@ -50,19 +40,26 @@ public class PacketDescriptor implements Descriptor {
         this.stream = stream;
     }
 
+    int streamRank;
+
     public int streamRank() {
         return streamRank;
     }
     
+    /**
+     * number of dimensions being streamed.  Zero means all the data is
+     * found in the first packet (no streaming).  One means a qube is being
+     * streamed.  Two means a this packet is one qube of a higher rank dataset.
+     */
     public void setStreamRank( int streamRank ) {
         this.streamRank= streamRank;
     }
-    
+
+    List<PlaneDescriptor> planes;
+
     public void addPlane( PlaneDescriptor planeDescriptor ) {
         planes.add(planeDescriptor);
     }
-    
-    private Element domElement;
 
     /**
      * return the list of planes in an unmodifiable list.
@@ -71,6 +68,8 @@ public class PacketDescriptor implements Descriptor {
     public List<PlaneDescriptor> getPlanes() {
         return Collections.unmodifiableList(planes);
     }
+    
+    private Element domElement;
 
     boolean valuesInDescriptor= false;
     
