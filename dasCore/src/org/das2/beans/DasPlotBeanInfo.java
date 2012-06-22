b12cd16876b2eb56feab1eade36985abc94dd343
Jeremy Faden <jbfaden@users.noreply.github.com>
2012-06-22 16:10:07 +0000

    add isotropic property
    svn path=/dasCore/community/autoplot2011/trunk/; revision=6126

--- dasCore/src/org/das2/beans/DasPlotBeanInfo.java
+++ dasCore/src/org/das2/beans/DasPlotBeanInfo.java
@@ -35,6 +35,7 @@ public class DasPlotBeanInfo extends AccessLevelBeanInfo {
         new Property("oversize", AccessLevel.DASML, "isOverSize", "setOverSize", null ),
         new Property("legendPosition", AccessLevel.DASML, "getLegendPosition", "setLegendPosition", null ),
         new Property("setLogLevel", AccessLevel.DASML, "getLogLevel", "setLogLevel", null ),
+        new Property("isotropic", AccessLevel.DASML, "isIsotropic", "setIsotropic", null ),
         new Property("renderers", AccessLevel.DASML, PersistenceLevel.PERSISTENT, "getRenderers", null, "getRenderer", null, null),
         new Property("xAxis", AccessLevel.DASML,  PersistenceLevel.PERSISTENT, "getXAxis", "setXAxis", null),
         new Property("yAxis", AccessLevel.DASML, PersistenceLevel.PERSISTENT, "getYAxis", "setYAxis", null),
