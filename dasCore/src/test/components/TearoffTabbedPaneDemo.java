d9d754feb2c53dc3c5d1c96add1151b9f093087e
Jeremy Faden <jbfaden@users.noreply.github.com>
2012-07-10 14:44:29 +0000

    add buttons to demo that allow test of remove tabs and allow peek at status for debugging.
    svn path=/dasCore/community/autoplot2011/trunk/; revision=6154

--- dasCore/src/test/components/TearoffTabbedPaneDemo.java
+++ dasCore/src/test/components/TearoffTabbedPaneDemo.java
@@ -9,9 +9,12 @@
 package test.components;
 
 import java.awt.Dimension;
+import java.awt.event.ActionEvent;
 import org.das2.components.TearoffTabbedPane;
 import java.awt.Font;
+import javax.swing.AbstractAction;
 import javax.swing.BoxLayout;
+import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
@@ -21,7 +24,7 @@ import javax.swing.JPanel;
  * @author Jeremy
  */
 public class TearoffTabbedPaneDemo {
-    private static JPanel getPanel( final int index ) {
+    private static JPanel getPanel( final TearoffTabbedPane pane, final int index ) {
         final JPanel panel= new JPanel();
         panel.setName( "tab"+index );
         panel.setLayout( new BoxLayout( panel, BoxLayout.Y_AXIS ) );
@@ -32,14 +35,35 @@ public class TearoffTabbedPaneDemo {
         JLabel sublabel= new JLabel( "name="+panel.getName()+" hash="+panel.hashCode() );
         sublabel.setFont( Font.decode( "HELVETICA" ).deriveFont(Font.ITALIC,8.0f) );
         panel.add( sublabel );
+
+        JButton action= new JButton( new AbstractAction("Remove First Tab") {
+            public void actionPerformed(ActionEvent e) {
+                System.err.println("Button "+index);
+                if ( pane.getTabCount()>1 ) {
+                    pane.removeTabAt(1);
+                    if ( pane.getTabCount()==1 ) ((JButton)e.getSource()).setEnabled(false);
+                } else {
+                    ((JButton)e.getSource()).setEnabled(false);
+                }
+            }
+        });
+        panel.add( action );
+
+        JButton peek= new JButton( new AbstractAction("Peek (see stderr)") {
+            public void actionPerformed(ActionEvent e) {
+                pane.peek();
+            }
+        });
+        panel.add( peek );
+
         return panel;
     }
 
     public static void main( String[] args ) {
         TearoffTabbedPane pane= new TearoffTabbedPane();
-        pane.addTab( "firstTab", getPanel(1) );
-        pane.addTab( "secondTab", getPanel(2) );
-        pane.addTab( "thirdTab", getPanel(3) );
+        pane.addTab( "firstTab", getPanel(pane,1) );
+        pane.addTab( "secondTab", getPanel(pane,2) );
+        pane.addTab( "thirdTab", getPanel(pane,3) );
 
         JFrame frame= new JFrame();
         frame.getContentPane().add( pane );
