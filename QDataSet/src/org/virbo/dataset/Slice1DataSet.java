347ac15c95d43c40d7530550d43026d8404d4975
Jeremy Faden <jbfaden@users.noreply.github.com>
2012-07-17 16:00:12 +0000

    remove old message about "use unbundle instead"
    svn path=/autoplot/trunk/QDataSet/; revision=10017

--- QDataSet/src/org/virbo/dataset/Slice1DataSet.java
+++ QDataSet/src/org/virbo/dataset/Slice1DataSet.java
@@ -48,9 +48,9 @@ public class Slice1DataSet extends AbstractDataSet {
         QDataSet dep1= (QDataSet) ds.property(QDataSet.DEPEND_1);
 
         QDataSet bundle1= (QDataSet)ds.property(QDataSet.BUNDLE_1);
-        if ( bundle1!=null && !unbundle ) {
-            System.err.println("we're not going to do this correctly, use unbundle instead");
-        }
+//        if ( bundle1!=null && !unbundle ) {
+//            System.err.println("we're not going to do this correctly, use unbundle instead");
+//        }
         String label=null;
         Units dep1units= dep1==null ? null : (Units) dep1.property(QDataSet.UNITS);//TODO stupid hurry, need to drive Sarah to work
         if ( dep1!=null && dep1.rank()==1 && ( dep1units instanceof EnumerationUnits ) ) { // check for legacy bundle where DEPEND_1 is labels.
