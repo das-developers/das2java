d5fbe3d4887f2923606426cd31a918cd90a8753e
Jeremy Faden <jbfaden@users.noreply.github.com>
2012-09-14 11:30:24 +0000

    read orbits in a try-finally block
    svn path=/dasCore/community/autoplot2011/trunk/; revision=6281

--- dasCoreDatum/src/org/das2/datum/Orbits.java
+++ dasCoreDatum/src/org/das2/datum/Orbits.java
@@ -18,6 +18,8 @@ import java.util.Iterator;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import org.das2.datum.TimeUtil.TimeStruct;
 
 /**
@@ -32,6 +34,8 @@ import org.das2.datum.TimeUtil.TimeStruct;
  * @author jbf
  */
 public class Orbits {
+
+    private static final Logger logger= Logger.getLogger("das2.datum.orbits");
 
     private String sc;
 
@@ -68,6 +72,7 @@ public class Orbits {
 
         InputStream in;
         try {
+            logger.log(Level.FINE, "Orbits trying to connect to {0}", url);
             URLConnection connect= url.openConnection();
             connect.setConnectTimeout(3000);
             in= connect.getInputStream();
@@ -89,59 +94,64 @@ public class Orbits {
         LinkedHashMap<String,DatumRange> result= new LinkedHashMap();
 
         BufferedReader rin= new BufferedReader( new InputStreamReader( in ) );
-
-        String s= rin.readLine();
-        int col= -1; // the first time column, 0 is the first column.
-        while ( s!=null ) {
-            String[] ss= s.trim().split("\\s+");
-            if ( ss.length==3 && ( ss[1].startsWith("1") || ss[1].startsWith("2") ) ) { // quick checks
-                Datum d1;
-                Datum d2;
-                String s0;
-                try {
-                    if ( col>-1 ) {
+        try {
+            String s= rin.readLine();
+            int col= -1; // the first time column, 0 is the first column.
+            while ( s!=null ) {
+                String[] ss= s.trim().split("\\s+");
+                if ( ss.length==3 && ( ss[1].startsWith("1") || ss[1].startsWith("2") ) ) { // quick checks
+                    Datum d1;
+                    Datum d2;
+                    String s0;
+                    try {
+                        if ( col>-1 ) {
+                            try {
+                                d1= TimeUtil.create(ss[col]);
+                                d2= TimeUtil.create(ss[col+1]);
+                                s0= ss[ col==0 ? 2 : 0 ];
+                            } catch ( ParseException ex ) {
+                                s= rin.readLine();
+                                continue;
+                            }
+                        } else {
+                            if ( ss[0].length()>4 ) {
+                                d1= TimeUtil.create(ss[0]);
+                                d2= TimeUtil.create(ss[1]);
+                            } else {
+                                throw new ParseException("time is too short",0); // nasty way to jump over to other branch
+                            }
+                            s0= ss[2];
+                            col= 0;
+                        }
+                    } catch ( ParseException ex ) {
                         try {
-                            d1= TimeUtil.create(ss[col]);
-                            d2= TimeUtil.create(ss[col+1]);
-                            s0= ss[ col==0 ? 2 : 0 ];
-                        } catch ( ParseException ex ) {
+                            d1= TimeUtil.create(ss[1]);
+                            d2= TimeUtil.create(ss[2]);
+                            s0= ss[0];
+                            col= 1;
+                        } catch ( ParseException ex1 ) {
                             s= rin.readLine();
                             continue;
                         }
+                    }
+                    if ( d1.gt(d2) ) {
+                        System.err.println("dropping invalid orbit: "+s );
                     } else {
-                        if ( ss[0].length()>4 ) {
-                            d1= TimeUtil.create(ss[0]);
-                            d2= TimeUtil.create(ss[1]);
-                        } else {
-                            throw new ParseException("time is too short",0); // nasty way to jump over to other branch
+                        try {
+                            result.put( trimOrbit(s0), new DatumRange(d1,d2) );
+                        } catch ( IllegalArgumentException ex ) {
+                            System.err.println(ex.getMessage()+": " +s );
                         }
-                        s0= ss[2];
-                        col= 0;
-                    }
-                } catch ( ParseException ex ) {
-                    try {
-                        d1= TimeUtil.create(ss[1]);
-                        d2= TimeUtil.create(ss[2]);
-                        s0= ss[0];
-                        col= 1;
-                    } catch ( ParseException ex1 ) {
-                        s= rin.readLine();
-                        continue;
                     }
                 }
-                if ( d1.gt(d2) ) {
-                    System.err.println("dropping invalid orbit: "+s );
-                } else {
-                    try {
-                        result.put( trimOrbit(s0), new DatumRange(d1,d2) );
-                    } catch ( IllegalArgumentException ex ) {
-                        System.err.println(ex.getMessage()+": " +s );
-                    }
-                }
+                s= rin.readLine();
             }
-            s= rin.readLine();
+            logger.log(Level.FINE, "read orbits for {0}", sc);
+        } finally {
+            rin.close();
+            logger.log(Level.FINE, "read orbits for {0}", sc);
         }
-        rin.close();
+        
         return result;
 
     }
