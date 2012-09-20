62f08062f3e9d8256bedbf46006296bb48893c75
Jeremy Faden <jbfaden@users.noreply.github.com>
2012-09-20 14:05:38 +0000

    add central place to keep track of loggers.
    svn path=/dasCore/community/autoplot2011/trunk/; revision=6316

---
+++ dasCoreUtil/src/org/das2/util/LoggerManager.java
@@ -0,0 +1,37 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package org.das2.util;
+
+import java.util.HashSet;
+import java.util.Set;
+import java.util.logging.Logger;
+
+/**
+ * Central place that keeps track of loggers.
+ * @author jbf
+ */
+public final class LoggerManager {
+
+    private static Set loggers= new HashSet();
+
+    /**
+     * return the requested logger, but add it to the list of known loggers.
+     * @param id
+     * @return
+     */
+    public static Logger getLogger( String id ) {
+        loggers.add(id);
+        return Logger.getLogger(id);
+    }
+
+    /**
+     * return the list of known loggers.
+     * @return
+     */
+    public static Set getLoggers() {
+        return loggers;
+    }
+}
