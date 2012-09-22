2878a435a04469b7d45a1a9f8845291f5b2ea077
Jeremy Faden <jbfaden@users.noreply.github.com>
2012-09-22 11:33:24 +0000

    implement comments enough that task progress messages can be carried.  Somehow they never made it into the spec.
    svn path=/autoplot/trunk/QStream/; revision=10552

--- QStream/src/org/virbo/qstream/StreamComment.java
+++ QStream/src/org/virbo/qstream/StreamComment.java
@@ -8,17 +8,59 @@ package org.virbo.qstream;
 import org.w3c.dom.Element;
 
 /**
- *
+ * StreamComment allows comments to be put onto the stream.
+ * TODO: consider that comments should be sourced by the stream producer, but what about comments from filters?
+ *   That is, if I'm a filter and I produce comments, should I throw out the comments I receive?
  * @author jbf
  */
 public class StreamComment implements Descriptor {
+
+    /**
+     * task progress comments are either of the form:
+     *   [xx]000000<comment type='taskProgress' message='0 of 100'>   or
+     *   [xx]000000<comment type='taskProgress' message='0 of -1'> for indeterminate
+     * These are currently unimplemented!
+     */
+    public final String TYPE_TASK_PROGRESS="taskProgress";
+
+    /**
+     * log comments are of the form:
+     *   [xx]000000<comment type='log:FINE' message='calc fine process'>   or
+     *   [xx]000000<comment type='log:INFO' message='reading calibration'>
+     * Note the log level should be requested by the client.
+     */
+    public final String TYPE_LOG="log:(.*)";
+
     private Element element;
     
     StreamComment( Element element ) {
+        this.type= element.getAttribute("type");
+        this.message= element.getAttribute("message");
         this.element= element;
     }
 
     public Element getDomElement() {
         return element;
     }
+
+    private String type;
+
+    String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    private String message;
+
+    String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
 }
