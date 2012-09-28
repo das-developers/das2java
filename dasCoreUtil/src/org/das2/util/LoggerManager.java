49853242ed4528b3dc2aaa730edb845efda12be0
Jeremy Faden <jbfaden@users.noreply.github.com>
2012-09-28 12:11:10 +0000

    bugfix: keep track of all the loggers created here, after I was getting two loggers for the same ID, and setting the level on one did not affect the other...
    svn path=/dasCore/community/autoplot2011/trunk/; revision=6335

--- dasCoreUtil/src/org/das2/util/LoggerManager.java
+++ dasCoreUtil/src/org/das2/util/LoggerManager.java
@@ -5,7 +5,9 @@
 
 package org.das2.util;
 
+import java.util.HashMap;
 import java.util.HashSet;
+import java.util.Map;
 import java.util.Set;
 import java.util.logging.Logger;
 
@@ -16,13 +18,21 @@ import java.util.logging.Logger;
 public final class LoggerManager {
 
     private static Set loggers= new HashSet();
+    private static Map<String,Logger> log= new HashMap();
 
     /**
      * return the requested logger, but add it to the list of known loggers.
      * @param id
      * @return
      */
-    public static Logger getLogger( String id ) {
+    public synchronized static Logger getLogger( String id ) {
+        Logger result= log.get(id);
+        if ( result!=null ) {
+            return result;
+        } else {
+            result= Logger.getLogger(id);
+            log.put( id, result );
+        }
         loggers.add(id);
         return Logger.getLogger(id);
     }
