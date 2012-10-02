f30425d83266933a6f95b5b47d59db983a8136ef
Jeremy Faden <jbfaden@users.noreply.github.com>
2012-10-02 22:08:57 +0000

    toString supports BINS_1= min,max.  Odd that this wasn't supported.
    svn path=/autoplot/trunk/QDataSet/; revision=10680

--- QDataSet/src/org/virbo/dataset/DataSetUtil.java
+++ QDataSet/src/org/virbo/dataset/DataSetUtil.java
@@ -620,6 +620,15 @@ public class DataSetUtil {
             return dr1.toString() + "; "+ dr2.toString() + "  (inclusive)";
         }
 
+        if ( ds.rank()==2 && ds.length()==2 && ds.length(0)==2 && "min,max".equals(ds.property( QDataSet.BINS_1) ) ) {
+            Units u1= (Units) ds.property(QDataSet.UNITS,0);
+            Units u2= (Units) ds.property(QDataSet.UNITS,1);
+
+            DatumRange dr1= new DatumRange( ds.value(0,0), ds.value(0,1), u1==null ? Units.dimensionless : u1 );
+            DatumRange dr2= new DatumRange( ds.value(1,0), ds.value(1,1), u2==null ? Units.dimensionless : u2 );
+            return dr1.toString() + "; "+ dr2.toString();
+        }
+
         String qubeStr = DataSetUtil.isQube(ds) ? "" : "*";
 
         String[] depNames = new String[4];
