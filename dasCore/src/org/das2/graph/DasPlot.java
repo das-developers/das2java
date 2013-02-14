f49b6cd2a4f02b12a0d7558b1a0529e11e41cc44
Jeremy Faden <jbfaden@users.noreply.github.com>
2013-02-14 16:49:21 +0000

    study bug 3599689:  agg sometimes not loading when two on same plot
    svn path=/dasCore/community/autoplot2011/trunk/; revision=6533

--- dasCore/src/org/das2/graph/DasPlot.java
+++ dasCore/src/org/das2/graph/DasPlot.java
@@ -1638,7 +1638,7 @@ public class DasPlot extends DasCanvasComponent {
     protected void invalidateCacheImage() {
         if ( cacheImageValid==false ) return;
         cacheImageValid = false;
-        repaint();
+        update();
     }
 
     @Override
