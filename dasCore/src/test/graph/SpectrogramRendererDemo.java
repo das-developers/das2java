c110d15c790986ac1b93167d2b7113fed1d49bf6
Jeremy Faden <jbfaden@users.noreply.github.com>
2013-02-15 18:12:09 +0000

    add demo for spectrograms, used to test initial coding of new NN rebinner.
    svn path=/dasCore/community/autoplot2011/trunk/; revision=6539

---
+++ dasCore/src/test/graph/SpectrogramRendererDemo.java
@@ -0,0 +1,39 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package test.graph;
+
+import javax.swing.JFrame;
+import org.das2.datum.Datum;
+import org.das2.datum.DatumRange;
+import org.das2.datum.Units;
+import org.das2.graph.DasColorBar;
+import org.das2.graph.SpectrogramRenderer;
+import org.virbo.dsops.Ops;
+
+/**
+ *
+ * @author jbf
+ */
+public class SpectrogramRendererDemo extends PlotDemo {
+    SpectrogramRendererDemo() {
+        super();
+
+        DasColorBar cb= new DasColorBar( Datum.create(0), Datum.create(1), false );
+        SpectrogramRenderer rend= new SpectrogramRenderer(null,cb);
+        //rend.setRebinner( SpectrogramRenderer.RebinnerEnum.lanlNN );
+        rend.setDataSet( Ops.ripples(20,20) );
+
+        plot.addRenderer( rend );
+        plot.getColumn().setEmMaximum(-10);
+        plot.getXAxis().setDatumRange( DatumRange.newDatumRange(0,20,Units.dimensionless) );
+
+    }
+
+    public static void main( String[] args ) {
+        JFrame frame= new SpectrogramRendererDemo().showFrame();
+        frame.setSize( 800,800 );
+    }
+}
