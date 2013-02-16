75e5d64958d40049b342b03b2797e51368ff72f6
Jeremy Faden <jbfaden@users.noreply.github.com>
2013-02-16 12:08:08 +0000

    fix this, it was probably never working
    svn path=/dasCore/community/autoplot2011/trunk/; revision=6542

--- dasCore/src/test/graph/RGBImageDemo.java
+++ dasCore/src/test/graph/RGBImageDemo.java
@@ -5,18 +5,11 @@
 
 package test.graph;
 
-import java.awt.BorderLayout;
 import javax.swing.JFrame;
-import javax.swing.JPanel;
-import org.das2.graph.RGBImageRenderer;
 import org.das2.datum.DatumRange;
 import org.das2.datum.Units;
-import org.das2.graph.DasCanvas;
-import org.das2.graph.DasPlot;
-import org.das2.graph.GraphUtil;
+import org.das2.graph.RGBImageRenderer;
 import org.das2.graph.Renderer;
-import org.virbo.dataset.DataSetUtil;
-import org.virbo.dataset.MutablePropertyDataSet;
 import org.virbo.dataset.QDataSet;
 import org.virbo.dataset.SDataSet;
 import org.virbo.dsops.Ops;
@@ -26,46 +19,11 @@ import org.virbo.dsops.Ops;
  * @author jbf
  */
 public class RGBImageDemo extends PlotDemo {
-    JPanel contentPane;
-
-    private synchronized JPanel getContentPane() {
-        if (contentPane == null) {
-            contentPane = new JPanel();
-        }
-        return contentPane;
-    }
-
-    public void showFrame() {
-        JFrame frame= new JFrame( "Axis Demo");
-        frame.getContentPane().add(getContentPane());
-        frame.pack();
-        frame.setDefaultCloseOperation( JFrame.EXIT_ON_CLOSE );
-        frame.setVisible(true);
-    }
 
     public RGBImageDemo() {
-        int width = 500;
-        int height = 500;
-
-        getContentPane().setLayout(new BorderLayout());
-
-        DasCanvas canvas = new DasCanvas(width, height);
-
-        getContentPane().add(canvas, BorderLayout.CENTER );
-
-        DatumRange xrange= DatumRange.newDatumRange(-100,400,Units.dimensionless);
-        DatumRange yrange= DatumRange.newDatumRange(-100,400,Units.dimensionless);
-
-        DasPlot plot= GraphUtil.newDasPlot(canvas, xrange, yrange);
-
-        plot.getXAxis().setUseDomainDivider(true);
-        plot.getYAxis().setUseDomainDivider(true);
+        super();
 
         Renderer r= new RGBImageRenderer();
-
-        QDataSet red= Ops.multiply( DataSetUtil.asDataSet(255), Ops.rand(200,200) );
-        QDataSet green= Ops.multiply( DataSetUtil.asDataSet(0), Ops.rand(200,200) );
-        QDataSet blue= Ops.multiply( DataSetUtil.asDataSet(0), Ops.rand(200,200) );
 
         SDataSet rgb= SDataSet.createRank3( 200,200,3 );
         for ( int i=0; i<rgb.length(); i++ ) {
@@ -78,12 +36,13 @@ public class RGBImageDemo extends PlotDemo {
         rgb.putProperty( QDataSet.DEPEND_2, Ops.labels( new String[] { "b", "g", "r" } ) );
         r.setDataSet( rgb );
         //r.setDataSet( Ops.multiply( DataSetUtil.asDataSet(255), Ops.rand(300,300) ) );
-
+        plot.getXAxis().setDatumRange( DatumRange.newDatumRange(0,20,Units.dimensionless) );
         plot.addRenderer( r );
         
     }
 
     public static void main( String[] args ) {
-        new RGBImageDemo().showFrame();
+        JFrame frame= new RGBImageDemo().showFrame();
+        frame.setSize( 800,800 );
     }
 }
