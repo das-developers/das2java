201cee21686ddfe8556a1898808876e42eeb3dac
Jeremy Faden <jbfaden@users.noreply.github.com>
2013-06-25 13:51:53 +0000

    implement native slice and trim.
    svn path=/autoplot/trunk/QDataSet/; revision=12089

--- QDataSet/src/org/virbo/dataset/TagGenDataSet.java
+++ QDataSet/src/org/virbo/dataset/TagGenDataSet.java
@@ -12,12 +12,16 @@ package org.virbo.dataset;
 import org.das2.datum.Units;
 
 /**
- *
+ * return a value based on the scale and offset.  For example, this
+ * is used to create timetags:
+ *   TagGenDataSet( 1440, 60, 0, Units.t2000 )
+ * These are the 1440 minutely timetags in the first day of 2000-01-01.
  * @author jbf
  */
 public class TagGenDataSet extends IndexGenDataSet {
     
     double scale, offset;
+    Units units;
 
     public TagGenDataSet( int length, double scale, double offset ) {
         this( length, scale, offset, null );
@@ -30,11 +34,25 @@ public class TagGenDataSet extends IndexGenDataSet {
         if ( units!=null ) {
             putProperty( QDataSet.CADENCE, DRank0DataSet.create(scale, units.getOffsetUnits() ) );
             putProperty( QDataSet.UNITS, units );
+            this.units= units;
+        } else {
+            this.units= Units.dimensionless;
         }
     }
     
+    @Override
     public double value(int i) {
-        return i * scale + offset;
+        return i * scale + offset; //TODO: check numerical stability, this is an extrapolation.
     }
 
+    @Override
+    public QDataSet trim(int start, int end) {
+        return new TagGenDataSet( end-start, scale, offset + start*scale );
+    }
+
+    @Override
+    public QDataSet slice(int i) {
+        return DRank0DataSet.create( i * scale + offset, units );
+    }
+    
 }
