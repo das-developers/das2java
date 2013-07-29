d9325e389ff44780b7be77ec123fa1b05b55aec3
Jeremy Faden <jbfaden@users.noreply.github.com>
2013-07-29 16:11:19 +0000

    add fourth pane for testing
    svn path=/dasCore/community/autoplot2011/trunk/; revision=6871

--- dasCore/src/test/components/TearoffTabbedPaneDemo.java
+++ dasCore/src/test/components/TearoffTabbedPaneDemo.java
@@ -12,6 +12,10 @@ import java.awt.Dimension;
 import java.awt.event.ActionEvent;
 import org.das2.components.TearoffTabbedPane;
 import java.awt.Font;
+import java.util.logging.ConsoleHandler;
+import java.util.logging.Handler;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.swing.AbstractAction;
 import javax.swing.BoxLayout;
 import javax.swing.JButton;
@@ -60,13 +64,21 @@ public class TearoffTabbedPaneDemo {
     }
 
     public static void main( String[] args ) {
+        Logger.getLogger("das2.gui").setLevel(Level.FINE);
+        ConsoleHandler h= new ConsoleHandler();
+        h.setLevel(Level.ALL);
+        Logger.getLogger("das2.gui").addHandler(h);
+        
         TearoffTabbedPane pane= new TearoffTabbedPane();
         pane.addTab( "firstTab", getPanel(pane,1) );
         pane.addTab( "secondTab", getPanel(pane,2) );
         pane.addTab( "thirdTab", getPanel(pane,3) );
+        pane.addTab( "fourthTab", getPanel(pane,4) );
 
         JFrame frame= new JFrame();
+        frame.setName("MainFrame");
         frame.getContentPane().add( pane );
+        frame.setTitle("MainFrame");
         pane.setPreferredSize( new Dimension(600,400) );
         frame.pack();
         frame.setVisible(true);
