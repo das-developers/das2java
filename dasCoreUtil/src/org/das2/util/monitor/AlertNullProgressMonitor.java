0ffa73ad02f3f409d822f6f275ee6a62b8fab330
Jeremy Faden <jbfaden@users.noreply.github.com>
2013-08-02 12:32:48 +0000

    add AlertNullProgressMonitor
    svn path=/dasCore/community/autoplot2011/trunk/; revision=6882

---
+++ dasCoreUtil/src/org/das2/util/monitor/AlertNullProgressMonitor.java
@@ -0,0 +1,29 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.das2.util.monitor;
+
+/**
+ * Like the NullProgressMonitor, but print to stderr when the task is
+ * taking a non-trivial amount of time.  The NullProgressMonitor is used
+ * when the developer thinks that a task is trivial, so this should be used
+ * to verify.
+ * @author jbf
+ */
+public class AlertNullProgressMonitor extends NullProgressMonitor {
+    long t0= System.currentTimeMillis();
+    long lastAlert= 0;
+
+    @Override
+    public void setTaskProgress(long position) throws IllegalArgumentException {
+        super.setTaskProgress(position); //To change body of generated methods, choose Tools | Templates.
+        long t= System.currentTimeMillis();
+        if ( ( t-t0 > 1000 ) && ( t-lastAlert > 250 ) ) {
+            System.err.print( String.format( "%s: %d of %d...",this.getLabel(),this.getTaskProgress(),this.getTaskSize()) );
+            lastAlert= t;
+        }
+    }
+    
+    
+}
