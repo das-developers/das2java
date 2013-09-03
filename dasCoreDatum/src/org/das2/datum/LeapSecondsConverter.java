c53d78ac23a0a54a40b4933883973a2bb5bc1fdf
Jeremy Faden <jbfaden@users.noreply.github.com>
2013-09-03 16:41:39 +0000

    findbugs OS_OPEN_STREAM
    svn path=/dasCore/community/autoplot2011/trunk/; revision=7012

--- dasCoreDatum/src/org/das2/datum/LeapSecondsConverter.java
+++ dasCoreDatum/src/org/das2/datum/LeapSecondsConverter.java
@@ -50,35 +50,39 @@ public class LeapSecondsConverter extends UnitsConverter {
         }
         
         BufferedReader r = new BufferedReader(new InputStreamReader(in));
-        String s = "";
-        leapSeconds = new ArrayList(50);
-        withoutLeapSeconds = new ArrayList(50);
-        String lastLine= s;
-        while (s != null) {
-            s = r.readLine();
-            if (s == null) {
-                logger.log( Level.FINE, "Last leap second read from {0} {1}", new Object[]{url, lastLine});
-                continue;
+        try {
+            String s = "";
+            leapSeconds = new ArrayList(50);
+            withoutLeapSeconds = new ArrayList(50);
+            String lastLine= s;
+            while ( s != null ) {  //netbeans asserts this is never null--which is not correct.  See 4 lines down...
+                s = r.readLine();
+                if (s == null) {
+                    logger.log( Level.FINE, "Last leap second read from {0} {1}", new Object[]{url, lastLine});
+                    continue;
+                }
+                if (s.startsWith(";")) {
+                    continue;
+                }
+                String[] ss = s.trim().split("\\s+", -2);
+                if (ss[0].compareTo("1972") < 0) {
+                    continue;
+                }
+                int iyear = Integer.parseInt(ss[0]);
+                int imonth = Integer.parseInt(ss[1]);
+                int iday = Integer.parseInt(ss[2]);
+                int ileap = (int) (Double.parseDouble(ss[3])); // I thought these could only be whole numbers
+                double us2000 = TimeUtil.createTimeDatum(iyear, imonth, iday, 0, 0, 0, 0).doubleValue(Units.us2000);
+                leapSeconds.add( Long.valueOf( ((long) us2000) * 1000L - 43200000000000L + (long) ( ileap-32 ) * 1000000000) );
+                withoutLeapSeconds.add( us2000 );
             }
-            if (s.startsWith(";")) {
-                continue;
-            }
-            String[] ss = s.trim().split("\\s+", -2);
-            if (ss[0].compareTo("1972") < 0) {
-                continue;
-            }
-            int iyear = Integer.parseInt(ss[0]);
-            int imonth = Integer.parseInt(ss[1]);
-            int iday = Integer.parseInt(ss[2]);
-            int ileap = (int) (Double.parseDouble(ss[3])); // I thought these could only be whole numbers
-            double us2000 = TimeUtil.createTimeDatum(iyear, imonth, iday, 0, 0, 0, 0).doubleValue(Units.us2000);
-            leapSeconds.add( Long.valueOf( ((long) us2000) * 1000L - 43200000000000L + (long) ( ileap-32 ) * 1000000000) );
-            withoutLeapSeconds.add( us2000 );
-        }
-        leapSeconds.add( Long.MAX_VALUE );
-        withoutLeapSeconds.add( Double.MAX_VALUE );
+            leapSeconds.add( Long.MAX_VALUE );
+            withoutLeapSeconds.add( Double.MAX_VALUE );
 
-        lastUpdateMillis = System.currentTimeMillis();
+            lastUpdateMillis = System.currentTimeMillis();
+        } finally {
+            r.close();
+        }
     }
 
     boolean us2000ToTT2000;
