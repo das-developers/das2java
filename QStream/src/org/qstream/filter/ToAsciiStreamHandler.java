9e83ffd362a42af0ccfbbb22fcbccd09fa2edb05
Jeremy Faden <jbfaden@users.noreply.github.com>
2013-10-23 01:48:43 +0000

    first sort-of working version.  This doesn't consider the types coming in, everything is encoded as ascii10.  This needs to look at the types, and pick appropriate output types.  Also, this needs to handle multiple packetDescriptors.
    svn path=/autoplot/trunk/QStream/; revision=13048

--- QStream/src/org/qstream/filter/ToAsciiStreamHandler.java
+++ QStream/src/org/qstream/filter/ToAsciiStreamHandler.java
@@ -8,12 +8,16 @@ import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.InputStream;
-import java.io.UnsupportedEncodingException;
 import java.nio.ByteBuffer;
 import java.nio.channels.Channels;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.xpath.XPath;
+import javax.xml.xpath.XPathConstants;
+import javax.xml.xpath.XPathExpression;
+import javax.xml.xpath.XPathExpressionException;
+import javax.xml.xpath.XPathFactory;
 import org.virbo.qstream.AsciiTransferType;
 import org.virbo.qstream.FormatStreamHandler;
 import org.virbo.qstream.PacketDescriptor;
@@ -23,6 +27,8 @@ import org.virbo.qstream.StreamDescriptor;
 import org.virbo.qstream.StreamException;
 import org.virbo.qstream.StreamHandler;
 import org.virbo.qstream.StreamTool;
+import org.w3c.dom.Element;
+import org.w3c.dom.NodeList;
 
 /**
  * This is not complete.  It needs to be finished off.
@@ -60,9 +66,25 @@ public class ToAsciiStreamHandler implements StreamHandler {
             for ( PlaneDescriptor p: pdout.getPlanes() ) {
                 p.setType( new AsciiTransferType(10,true) );
                 newPacketSize+= 10 * p.getElements();
-                //newPacketSize+= p.getElements() * p.getType().sizeBytes();
             }
             sdout.addDescriptor(pdout);
+            try {
+                XPath xpath= XPathFactory.newInstance().newXPath();
+                Element e = (Element)pd.getDomElement();
+                XPathExpression expr = xpath.compile("/packet/qdataset/values");
+                Object o = expr.evaluate(e, XPathConstants.NODESET);
+                NodeList nodes = (NodeList) o;
+
+                for ( int i=0; i<nodes.getLength(); i++ ) {
+                        Element n= (Element)nodes.item(i);
+                        if ( n.hasAttribute("encoding") ) {
+                            n.setAttribute("encoding","ascii10");
+                        }
+                }
+                pdout.setDomElement( e );
+            } catch ( XPathExpressionException ex ) {
+                
+            }
             format.packetDescriptor(pdout);
             this.pdout= pdout;
         } catch (CloneNotSupportedException ex) {
@@ -74,13 +96,22 @@ public class ToAsciiStreamHandler implements StreamHandler {
     public void packet(PacketDescriptor pd, ByteBuffer data) throws StreamException {
         ByteBuffer dataOut= ByteBuffer.allocate(newPacketSize);
         data.flip();
-        for ( int ip=0; ip<pd.getPlanes().size(); ip++ ) {
+        int np= pd.getPlanes().size();
+        for ( int ip=0; ip<np; ip++ ) {
             PlaneDescriptor pout= pdout.getPlanes().get(ip);
             PlaneDescriptor pin= pd.getPlanes().get(ip);
             data.limit( data.limit() + pin.getElements() * pin.getType().sizeBytes() );
             for ( int i=0; i<pin.getElements(); i++ ) {
                 double d= pin.getType().read(data);
-                dataOut.put( String.format( "f10.3", d ).getBytes() );
+                if ( ip==np-1 && i==pin.getElements()-1 ) {
+                    String s= String.format( "%9.3f", d );
+                    dataOut.put( s.substring(0,9).getBytes() );
+                    dataOut.put( (byte)'\n' );
+                } else {
+                    String s= String.format( "%9.3f", d );
+                    dataOut.put( s.substring(0,9).getBytes() );
+                    dataOut.put( (byte)' ' );
+                }
             }            
         }
         dataOut.flip();
@@ -103,7 +134,7 @@ public class ToAsciiStreamHandler implements StreamHandler {
     }
     
     public static void main( String[] args ) throws FileNotFoundException, StreamException {
-        InputStream in= new FileInputStream("/home/jbf/project/autoplot/autoplot-code/QStream/src/test/binary.qds");
+        InputStream in= new FileInputStream("/Users/jbf/NetBeansProjects/autoplot/QStream/src/test/binary.qds");
         StreamTool st= new StreamTool();
         StreamHandler sink= new ToAsciiStreamHandler();
         st.readStream( Channels.newChannel(in), sink );
