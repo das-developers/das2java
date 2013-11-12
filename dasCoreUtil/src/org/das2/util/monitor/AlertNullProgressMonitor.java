5e38cf286cfd74e1830c0b8df867b8390ce7b923
Jeremy Faden <jbfaden@users.noreply.github.com>
2013-11-12 13:39:27 +0000

    clean up AlertNullProgressMonitor.  Now it indicates the source if it doesn't have a label.
    svn path=/dasCore/community/autoplot2011/trunk/; revision=7265

--- dasCoreUtil/src/org/das2/util/monitor/AlertNullProgressMonitor.java
+++ dasCoreUtil/src/org/das2/util/monitor/AlertNullProgressMonitor.java
@@ -15,13 +15,25 @@ public class AlertNullProgressMonitor extends NullProgressMonitor {
     long t0= System.currentTimeMillis();
     long lastAlert= 0;
 
+    public AlertNullProgressMonitor() {
+    }
+    
+    public AlertNullProgressMonitor(String label) {
+        this();
+        this.setLabel(label);
+    }
+    
     @Override
     public void setTaskProgress(long position) throws IllegalArgumentException {
         super.setTaskProgress(position); //To change body of generated methods, choose Tools | Templates.
         long t= System.currentTimeMillis();
-        if ( ( t-t0 > 1000 ) && ( t-lastAlert > 250 ) ) {
+        if ( ( t-t0 > 1000 ) && ( t-lastAlert > 500 ) ) {
             //TODO: on 20130923, this was failing to output on either the console or the Netbeans stderr console.
-            System.err.print( String.format( "%s: %d of %d...",this.getLabel(),this.getTaskProgress(),this.getTaskSize()) );
+            System.err.println( String.format( "%s: %d of %d... (trivial task is taking longer than expected)",this.getLabel(),this.getTaskProgress(),this.getTaskSize()) );
+            if ( this.getLabel()==null ) {
+                StackTraceElement[] sts= new Exception("getStackTrace").getStackTrace();
+                System.err.println( sts[1] );
+            }
             lastAlert= t;
         }
     }
