7906577578da57a218baaacdf69f310c927e7769
Jeremy Faden <jbfaden@users.noreply.github.com>
2014-02-12 22:25:26 +0000

    findbugs URF_UNREAD_FIELD
    svn path=/dasCore/community/autoplot2011/trunk/; revision=7524

--- dasCoreUtil/src/org/das2/util/Base64.java
+++ dasCoreUtil/src/org/das2/util/Base64.java
@@ -1316,7 +1316,7 @@ public class Base64
         private int     lineLength;
         private boolean breakLines;     // Break lines at less than 80 characters
 		private int     options;        // Record options used to create the stream.
-		private byte[]  alphabet;	    // Local copies to avoid extra method calls
+		//private byte[]  alphabet;	    // Local copies to avoid extra method calls
 		private byte[]  decodabet;		// Local copies to avoid extra method calls
         
         
@@ -1363,7 +1363,7 @@ public class Base64
             this.position     = -1;
             this.lineLength   = 0;
 			this.options      = options; // Record for later, mostly to determine which alphabet to use
-			this.alphabet     = getAlphabet(options);
+			//this.alphabet     = getAlphabet(options);
 			this.decodabet    = getDecodabet(options);
         }   // end constructor
         
@@ -1553,7 +1553,7 @@ public class Base64
         private byte[]  b4; // Scratch used in a few places
         private boolean suspendEncoding;
 		private int options; // Record for later
-		private byte[]  alphabet;	    // Local copies to avoid extra method calls
+		//private byte[]  alphabet;	    // Local copies to avoid extra method calls
 		private byte[]  decodabet;		// Local copies to avoid extra method calls
         
         /**
@@ -1600,7 +1600,7 @@ public class Base64
             this.suspendEncoding = false;
             this.b4           = new byte[4];
 			this.options      = options;
-			this.alphabet     = getAlphabet(options);
+			//this.alphabet     = getAlphabet(options);
 			this.decodabet    = getDecodabet(options);
         }   // end constructor
         
