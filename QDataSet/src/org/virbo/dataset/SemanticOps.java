4dc00e82820682fbde0380d083449d046b41fdaf
Jeremy Faden <jbfaden@users.noreply.github.com>
2014-02-26 18:21:53 +0000

    bugfix: bounds check checked for BUNDLE_1 or BINS_1 but would not check for DEPEND_1.
    svn path=/autoplot/trunk/QDataSet/; revision=14007

--- QDataSet/src/org/virbo/dataset/SemanticOps.java
+++ QDataSet/src/org/virbo/dataset/SemanticOps.java
@@ -670,7 +670,7 @@ public class SemanticOps {
         QDataSet yrange;
 
         if ( ds.rank()==2 ) {
-            if ( ds.property(QDataSet.BUNDLE_1)!=null && ds.property(QDataSet.BINS_1)==null ) {
+            if ( ds.property(QDataSet.DEPEND_1)==null && ds.property(QDataSet.BUNDLE_1)!=null && ds.property(QDataSet.BINS_1)==null ) {
                 throw new IllegalArgumentException("scheme not supported: "+ds ); 
             } else {
                 xrange= Ops.extent( SemanticOps.xtagsDataSet(ds), null );
