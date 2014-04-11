f331390b9879d6df84a0051dc42cef9f93707180
Jeremy Faden <jbfaden@users.noreply.github.com>
2014-04-11 16:20:43 +0000

    fix imports
    svn path=/dasCore/community/autoplot2011/trunk/; revision=7669

--- dasCore/src/org/das2/graph/DasCanvas.java
+++ dasCore/src/org/das2/graph/DasCanvas.java
@@ -41,7 +41,6 @@ import java.awt.Color;
 import java.awt.Component;
 import java.awt.Container;
 import java.awt.Dimension;
-import java.awt.EventQueue;
 import java.awt.Font;
 import java.awt.FontMetrics;
 import java.awt.Graphics;
