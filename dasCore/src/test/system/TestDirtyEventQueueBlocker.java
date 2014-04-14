19b9378f3fa06c4e70125adc3bd8135415963abc
Jeremy Faden <jbfaden@users.noreply.github.com>
2014-04-14 12:27:52 +0000

    1129: experiment with putting actions that occur in getImage within the waitUntilIdle loop.
    svn path=/dasCore/community/autoplot2011/trunk/; revision=7686

---
+++ dasCore/src/test/system/TestDirtyEventQueueBlocker.java
@@ -0,0 +1,50 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package test.system;
+
+import java.io.IOException;
+import java.text.MessageFormat;
+import java.util.logging.Handler;
+import java.util.logging.Level;
+import java.util.logging.LogRecord;
+import org.das2.graph.DasCanvas;
+import org.das2.graph.DasColumn;
+import org.das2.graph.DasPlot;
+import org.das2.graph.DasRow;
+import org.das2.system.DasLogger;
+
+/**
+ * This shows that the isDirty flag is still set on the 
+ * @author jbf
+ */
+public class TestDirtyEventQueueBlocker {
+    public static void main( String[] args ) throws IOException {
+        
+        DasLogger.getLogger(DasLogger.GRAPHICS_LOG).setLevel( Level.ALL );
+        DasLogger.getLogger(DasLogger.GRAPHICS_LOG).addHandler( new Handler() {
+            @Override
+            public void publish(LogRecord record) {
+                System.err.println( MessageFormat.format( record.getMessage(), record.getParameters() ) );
+            }
+            @Override
+            public void flush() {
+            }
+            @Override
+            public void close() throws SecurityException {
+            }
+        });
+         
+        DasCanvas c= new DasCanvas(300,200);
+        
+        DasPlot p= DasPlot.createDummyPlot();
+        DasRow row= DasRow.create( c, 0, 1 );
+        DasColumn column= DasColumn.create( c, 0, 1 );
+        c.add( p, row, column );
+        c.setSize(640,480);
+        row.setDMinimum(10);
+        
+        c.writeToPng("/tmp/ap/canvasTestDirtyEventQueueBlocker.png");
+    }
+}
