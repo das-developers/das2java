0eebf43b6270b510f6a834cfee57065acbfd6eae
Jeremy Faden <jbfaden@users.noreply.github.com>
2015-01-16 21:55:47 +0000

    migrate Ed's changes that make PropertyType an enumeration.  Add TimeRange and Time types, see sftp://klunk.physics.uiowa.edu/home/jbf/project/autoplot/data/qds/timerange.d2s
    svn path=/dasCore/community/autoplot2011/trunk/; revision=8265

--- dasCore/src/org/das2/stream/PropertyType.java
+++ dasCore/src/org/das2/stream/PropertyType.java
@@ -20,138 +20,150 @@
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  */
-
 package org.das2.stream;
 
 import java.util.HashMap;
 import java.util.Map;
 import org.das2.datum.Datum;
 import org.das2.datum.DatumRange;
+import org.das2.datum.DatumRangeUtil;
 import org.das2.datum.Units;
-import org.virbo.dataset.SemanticOps;
 
 /**
  *
- * @author  eew
+ * @author eew
  */
-public final class PropertyType {
-    
+public enum PropertyType {
+
+    DOUBLE("double"),
+    DOUBLE_ARRAY("doubleArray"),
+    DATUM("Datum"),
+    DATUM_RANGE("DatumRange"),
+    INTEGER("int"),
+    STRING("String"),
+    TIME("Time"),
+    TIME_RANGE("TimeRange"),;
+
     private static final Map map = new HashMap();
-    public static final PropertyType DOUBLE = new PropertyType("double");
-    public static final PropertyType DOUBLE_ARRAY = new PropertyType("doubleArray");
-    public static final PropertyType DATUM = new PropertyType("Datum");
-	 public static final PropertyType DATUM_RANGE = new PropertyType("DatumRange");
-    public static final PropertyType INTEGER = new PropertyType("int");
-	 public static final PropertyType STRING = new PropertyType("String");
-    
+
+    static {
+        for (PropertyType t : values()) {
+            map.put(t.name, t);
+        }
+    }
+
     public static PropertyType getByName(String name) {
-        PropertyType result= (PropertyType)map.get(name);
-        if ( result==null ) {
-            throw new IllegalArgumentException( "Unrecognized property type: "+name );
+        PropertyType result = (PropertyType) map.get(name);
+        if (result == null) {
+            throw new IllegalArgumentException("Unrecognized property type: " + name);
         }
         return result;
     }
-    
+
     private final String name;
-    
-    /** Creates a new instance of PropertyType */
+
+    /**
+     * Creates a new instance of PropertyType
+     */
     private PropertyType(String name) {
         this.name = name;
-        map.put(name, this);
     }
-    
+
     public Object parse(String s) throws java.text.ParseException {
-		 if (name.equals("String")){
-			 return s;
-		 }
-		 else if(name.equals("double")) {
-            try {
-                return new Double(s);
+        switch (this) {
+            case STRING: {
+                return s;
             }
-            catch (NumberFormatException nfe) {
-                throw new java.text.ParseException(nfe.getMessage(), 0);
-            }
-        }
-        else if (name.equals("int")) {
-            try {
-                return new Integer(s);
-            }
-            catch (NumberFormatException nfe) {
-                throw new java.text.ParseException(nfe.getMessage(), 0);
-            }
-        }
-        else if (name.equals("doubleArray")) {
-            try {
-                String[] strings = s.split(",");
-                double[] doubles = new double[strings.length];
-                for (int i = 0; i < strings.length; i++) {
-                    doubles[i] = Double.parseDouble(strings[i]);
+            case DOUBLE:
+                try {
+                    return new Double(s);
+                } catch (NumberFormatException nfe) {
+                    throw new java.text.ParseException(nfe.getMessage(), 0);
                 }
-                return doubles;
-            }
-            catch (NumberFormatException nfe) {
-                throw new java.text.ParseException(nfe.getMessage(), 0);
-            }
-        }
-        else if (name.equals("Datum")) {
-            String[] split = s.split("\\s+");
-				if (split.length < 1)
-					throw new IllegalArgumentException("Empty datum string");
-				
-				if (split.length == 1)
-                return Units.dimensionless.parse(split[0]);
-            
-				if (split.length == 2) {
-                Units units = Units.lookupUnits(split[1]);
+            case INTEGER:
+                try {
+                    return new Integer(s);
+                } catch (NumberFormatException nfe) {
+                    throw new java.text.ParseException(nfe.getMessage(), 0);
+                }
+            case DOUBLE_ARRAY:
+                try {
+                    String[] strings = s.split(",");
+                    double[] doubles = new double[strings.length];
+                    for (int i = 0; i < strings.length; i++) {
+                        doubles[i] = Double.parseDouble(strings[i]);
+                    }
+                    return doubles;
+                } catch (NumberFormatException nfe) {
+                    throw new java.text.ParseException(nfe.getMessage(), 0);
+                }
+            case DATUM: {
+                String[] split = s.split("\\s+");
+                if (split.length == 1) {
+                    return Units.dimensionless.parse(split[0]);
+                }
+                if (split.length == 2) {
+                    Units units = Units.lookupUnits(split[1]);
+                    return units.parse(split[0]);
+                }
+
+                //Allow for units that contain single spaces
+                StringBuilder bldr = new StringBuilder();
+                for (int i = 1; i < split.length; i++) {
+                    if (i > 1) {
+                        bldr.append(" ");
+                    }
+                    bldr.append(split[i]);
+                }
+
+                Units units = Units.lookupUnits(bldr.toString());
                 return units.parse(split[0]);
             }
-				
-				//Allow for units that contain single spaces
-				StringBuilder bldr = new StringBuilder();
-				for(int i = 1; i < split.length; i++){
-					if(i > 1) bldr.append(" ");
-					bldr.append(split[i]);
-				}
-				
-            Units units = Units.lookupUnits(bldr.toString());
-				return units.parse(split[0]);
-        }
-		  else if (name.equals("DatumRange")){
-			   String[] split = s.split("\\s+");
-				if (split.length < 3){
-					throw new IllegalArgumentException("Too few tokens in range: '" + s + "'");
-				}
-				if(! split[1].toLowerCase().equals("to"))
-						throw new java.text.ParseException("Range '"+s+"' is missing the word 'to'", 0);
-					
-				if (split.length == 3){
-					Datum begin = Units.dimensionless.parse(split[0]);
-					Datum end = Units.dimensionless.parse(split[2]);
-					return new DatumRange(begin, end);
-				}
-				
+            case DATUM_RANGE: {
+                String[] split = s.split("\\s+");
+                if (split.length < 3) {
+                    throw new IllegalArgumentException("Too few tokens in range: '" + s + "'");
+                }
+                if (!split[1].toLowerCase().equals("to")) {
+                    throw new java.text.ParseException("Range '" + s + "' is missing the word 'to'", 0);
+                }
+
+                if (split.length == 3) {
+                    Datum begin = Units.dimensionless.parse(split[0]);
+                    Datum end = Units.dimensionless.parse(split[2]);
+                    return new DatumRange(begin, end);
+                }
+
 				// New for 2014-06-12, allow units strings to have spaces, thus
-				// "V**2 m**-2 Hz**-1" is legal
-				StringBuilder bldr = new StringBuilder();
-				for(int i = 3; i < split.length; i++){
-					if(i > 3) bldr.append(" ");
-					bldr.append(split[i]);
-				}
-			
-				Units units = Units.lookupUnits(bldr.toString());	
-				Datum begin = units.parse(split[0]);
-				Datum end = units.parse(split[2]);
-				return new DatumRange(begin, end);
-				
-		  }
-        else {
-            throw new IllegalStateException("unrecognized name: " + name);
+                // "V**2 m**-2 Hz**-1" is legal
+                StringBuilder bldr = new StringBuilder();
+                for (int i = 3; i < split.length; i++) {
+                    if (i > 3) {
+                        bldr.append(" ");
+                    }
+                    bldr.append(split[i]);
+                }
+
+                Units units = Units.lookupUnits(bldr.toString());
+                Datum begin = units.parse(split[0]);
+                Datum end = units.parse(split[2]);
+                return new DatumRange(begin, end);
+
+            }
+            case TIME: {
+                return Units.us2000.parse(s);
+            }
+            case TIME_RANGE: {
+                return DatumRangeUtil.parseTimeRange(s);
+            }
+            default:
+                throw new IllegalStateException("unrecognized name: " + name);
         }
     }
-    
-	 @Override
+
+    @Override
     public String toString() {
         return name;
     }
-    
+
 }
