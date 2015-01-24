62e1628407ef25f42e206a1cb386ba9ee85e27b4
Jeremy Faden <jbfaden@users.noreply.github.com>
2015-01-24 14:49:54 +0000

    GUI tweaks, after verifying that 3 is okay.
    svn path=/autoplot/trunk/QDataSet/; revision=16087

--- QDataSet/src/org/virbo/filters/MedianFilterEditorPanel.java
+++ QDataSet/src/org/virbo/filters/MedianFilterEditorPanel.java
@@ -37,12 +37,15 @@ public class MedianFilterEditorPanel extends AbstractFilterEditorPanel {
         jLabel1 = new javax.swing.JLabel();
         sizeTF = new javax.swing.JTextField();
 
+        FormListener formListener = new FormListener();
+
         setToolTipText("The median boxcar size can be between 5 and n/2, where n is the length of the data");
 
-        jLabel1.setText("Median Boxcar Size (5 to n/2):  ");
+        jLabel1.setText("Median Boxcar Size:  ");
 
         sizeTF.setText("5");
         sizeTF.setPreferredSize(new java.awt.Dimension(50, 27));
+        sizeTF.addActionListener(formListener);
 
         org.jdesktop.layout.GroupLayout layout = new org.jdesktop.layout.GroupLayout(this);
         this.setLayout(layout);
@@ -50,10 +53,10 @@ public class MedianFilterEditorPanel extends AbstractFilterEditorPanel {
             layout.createParallelGroup(org.jdesktop.layout.GroupLayout.LEADING)
             .add(layout.createSequentialGroup()
                 .addContainerGap()
-                .add(jLabel1)
-                .add(2, 2, 2)
+                .add(jLabel1, org.jdesktop.layout.GroupLayout.PREFERRED_SIZE, 156, org.jdesktop.layout.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(org.jdesktop.layout.LayoutStyle.RELATED)
                 .add(sizeTF, org.jdesktop.layout.GroupLayout.PREFERRED_SIZE, org.jdesktop.layout.GroupLayout.DEFAULT_SIZE, org.jdesktop.layout.GroupLayout.PREFERRED_SIZE)
-                .addContainerGap(org.jdesktop.layout.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                .addContainerGap(46, Short.MAX_VALUE))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(org.jdesktop.layout.GroupLayout.LEADING)
@@ -64,7 +67,22 @@ public class MedianFilterEditorPanel extends AbstractFilterEditorPanel {
                     .add(sizeTF, org.jdesktop.layout.GroupLayout.PREFERRED_SIZE, org.jdesktop.layout.GroupLayout.DEFAULT_SIZE, org.jdesktop.layout.GroupLayout.PREFERRED_SIZE))
                 .addContainerGap(org.jdesktop.layout.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
         );
+    }
+
+    // Code for dispatching events from components to event handlers.
+
+    private class FormListener implements java.awt.event.ActionListener {
+        FormListener() {}
+        public void actionPerformed(java.awt.event.ActionEvent evt) {
+            if (evt.getSource() == sizeTF) {
+                MedianFilterEditorPanel.this.sizeTFActionPerformed(evt);
+            }
+        }
     }// </editor-fold>//GEN-END:initComponents
+
+    private void sizeTFActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_sizeTFActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_sizeTFActionPerformed
 
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
