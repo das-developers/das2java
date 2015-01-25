f57b79c9e89de960f9f5dd084e88b309c01e843b
Jeremy Faden <jbfaden@users.noreply.github.com>
2015-01-25 15:28:09 +0000

    special handling for ENTRY log messages
    svn path=/dasCore/community/autoplot2011/trunk/; revision=8277

--- dasCoreUtil/src/org/das2/util/TimingConsoleFormatter.java
+++ dasCoreUtil/src/org/das2/util/TimingConsoleFormatter.java
@@ -15,8 +15,40 @@ public class TimingConsoleFormatter extends Formatter {
     @Override
     public String format( LogRecord rec ) {
         long dt= System.currentTimeMillis() - t0;
-        String msg= MessageFormat.format( String.valueOf( rec.getMessage() ), rec.getParameters() );
-        return String.format("%9.3f %s: %s\n", dt/1000., rec.getLoggerName(), msg );
+        Object[] parms= rec.getParameters();
+
+        // the following copied from Autoplot's org.virbo.autoplot.scriptconsole.LogConsole
+        String recMsg;
+        String rm1= rec.getMessage();
+        if ( rm1!=null ) {
+            if ( rec.getMessage().equals("ENTRY {0}") ) {
+                recMsg= "ENTRY "+ rec.getSourceClassName() + "." +rec.getSourceMethodName() + " {0}";
+            } else if ( rec.getMessage().equals("ENTRY") ) {
+                recMsg= "ENTRY "+ rec.getSourceClassName() + "." +rec.getSourceMethodName();
+            } else {
+                recMsg = rec.getMessage();
+            }
+        } else {
+            recMsg= null;
+        }
+
+        if ( parms!=null && parms.length>0 ) {
+            try {
+                recMsg = MessageFormat.format( recMsg, parms );
+            } catch ( NullPointerException ex ) {
+                recMsg= String.valueOf( rec.getMessage() ); //TODO: fix this log message! bug https://sourceforge.net/p/autoplot/bugs/1194/
+            }
+        }
+        if ( ( recMsg==null || recMsg.length()==0 ) && rec.getThrown()!=null ) {
+            recMsg= rec.getThrown().toString();
+            //TODO: consider if "debug" property should be set instead.  Also it would be nice to digest this for jython errors.
+            rec.getThrown().printStackTrace();
+            // This is interesting--I've wondered where the single-line-message items have been coming from...
+        } else {
+            // no message.  breakpoint here for debugging.
+            int i=0;
+        }
+        return String.format("%9.3f %s: %s\n", dt/1000., rec.getLoggerName(), recMsg );
     }
         
     public TimingConsoleFormatter() {
