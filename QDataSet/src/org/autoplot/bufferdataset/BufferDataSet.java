a3f916980fb3c9c912fd805e9465a707e26b56ce
Jeremy Faden <jbfaden@users.noreply.github.com>
2015-01-27 13:44:50 +0000

    trivial javadoc and netbeans refactorings. netbeans pointed out that join properties did not properly set TYPICAL_MAX.
    svn path=/autoplot/trunk/QDataSet/; revision=16128

--- QDataSet/src/org/autoplot/bufferdataset/BufferDataSet.java
+++ QDataSet/src/org/autoplot/bufferdataset/BufferDataSet.java
@@ -8,6 +8,7 @@ import java.lang.reflect.Array;
 import java.nio.ByteBuffer;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.logging.Level;
 import java.util.logging.Logger;
 import org.das2.datum.CacheTag;
 import org.das2.datum.EnumerationUnits;
@@ -344,8 +345,8 @@ public abstract class BufferDataSet extends AbstractDataSet implements WritableD
      * Note this does not consider isMutable.  If the dataset is not mutable, then the
      * original data could be returned (probably).
      * 
-     * @param ds
-     * @return 
+     * @param ds any qube dataset.
+     * @return a BufferDataSet copy of the dataset.
      */
     public static BufferDataSet copy( QDataSet ds ) {
         //TODO: this should check that the data is a qube.
@@ -572,14 +573,13 @@ public abstract class BufferDataSet extends AbstractDataSet implements WritableD
                     djoin= append( djoin, dd1 );
                     result.put( prop, djoin );
                 } else {
-                    logger.info( "dataset doesn't have property \""+prop+"\" but other dataset does: "+ths);
+                    logger.log(Level.INFO, "dataset doesn''t have property \"{0}\" but other dataset does: {1}", new Object[]{prop, ths});
                 }
             }
         }
 
         props= DataSetUtil.dimensionProperties();
-        for ( int i=0; i<props.length; i++ ) {
-            String prop= props[i];
+        for (String prop : props) {
             Object value= ths.property(prop);
             if ( value!=null && value.equals(ds.property(prop) ) ) {
                 result.put( prop, ths.property(prop) );
@@ -587,10 +587,10 @@ public abstract class BufferDataSet extends AbstractDataSet implements WritableD
         }
         // special handling for QDataSet.CADENCE, and QDataSet.MONOTONIC
         props= new String[] { QDataSet.CADENCE, QDataSet.BINS_1 };
-        for ( int iprop= 0; iprop<props.length; iprop++ ) {
-            Object o= ths.property( props[iprop] );
-            if ( o!=null && o.equals( ds.property( props[iprop] ) ) ) {
-                result.put( props[iprop], o );
+        for (String prop : props) {
+            Object o = ths.property(prop);
+            if (o!=null && o.equals(ds.property(prop))) {
+                result.put(prop, o);
             }
         }
 
@@ -634,7 +634,7 @@ public abstract class BufferDataSet extends AbstractDataSet implements WritableD
         Number dmin1= (Number) ds.property(QDataSet.TYPICAL_MIN );
         Number dmax1= (Number) ds.property(QDataSet.TYPICAL_MAX );
         if ( dmin0!=null && dmin1!=null ) result.put( QDataSet.TYPICAL_MIN, Math.min( dmin0.doubleValue(), dmin1.doubleValue() ) );
-        if ( dmax0!=null && dmax1!=null ) result.put( QDataSet.TYPICAL_MAX, Math.max( dmin0.doubleValue(), dmin1.doubleValue() ) );
+        if ( dmax0!=null && dmax1!=null ) result.put( QDataSet.TYPICAL_MAX, Math.max( dmax0.doubleValue(), dmax1.doubleValue() ) );
 
         return result;
     }
