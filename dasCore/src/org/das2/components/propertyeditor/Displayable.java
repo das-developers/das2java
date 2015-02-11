0e04b6b7f0a5736d4d1810bf23ff517168c378b4
Jeremy Faden <jbfaden@users.noreply.github.com>
2015-02-11 02:35:20 +0000

    javadoc improvements
    svn path=/dasCore/community/autoplot2011/trunk/; revision=8314

--- dasCore/src/org/das2/components/propertyeditor/Displayable.java
+++ dasCore/src/org/das2/components/propertyeditor/Displayable.java
@@ -3,29 +3,35 @@ package org.das2.components.propertyeditor;
 import java.awt.Graphics2D;
 import javax.swing.Icon;
 
-/** Type-safe enumerations that are used as property types
+/** 
+ * Type-safe enumerations that are used as property types
  * that are editable with a PropertyEditor should
  * implement this interface.
- *
  */
 public interface Displayable {
     
-    /** return a <code>String</code> that will help the user
+    /** 
+     * return a <code>String</code> that will help the user
      * identify this item when choosing from a list.
+     * @return the list label.
      */
     String getListLabel();
     
-    /** An icon can be provided that will be shown in a list
+    /** 
+     * An icon can be provided that will be shown in a list
      * along with the textual description of the element.
      * This method should return <code>null</code> if there
      * is no icon available.
      *
+     * @return the icon.
      */
     Icon getListIcon();
     
     /**
      * implement this to provide nice drawing of icon on printing graphics context.
-     * @param g
+     * @param g the graphics context.
+     * @param x the x position, typically 0.
+     * @param y the y position, typically 0.
      */
     void drawListIcon( Graphics2D g, int x, int y );
 }
