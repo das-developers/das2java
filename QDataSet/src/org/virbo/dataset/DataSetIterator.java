60b63d3104276562bcc5a45cbbede4b687337952
Jeremy Faden <jbfaden@users.noreply.github.com>
2015-02-18 12:40:23 +0000

    javadoc improvements for DataSetIterator
    svn path=/autoplot/trunk/QDataSet/; revision=16293

--- QDataSet/src/org/virbo/dataset/DataSetIterator.java
+++ QDataSet/src/org/virbo/dataset/DataSetIterator.java
@@ -1,42 +1,58 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
-
 package org.virbo.dataset;
 
 /**
- *
+ * Iterator for accessing each value of a dataset.
+ * See http://jfaden.net:8080/hudson/job/autoplot-test037/ws/dataSetIterator.jy
  * @author jbf
  */
 public interface DataSetIterator {
 
+    /**
+     * return true while the iterator has a next element.
+     * @return true while the iterator has a next element.
+     */
     boolean hasNext();
 
+    /**
+     * return the current index for the dimension.
+     * @param dim the dimension number (0&lt;=dim&lt;inputRank) 
+     * @return the current index.
+     */
     int index(int dim);
 
+    /**
+     * return the length of the dimension
+     * @param dim the dimension number (0&lt;=dim&lt;inputRank) 
+     * @return the length of the dimension
+     */
     int length(int dim);
 
+    /**
+     * iterate to the next position.
+     */
     void next();
 
     /**
      * get the value from ds at the current iterator position.
-     * @param ds a dataset with capatible geometry as the iterator's geometry.
+     * @param ds a dataset with compatible geometry as the iterator's geometry.
      * @return the value of ds at the current iterator position.
      */
     double getValue(QDataSet ds);
 
     /**
      * replace the value in ds at the current iterator position.
-     * @param ds a writable dataset with capatible geometry as the iterator's geometry.
+     * @param ds a writable dataset with compatible geometry as the iterator's geometry.
      * @param v the value to insert.
      */
     void putValue(WritableDataSet ds, double v);
 
     /**
      * return the rank of the dataset which the iterator will walk through.
-     * Note this needn't be the same rank as the dataset!
-     * @return
+     * Note this needn't be the same rank as the dataset!  For example,
+     * when QubeDataSetIterator walks through ds[:,0,:], the rank is 2 even 
+     * though ds is rank 3.
+     * 
+     * @return the rank of the dataset which the iterator will walk through.
      */
     int rank();
 
@@ -49,7 +65,8 @@ public interface DataSetIterator {
      * Do not pass the result of this into the putValue of this iterator,
      * the result should have its own iterator.
      * 
-     * @return
+     * @return a dataset that will have the same geometry at the
+     * dataset implied by each dimension iterator. 
      */
     public DDataSet createEmptyDs();
 
