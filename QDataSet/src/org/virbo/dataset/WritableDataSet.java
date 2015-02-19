9de0e9fe4c1d486d6e509a36a63018bee3f180de
Jeremy Faden <jbfaden@users.noreply.github.com>
2015-02-19 13:57:22 +0000

    javadoc for WritableDataSet.
    svn path=/autoplot/trunk/QDataSet/; revision=16306

--- QDataSet/src/org/virbo/dataset/WritableDataSet.java
+++ QDataSet/src/org/virbo/dataset/WritableDataSet.java
@@ -10,18 +10,62 @@
 package org.virbo.dataset;
 
 /**
+ * QDataSets can be mutable as well, meaning their values can be assigned
+ * as well as read.  These datasets cannot be written to once they are made 
+ * immutable, and clients must check the isImmutable flag or call Ops.maybeCopy
+ * when they need write access to the data.
  *   
  * Mutable datasets warning: No dataset should be mutable once it is accessible to the
  * rest of the system.  This would require clients make defensive copies which would 
  * seriously degrade performance.  
  *
- * putValue method implementations should check isMutable() method and should call 
  * @author jbf
+ * @see org.virbo.dsops.Ops#maybeCopy(org.virbo.dataset.QDataSet) 
+ * @see MutablePropertyDataSet#isImmutable() 
  */
 public interface WritableDataSet extends MutablePropertyDataSet {
+    /**
+     * put a value into the rank 0 dataset.
+     * @param d the value
+     * @throws IllegalArgumentException if the dataset immutable or is not rank 0.
+     */
     void putValue( double d );
+
+    /**
+     * put a value into the rank 1 dataset.
+     * @param i0 the index
+     * @param d the value
+     * @throws IllegalArgumentException if the dataset immutable or is not rank 1.
+     */
     void putValue( int i0, double d );
+
+    /**
+     * put a value into the rank 2 dataset.
+     * @param i0 the index
+     * @param i1 the index
+     * @param d the value
+     * @throws IllegalArgumentException if the dataset immutable or is not rank 2.
+     */
     void putValue( int i0, int i1, double d );
+
+    /**
+     * put a value into the rank 3 dataset.
+     * @param i0 the index
+     * @param i1 the index
+     * @param i2 the index
+     * @param d the value
+     * @throws IllegalArgumentException if the dataset immutable or is not rank 3.
+     */
     void putValue( int i0, int i1, int i2, double d );
+       
+    /**
+     * put a value into the rank 4 dataset.
+     * @param i0 the index
+     * @param i1 the index
+     * @param i2 the index
+     * @param i3 the index
+     * @param d the value
+     * @throws IllegalArgumentException if the dataset immutable or is not rank 4.
+     */
     void putValue( int i0, int i1, int i2, int i3, double d);
 }
