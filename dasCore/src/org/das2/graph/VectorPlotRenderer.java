e8a50078314744c125f7398587c666f1c57759c7
Jeremy Faden <jbfaden@users.noreply.github.com>
2015-02-20 13:53:22 +0000

    javadoc improvements.
    svn path=/dasCore/community/autoplot2011/trunk/; revision=8337

--- dasCore/src/org/das2/graph/VectorPlotRenderer.java
+++ dasCore/src/org/das2/graph/VectorPlotRenderer.java
@@ -8,7 +8,6 @@ package org.das2.graph;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.Point;
-import java.util.logging.Level;
 import org.das2.datum.Units;
 import org.das2.util.monitor.ProgressMonitor;
 import org.virbo.dataset.DDataSet;
@@ -22,11 +21,17 @@ import org.virbo.dsops.Ops;
 
 /**
  * Draw vectors as arrows, from a dataset[:,4] where
- * dataset[0,:] is x,y,dx,dy.
+ * dataset[0,:] is x,y,dx,dy.  
  * @author jbf
  */
 public class VectorPlotRenderer extends Renderer {
 
+    /**
+     * return true if the renderer accepts data in this form.  This
+     * renderer needs rank 2 data with 4 columns,
+     * @param ds
+     * @return 
+     */
     public static boolean acceptsData( QDataSet ds ) {
         if ( ds.rank()!=2 ) {
             return false;
@@ -38,8 +43,8 @@ public class VectorPlotRenderer extends Renderer {
     /**
      * autorange on the data, returning a rank 2 bounds for the dataset.
      *
-     * @param fillDs
-     * @return
+     * @param ds the dataset to autorange.
+     * @return a bounds dataset, where result[0,:] is the xrange and result[1,:] is the yrange.
      */
     public static QDataSet doAutorange( QDataSet ds ) {
         
@@ -77,12 +82,28 @@ public class VectorPlotRenderer extends Renderer {
     }
 
     protected double scale = 1.0;
+    
+    /**
+     * property name for the scale.
+     * @see #getScale() 
+     */
     public static final String PROP_SCALE = "scale";
 
+    /**
+     * get the scale relating the length of the vector (dx and dy, the last two 
+     * columns) to the positions of the vector (in the first two columns).
+     * @return the scale 
+     */
     public double getScale() {
         return scale;
     }
 
+    /**
+     * set the scale relating the length of the vector (dx and dy, the last two 
+     * columns) to the positions of the vector (in the first two columns).
+     * Default is 1.
+     * @param scale the new scale
+     */
     public void setScale(double scale) {
         double oldScale = this.scale;
         this.scale = scale;
@@ -95,10 +116,10 @@ public class VectorPlotRenderer extends Renderer {
 
         Graphics2D g= (Graphics2D)g1;
 
-        QDataSet ds= getDataSet();
+        QDataSet lds= getDataSet();
         DasPlot parent= getParent();
         
-        if ( ds==null ) {
+        if ( lds==null ) {
             if ( getLastException()!=null ) {
                 renderException(g, xAxis, yAxis, lastException);
             } else {
@@ -107,20 +128,20 @@ public class VectorPlotRenderer extends Renderer {
             return;
         }
 
-        if ( ds.length()==0 ) {
+        if ( lds.length()==0 ) {
             parent.postException( this, new IllegalArgumentException("no data to plot") );
             return;
         }
 
-        if ( ds.rank()!=2 || ds.length(0)!=4 ) {
+        if ( lds.rank()!=2 || lds.length(0)!=4 ) {
             parent.postException( this, new IllegalArgumentException("plot must be rank 2 and be of the form ds[:,4]") );
             return;
         }
 
-        QDataSet x= DataSetOps.unbundle(ds,0);
-        QDataSet y= DataSetOps.unbundle(ds,1);
-        QDataSet dx= DataSetOps.unbundle(ds,2);
-        QDataSet dy= DataSetOps.unbundle(ds,3);
+        QDataSet x= DataSetOps.unbundle(lds,0);
+        QDataSet y= DataSetOps.unbundle(lds,1);
+        QDataSet dx= DataSetOps.unbundle(lds,2);
+        QDataSet dy= DataSetOps.unbundle(lds,3);
 
         Units xunits= SemanticOps.getUnits( x );
         Units yunits= SemanticOps.getUnits( y );
@@ -130,7 +151,7 @@ public class VectorPlotRenderer extends Renderer {
         QDataSet wx= SemanticOps.weightsDataSet(x);
         QDataSet wy= SemanticOps.weightsDataSet(y);
 
-        for ( int i=0; i<ds.length(); i++ ) {
+        for ( int i=0; i<lds.length(); i++ ) {
             if ( wdx.value(i) * wdy.value(i) * wx.value(i) * wy.value(i)==0 ) continue;
             
             double ix= xAxis.transform( x.value(i), xunits );
