a3920b8cd7fbda569de49f43f6b7abf5d3450a47
Jeremy Faden <jbfaden@users.noreply.github.com>
2015-02-22 12:48:39 +0000

    experiment with Schemes class, adding more schemes and adding tests to assert that a dataset is a particular type.
    svn path=/autoplot/trunk/QDataSet/; revision=16327

--- QDataSet/src/org/virbo/dataset/examples/Schemes.java
+++ QDataSet/src/org/virbo/dataset/examples/Schemes.java
@@ -6,16 +6,19 @@
 package org.virbo.dataset.examples;
 
 import java.text.ParseException;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 import org.virbo.dataset.JoinDataSet;
 import org.virbo.dataset.QDataSet;
+import org.virbo.dataset.SemanticOps;
 import org.virbo.dsops.Ops;
 
 /**
  * For the various QDataSet schemes, show examples and documentation for
  * each.  This was motivated when trying to describe the output of 
  * org.das2.graph.ContoursRenderer.doAutorange()
+ * 
+ * Note all QDataSets are "duck-typed," meaning if they happen to meet the 
+ * requirements of an interface then they are an instance of the interface.
+ * 
  * @author jbf 
  */
 public class Schemes {
@@ -52,4 +55,122 @@ public class Schemes {
         }
     }
     
+    /**
+     * return true if the data is a boundingBox.
+     * @param ds a dataset
+     * @return true if the dataset is a bounding box.
+     */
+    public static boolean isBoundingBox( QDataSet ds ) {
+        return ds.rank()==2 && ds.length(0)==2 && ds.length()>0;
+    }
+    
+    /**
+     * return a rank 2 waveform, where the waveform is stored in packets.
+     * DEPEND_0 is the time for each packet, and DEPEND_1 is the difference in
+     * time for each measurement to the packet time.  Note the additional requirement
+     * that the offsets be uniform, e.g.:
+     *<blockquote><pre><small>{@code
+     *from org.virbo.dataset.examples import Schemes
+     *ds= Schemes.rank2Waveform()
+     *deltaT= ds.property( QDataSet.DEPEND_1 )
+     *ddeltaT= diffs(dep1)
+     *print ddeltaT[0], ddeltT[-1] # should be the same
+     *}</small></pre></blockquote>
+     * 
+     * @return rank 2 waveform.
+     */
+    public static QDataSet rank2Waveform( ) {
+        return Ops.ripplesWaveformTimeSeries(20);
+    }
+    
+    /**
+     * return true if the data is a rank 2 waveform.
+     * @param ds a dataset
+     * @return  true if the data is a rank 2 waveform.
+     */
+    public static boolean isRank2Waveform( QDataSet ds ) {
+        if ( ds.rank()!=2 ) return false;
+        QDataSet dep0= (QDataSet)ds.property(QDataSet.DEPEND_0);
+        if ( dep0==null ) return false;
+        QDataSet dep1= (QDataSet)ds.property(QDataSet.DEPEND_1);
+        if ( dep1==null ) return false;
+        if ( SemanticOps.getUnits(dep0).getOffsetUnits().isConvertableTo(SemanticOps.getUnits(dep1)) ) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+    
+    /**
+     * return a rank 2 vectorTimeSeries, which is a bundle
+     * of m rank 1 measurements.  This tacitly asserts orthogonality,
+     * but the bundled data should at least all be rank 1 and in the same units.
+     *<blockquote><pre><small>{@code
+     *from org.virbo.dataset.examples import Schemes
+     *ds= Schemes.vectorTimeSeries()
+     *plot( magnitude( ds ) )
+     *plot( unbundle( ds, 0 ) )
+     *}</small></pre></blockquote>
+     * @return rank 2 vector time series.
+     *dataset>rank2bundle>vectorTimeSeries.
+     */
+    public static QDataSet vectorTimeSeries( ) {
+        return Ops.ripplesVectorTimeSeries(1440);
+    }
+    
+    /**
+     * return true if the data is a vector time series.
+     * @param ds a dataset
+     * @return  true if the data is a vector time series.
+     */
+    public static boolean isVectorTimeSeries( QDataSet ds ) {
+        return ds.rank()==2 && ( Ops.isLegacyBundle(ds) || Ops.isBundle(ds) ) && isTimeSeries(ds);
+    }
+    
+    /**
+     * return a rank 2 simple spectrogram, which has two indeces.
+     * @return rank 2 simple spectrogram
+     */
+    public static QDataSet simpleSpectrogram() {
+        return Ops.ripples(40,30);
+    }
+        
+    /**
+     * return true if the data is a simple spectrogram.
+     * @param ds a dataset
+     * @return  true if the data is a simple spectrogram.
+     */
+    public static boolean isSimpleSpectrogram( QDataSet ds ) {
+        return ds.rank()==2;
+    }
+
+    /**
+     * return a rank 2 simple spectrogram, which has two indeces
+     * and is a TimeSeries.
+     * @return simple spectrogram time series
+     */
+    public static QDataSet simpleSpectrogramTimeSeries() {
+        return Ops.ripplesSpectrogramTimeSeries(1440);
+    }
+        
+    /**
+     * return true if the data is a simple spectrogram.
+     * @param ds a dataset
+     * @return  true if the data is a simple spectrogram.
+     */
+    public static boolean isSimpleSpectrogramTimeSeries( QDataSet ds ) {
+        return isSimpleSpectrogram(ds) && isTimeSeries(ds);
+    }
+    
+    /**
+     * returns true if the dataset is a time series.  This is either something 
+     * that has DEPEND_0 as a dataset with time location units, or a join of 
+     * other datasets that are time series.
+     * @param ds a dataset
+     * @return true if the dataset is a time series.
+     * @see SemanticOps#isTimeSeries(org.virbo.dataset.QDataSet) 
+     */
+    public static boolean isTimeSeries( QDataSet ds ) {
+        return SemanticOps.isTimeSeries(ds);
+    }
 }
