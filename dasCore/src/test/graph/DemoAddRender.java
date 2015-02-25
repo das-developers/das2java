5a8c771a500856abc67c8b3bc55daf1fffca3237
Jeremy Faden <jbfaden@users.noreply.github.com>
2015-02-25 03:11:24 +0000

    example renderer
    svn path=/dasCore/community/autoplot2011/trunk/; revision=8388

---
+++ dasCore/src/test/graph/DemoAddRender.java
@@ -0,0 +1,48 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package test.graph;
+
+import javax.swing.JDialog;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import org.das2.datum.DatumRange;
+import org.das2.datum.Units;
+import org.das2.graph.DasCanvas;
+import org.das2.graph.DasPlot;
+import org.das2.graph.GraphUtil;
+import org.das2.graph.Renderer;
+import org.virbo.dataset.QDataSet;
+import org.virbo.dsutil.DataSetBuilder;
+
+/**
+ *
+ * @author jbf
+ */
+public class DemoAddRender {
+    public static void main( String[] args ) {
+        DasCanvas c= new DasCanvas(480,480);
+        DasPlot p= GraphUtil.newDasPlot( c, DatumRange.newDatumRange(0,10,Units.dimensionless), 
+                DatumRange.newDatumRange(0,10,Units.dimensionless) );
+        Renderer myRenderer= new MyRenderer();
+        DataSetBuilder build=new DataSetBuilder(2,100,4);
+        build.setUnits(0,Units.dimensionless);
+        build.setUnits(1,Units.dimensionless);
+        build.setUnits(2,Units.dimensionless);
+        build.putValue( -1, 0, 5 ); build.putValue( -1, 1, 5 ); build.putValue( -1, 2, 3 ); build.putValue( -1, 3, 0 );
+        build.nextRecord();
+        build.putValue( -1, 0, 6 ); build.putValue( -1, 1, 6 ); build.putValue( -1, 2, 2 ); build.putValue( -1, 3, 0xFF0000 );
+        build.nextRecord();
+        build.putValue( -1, 0, 3 ); build.putValue( -1, 1, 3 ); build.putValue( -1, 2, 1 ); build.putValue( -1, 3, 0xFFFF00 );
+        build.nextRecord();
+        QDataSet myds= build.getDataSet();
+        myRenderer.setDataSet( myds );
+        p.addRenderer( myRenderer );
+        JDialog d= new JDialog();
+        d.add(c);
+        d.pack();
+        d.setVisible(true);
+    }
+}
