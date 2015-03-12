0ab09ad514677c80cadb176d2ab9ab35b0e583b6
Jeremy Faden <jbfaden@users.noreply.github.com>
2015-03-12 12:03:34 +0000

    javadoc improvements
    svn path=/autoplot/trunk/QDataSet/; revision=16426

--- QDataSet/src/org/virbo/dataset/AbstractQFunction.java
+++ QDataSet/src/org/virbo/dataset/AbstractQFunction.java
@@ -13,8 +13,16 @@ package org.virbo.dataset;
  */
 public abstract class AbstractQFunction implements QFunction {
 
+    @Override
     public abstract QDataSet value(QDataSet parm);
 
+    /**
+     * calculate the values by calling the {@code value} function for
+     * each element of vs
+     * @param vs rank N+1 set of values, where {@code exampleInput} returns rank N.
+     * @return rank M+1 set of values, where {@code value} returns rank M.
+     */
+    @Override
     public QDataSet values(QDataSet vs ) {
         QDataSet v1= value( vs.slice(0) );
         JoinDataSet result= new JoinDataSet( v1 );
@@ -24,8 +32,14 @@ public abstract class AbstractQFunction implements QFunction {
         return result;
     }
 
+    @Override
     public abstract QDataSet exampleInput();
 
+    /**
+     * this simply calls {@code value( exampleInput() )}.
+     * @return 
+     */
+    @Override
     public QDataSet exampleOutput() {
         return value( exampleInput() );
     }
