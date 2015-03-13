6c6574d65fcca242f307dd7184eb2b796c8e48fa
Jeremy Faden <jbfaden@users.noreply.github.com>
2015-03-13 19:18:30 +0000

    add test3 which shows problem with the result.
    svn path=/autoplot/trunk/QStream/; revision=16440

--- QStream/src/test/QDataSetStreamHandlerTest.java
+++ QStream/src/test/QDataSetStreamHandlerTest.java
@@ -8,7 +8,13 @@ package test;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.nio.channels.Channels;
 import java.nio.channels.ReadableByteChannel;
+import java.util.ArrayList;
+import java.util.List;
+import org.virbo.dataset.DataSetUtil;
 import org.virbo.dataset.QDataSet;
 import org.virbo.qstream.QDataSetStreamHandler;
 import org.virbo.qstream.StreamException;
@@ -49,14 +55,35 @@ public class QDataSetStreamHandlerTest {
         }
         
     }
+
+    private static void test3() throws FileNotFoundException, StreamException, MalformedURLException, IOException {
+        long t0= System.currentTimeMillis();
+        URL url= new URL("http://jfaden.net/~jbf/autoplot/data/hudson_data/qds/agg/2014-02-25_2day.qds");
+        ReadableByteChannel stream= Channels.newChannel( url.openStream() );
+        //ReadableByteChannel stream= new FileInputStream("/home/jbf/ct/hudson/data.backup/qds/junoRank3_20120923.qds").getChannel();
+        QDataSetStreamHandler handler = new QDataSetStreamHandler();
+        StreamTool.readStream( stream, handler );
+
+        System.err.println( String.format( "read in %d ms", ( System.currentTimeMillis()-t0 ) ) );
+        System.err.println( handler.getDataSet() );
+        List<String> problems= new ArrayList();
+        DataSetUtil.validate( handler.getDataSet(), problems );
+        QDataSet dts= handler.getDataSet("TimeStop") ;
+        QDataSet t= handler.getDataSet("Time");
+        for ( int i=0; i<dts.length(); i++ ) {
+            System.err.printf("%s %s\n", t.slice(i).slice(0), dts.value(i,0) );
+        }
         
+    }
+    
     public static void main( String[] args ) throws StreamException, IOException {
         //Logger.getLogger("qstream").setLevel(Level.FINER);
         //Handler h= new ConsoleHandler();
         //h.setLevel(Level.ALL);
         //Logger.getLogger("qstream").addHandler( h );
         //test1();
-        test2();
+        //test2();
+        test3();
         
     }
 }
