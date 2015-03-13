cb05dbe955ac0de590804d98f217465fe12cb1f0
Jeremy Faden <jbfaden@users.noreply.github.com>
2015-03-13 19:19:04 +0000

    check for string in BUNDLE_0, which was happening with one of the hudson tests.
    svn path=/autoplot/trunk/QDataSet/; revision=16441

--- QDataSet/src/org/virbo/dataset/DataSetUtil.java
+++ QDataSet/src/org/virbo/dataset/DataSetUtil.java
@@ -2127,18 +2127,18 @@ public class DataSetUtil {
             Object obds= ds.property(QDataSet.BUNDLE_0);
             if ( obds!=null ) {
                 QDataSet bds= (QDataSet)obds;
-                if ( ds.rank()<1 ) { // this happens with CDF slice1, when we don't completely implement slice1.
+                if ( !QDataSet.class.isInstance(obds) ) {
+                    problems.add( "BUNDLE_0 is defined but not a QDataSet");
+                } else if ( ds.rank()<1 ) { // this happens with CDF slice1, when we don't completely implement slice1.
                     problems.add( "BUNDLE_0 found but dataset is only rank 0");
+                } else if ( ds.length()!=bds.length() ) {
+                    problems.add( "ds.length() doesn't equals BUNDLE_0 length");
                 } else {
-                    if ( ds.length()!=bds.length() ) {
-                        problems.add( "ds.length() doesn't equals BUNDLE_0 length");
-                    } else {
-                        for ( int i=0; i< Math.min(1,bds.length()); i++ ) {
-                            QDataSet bds1= DataSetOps.unbundle(ds,i,true); // assumes rank1, so we have excessive work for rank>1
-                            Object o= bds1.property(QDataSet.DEPEND_1);
-                            if ( o!=null && !(o instanceof QDataSet) ) {
-                                validate( bds1,problems,1) ;
-                            }
+                    for ( int i=0; i< Math.min(1,bds.length()); i++ ) {
+                        QDataSet bds1= DataSetOps.unbundle(ds,i,true); // assumes rank1, so we have excessive work for rank>1
+                        Object o= bds1.property(QDataSet.DEPEND_1);
+                        if ( o!=null && !(o instanceof QDataSet) ) {
+                            validate( bds1,problems,1) ;
                         }
                     }
                 }
