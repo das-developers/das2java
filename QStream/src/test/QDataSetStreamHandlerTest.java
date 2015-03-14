ef6cfc21644d28a644be07a50ec42785e3b0e1c6
Jeremy Faden <jbfaden@users.noreply.github.com>
2015-03-14 15:32:05 +0000

    flatten the dataset properly in the getDataSet method.  Before this was done by the one client in Autoplot's reader.
    svn path=/autoplot/trunk/QStream/; revision=16452

--- QStream/src/test/QDataSetStreamHandlerTest.java
+++ QStream/src/test/QDataSetStreamHandlerTest.java
@@ -58,20 +58,20 @@ public class QDataSetStreamHandlerTest {
 
     private static void test3() throws FileNotFoundException, StreamException, MalformedURLException, IOException {
         long t0= System.currentTimeMillis();
-        URL url= new URL("http://jfaden.net/~jbf/autoplot/data/hudson_data/qds/agg/2014-02-25_2day.qds");
+        URL url= new URL("http://jfaden.net/~jbf/autoplot/data/hudson_data/qds/agg/2014-02-25_2day.qdst");
         ReadableByteChannel stream= Channels.newChannel( url.openStream() );
         //ReadableByteChannel stream= new FileInputStream("/home/jbf/ct/hudson/data.backup/qds/junoRank3_20120923.qds").getChannel();
         QDataSetStreamHandler handler = new QDataSetStreamHandler();
         StreamTool.readStream( stream, handler );
 
         System.err.println( String.format( "read in %d ms", ( System.currentTimeMillis()-t0 ) ) );
-        System.err.println( handler.getDataSet() );
+        QDataSet ds= handler.getDataSet();
+        System.err.println( ds );
         List<String> problems= new ArrayList();
-        DataSetUtil.validate( handler.getDataSet(), problems );
-        QDataSet dts= handler.getDataSet("TimeStop") ;
-        QDataSet t= handler.getDataSet("Time");
-        for ( int i=0; i<dts.length(); i++ ) {
-            System.err.printf("%s %s\n", t.slice(i).slice(0), dts.value(i,0) );
+        DataSetUtil.validate( ds, problems );
+        QDataSet t= handler.getDataSet("ds_1");
+        for ( int i=0; i<ds.length(); i++ ) {
+            System.err.printf("%s %s\n", t.slice(i), ds.value(i,0) );
         }
         
     }
