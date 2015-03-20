61205137f08b44de07a807891254c1b85374f499
Jeremy Faden <jbfaden@users.noreply.github.com>
2015-03-20 14:55:41 +0000

    TagGenDataSet shouldn't be a subclass of IndexGenDataSet, since it means we can't assume that a IndexGenDataSet is just Integers.
    svn path=/autoplot/trunk/QDataSet/; revision=16485

--- QDataSet/src/org/virbo/dataset/TagGenDataSet.java
+++ QDataSet/src/org/virbo/dataset/TagGenDataSet.java
@@ -14,21 +14,25 @@ import org.das2.datum.Units;
 /**
  * return a value based on the scale and offset.  For example, this
  * is used to create timetags:
+ * <blockquote><pre>
  *   TagGenDataSet( 1440, 60, 0, Units.t2000 )
+ * </pre></blockquote>
  * These are the 1440 minutely timetags in the first day of 2000-01-01.
  * @author jbf
  */
-public class TagGenDataSet extends IndexGenDataSet {
+public class TagGenDataSet extends AbstractDataSet {
     
     double scale, offset;
     Units units;
 
+    int length;
+    
     public TagGenDataSet( int length, double scale, double offset ) {
         this( length, scale, offset, null );
     }
     
     public TagGenDataSet( int length, double scale, double offset, Units units ) {
-        super( length );
+        this.length= length;
         this.scale= scale;
         this.offset= offset;
         if ( units!=null ) {
@@ -39,7 +43,12 @@ public class TagGenDataSet extends IndexGenDataSet {
             this.units= Units.dimensionless;
         }
     }
-    
+
+    @Override
+    public int rank() {
+        return 1;
+    }
+
     @Override
     public double value(int i) {
         return i * scale + offset; //TODO: check numerical stability, this is an extrapolation.
