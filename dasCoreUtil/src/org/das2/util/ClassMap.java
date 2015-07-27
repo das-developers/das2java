d1ea382b32c12bc8fd1da0127a431aac33c447f0
Jeremy Faden <jbfaden@users.noreply.github.com>
2015-07-27 20:15:11 +0000

    move getEnumElement over to dasCoreUtil.
    svn path=/dasCore/community/autoplot2011/trunk/; revision=8774

--- dasCoreUtil/src/org/das2/util/ClassMap.java
+++ dasCoreUtil/src/org/das2/util/ClassMap.java
@@ -9,10 +9,16 @@
 
 package org.das2.util;
 
+import java.lang.reflect.Field;
+import java.lang.reflect.Modifier;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collection;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.Set;
+import java.util.logging.Level;
 
 /**
  * Map that takes a Class for keys, and the get method finds the closest matching class.
@@ -92,4 +98,46 @@ public class ClassMap<T> implements Map<Class,T> {
         return map.entrySet();
     }
     
+    /**
+     * return the object or null for this string  "RED" -&gt; Color.RED
+     * @param c the class defining the target type
+     * @param ele the string representation to be interpreted for this type.
+     * @return the instance of the type.
+     */
+    public static Object getEnumElement( Class c, String ele ) {
+        int PUBLIC_STATIC_FINAL = Modifier.PUBLIC | Modifier.STATIC | Modifier.FINAL;
+        //List lvals;
+        if (c.isEnum()) {
+            Object[] vals = c.getEnumConstants();
+            for (Object o : vals) {
+                Enum e = (Enum) o;
+                if ( e.toString().equalsIgnoreCase(ele) ) return e;
+            }
+            //lvals= Arrays.asList(vals);
+        } else {
+            Field[] fields = c.getDeclaredFields();
+            //lvals= new ArrayList();
+            for ( Field f: fields ) {
+                try {
+                    String name = f.getName();
+                    if ( ( ( f.getModifiers() & PUBLIC_STATIC_FINAL) == PUBLIC_STATIC_FINAL ) ) {
+                        Object value = f.get(null);
+                        if ( value!=null && c.isInstance(value) ) {
+                            //lvals.add(value);
+                            if ( name.equalsIgnoreCase(ele) || value.toString().equalsIgnoreCase(ele) ) {
+                               return value;
+                            }
+                        }
+                    }
+                } catch (IllegalAccessException iae) {
+                    IllegalAccessError err = new IllegalAccessError(iae.getMessage());
+                    err.initCause(iae);
+                    throw err;
+                }
+            }
+        }
+        //logger.log( Level.INFO, "looking for {0}, found {1}\n", new Object[]{ele, lvals.toString()});
+        return null;
+    }
+
 }
