6ba00dfaa35a13de647de59909c84c584772b4a0
Jeremy Faden <jbfaden@users.noreply.github.com>
2015-08-03 20:41:22 +0000

    1439: add "delete selected items" to edit menu.
    svn path=/dasCore/community/autoplot2011/trunk/; revision=8781

--- dasCore/src/org/das2/components/DataPointRecorderNew.java
+++ dasCore/src/org/das2/components/DataPointRecorderNew.java
@@ -23,6 +23,7 @@ import java.awt.BorderLayout;
 import java.awt.Rectangle;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import java.io.BufferedReader;
@@ -944,6 +945,15 @@ public class DataPointRecorderNew extends JPanel {
                 table.setAutoCreateRowSorter(true);
             }
         } ) );
+        JMenuItem mi;
+        mi= new JMenuItem( new AbstractAction("Delete Selected Items") {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                int[] selectedRows = getSelectedRowsInModel();
+                deleteRows(selectedRows);         
+            }
+        } );        
+        editMenu.add( mi );        
         menuBar.add(editMenu);
 
         this.add(menuBar, BorderLayout.NORTH);
