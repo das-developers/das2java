e44f9c171dc81e83fe5623e86d10d633cd3c24e3
Jeremy Faden <jbfaden@users.noreply.github.com>
2015-09-18 22:07:24 +0000

    use Units.cdfTT2000 which includes support for the leap second.
    svn path=/autoplot/trunk/QDataSet/; revision=17494

--- QDataSet/src/org/virbo/dsutil/AsciiParser.java
+++ QDataSet/src/org/virbo/dsutil/AsciiParser.java
@@ -361,7 +361,7 @@ public class AsciiParser {
                             String[] ff= p.fields(line);
                             for ( int j=0; j<ff.length; j++ ) {
                                 if ( TimeParser.isIso8601String(ff[j]) ) {
-                                    setUnits(j,Units.us2000);
+                                    setUnits(j,Units.cdfTT2000);
                                 }
                             }
                             if ( p.tryParseRecord(lines.get(i), 0, null) ) {
@@ -390,7 +390,7 @@ public class AsciiParser {
                 for ( int i=0; i<fields.length; i++ ) {
                     if ( isIso8601Time(fields[i]) ) {
                         this.setFieldParser( i, UNITS_PARSER );
-                        this.setUnits( i,Units.t2000 );
+                        this.setUnits( i,Units.cdfTT2000 );
                     }
                 }
             }
@@ -1619,7 +1619,7 @@ public class AsciiParser {
     private void initializeUnitsByGuessing( String[] ss ) {
         for (int i = 0; i < ss.length; i++) {
             if ( isIso8601Time(ss[i].trim()) ) {
-                units[i]= Units.t2000;
+                units[i]= Units.cdfTT2000;
                 fieldParsers[i]= UNITS_PARSER;
             } else {
                 units[i] = Units.dimensionless;
