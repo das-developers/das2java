26119776a6e0148ecf5e3a335e557bb5df0c6f69
Jeremy Faden <jbfaden@users.noreply.github.com>
2015-09-30 13:49:11 +0000

    generalize LegendPosition to AnchorPosition.  Add icons.
    svn path=/dasCore/community/autoplot2011/trunk/; revision=8870

--- dasCore/src/org/das2/graph/LegendPosition.java
+++ dasCore/src/org/das2/graph/LegendPosition.java
@@ -1,8 +1,17 @@
 
 package org.das2.graph;
 
+import java.awt.Color;
+import java.awt.Graphics2D;
+import java.awt.image.BufferedImage;
 import javax.swing.Icon;
+import javax.swing.ImageIcon;
 import org.das2.components.propertyeditor.Enumeration;
+import static org.das2.graph.AnchorPosition.NE;
+import static org.das2.graph.AnchorPosition.NW;
+import static org.das2.graph.AnchorPosition.OutsideNE;
+import static org.das2.graph.AnchorPosition.SE;
+import static org.das2.graph.AnchorPosition.SW;
 
 /**
  * enumeration of legend positions
@@ -17,7 +26,24 @@ public enum LegendPosition implements Enumeration {
 
     @Override
     public Icon getListIcon() {
-        return null;
+        BufferedImage im= new BufferedImage(16,16,BufferedImage.TYPE_INT_RGB);
+        Graphics2D g= im.createGraphics();
+        g.setColor(Color.WHITE);
+        g.fillRect(0,0,16,16);
+        g.setColor(Color.GRAY);
+        g.drawRect( 2,2,10,10 );
+        if ( this==NW ) {
+            g.fillRect(2,2,5,5);
+        } else if ( this==NE ) {
+            g.fillRect(7,2,5,5);
+        } else if ( this==SW ) {
+            g.fillRect(2,7,5,5);
+        } else if ( this==SE ) {
+            g.fillRect(7,7,5,5);
+        } else if ( this==OutsideNE ) {
+            g.fillRect(12,2,5,5);
+        }
+        return new ImageIcon(im);
     }
     
 
