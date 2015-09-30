09a46f87eb925e0a8f92ca6db70cea2ded5d617e
Jeremy Faden <jbfaden@users.noreply.github.com>
2015-09-30 13:23:29 +0000

    trivial comments
    svn path=/dasCore/community/autoplot2011/trunk/; revision=8868

--- dasCore/src/org/das2/graph/LegendPosition.java
+++ dasCore/src/org/das2/graph/LegendPosition.java
@@ -1,7 +1,3 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 
 package org.das2.graph;
 
@@ -18,8 +14,8 @@ public enum LegendPosition implements Enumeration {
     SW,
     SE,
     OutsideNE;
-    // bugs with bounds prevent OutsideNE; // outside right position on plot
 
+    @Override
     public Icon getListIcon() {
         return null;
     }
