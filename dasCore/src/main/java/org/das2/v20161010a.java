8fc0c7b61d9a9518df6d83ceb2a26351e0217813
Jeremy Faden <jbfaden@users.noreply.github.com>
2016-11-10 20:52:35 +0000

    print information about the jar used.
    svn path=/core/stable/dasCore/; revision=9582

---
+++ dasCore/src/main/java/org/das2/v20161010a.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.das2;
+
+/**
+ *
+ * @author jbf
+ */
+public class v20161010a {
+    public static void main(String[] args) {
+        System.err.println("v20161010a");
+    }
+}
