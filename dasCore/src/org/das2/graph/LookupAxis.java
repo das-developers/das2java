baec13638756a86099f124f0d3b9e0d0a3a81eee
Jeremy Faden <jbfaden@users.noreply.github.com>
2016-11-17 21:22:51 +0000

    add update method that keeps track of the hard work. add check for repeating y values, which are not handled.
    svn path=/dasCore/community/autoplot2011/trunk/; revision=9598

--- dasCore/src/org/das2/graph/LookupAxis.java
+++ dasCore/src/org/das2/graph/LookupAxis.java
@@ -2,6 +2,7 @@
 package org.das2.graph;
 
 import java.awt.Color;
+import java.awt.Component;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.Rectangle;
@@ -19,9 +20,11 @@ import org.das2.datum.format.DatumFormatter;
 import org.das2.util.GrannyTextRenderer;
 import org.das2.util.LoggerManager;
 import org.virbo.dataset.DataSetOps;
+import org.virbo.dataset.DataSetUtil;
 import org.virbo.dataset.QDataSet;
 import org.virbo.dataset.SemanticOps;
 import org.virbo.dsops.Ops;
+import org.virbo.dsutil.DataSetBuilder;
 
 /**
  * This is like a TCA, but is an axis which has ticks positioned by another axis.
@@ -45,11 +48,16 @@ public class LookupAxis extends DasCanvasComponent {
     QDataSet tt;
     QDataSet ff;
     
-    
+    QDataSet xpos;
+    QDataSet fpos;
+    QDataSet xposMinor;
+    QDataSet fposMinor;
+    DatumFormatter format;
+        
     public LookupAxis( DasAxis axis ) {
         this.maxWidth=100;
         this.maxHeight=100;
-        this.axis= axis;
+        setAxis(axis);
     }
     
     public void setDataSet( QDataSet yy ) {
@@ -77,7 +85,9 @@ public class LookupAxis extends DasCanvasComponent {
         @Override
         public void propertyChange(PropertyChangeEvent evt) {
             LookupAxis.this.resize();
-            LookupAxis.this.repaint();
+            LookupAxis.this.updateTicks();
+            //LookupAxis.this.repaint();
+            
         }
     };
 
@@ -89,15 +99,28 @@ public class LookupAxis extends DasCanvasComponent {
     }
     
     
+    /**
+     * return the X positions of the Y value.
+     * TODO: Rewrite this.  IDL-like code makes this impossible to read.
+     * @param y the Y value
+     * @param xx the monotonically increasing tags
+     * @param yy rank 1 function of xx
+     * @return the X positions of the Y value.
+     */
     private static QDataSet interpWow( Datum y, QDataSet xx, QDataSet yy ) {
         QDataSet zipzip= Ops.multiply( Ops.subtract(y, yy.trim(0,yy.length()-1) ), Ops.subtract( yy.trim(1,yy.length()), y ) );
         QDataSet r= Ops.where( Ops.ge( zipzip, 0 ) ); // find the points which bracket y.
         //QDataSet t1= Ops.subtract( y, DataSetOps.applyIndex( yy, 0, r, false ) );
         //QDataSet t0= DataSetOps.applyIndex( yy, Ops.add( r,1 ) );
         //QDataSet t2= Ops.subtract( DataSetOps.applyIndex( yy, Ops.add( r,1 ) ), DataSetOps.applyIndex( yy, r ) );
+        
+        QDataSet div= Ops.subtract( DataSetOps.applyIndex( yy, Ops.add(r,1) ), DataSetOps.applyIndex( yy,r ) );
         QDataSet ff= Ops.divide( Ops.subtract( Ops.dataset(y),                            DataSetOps.applyIndex( yy,r ) ) , 
-                                 Ops.subtract( DataSetOps.applyIndex( yy, Ops.add(r,1) ), DataSetOps.applyIndex( yy,r ) ) );
-        if ( UnitsUtil.isIntervalMeasurement( SemanticOps.getUnits(xx) ) ) {
+                                 div );
+        QDataSet rzero= Ops.where( Ops.magnitude(div).equals(0) );
+        if ( rzero.length()>0 ) throw new IllegalArgumentException("yy cannot have repeating values");
+
+        if ( UnitsUtil.isIntervalMeasurement( SemanticOps.getUnits(xx) ) ) {            
             return Ops.interpolate( xx, ff );
             //return Ops.add( Ops.multiply( DataSetOps.applyIndex( xx,r ), Ops.subtract(1,ff) ), 
             //            Ops.multiply( DataSetOps.applyIndex( xx,Ops.add(r,1)), ff ) );            
@@ -113,7 +136,7 @@ public class LookupAxis extends DasCanvasComponent {
         g.setColor( Color.GRAY );
         g.fillRoundRect( 0, 0, r.width-1, r.height-1,7,7 );
         g.setColor( Color.BLACK );
-        g.drawRect( 0, 0, r.width-1, r.height-1 );
+        g.drawRoundRect( 0, 0, r.width-1, r.height-1,7,7 );
         g.drawString( message, 0, g.getFont().getSize() );
     }
     
@@ -123,8 +146,6 @@ public class LookupAxis extends DasCanvasComponent {
         if ( tickV==null ) return;
         
         DatumVector ticks= tickV.getMinorTicks();
-        Datum fmin= Ops.datum(1e20);
-        Datum fmax= Ops.datum(-1e20);
 
         if ( tt==null ) {
             drawMessage( g,"no times" );
@@ -143,8 +164,111 @@ public class LookupAxis extends DasCanvasComponent {
                 tt= Ops.putProperty(tt, QDataSet.UNITS, ticks.getUnits() );
             }
         }
+                
+        g.setColor( Color.BLACK );
+        int ascent= g.getFontMetrics().getAscent();
+        int height= this.getHeight();
+        
+        int myY= this.getY();
+        int myX= this.getX();
+
+        maxWidth= 0;
+        maxHeight= 0;
+        
+        if ( xpos==null ) {
+            updateTicks();
+        }
+        
+        if ( xpos==null ) {
+            drawMessage( g,"no ticks found!" );
+            return;
+        }
+        
+        //draw the major ticks
+            for ( int j=0; j<xpos.length(); j++ ) {
+                int ix0= -999;
+                QDataSet d = xpos.slice(j);
+                int ix = (int)axis.transform( d );
+                if ( ix==ix0 ) continue;
+                ix0= ix;
+                if ( axis.isHorizontal() ) {
+                    if ( ix<getColumn().getDMinimum() ) continue;
+                    if ( ix>getColumn().getDMaximum() ) continue;
+                    g.drawLine( ix-myX, height+1, ix-myX, height-5 );
+                } else {
+                    if ( ix<getRow().getDMinimum() ) continue;
+                    if ( ix>getRow().getDMaximum() ) continue;
+                    g.drawLine( 0, ix-myY, 5, ix-myY );
+                }
+                GrannyTextRenderer gtr= new GrannyTextRenderer( );
+                gtr.setString( g, format.format( DataSetUtil.asDatum(fpos.slice(j)) ) );
+                if ( axis.isHorizontal() ) {
+                    gtr.draw( g, ix - myX - (int)gtr.getWidth()/2, height-5-3 );
+                    int height0= (int)gtr.getHeight()+8;
+                    if ( height0>maxWidth ) maxHeight=height0;              
+                } else {
+                    gtr.draw( g, 5+3, ix+ascent/2-myY );
+                    int width0= (int)gtr.getWidth();
+                    if ( width0>maxWidth ) maxWidth=width0;
+                }
+            }
+        
+        maxHeight= Math.max( maxHeight, 6 );
+        maxWidth= Math.max( maxWidth, 6 );
+        
+        // draw the minor ticks
+            g.setColor( Color.BLACK );
+            myY= this.getY();
+            int ix0= -999;
+            for ( int j=0; j<xposMinor.length(); j++ ) {
+                QDataSet d = xposMinor.slice(j);
+                int ix = (int) axis.transform( d );
+                if ( ix==ix0 ) continue;
+                ix0= ix;
+                if ( axis.isHorizontal() ) {
+                    g.drawLine( ix-myX, height+1, ix-myX, height-3 );
+                } else {
+                    g.drawLine( 0, ix-myY, 3, ix-myY ); 
+                }
+            }
+    }
+    
+    /**
+     * the axis has changed, so update the tick positions.
+     * preconditions: the axis ticks have changed.
+     * postconditions: new x values have been calculated in xpos and fpos
+     */
+    public void updateTicks() {
+        TickVDescriptor tickV= axis.tickV;
+        if ( tickV==null ) return;
+        
+        DatumVector ticks= tickV.getMinorTicks();
+        Datum fmin= Ops.datum(1e20);
+        Datum fmax= Ops.datum(-1e20);
+
+        if ( tt==null ) {
+            logger.fine("no xtags");
+            return;
+        }
+        if ( ff==null ) {
+            logger.fine( "no data" );
+            return;
+        }
+
+        if ( !SemanticOps.getUnits(tt).isConvertibleTo(ticks.getUnits() ) ) {
+            if ( UnitsUtil.isTimeLocation(SemanticOps.getUnits(tt)) ) {
+                logger.fine( "inconvertible units" );
+                return;
+            } else {
+                tt= Ops.putProperty(tt, QDataSet.UNITS, ticks.getUnits() );
+            }
+        }
+        
+        // calculate the min and max values seen, at each of the axis minor tick positions.
+        DatumRange axisRange= axis.getDatumRange();
         for ( int i=0; i<ticks.getLength(); i++ ) {
             Datum x= ticks.get(i);
+            if ( !axisRange.contains(x) ) continue;
             QDataSet f= Ops.findex(tt, Ops.dataset(x) );
             if ( f.value()>0 && f.value()<tt.length() ) {
                 QDataSet ffint= Ops.interpolate(ff, f );
@@ -173,19 +297,16 @@ public class LookupAxis extends DasCanvasComponent {
 
         ticks= ytickvdd.boundaries( fmin, fmax );
 
-        g.setColor( Color.BLACK );
-        int ascent= g.getFontMetrics().getAscent();
-        int height= this.getHeight();
-        
-        int myY= this.getY();
-        int myX= this.getX();
         DatumRange dr= new DatumRange(fmin,fmax);
-        DatumFormatter format= DomainDividerUtil.getDatumFormatter(ytickvdd,dr);
+        format= DomainDividerUtil.getDatumFormatter(ytickvdd,dr);
 
         maxWidth= 0;
         maxHeight= 0;
         
-        //draw the major ticks
+        DataSetBuilder xposBuilder= new DataSetBuilder(1,100);
+        DataSetBuilder fposBuilder= new DataSetBuilder(1,100);
+        
+        //calculate the major ticks
         for ( int i=0; i<ticks.getLength(); i++ ) {
             Datum atick= ticks.get(i);
             QDataSet dds= interpWow(atick, tt, ff );
@@ -196,28 +317,26 @@ public class LookupAxis extends DasCanvasComponent {
                 if ( ix==ix0 ) continue;
                 ix0= ix;
                 if ( axis.isHorizontal() ) {
-                    g.drawLine( ix-myX, height+1, ix-myX, height-5 );
+                    if ( ix<getColumn().getDMinimum() ) continue;
+                    if ( ix>getColumn().getDMaximum() ) continue;
+                    xposBuilder.nextRecord(d);
+                    fposBuilder.nextRecord(atick);
+
                 } else {
-                    g.drawLine( 0, ix-myY, 5, ix-myY );
-                }
-                GrannyTextRenderer gtr= new GrannyTextRenderer( );
-                gtr.setString( g, format.format( atick ) );
-                if ( axis.isHorizontal() ) {
-                    gtr.draw( g, ix - myX - (int)gtr.getWidth()/2, height-5-3 );
-                    int height0= (int)gtr.getHeight()+8;
-                    if ( height0>maxWidth ) maxHeight=height0;              
-                } else {
-                    gtr.draw( g, 5+3, ix+ascent/2-myY );
-                    int width0= (int)gtr.getWidth();
-                    if ( width0>maxWidth ) maxWidth=width0;
+                    if ( ix<getRow().getDMinimum() ) continue;
+                    if ( ix>getRow().getDMaximum() ) continue;
+                    xposBuilder.nextRecord(d);
+                    fposBuilder.nextRecord(atick);
                 }
             }
         } 
+           
+        DataSetBuilder xposMinorBuilder= new DataSetBuilder(1,100);
+        DataSetBuilder fposMinorBuilder= new DataSetBuilder(1,100);
+        
         // draw the minor ticks
         ticks= ytickvdd.finerDivider( true ).boundaries( fmin, fmax );
         for (  int i=0; i<ticks.getLength(); i++ ) {
-            g.setColor( Color.BLACK );
-            myY= this.getY();
             Datum atick= ticks.get(i);
             QDataSet dds= interpWow(atick, tt, ff );
             int ix0= -999;
@@ -227,12 +346,26 @@ public class LookupAxis extends DasCanvasComponent {
                 if ( ix==ix0 ) continue;
                 ix0= ix;
                 if ( axis.isHorizontal() ) {
-                    g.drawLine( ix-myX, height+1, ix-myX, height-3 );
+                    if ( ix<getColumn().getDMinimum() ) continue;
+                    if ( ix>getColumn().getDMaximum() ) continue;
+                    xposMinorBuilder.nextRecord(d);
+                    fposMinorBuilder.nextRecord(atick);
+
                 } else {
-                    g.drawLine( 0, ix-myY, 3, ix-myY ); 
+                    if ( ix<getRow().getDMinimum() ) continue;
+                    if ( ix>getRow().getDMaximum() ) continue;
+                    xposMinorBuilder.nextRecord(d);
+                    fposMinorBuilder.nextRecord(atick);
                 }
             }
         }
+        
+        xpos= xposBuilder.getDataSet();
+        fpos= fposBuilder.getDataSet();
+
+        xposMinor= xposMinorBuilder.getDataSet();
+        fposMinor= fposMinorBuilder.getDataSet();
+        
     }
     
     @Override
@@ -240,6 +373,9 @@ public class LookupAxis extends DasCanvasComponent {
         if ( getColumn()==null || getColumn().getParent()==null ) { 
              return;
         }
+        if ( maxHeight<6 ) maxHeight=6;
+        if ( maxWidth<6 ) maxWidth=6;
+        
         if ( axis.isHorizontal() ) {
             int x= getColumn().getDMinimum();
             int y= getRow().getDMinimum();
