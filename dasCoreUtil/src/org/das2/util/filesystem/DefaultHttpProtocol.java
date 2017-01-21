ed648f44e51ddbbba9dafca31c26a0a47ce50e39
Jeremy Faden <jbfaden@users.noreply.github.com>
2017-01-21 13:47:48 +0000

    bug 1566: improper escaping meant that pngwalk tool was not using thumbs400 directory.   However, I'm not sure that this was ever released.
    svn path=/dasCore/community/autoplot2011/trunk/; revision=9666

--- dasCoreUtil/src/org/das2/util/filesystem/DefaultHttpProtocol.java
+++ dasCoreUtil/src/org/das2/util/filesystem/DefaultHttpProtocol.java
@@ -5,6 +5,7 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.UnsupportedEncodingException;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.net.URLConnection;
@@ -13,6 +14,9 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import org.das2.util.LoggerManager;
 import org.das2.util.monitor.ProgressMonitor;
 
 /**
@@ -21,7 +25,9 @@ import org.das2.util.monitor.ProgressMonitor;
  * @author jbf
  */
 public class DefaultHttpProtocol implements WebProtocol {
-
+    
+    private static final Logger logger= LoggerManager.getLogger( "das2.filesystem" );
+    
     @Override
     public InputStream getInputStream(WebFileObject fo, ProgressMonitor mon) throws IOException {
         if (fo.isFolder) {
@@ -31,13 +37,39 @@ public class DefaultHttpProtocol implements WebProtocol {
         return new FileInputStream(ff);
     }
 
+    /**
+     * encode each element of the path.  Slashes are left alone.
+     * @param realName
+     * @return 
+     */
+    private static String urlEncodeSansSlash( String realName ) {
+        String[] ss= realName.split("/",-2);
+        for ( int i=0; i<ss.length; i++ ) {
+            try {
+                ss[i]= URLEncoder.encode(ss[i],"UTF-8");
+            } catch (UnsupportedEncodingException ex) {
+                logger.log(Level.SEVERE, null, ex);
+            }
+        }
+        StringBuilder builder= new StringBuilder();
+        int i=0;
+        while ( ss[i].length()==0 && i<ss.length ) i++;
+        builder.append(ss[i]);
+        for ( i++; i<ss.length; i++ ) {
+            if ( ss[i].length()>0 ) {
+                builder.append("/").append(ss[i]);
+            }
+        }
+        return builder.toString();
+    }
+    
     @Override
     public Map<String, String> getMetadata(WebFileObject fo) throws IOException {
         if (!fo.wfs.getRootURL().getProtocol().equals("ftp")) {
             String realName = fo.pathname;
             boolean exists;
-
-            URL ur = new URL(fo.wfs.getRootURL(), URLEncoder.encode(realName,"UTF-8").replaceAll("\\+", "%20") );
+            URL ur = new URL(fo.wfs.getRootURL(), urlEncodeSansSlash(realName).replaceAll("\\+", "%20") );
+            //URL ur = new URL(fo.wfs.getRootURL(), URLEncoder.encode(realName,"UTF-8").replaceAll("\\+", "%20") );
             HttpURLConnection connect = (HttpURLConnection) ur.openConnection();
             connect.setRequestMethod("HEAD");
             HttpURLConnection.setFollowRedirects(false);
@@ -48,6 +80,7 @@ public class DefaultHttpProtocol implements WebProtocol {
                     connect.setRequestProperty("Cookie", hfs.getCookie());
                 }
             }
+            FileSystem.loggerUrl.log(Level.FINE, "HEAD to get metadata: {0}", new Object[] { ur } );
             connect.connect();
             HttpURLConnection.setFollowRedirects(true);
             // check for rename, which means we'll do another request
@@ -60,11 +93,12 @@ public class DefaultHttpProtocol implements WebProtocol {
                 ur = new URL(fo.wfs.getRootURL(), realName);
                 connect = (HttpURLConnection) ur.openConnection();
                 connect.setRequestMethod("HEAD");
+                FileSystem.loggerUrl.log(Level.FINE, "HEAD to get metadata after 303: {0}", new Object[] { ur } );
                 connect.connect();
             }
             exists = connect.getResponseCode() != 404;
 
-            Map<String, String> result = new HashMap<String, String>();
+            Map<String, String> result = new HashMap<>();
 
             Map<String, List<String>> fields = connect.getHeaderFields();
             for (Entry<String,List<String>> e : fields.entrySet()) {
@@ -81,11 +115,12 @@ public class DefaultHttpProtocol implements WebProtocol {
 
         } else {
             
-            Map<String, String> result = new HashMap<String, String>();
+            Map<String, String> result = new HashMap<>();
 
             URL url= new URL( fo.wfs.getRootURL(), fo.pathname );
             URLConnection urlc = url.openConnection();
             try { 
+                FileSystem.loggerUrl.log(Level.FINE, "FTP connection: {0}", new Object[] { url } );
                 urlc.connect();
                 urlc.getInputStream().close();
                 result.put( META_EXIST, "true" );
