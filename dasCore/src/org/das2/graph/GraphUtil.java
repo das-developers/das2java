2bd1e37c8bdadee0871f1df83d8cedacff885c80
Jeremy Faden <jbfaden@users.noreply.github.com>
2017-10-03 21:21:24 +0000

    add getGaussianBlurFilter
    svn path=/dasCore/community/autoplot2011/trunk/; revision=10099

--- dasCore/src/org/das2/graph/GraphUtil.java
+++ dasCore/src/org/das2/graph/GraphUtil.java
@@ -12,6 +12,8 @@ import org.das2.datum.UnitsUtil;
 import java.awt.*;
 import java.awt.geom.*;
 import java.awt.image.BufferedImage;
+import java.awt.image.ConvolveOp;
+import java.awt.image.Kernel;
 import java.text.DecimalFormat;
 import java.text.NumberFormat;
 import java.util.Locale;
@@ -815,6 +817,47 @@ public class GraphUtil {
         return ColorUtil.getRicePaperColor();
     }
 
+    /**
+     * return a Gaussian filter for blurring images.
+     * @param radius the radius filter in pixels.
+     * @param horizontal true if horizontal blur. 
+     * @return the ConvolveOp
+     */
+    public static ConvolveOp getGaussianBlurFilter(int radius,
+            boolean horizontal) {
+        if (radius < 1) {
+            throw new IllegalArgumentException("Radius must be >= 1");
+        }
+        
+        int size = radius * 2 + 1;
+        float[] data = new float[size];
+        
+        float sigma = radius / 3.0f;
+        float twoSigmaSquare = 2.0f * sigma * sigma;
+        float sigmaRoot = (float) Math.sqrt(twoSigmaSquare * Math.PI);
+        float total = 0.0f;
+        
+        for (int i = -radius; i <= radius; i++) {
+            float distance = i * i;
+            int index = i + radius;
+            data[index] = (float) Math.exp(-distance / twoSigmaSquare) / sigmaRoot;
+            total += data[index];
+        }
+        
+        for (int i = 0; i < data.length; i++) {
+            data[i] /= total;
+        }        
+        
+        Kernel kernel;
+        if (horizontal) {
+            kernel = new Kernel(size, 1, data);
+        } else {
+            kernel = new Kernel(1, size, data);
+        }
+        return new ConvolveOp(kernel, ConvolveOp.EDGE_NO_OP, null);
+    }
+        
+        
     /**
      * describe the path for debugging.
      * @param path the Path to describe
