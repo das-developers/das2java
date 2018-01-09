8021cfbf134e93dac43aa0c9a2f5b4c639e86dfb
Jeremy Faden <jbfaden@users.noreply.github.com>
2018-01-09 17:01:34 +0000

    migrate changeCount code from Autoplot's ChangesSupport to Das2's, after seeing where the write-to-png failed to wait for a second TCA resolution in test140 (http://jfaden.net/~jbf/autoplot/tests/ephemerisLoadDas2Server.vap)
    svn path=/dasCore/community/autoplot2011/trunk/; revision=10347

--- dasCore/src/org/das2/system/ChangesSupport.java
+++ dasCore/src/org/das2/system/ChangesSupport.java
@@ -5,7 +5,10 @@ package org.das2.system;
 
 import java.beans.PropertyChangeListener;
 import java.beans.PropertyChangeSupport;
+import java.lang.ref.WeakReference;
+import java.util.Collections;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.ReentrantLock;
@@ -25,7 +28,14 @@ import java.util.logging.Logger;
 public class ChangesSupport {
 
     Map<Object, Object> changesPending;
-    Object parent;
+    
+    // Number of said changes, typically 1.
+    Map<Object, Integer> changeCount;
+    
+    // threads, for debugging.
+    Map<Object,String> threads;
+    
+    WeakReference<Object> parent;
     private static final Logger logger = Logger.getLogger("das2.system");
 
     /**
@@ -35,13 +45,32 @@ public class ChangesSupport {
      * @param parent  the object this is supporting, for debugging purposes.
      */
     public ChangesSupport(PropertyChangeSupport pcs, Object parent) {
-        this.parent = parent;
-        this.changesPending = new HashMap<Object, Object>(); // client->lock
-        if (pcs == null) {
-            pcs = new PropertyChangeSupport(parent);
+        this.parent = new WeakReference<>(parent);
+        this.changesPending = new HashMap<>(); // client->lock
+        this.changeCount= new HashMap<>();
+        this.threads= new HashMap<>(); // lockObject -> thread ID.
+        if ( pcs==null ) {
+            pcs= new PropertyChangeSupport(parent);
         }
         this.propertyChangeSupport = pcs;
     }
+    
+    /**
+     * returns the clients who have registered the change.  Note this
+     * implementation only allows for one client for each lock object.
+     * @param lockObject object identifying the change.
+     * @return clients who have registered the change.
+     */
+    synchronized List<Object> whoIsChanging( Object lockObject ) {
+        String msg= "whoIsChanging "+lockObject;
+        logger.fine( msg );
+        Object client= changesPending.get(lockObject);
+        if ( client==null ) {
+            return Collections.emptyList();
+        } else {
+            return Collections.singletonList(client);
+        }
+    }    
 
     /**
      * the client knows a change will be coming, and the canvas' clients should
@@ -56,12 +85,14 @@ public class ChangesSupport {
      * @param lockObject object identifying the change.
      */
     public synchronized void registerPendingChange(Object client, Object lockObject) {
-        String msg = "registerPendingChange " + lockObject + " by " + client + "  in " + parent;
+        String msg = "registerPendingChange " + lockObject + " by " + client + "  in " + parent.get();
         logger.fine(msg);
         Object existingClient = changesPending.get(lockObject);
         if (existingClient != null) {
             if (existingClient != client) {
                 throw new IllegalStateException("lock object in use: " + lockObject + ", by " + changesPending.get(lockObject));
+            } else if ( existingClient==client ) {
+                logger.log( Level.FINE, "bug 1075: second change registered but the first was not done."); // this is somewhat harmless.  Don't bug clients with this message.
             } else {
                 //Note, it is okay to call this multiple times for the same client and lock object.
                 return;
@@ -69,6 +100,7 @@ public class ChangesSupport {
         }
         boolean oldVal = this.isPendingChanges();
         changesPending.put(lockObject, client);
+        threads.put( lockObject, Thread.currentThread().toString() );
         propertyChangeSupport.firePropertyChange(PROP_PENDINGCHANGES, oldVal, true);
     }
 
@@ -86,30 +118,63 @@ public class ChangesSupport {
      * performingChange tells that the change is about to be performed.  This
      * is a place holder in case we use a mutator lock, but currently does
      * nothing.  If the change has not been registered, it will be registered implicitly.
+     * This will increment the internal count of how many times the change
+     * ought to occur.
      * @param client the object that is mutating the bean.
      * @param lockObject an object identifying the change.  
      */
     public synchronized void performingChange(Object client, Object lockObject) {
-        Object c = changesPending.get(lockObject);
-        if (c == null || c != client) {
-            logger.log(Level.FINE, "change was not registered, automatically registering change lockObject={0}", lockObject); // TODO: this happens (for now) with lockObject=autolayout in Autoplot.
-            registerPendingChange(client, lockObject);
+        Object ownerClient= changesPending.get(lockObject);
+        if ( ownerClient==null || ownerClient!=client ) {
+            if ( ownerClient!=null && ownerClient!=client ) {
+                logger.log(Level.INFO, "performingChange by client object is not owner {0}", client );
+            }
+            registerPendingChange( client, lockObject );
         }
-        logger.log(Level.FINE, "performingChange {0} by {1}  in {2}", new Object[]{lockObject, client, parent});
+        Integer count= changeCount.get(lockObject);
+        if ( count==null ) {
+            changeCount.put( lockObject, 1 );
+        } else {
+            changeCount.put( lockObject, count+1 );
+        }
+
+        logger.log( Level.FINE, "performingChange {0} by {1}  in {2}", new Object[]{lockObject, client, parent});
     }
 
     /**
      * the change is complete, and as far as the client is concerned, 
      * the canvas is valid.
-     * @param lockObject
+     * @param client the object that is mutating the bean.
+     * @param lockObject an object identifying the change.  
      */
     public synchronized void changePerformed(Object client, Object lockObject) {
         logger.log(Level.FINE, "clearPendingChange {0} by {1}  in {2}", new Object[]{lockObject, client, parent});
-        if (changesPending.get(lockObject) == null) {
+        Integer count= changeCount.get(lockObject);
+        Object ownerClient= changesPending.get(lockObject);        
+        if ( ownerClient == null) {
             // throw new IllegalStateException( "no such lock object: "+lockObject );  //TODO: handle multiple registrations by the same client
+            logger.log(Level.INFO, "no lock object found for {0}", lockObject);
+        } else if ( ownerClient!=client ) {
+            logger.log(Level.INFO, "change performed client object is not owner {0}", ownerClient );
         }
         boolean oldVal = this.isPendingChanges();
-        changesPending.remove(lockObject);
+        if ( count==null ) {
+            logger.log(Level.INFO, "expect value for changeCount {0}, was performingChange called?", lockObject);
+            count= 0;
+        } else {
+            count= count-1;
+        }
+        
+        if ( count==0 ) {
+            changesPending.remove(lockObject);
+            threads.remove( lockObject );
+            changeCount.remove(lockObject);
+        } else if ( count>0) {
+            changeCount.put(lockObject,count);
+        } else {
+            throw new IllegalStateException("what happened here--changeCount<0!");
+        }
+        
         propertyChangeSupport.firePropertyChange(PROP_PENDINGCHANGES, oldVal, true);
     }
     public static final String PROP_PENDINGCHANGES = "pendingChanges";
@@ -120,6 +185,28 @@ public class ChangesSupport {
      */
     public boolean isPendingChanges() {
         return changesPending.size() > 0;
+    }
+    
+    /**
+     * allow check for particular change.
+     * @param lockObject object identifying the change.
+     * @return true if that particular change is pending.
+     */
+    public boolean isPendingChanges( Object lockObject ) {
+        return changesPending.containsKey(lockObject);
+    }
+    
+    /**
+     * return a map listing the pending changes.  This is a thread-safe
+     * read-only copy.
+     * @return a map listing the pending changes.
+     */
+    public synchronized Map getChangesPending() {
+        if ( changesPending.isEmpty() ) {
+            return Collections.emptyMap();            
+        } else {
+            return new HashMap(changesPending);
+        }
     }
     
     public static final String PROP_VALUEADJUSTING = "valueAdjusting";
