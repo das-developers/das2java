ebac80103765b93394cb681bd88759f612b77d19
Jeremy Faden <jbfaden@users.noreply.github.com>
2018-01-26 23:33:43 +0000

    https://sourceforge.net/p/autoplot/bugs/1950/#0745: allow either side of ratio,dimensionless in subtraction to mean implicitly the same units.
    svn path=/autoplot/trunk/QDataSet/; revision=21979

--- QDataSet/src/org/das2/qds/ops/Ops.java
+++ QDataSet/src/org/das2/qds/ops/Ops.java
@@ -446,6 +446,7 @@ public class Ops {
             result.putProperty( QDataSet.UNITS, units1 );
         } else if ( UnitsUtil.isIntervalMeasurement(units2) && !UnitsUtil.isIntervalMeasurement(units1)) {
             throw new IllegalArgumentException("cannot subtract interval measurement from ratio measurement: " + units1 + " - "+ units2 );
+
         } else if ( UnitsUtil.isRatioMeasurement(units1) && units2.isConvertibleTo(Units.dimensionless) ) {
             result= (MutablePropertyDataSet) applyBinaryOp(ds1, ds2, new BinaryOp() {
                 @Override
@@ -454,6 +455,15 @@ public class Ops {
                 }
             } );
             result.putProperty( QDataSet.UNITS, units1 );
+
+        } else if ( UnitsUtil.isRatioMeasurement(units2) && units1.isConvertibleTo(Units.dimensionless) ) {
+            result= (MutablePropertyDataSet) applyBinaryOp(ds1, ds2, new BinaryOp() {
+                @Override
+                public double op(double d1, double d2) {
+                    return d1 - d2;
+                }
+            } );
+            result.putProperty( QDataSet.UNITS, units2 );            
         } else {
             throw new IllegalArgumentException("cannot subtract: " + units1 + " - "+ units2 );
         }
