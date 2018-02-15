82a6c57062fac4145605f5965dc1e6be613b8188
Jeremy Faden <jbfaden@users.noreply.github.com>
2018-02-15 22:56:25 +0000

    add OutsideSE
    svn path=/dasCore/community/autoplot2011/trunk/; revision=10476

--- dasCore/src/org/das2/graph/AnchorPosition.java
+++ dasCore/src/org/das2/graph/AnchorPosition.java
@@ -22,11 +22,12 @@ public enum AnchorPosition implements Enumeration {
     E,
     W,
     S,
-    OutsideE,
+    OutsideE,   // right-side
     OutsideN,   // titles...
     OutsideNNW, // top-left label.
-    OutsideNNE,  // right-side label.
-    OutsideNE;  // right-side label.
+    OutsideNNE, // top-right label.
+    OutsideNE,  // right-side, upper label.
+    OutsideSE;  // right-side, lower label.
     
     @Override
     public Icon getListIcon() {
@@ -36,36 +37,57 @@ public enum AnchorPosition implements Enumeration {
         g.fillRect(0,0,16,16);
         g.setColor(Color.GRAY);
         g.drawRect( 2,2,10,10 );
-        if ( this==NW ) {
-            g.fillRect(2,2,5,5);
-        } else if ( this==NE ) {
-            g.fillRect(7,2,5,5);
-        } else if ( this==SW ) {
-            g.fillRect(2,7,5,5);
-        } else if ( this==SE ) {
-            g.fillRect(7,7,5,5);            
-        } else if ( this==Center ) {
-            g.fillRect(5,5,5,5);
-        } else if ( this==N ) {
-            g.fillRect(5,2,5,5);
-        } else if ( this==E ) {
-            g.fillRect(7,5,5,5);
-        } else if ( this==W ) {
-            g.fillRect(2,5,5,5);
-        } else if ( this==S ) {
-            g.fillRect(5,7,5,5);
-        } else if ( this==OutsideE ) {
-            g.fillRect(12,5,5,5);
-        } else if ( this==OutsideN ) {
-            g.fillRect(5,-3,5,5);
-        } else if ( this==OutsideNNW ) {
-            g.fillRect(2,-3,5,5);
-        } else if ( this==OutsideNNE ) {
-            g.fillRect(7,-3,6,5);
-        } else if ( this==OutsideNE ) {
-            g.fillRect(12,2,5,5);            
-        } else {
+        if ( null==this ) {
             g.drawString("?", 0, 14);
+        } else switch (this) {
+            case NW:
+                g.fillRect(2,2,5,5);
+                break;
+            case NE:
+                g.fillRect(7,2,5,5);
+                break;
+            case SW:
+                g.fillRect(2,7,5,5);
+                break;
+            case SE:
+                g.fillRect(7,7,5,5);
+                break;
+            case Center:
+                g.fillRect(5,5,5,5);
+                break;
+            case N:
+                g.fillRect(5,2,5,5);
+                break;
+            case E:
+                g.fillRect(7,5,5,5);
+                break;
+            case W:
+                g.fillRect(2,5,5,5);
+                break;
+            case S:
+                g.fillRect(5,7,5,5);
+                break;
+            case OutsideE:
+                g.fillRect(12,5,5,5);
+                break;
+            case OutsideN:
+                g.fillRect(5,-3,5,5);
+                break;
+            case OutsideNNW:
+                g.fillRect(2,-3,5,5);
+                break;
+            case OutsideNNE:
+                g.fillRect(7,-3,6,5);
+                break;
+            case OutsideNE:
+                g.fillRect(12,2,5,5);
+                break;
+            case OutsideSE:
+                g.fillRect(12,5,5,5);
+                break;
+            default:
+                g.drawString("?", 0, 14);
+                break;
         }
         return new ImageIcon(im);
     }
