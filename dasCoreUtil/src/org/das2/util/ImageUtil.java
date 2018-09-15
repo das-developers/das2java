92398541ad6c21e1f0e36a4d6e9963a508808fdb
Jeremy Faden <jbfaden@users.noreply.github.com>
2018-09-15 10:59:55 +0000

    move over getScaledInstance from Autoplot's Pngwalk codes
    svn path=/dasCore/community/autoplot2011/trunk/; revision=10877

--- dasCoreUtil/src/org/das2/util/ImageUtil.java
+++ dasCoreUtil/src/org/das2/util/ImageUtil.java
@@ -1,9 +1,10 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package org.das2.util;
 
+import java.awt.Graphics2D;
+import java.awt.RenderingHints;
+import java.awt.Transparency;
+import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
 import java.util.Iterator;
@@ -23,6 +24,8 @@ import org.w3c.dom.NodeList;
  */
 public class ImageUtil {
     
+    private static final Logger logger= LoggerManager.getLogger("das2.util");
+    
     /**
      * return the node containing JSON metadata showing where the plots are in images
      * produced by the Autoplot Das2 library.
@@ -31,9 +34,7 @@ public class ImageUtil {
      * @throws IOException 
      */
     public static String getJSONMetadata( File file ) throws IOException {
-        
-        Logger logger= LoggerManager.getLogger("das2.util");
-        
+                
         try (ImageInputStream iis = ImageIO.createImageInputStream(file)) {
             Iterator<ImageReader> readers = ImageIO.getImageReaders(iis);
 
@@ -69,4 +70,104 @@ public class ImageUtil {
         
         return null;
     }    
+    
+    
+    /**
+     * convenient typical use.
+     * @param img image to resize.
+     * @param thumbSize corner-to-corner size, preserving aspect ratio.
+     * @return buffered image that is thumbSize across.
+     */
+    public static BufferedImage getScaledInstance( BufferedImage img, int thumbSize ) {
+        int w0= img.getWidth();
+        int h0= img.getHeight();
+        int thumbH, thumbW;
+
+        double aspect = 1. * w0 / h0;
+        thumbH = (int) (Math.sqrt(Math.pow(thumbSize, 2) / (aspect * aspect + 1.)));
+        thumbW = (int) (thumbH * aspect);
+
+        return getScaledInstance( img, thumbW, thumbH, RenderingHints.VALUE_INTERPOLATION_BILINEAR, true );
+    }
+
+    /**
+     * Convenience method that returns a scaled instance of the
+     * provided {@code BufferedImage}.
+     *
+     * @param img the original image to be scaled
+     * @param targetWidth the desired width of the scaled instance,
+     *    in pixels
+     * @param targetHeight the desired height of the scaled instance,
+     *    in pixels
+     * @param hint one of the rendering hints that corresponds to
+     *    {@code RenderingHints.KEY_INTERPOLATION} (e.g.
+     *    {@code RenderingHints.VALUE_INTERPOLATION_NEAREST_NEIGHBOR},
+     *    {@code RenderingHints.VALUE_INTERPOLATION_BILINEAR},
+     *    {@code RenderingHints.VALUE_INTERPOLATION_BICUBIC})
+     * @param higherQuality if true, this method will use a multi-step
+     *    scaling technique that provides higher quality than the usual
+     *    one-step technique (only useful in downscaling cases, where
+     *    {@code targetWidth} or {@code targetHeight} is
+     *    smaller than the original dimensions, and generally only when
+     *    the {@code BILINEAR} hint is specified)
+     * @return a scaled version of the original {@code BufferedImage}
+     */
+    public static BufferedImage getScaledInstance(BufferedImage img,
+                                           int targetWidth,
+                                           int targetHeight,
+                                           Object hint,
+                                           boolean higherQuality)
+    {
+        int type = (img.getTransparency() == Transparency.OPAQUE) ?
+            BufferedImage.TYPE_INT_RGB : BufferedImage.TYPE_INT_ARGB;
+        BufferedImage ret = (BufferedImage)img;
+        int w, h;
+        if (higherQuality) {
+            // Use multi-step technique: start with original size, then
+            // scale down in multiple passes with drawImage()
+            // until the target size is reached
+            w = img.getWidth();
+            h = img.getHeight();
+        } else {
+            // Use one-step technique: scale directly from original
+            // size to target size with a single drawImage() call
+            w = targetWidth;
+            h = targetHeight;
+        }
+
+        int count= 0;
+        do {
+            if (higherQuality && w > targetWidth) {
+                w /= 2;
+                if (w < targetWidth) {
+                    w = targetWidth;
+                }
+            }
+
+            if (higherQuality && h > targetHeight) {
+                h /= 2;
+                if (h < targetHeight) {
+                    h = targetHeight;
+                }
+            }
+
+            BufferedImage tmp = new BufferedImage(w, h, type);
+            Graphics2D g2 = tmp.createGraphics();
+            g2.setRenderingHint(RenderingHints.KEY_INTERPOLATION, hint );
+            g2.drawImage(ret, 0, 0, w, h, null);
+            g2.dispose();
+
+            ret = tmp;
+            count++; // I noticed a case where it hung in this loop.
+            
+        } while ( count<50 && ( w != targetWidth || h != targetHeight) );
+
+        if ( count==50 ) {
+            logger.log( Level.WARNING, "ran out of iterations in imageResize" );
+        }
+
+        return ret;
+    }
+
+    
 }
