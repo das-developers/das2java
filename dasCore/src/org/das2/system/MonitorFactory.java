255f77f7a2a192d9bfb92ef62cdbfa380b314bc9
Jeremy Faden <jbfaden@users.noreply.github.com>
2018-09-17 11:37:52 +0000

    javadoc indicating that getMonitor
    svn path=/dasCore/community/autoplot2011/trunk/; revision=10879

--- dasCore/src/org/das2/system/MonitorFactory.java
+++ dasCore/src/org/das2/system/MonitorFactory.java
@@ -22,13 +22,20 @@ public interface MonitorFactory {
     ProgressMonitor getMonitor(DasCanvas canvas, String string, String string0);
     
     /**
-     * returns a monitor in the given context.  For example, if the user is waiting for a DasPlot to be drawn, then
+     * returns a new monitor in the given context.  For example, if the user is waiting for a DasPlot to be drawn, then
      * the context is the plot, and therefore a DasProgressPanel will be added on top of the plot.
+     * @param context component which needs to indicate progress during a long process
+     * @param label initial label for the task
+     * @param description 
+     * @return the monitor
      */
     ProgressMonitor getMonitor(DasCanvasComponent context, String label, String description);
 
     /**
-     * returns a monitor without regard to context.  
+     * returns a new monitor without regard to context.  
+     * @param label initial label for the task
+     * @param description
+     * @return the monitor
      */
     ProgressMonitor getMonitor(String label, String description);
     
