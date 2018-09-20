63d028a695caf349c47c4871d7f994d4fc5aa1f9
Jeremy Faden <jbfaden@users.noreply.github.com>
2018-09-20 21:14:11 +0000

    2033: commented code shows where I was trying to find a case where the server responded with data for 2000-001T00:00:00Z which was different.
    svn path=/autoplot/trunk/QDataSet/; revision=22852

--- QDataSet/src/org/das2/qds/AbstractQFunction.java
+++ QDataSet/src/org/das2/qds/AbstractQFunction.java
@@ -1,6 +1,8 @@
 
 package org.das2.qds;
 
+import org.das2.qds.ops.Ops;
+
 /**
  * Abstract class implements values and exampleOutput based on
  * value and exampleInput.
@@ -27,8 +29,19 @@ public abstract class AbstractQFunction implements QFunction {
         JoinDataSet result= new JoinDataSet( v1 );
         for ( int i=1; i<vs.length(); i++ ) {
             v1= value( vs.slice(i) );
-            if ( v1.rank()!=rank ) throw new IllegalArgumentException("incompatible datasets: two value calls result in datasets of differing rank");
-            if ( v1.length()!=len ) throw new IllegalArgumentException("incompatible datasets: two value calls result in datasets of differing lengths");
+//            //vap+das2server:http://planet.physics.uiowa.edu/das/das2Server?dataset=Voyager/1/Ephemeris/Heliographic_Inertial&start_time=1998-09-16T17:53:30.402Z&end_time=2001-01-31T14:08:19.855Z&interval=60
+//            if ( Ops.subtract( vs.slice(i).slice(0), Ops.dataset("2000-01-01T00:00:00Z" ) ).value()==0.0 ) {
+//                System.err.println( String.format( "%s: %s", vs.slice(i).slice(0).svalue(), v1.slice(0).svalue() ) );
+//                if ( !v1.slice(0).svalue().startsWith("76.1595") ) {
+//                    v1= value( vs.slice(i) );
+//                }
+//            }
+            if ( v1.rank()!=rank ) {
+                throw new IllegalArgumentException("incompatible datasets: two value calls result in datasets of differing rank");
+            }
+            if ( v1.length()!=len ) {
+                throw new IllegalArgumentException("incompatible datasets: two value calls result in datasets of differing lengths");
+            }
             result.join( v1 );
         }
         return result;
