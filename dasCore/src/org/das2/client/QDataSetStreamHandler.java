71b76d9014ed76a21daceb1d51d03815a9e5780c
Jeremy Faden <jbfaden@users.noreply.github.com>
2019-01-25 20:01:05 +0000

    getting closer, finished up most of XMultiY
    svn path=/dasCore/community/autoplot2011/trunk/; revision=11133

--- dasCore/src/org/das2/client/QDataSetStreamHandler.java
+++ dasCore/src/org/das2/client/QDataSetStreamHandler.java
@@ -1,13 +1,20 @@
 
 package org.das2.client;
 
+import java.text.ParseException;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import org.das2.datum.CacheTag;
 import org.das2.datum.Datum;
+import org.das2.datum.DatumRange;
+import org.das2.datum.DatumRangeUtil;
+import org.das2.datum.DatumUtil;
 import org.das2.datum.DatumVector;
-import org.das2.datum.Units;
 import org.das2.qds.DataSetUtil;
 import org.das2.qds.QDataSet;
+import org.das2.qds.SemanticOps;
 import org.das2.qds.ops.Ops;
 import org.das2.qds.util.DataSetBuilder;
 import org.das2.stream.PacketDescriptor;
@@ -18,6 +25,7 @@ import org.das2.stream.StreamException;
 import org.das2.stream.StreamHandler;
 import org.das2.stream.StreamMultiYDescriptor;
 import org.das2.stream.StreamYScanDescriptor;
+import org.das2.util.LoggerManager;
 
 /**
  * Write out QDataSet instead of legacy Das2 DataSet.
@@ -25,41 +33,73 @@ import org.das2.stream.StreamYScanDescriptor;
  */
 public class QDataSetStreamHandler implements StreamHandler {
 
+    private static final Logger logger= LoggerManager.getLogger("das2.dataTransfer");
+    
     Map<PacketDescriptor,DataSetBuilder> xbuilders;
     Map<PacketDescriptor,DataSetBuilder[]> builders;
     PacketDescriptor currentPd;
     DataSetBuilder[] currentBuilders;
     DataSetBuilder currentXBuilder;
+    
+    String streamTitle;
+    Map streamProperties;
         
     @Override
     public void streamDescriptor(StreamDescriptor sd) throws StreamException {
         builders= new HashMap<>();
         xbuilders= new HashMap<>();
+        streamTitle= String.valueOf( sd.getProperty("title") );
+        streamProperties= sd.getProperties();
     }
 
+    private void putProperty( DataSetBuilder builder, String name, Object value ) {
+        if ( value instanceof Datum ) {
+            logger.warning("kludge to fix Datum property values");
+            value= ((Datum)value).doubleValue(((Datum)value).getUnits());
+        }
+        if ( SemanticOps.checkPropertyType( name, value, false ) ) {
+            builder.putProperty( name, value );
+        } else {
+            logger.log(Level.WARNING, "property \"{0}\" should be type \"{1}\"", new Object[]{name, SemanticOps.getPropertyType(name)});
+        }
+    }
+    
     @Override
     public void packetDescriptor(PacketDescriptor pd) throws StreamException {
-        DataSetBuilder[] builders= new DataSetBuilder[pd.getYCount()];
+        DataSetBuilder[] lbuilders= new DataSetBuilder[pd.getYCount()];
         
         for ( int i=0; i<pd.getYCount(); i++ ) {
             SkeletonDescriptor sd= pd.getYDescriptor(i);
+            logger.log(Level.FINER, "got packet: {0}", sd);
             DataSetBuilder builder;
             if ( sd instanceof StreamYScanDescriptor ) {
                 StreamYScanDescriptor yscan= (StreamYScanDescriptor)sd;
                 builder= new DataSetBuilder(2,1000,yscan.getNItems());
+                putProperty( builder, QDataSet.UNITS, yscan.getZUnits() );
+                putProperty( builder, QDataSet.NAME, yscan.getName() );
+                
             } else if ( sd instanceof StreamMultiYDescriptor ) {
+                StreamMultiYDescriptor multiy= (StreamMultiYDescriptor)sd;
                 builder= new DataSetBuilder(1,1000);
+                putProperty( builder, QDataSet.UNITS, multiy.getUnits() );
+                putProperty( builder, QDataSet.NAME, multiy.getName() );
+                putProperty( builder, QDataSet.LABEL, multiy.getProperty("yLabel") ); // any of the following may return null.
+                putProperty( builder, QDataSet.FORMAT, multiy.getProperty("yFormat") );
+                putProperty( builder, QDataSet.VALID_MIN, multiy.getProperty("yValidMin") );
+                putProperty( builder, QDataSet.VALID_MAX, multiy.getProperty("yValidMax") );
+                putProperty( builder, QDataSet.FILL_VALUE, multiy.getProperty("yFill") );
             } else {
                 throw new IllegalArgumentException("not supported: "+sd);
             }
-            builder.putProperty( QDataSet.UNITS, Units.lookupUnits( (String)sd.getProperty( "units") ) );
-            builders[i]= builder;
+            lbuilders[i]= builder;
         }
         
         DataSetBuilder xbuilder= new DataSetBuilder(1,1000);
         xbuilder.putProperty( QDataSet.UNITS, pd.getXDescriptor().getUnits() );
+        xbuilder.putProperty( QDataSet.LABEL, streamProperties.get("xLabel") );
+            
         xbuilders.put( pd, xbuilder );
-        this.builders.put( pd, builders );
+        this.builders.put( pd, lbuilders );
     }
 
     @Override
@@ -106,14 +146,32 @@ public class QDataSetStreamHandler implements StreamHandler {
                 ds1= currentBuilders[0].getDataSet();
             } else {
                 ds1= null;
-                for ( int i=0; i<currentBuilders.length; i++ ) {
-                    ds1= Ops.bundle( ds1, currentBuilders[i].getDataSet() );
+                for (DataSetBuilder currentBuilder : currentBuilders) {
+                    ds1 = Ops.bundle(ds1, currentBuilder.getDataSet());
                 }
             }
             ds= Ops.link( xds1, ds1 );
         } else {
             throw new UnsupportedOperationException("not implemented");
         }
+        ds= Ops.putProperty( ds, QDataSet.TITLE, streamTitle );
+        Object oxCacheRange= streamProperties.get( "xCacheRange" );
+        if ( oxCacheRange!=null ) {
+            Object oxCacheResolution= streamProperties.get("xCacheResolution");
+            try {
+                CacheTag ct;
+                if ( oxCacheResolution!=null ) {
+                    ct= new CacheTag( (DatumRange)oxCacheRange, (Datum)oxCacheResolution );
+                } else {
+                    ct= new CacheTag( (DatumRange)oxCacheRange, null );
+                }
+                ds= Ops.putProperty( ds, QDataSet.CACHE_TAG, ct );
+            } catch ( RuntimeException ex) {
+                logger.log(Level.SEVERE, "unable to use properties for cacheTag", ex);
+            }
+        }
+        ds= Ops.putProperty( ds, QDataSet.USER_PROPERTIES, streamProperties );
+        
         return ds;
     }
     
