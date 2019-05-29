f74bb4676cb86703b95083cd1f848d0b03562ada
Jeremy Faden <jbfaden@users.noreply.github.com>
2019-05-29 17:09:35 +0000

    the initial value of contours can be "+.1" now
    svn path=/dasCore/community/autoplot2011/trunk/; revision=11520

--- dasCore/src/org/das2/graph/ContoursRenderer.java
+++ dasCore/src/org/das2/graph/ContoursRenderer.java
@@ -559,7 +559,7 @@ public class ContoursRenderer extends Renderer {
     /**
      * the contour locations, a comma-separated list
      */
-    private String contours = "-.7,-.6,-.5,-.4,-.3,-.2,-.1,0,0.1,0.2,0.3,0.4,0.5,0.6,0.7,0.8,0.9";
+    private String contours = "+.1";
 
     /**
      * return the contour locations, a comma-separated list
