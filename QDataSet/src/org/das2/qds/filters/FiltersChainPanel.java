c70fd07147465c91635579828b2c83577d25b69c
Jeremy Faden <jbfaden@users.noreply.github.com>
2019-08-26 20:06:12 +0000

    add binAverage2d filter process.
    svn path=/autoplot/trunk/QDataSet/; revision=24059

--- QDataSet/src/org/das2/qds/filters/FiltersChainPanel.java
+++ QDataSet/src/org/das2/qds/filters/FiltersChainPanel.java
@@ -236,6 +236,8 @@ public final class FiltersChainPanel extends javax.swing.JPanel implements Filte
             result= new HistogramFilterEditorPanel();            
         } else if ( f.matches(Histogram2dFilterEditorPanel.PROP_REGEX) ) {
             result= new Histogram2dFilterEditorPanel();            
+        } else if ( f.matches(BinAverage2dFilterEditorPanel.PROP_REGEX) ) {
+            result= new BinAverage2dFilterEditorPanel();            
         } else if ( f.matches( UnbundleFilterEditorPanel.PROP_REGEX ) ) { // TODO: FilterEditorPanel might choose to accept a filter.
             result= new UnbundleFilterEditorPanel();
         } else if ( f.matches("\\|dbAboveBackgroundDim1\\((\\S+)\\)") ) { // TODO: FilterEditorPanel might choose to accept a filter.
