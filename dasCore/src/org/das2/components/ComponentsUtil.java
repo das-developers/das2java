76fffa19952cfbbffa0a521fe61dfbcdd89319d7
Jeremy Faden <jbfaden@users.noreply.github.com>
2019-09-19 21:50:57 +0000

    2122: introduce new helper code to decide if window is visible.
    svn path=/dasCore/community/autoplot2011/trunk/; revision=11734

--- dasCore/src/org/das2/components/ComponentsUtil.java
+++ dasCore/src/org/das2/components/ComponentsUtil.java
@@ -11,24 +11,33 @@ package org.das2.components;
 import org.das2.graph.DasCanvas;
 import java.awt.BorderLayout;
 import java.awt.Component;
-import java.awt.Dialog;
-import java.awt.Frame;
+import java.awt.Dimension;
+import java.awt.GraphicsDevice;
+import java.awt.GraphicsEnvironment;
 import java.awt.Point;
+import java.awt.Rectangle;
+import java.awt.Toolkit;
 import java.awt.Window;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.swing.Box;
 import javax.swing.BoxLayout;
 import javax.swing.JButton;
 import javax.swing.JDialog;
 import javax.swing.JPanel;
 import javax.swing.SwingUtilities;
+import org.das2.datum.LoggerManager;
 
 /**
- *
+ * Utilities for managing components.
  * @author Jeremy
  */
 public class ComponentsUtil {
+    
+    private static Logger logger= LoggerManager.getLogger("das2.gui");
+            
     public static DasCanvas createPopupCanvas( Component parent, String title, int width, int height ) {
         DasCanvas canvas = new DasCanvas(width, height);
         
@@ -67,4 +76,39 @@ public class ComponentsUtil {
         
         return canvas;
     }
+    
+    /**
+     * return null if the rectangle is visible, or a new rectangle where it
+     * should be moved if it is not.
+     * @param r
+     * @return null or a new suggested rectangle.
+     */
+    public static synchronized Rectangle verifyVisible( Rectangle r ) {
+        
+        long t0= System.currentTimeMillis();
+        
+        Dimension dimensions = Toolkit.getDefaultToolkit().getScreenSize();
+        Rectangle dims= new Rectangle(0,0,dimensions.width,dimensions.height);
+        logger.log(Level.FINE, "screen dimensions: {0}", dimensions);
+
+        GraphicsEnvironment env= GraphicsEnvironment.getLocalGraphicsEnvironment();
+        GraphicsDevice[] devices = env.getScreenDevices();
+        if ( devices.length>1 ) {
+            for ( GraphicsDevice d : devices ) {
+                dims.add( d.getDefaultConfiguration().getBounds() );
+            }
+        }
+        
+        logger.log(Level.FINE, "calculate screen dimensions in ms: {0}", System.currentTimeMillis()-t0);
+        
+        Rectangle visibleRect= new Rectangle(0,0,dims.width,dims.height);
+
+        if ( visibleRect.intersects(r) ) {
+            return null;
+        } else {
+            return visibleRect;
+        }
+    }
+    
+    
 }
