d1079e2a2874dd0e6a09d97c6350b34f0f0919c4
Jeremy Faden <jbfaden@users.noreply.github.com>
2020-05-27 22:08:53 +0000

    turn antialiasing on by default.
    svn path=/dasCore/community/autoplot2011/trunk/; revision=11901

--- dasCore/src/org/das2/graph/DasCanvas.java
+++ dasCore/src/org/das2/graph/DasCanvas.java
@@ -2182,6 +2182,7 @@ public class DasCanvas extends JLayeredPane implements Printable, Editable, Scro
         @Override
         protected void paintComponent(Graphics g) {
             Graphics2D g2 = (Graphics2D) g.create();
+            g2.setRenderingHint(RenderingHints.KEY_TEXT_ANTIALIASING, RenderingHints.VALUE_TEXT_ANTIALIAS_ON );
             g2.setRenderingHint( RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON );
             if (blocking) {
                 paintLoading(g2);
