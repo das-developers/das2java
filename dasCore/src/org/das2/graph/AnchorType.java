e987439a8e50a823e191c51724b612859fb633c7
Jeremy Faden <jbfaden@users.noreply.github.com>
2020-07-14 20:31:05 +0000

    add javadoc to AnchorType
    svn path=/dasCore/community/autoplot2011/trunk/; revision=11926

--- dasCore/src/org/das2/graph/AnchorType.java
+++ dasCore/src/org/das2/graph/AnchorType.java
@@ -1,8 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package org.das2.graph;
 
 /**
@@ -10,7 +6,16 @@ package org.das2.graph;
  * @author jbf
  */
 public enum AnchorType {
-    CANVAS, // anchored by row and column
-    PLOT,   // anchored by datum ranges
-    DATA,   // anchored to the data (not implemented, same as PLOT for now).
+    /**
+     * anchored by row and column
+     */
+    CANVAS, 
+    /**
+     * anchored by datum ranges
+     */
+    PLOT,   
+    /**
+     * anchored to the data (not implemented, same as PLOT for now).
+     */
+    DATA,   
 }
