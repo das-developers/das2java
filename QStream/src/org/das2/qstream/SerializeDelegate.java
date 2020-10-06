cb248ff5c1c8e53743f948fb9adb2f3b53692157
Jeremy Faden <jbfaden@users.noreply.github.com>
2020-10-06 13:41:33 +0000

    javadoc for SerializeDelegate
    svn path=/autoplot/trunk/QStream/; revision=25179

--- QStream/src/org/das2/qstream/SerializeDelegate.java
+++ QStream/src/org/das2/qstream/SerializeDelegate.java
@@ -1,23 +1,33 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 
 package org.das2.qstream;
 
 import java.text.ParseException;
 
 /**
- *
+ * interface for serializing (formatting) and parsing objects.
  * @author jbf
  */
 public interface SerializeDelegate {
-    String format( Object o );
     
-    public Object parse( String typeId,String s  ) throws ParseException;
+    /**
+     * format the object.
+     * @param o the object
+     * @return the string representation of the object.
+     */
+    public String format( Object o );
+    
+    /**
+     * parse the object from the string
+     * @param typeId the type ID
+     * @param s the string, e.g. "royalBlue"
+     * @return the object, e.g. Color.decode("#4169E1")
+     * @throws ParseException 
+     */
+    public Object parse( String typeId, String s  ) throws ParseException;
     
     /**
      * identifier for the delegate, that is used to qualify the name of the thing formatted.
+     * @param clas the class
      * @return
      */
     String typeId( Class clas );
