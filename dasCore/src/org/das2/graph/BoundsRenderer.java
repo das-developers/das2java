92b6c8447510f0f879deb7822dc3b5dfd49b09d6
Jeremy Faden <faden@cottagesystems.com>
2022-01-31 18:38:59 -0600

    autoranging, etc.  Support for bounding qube

--- dasCore/src/org/das2/graph/BoundsRenderer.java
+++ dasCore/src/org/das2/graph/BoundsRenderer.java
@@ -7,6 +7,7 @@ import java.awt.geom.GeneralPath;
 import java.util.LinkedHashMap;
 import java.util.Map;
 import org.das2.qds.QDataSet;
+import org.das2.qds.examples.Schemes;
 import org.das2.qds.ops.Ops;
 
 /**
@@ -18,14 +19,42 @@ public class BoundsRenderer extends Renderer {
     private void expectDs() {
         throw new IllegalArgumentException("Expect rank 2 bins dataset");
     }
+
+    @Override
+    public boolean acceptsDataSet(QDataSet ds) {
+        return ds.rank()==2;
+    }
+    
+    public static QDataSet doAutorange( QDataSet ds ) {
+        if ( Schemes.isBoundingBox(ds) ) {
+            QDataSet xxx= Ops.slice0( ds,0 );
+            QDataSet yyy= Ops.slice0( ds,1 );
+            xxx= Ops.rescaleRangeLogLin( xxx, -0.1, 1.1 );
+            yyy= Ops.rescaleRangeLogLin( yyy, -0.1, 1.1 );
+            return Ops.join( xxx, yyy );            
+        } else {
+            QDataSet mins= Ops.slice1( ds,0 );
+            QDataSet maxs= Ops.slice1( ds,1 );
+            QDataSet yext= Ops.extent( mins, Ops.extent( maxs ) );
+            QDataSet xext= Ops.extent( Ops.xtags(mins), Ops.extent( Ops.xtags(maxs) ) );
+            yext= Ops.rescaleRangeLogLin( yext, -0.1, 1.1 );
+            xext= Ops.rescaleRangeLogLin( xext, -0.1, 1.1 );
+            return Ops.join( xext, yext );
+        }
+    }
+    
     
     private Color color = Color.BLACK;
 
+    private Color fillColor= Color.BLACK;
+    
     public static final String PROP_COLOR = "color";
 
+    public static final String PROP_FILL_COLOR = "fillColor";
         @Override
     public String getControl() {
         Map<String,String> controls= new LinkedHashMap();
+        controls.put( "fillColor", encodeColorControl(fillColor) );
         controls.put( "color", encodeColorControl(color) );
         return Renderer.formatControl(controls);
     }
@@ -35,6 +64,7 @@ public class BoundsRenderer extends Renderer {
     public void setControl(String s) {
         super.setControl(s);
         this.color= getColorControl( "color", color );
+        this.fillColor= getColorControl( "fillColor", fillColor );
     }    
     
     public Color getColor() {
@@ -46,17 +76,49 @@ public class BoundsRenderer extends Renderer {
         this.color = string;
         propertyChangeSupport.firePropertyChange(PROP_COLOR, oldColor, string);
     }
+
+    public Color getFillColor() {
+        return fillColor;
+    }
+    
+    public void setFillColor( Color color ) {
+        Color oldColor = this.fillColor;
+        this.fillColor = color;
+        propertyChangeSupport.firePropertyChange(PROP_COLOR, oldColor, color );
+    }
     
     @Override
     public void render(Graphics2D g1, DasAxis xAxis, DasAxis yAxis ) {
         Graphics2D g= (Graphics2D)g1;
         QDataSet d= this.getDataSet();
         if ( d.rank()!=2 ) expectDs();
-        QDataSet mins= Ops.slice1( d,0 );
-        QDataSet maxs= Ops.slice1( d,1 );
-        GeneralPath pmin= GraphUtil.getPath(xAxis,yAxis,Ops.append(mins,Ops.reverse(maxs)),false,false);
-        g.setColor( this.getColor() );
-        g.fill(pmin);
+        QDataSet mins;
+        QDataSet maxs;
+        if ( Schemes.isBoundingBox(ds) ) {
+            //mins= Ops.dataset( ds.slice(0).slice(0), ds.slice(1).slice(0) )
+            mins= Ops.link( 
+                Ops.slice0( d,0 ), 
+                Ops.join( d.slice(1).slice(0), d.slice(1).slice(0) ) );
+            maxs= Ops.link( 
+                Ops.slice0( d,0 ), 
+                Ops.join( d.slice(1).slice(1), d.slice(1).slice(1) ) );
+            GeneralPath pmin= GraphUtil.getPath(xAxis,yAxis,
+                Ops.append(mins,Ops.append(Ops.reverse(maxs),mins.slice(0))),false,false);
+            g.setColor( this.fillColor );
+            g.fill(pmin);
+            g.setColor( this.getColor() );
+            g.draw(pmin);
+        } else {
+            mins= Ops.slice1( d,0 );
+            maxs= Ops.slice1( d,1 );
+            GeneralPath pmin= GraphUtil.getPath(xAxis,yAxis,
+                Ops.append(mins,Ops.append(Ops.reverse(maxs),mins.slice(0))),false,false);
+            g.setColor( this.fillColor );
+            g.fill(pmin);
+            g.setColor( this.getColor() );
+            g.draw(pmin);
+        }
+        
     }
     
     
