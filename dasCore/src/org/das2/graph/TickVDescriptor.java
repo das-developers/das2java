5195a47d77f5cea6a5d3101a5f5ca0cefe40fa84
Jeremy Faden <faden@cottagesystems.com>
2022-08-19 06:45:05 -0500

    https://github.com/das-developers/das2java/issues/18: more experiments

--- dasCore/src/org/das2/graph/TickVDescriptor.java
+++ dasCore/src/org/das2/graph/TickVDescriptor.java
@@ -489,11 +489,9 @@ public class TickVDescriptor {
             DatumVector dvMinor = dd.finerDivider(true).boundaries(minD, maxD);
             dvMinor.getLength();
             if ( dv.getLength()>1 ) { // this should true in 99.99999% of cases
-                if ( nTicksMax>7 ) { // and we have room
-                    int numMinorPerMajor= (dvMinor.getLength()-1) / (dv.getLength()-1);
-                    if ( numMinorPerMajor==2 ) {
-                        dvMinor= dd.finerDivider(true).finerDivider(true).boundaries(minD, maxD);
-                    }
+                int numMinorPerMajor= (dvMinor.getLength()-1) / (dv.getLength()-1);
+                if ( numMinorPerMajor==2 ) {
+                    dvMinor= dd.finerDivider(true).finerDivider(true).boundaries(minD, maxD);
                 }
             }
             
