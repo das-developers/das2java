8f352933ebaff76c69c5275ae136f083d89e16a8
Jeremy Faden <faden@cottagesystems.com>
2022-09-27 08:56:20 -0500

    trivial javadoc

--- dasCore/src/org/das2/event/MouseBoxEvent.java
+++ dasCore/src/org/das2/event/MouseBoxEvent.java
@@ -27,36 +27,66 @@ import java.awt.*;
 
 public class MouseBoxEvent extends MouseDragEvent {
     
-    private Point pressPoint;
-    private Point releasePoint;
+    private final Point pressPoint;
+    private final Point releasePoint;
     
-    /** Creates a new instance of MouseBoxEvent */
+    /** 
+     * Creates a new instance of MouseBoxEvent
+     * @param source the source, typically a GUI component.
+     * @param pressPoint the mouse press point
+     * @param releasePoint the mouse release point
+     * @param isModified this is ignored.
+     */
     public MouseBoxEvent(Object source, Point pressPoint, Point releasePoint, boolean isModified) {
         super(source);
         this.pressPoint= pressPoint;
         this.releasePoint= releasePoint;
     }
     
+    /**
+     * return the point where the event started
+     * @return the point where the event started
+     */
     public Point getPressPoint() {
         return pressPoint;
     }
     
+    /**
+     * return the point locating the mouse position, which is also the release point
+     * @return the point locating the mouse position, which is also the release point
+     */
     public Point getPoint() {
         return releasePoint;
     }
     
+    /**
+     * return the leftmost, or minimum, point of the two
+     * @return the leftmost, or minimum, point of the two
+     */
     public int getXMinimum() {
         return ( pressPoint.x < releasePoint.x ) ?  pressPoint.x : releasePoint.x ;
     }
         
+    /**
+     * return the rightmost, or maximum, point of the two
+     * @return the rightmost, or maximum, point of the two
+     */
     public int getXMaximum() {
         return ( pressPoint.x > releasePoint.x ) ?  pressPoint.x : releasePoint.x ;
     }
     
+    /**
+     * return the topmost, or maximum, point of the two
+     * @return the topmost, or maximum, point of the two
+     */
     public int getYMinimum() {
         return ( pressPoint.y < releasePoint.y ) ?  pressPoint.y : releasePoint.y ;
     }
         
+    /**
+     * return the bottommost, or maximum, point of the two
+     * @return the bottommost, or maximum, point of the two
+     */
     public int getYMaximum() {
         return ( pressPoint.y > releasePoint.y ) ?  pressPoint.y : releasePoint.y ;
     }
