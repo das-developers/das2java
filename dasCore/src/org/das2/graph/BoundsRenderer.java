9d79f2ea65fb7bf6bb409ad09f7c5796c32be9bb
Jeremy Faden <faden@cottagesystems.com>
2022-11-21 06:46:45 -0600

    introduce "polar" mode which expects array of bounding boxes with units of radians.

--- dasCore/src/org/das2/graph/BoundsRenderer.java
+++ dasCore/src/org/das2/graph/BoundsRenderer.java
@@ -6,6 +6,7 @@ import java.awt.Graphics2D;
 import java.awt.Polygon;
 import java.awt.Shape;
 import java.awt.TexturePaint;
+import java.awt.geom.Arc2D;
 import java.awt.geom.Area;
 import java.awt.geom.GeneralPath;
 import java.awt.geom.Line2D;
@@ -13,10 +14,13 @@ import java.awt.geom.Path2D;
 import java.awt.geom.Rectangle2D;
 import java.util.LinkedHashMap;
 import java.util.Map;
+import org.das2.datum.Units;
 import org.das2.datum.UnitsUtil;
 import org.das2.qds.QDataSet;
 import org.das2.qds.examples.Schemes;
 import org.das2.qds.ops.Ops;
+import static java.lang.Math.cos;
+import static java.lang.Math.sin;
 
 /**
  * Draw the region bounded by the dataset.  If the dataset is a bounding box, the box is drawn.  If the
@@ -57,7 +61,7 @@ public class BoundsRenderer extends Renderer {
             QDataSet xx=null;
             QDataSet yy=null;
             for ( int i=0; i<ds.length(); i++ ) {
-                QDataSet ds1= ds.slice(0);
+                QDataSet ds1= ds.slice(i);
                 QDataSet xxx= Ops.slice0( ds1,0 );
                 QDataSet yyy= Ops.slice0( ds1,1 );
                 xx= ( xx==null ? xxx : Ops.extent( xx, xxx ) );
@@ -91,6 +95,7 @@ public class BoundsRenderer extends Renderer {
         controls.put( "fillColor", encodeColorControl(fillColor) );
         controls.put( "color", encodeColorControl(color) );
         if ( !fillTexture.isEmpty() ) controls.put( "fillTexture", fillTexture );
+        if ( polar ) controls.put( "polar", encodeBooleanControl( polar ) );
         return Renderer.formatControl(controls);
     }
     
@@ -101,6 +106,7 @@ public class BoundsRenderer extends Renderer {
         this.color= getColorControl( "color", color );
         this.fillColor= getColorControl( "fillColor", fillColor );
         this.fillTexture= getControl( "fillTexture", fillTexture );
+        this.polar= getBooleanControl( "polar", false );
     }    
     
     public Color getColor() {
@@ -137,6 +143,24 @@ public class BoundsRenderer extends Renderer {
         propertyChangeSupport.firePropertyChange(PROP_FILLTEXTURE, oldFillTexture, fillTexture);
     }
 
+    private boolean polar = false;
+
+    public static final String PROP_POLAR = "polar";
+
+    public boolean isPolar() {
+        return polar;
+    }
+
+    /**
+     * if true then transform render the data in polar coordinates
+     * @param polar 
+     */
+    public void setPolar(boolean polar) {
+        boolean oldPolar = this.polar;
+        this.polar = polar;
+        propertyChangeSupport.firePropertyChange(PROP_POLAR, oldPolar, polar);
+    }
+
     
     @Override
     public void render(Graphics2D g1, DasAxis xAxis, DasAxis yAxis ) {
@@ -161,11 +185,45 @@ public class BoundsRenderer extends Renderer {
                 //mins= Ops.dataset( ds.slice(0).slice(0), ds.slice(1).slice(0) )
                     QDataSet d1= d.slice(i);
                     Path2D.Double p= new Path2D.Double();
-                    p.moveTo( xAxis.transform(d1.slice(0).slice(0)), yAxis.transform(d1.slice(1).slice(1) ) );
-                    p.lineTo( xAxis.transform(d1.slice(0).slice(1)), yAxis.transform(d1.slice(1).slice(1) ) );
-                    p.lineTo( xAxis.transform(d1.slice(0).slice(1)), yAxis.transform(d1.slice(1).slice(0) ) );
-                    p.lineTo( xAxis.transform(d1.slice(0).slice(0)), yAxis.transform(d1.slice(1).slice(0) ) );
-                    p.lineTo( xAxis.transform(d1.slice(0).slice(0)), yAxis.transform(d1.slice(1).slice(1) ) );
+                    if ( polar ) { 
+                        double x0= xAxis.transform(0,xAxis.getUnits());
+                        double y0= yAxis.transform(0,yAxis.getUnits());
+                        QDataSet radDs= d1.slice(1); // 2.8830 to 22.495 
+                        double v1= radDs.slice(0).value();
+                        double v2= radDs.slice(1).value();
+                        QDataSet angds= d1.slice(0); // 22.363 to 78.915
+                        double a0= Ops.datum( angds.slice(0) ).value( ); // Units.radians );
+                        double a1= Ops.datum( angds.slice(1) ).value( ); // Units.radians );
+                        double r0x= ( xAxis.transform(v1,xAxis.getUnits()) ) - x0; // inner ring radius at y=0
+                        double r0y= y0 - ( yAxis.transform(v1,yAxis.getUnits()) ); // inner ring radius at x=0, equal to r0x when isotropic (round)
+                        double r1x= ( xAxis.transform(v2,xAxis.getUnits()) ) - x0; // outer ring radius at y=0
+                        double r1y= y0 - ( yAxis.transform(v2,yAxis.getUnits()) ); // outer ring radius at x=0, equal to r1x when isotropic (round)
+                    
+                        //GeneralPath gp= new GeneralPath( GeneralPath.WIND_NON_ZERO,6);
+                        
+                        p.moveTo( x0 + cos(a0) * r0x,  y0 - sin(a0) * r0y );
+                        p.lineTo( x0 + cos(a0) * r1x,  y0 - sin(a0) * r1y );
+                        
+                        Arc2D arc0 = new Arc2D.Double( x0-r1x, y0-r1y, r1x*2, r1y*2, Math.toDegrees(a0), Math.toDegrees(a1-a0), Arc2D.OPEN );
+                        p.append( arc0.getPathIterator(null), true );
+                        //p.lineTo( x0 + cos(a1) * r1x, y0 - sin(a1) * r1y );
+                        p.lineTo( x0 + cos(a1) * r0x, y0 - sin(a1) * r0y );
+                        
+                        Arc2D arc1 = new Arc2D.Double( x0-r0x, y0-r0y, r0x*2, r0y*2, Math.toDegrees(a1), Math.toDegrees(a0-a1), Arc2D.OPEN );
+                        p.append( arc1.getPathIterator(null), true );
+                        p.lineTo( x0 + cos(a0) * r0x,  y0 - sin(a0) * r0y );
+                        
+                    } else {
+                        double x0= xAxis.transform(d1.slice(0).slice(0));
+                        double x1= xAxis.transform(d1.slice(0).slice(1));
+                        double y0= yAxis.transform(d1.slice(1).slice(0));
+                        double y1= yAxis.transform(d1.slice(1).slice(1));
+                        p.moveTo( x0, y1 );
+                        p.lineTo( x1, y1 );
+                        p.lineTo( x1, y0 );
+                        p.lineTo( x0, y0 );
+                        p.lineTo( x0, y1 );
+                    }
                     pbox.append( p, false );
                     
                 }
