b699bd3de2a0c722f7d1a21f7c872b4a34fda4c5
Jeremy Faden <faden@cottagesystems.com>
2023-02-09 17:32:14 -0600

    bugfix https://github.com/das-developers/das2java/issues/45.

--- QDataSet/src/org/das2/qds/ops/Ops.java
+++ QDataSet/src/org/das2/qds/ops/Ops.java
@@ -8751,7 +8751,7 @@ public final class Ops {
         DDataSet result = DDataSet.createRank1(ds.length()/2);
         DDataSet resultDep0 = DDataSet.createRank1(ds.length()/2);
         for (int i = 0; i < ds.length()/2; i++) {
-            result.putValue(i, ( i==0 ? 1 : 2 ) * ComplexArray.magnitude2(ca,i) / binsize ); 
+            result.putValue(i, ( i==0 ? 1 : 2 ) * ComplexArray.magnitude2(ca,i) / binsize );  
             resultDep0.putValue( i, xtags[i] );
         }
         if ( invUnits!=null ) resultDep0.putProperty( QDataSet.UNITS, invUnits );
