8c931385663f3c0e35417c01432fa86d7e2783e7
Jeremy Faden <faden@cottagesystems.com>
2023-04-12 10:06:19 -0500

    https://github.com/das-developers/das2java/issues/57: added "modulo" colorbar which wraps around for angles.  Added two more requested by Ivar.

--- dasCore/src/org/das2/graph/DasColorBar.java
+++ dasCore/src/org/das2/graph/DasColorBar.java
@@ -431,6 +431,21 @@ public class DasColorBar extends DasAxis {
         public static final Type BLUE_TO_ORANGE = new Type("blue_to_orange");
 
         /**
+         * blue-white-orange is a modified BLUE_TO_ORANGE, but visits white in the middle.  Requested by iwc.
+         */
+        public static final Type BLUE_WHITE_ORANGE = new Type("blue_white_orange");
+
+        /**
+         * blue-green-white-yellow-red was provided by iwc.
+         */
+        public static final Type BLUE_GREEN_WHITE_YELLOW_RED = new Type("blue_green_white_yellow_red");
+
+        /**
+         * modulo colorbar has same start and end, so it can be used to show angles.
+         */
+        public static final Type RED_CYAN_BLUE_YELLOW_RED = new Type("red_cyan_blue_yellow_red");
+
+        /**
          * gray scale with white at the minimum (bottom) and black at the maximum.
          */
         public static final Type GRAYSCALE = new Type("grayscale");
@@ -733,6 +748,12 @@ public class DasColorBar extends DasAxis {
                 initializeRevWhiteBlueBlack(size, bottom, top);
             } else if (this == VIOLET_YELLOW ) {
                 initializeVioletYellow(size, bottom, top);
+            } else if (this == BLUE_WHITE_ORANGE ) {
+                initializeBlueWhiteOrange(size, bottom, top);
+            } else if (this == BLUE_GREEN_WHITE_YELLOW_RED ) {
+                initializeBlueGreenWhiteYellowRed(size, bottom, top);
+            } else if (this == RED_CYAN_BLUE_YELLOW_RED ) {
+                initializeRedCyanBlueYellowRed(size, bottom, top);
             } else if (this == BLUE_TO_ORANGE ) {
                 initializeBlueToOrange(size, bottom, top);
             } else if (this == SCIPY_PLASMA ) {
@@ -789,11 +810,41 @@ public class DasColorBar extends DasAxis {
             colorTable = makeColorTable( index, red, green, blue, size, bottom, top );
         }
 
+        private void initializeRedCyanBlueYellowRed( int size, int bottom, int top ) {
+            // cat | awk '{ print $3 "," }'  | xargs
+            int[] index = {     0,   64,   128, 192, 254, 255 };
+            int[] red =   {   255,    0,     0, 255, 255, 255 };
+            int[] green = {     0,  255,     0, 255,   0,  0 };
+            int[] blue =  {     0,  255,   255,   0,   0,  0 };
+            colorTable = makeColorTable( index, red, green, blue, size, bottom, top );
+            //colorTable[0] = ( colorTable[0] & 0xFFFFFF00 ) | 1;
+        }
+        
+        private void initializeBlueWhiteOrange( int size, int bottom, int top ) {
+            // cat | awk '{ print $3 "," }'  | xargs
+            int[] index = {   0,  23,  46,  69,  92, 115, 127, 139, 162, 185, 208, 231, 255 };
+            int[] red =   {   0,  25,  50, 101, 153, 204, 255, 255, 255, 255, 255, 255, 255 };
+            int[] green = {  42, 101, 153, 204, 237, 255, 255, 255, 238, 204, 153, 102, 42 };
+            int[] blue =  { 255, 255, 255, 255, 255, 255, 255, 204, 153, 101, 50, 25, 0 };
+            colorTable = makeColorTable( index, red, green, blue, size, bottom, top );
+            //colorTable[0] = ( colorTable[0] & 0xFFFFFF00 ) | 1;
+        }
+        
+        private void initializeBlueGreenWhiteYellowRed( int size, int bottom, int top ) {
+            // cat | awk '{ print $3 "," }'  | xargs
+            int [] index= {  0 , 1 , 2 , 3 , 4 , 5 , 6 , 7 , 8 , 9 , 10 , 11 , 12 , 13 , 14 , 15 , 16 , 17 , 18 , 19 , 20 , 21 , 22 , 23 , 24 , 25 , 26 , 27 , 28 , 29 , 30 , 31 , 32 , 33 , 34 , 35 , 36 , 37 , 38 , 39 , 40 , 41 , 42 , 43 , 44 , 45 , 46 , 47 , 48 , 49 , 50 , 51 , 52 , 53 , 54 , 55 , 56 , 57 , 58 , 59 , 60 , 61 , 62 , 63 , 64 , 65 , 66 , 67 , 68 , 69 , 70 , 71 , 72 , 73 , 74 , 75 , 76 , 77 , 78 , 79 , 80 , 81 , 82 , 83 , 84 , 85 , 86 , 87 , 88 , 89 , 90 , 91 , 92 , 93 , 94 , 95 , 96 , 97 , 98 , 99 , 100 , 101 , 102 , 103 , 104 , 105 , 106 , 107 , 108 , 109 , 110 , 111 , 112 , 113 , 114 , 115 , 116 , 117 , 118 , 119 , 120 , 121 , 122 , 123 , 124 , 125 , 126 , 127 , 128 , 129 , 130 , 131 , 132 , 133 , 134 , 135 , 136 , 137 , 138 , 139 , 140 , 141 , 142 , 143 , 144 , 145 , 146 , 147 , 148 , 149 , 150 , 151 , 152 , 153 , 154 , 155 , 156 , 157 , 158 , 159 , 160 , 161 , 162 , 163 , 164 , 165 , 166 , 167 , 168 , 169 , 170 , 171 , 172 , 173 , 174 , 175 , 176 , 177 , 178 , 179 , 180 , 181 , 182 , 183 , 184 , 185 , 186 , 187 , 188 , 189 , 190 , 191 , 192 , 193 , 194 , 195 , 196 , 197 , 198 , 199 , 200 , 201 , 202 , 203 , 204 , 205 , 206 , 207 , 208 , 209 , 210 , 211 , 212 , 213 , 214 , 215 , 216 , 217 , 218 , 219 , 220 , 221 , 222 , 223 , 224 , 225 , 226 , 227 , 228 , 229 , 230 , 231 , 232 , 233 , 234 , 235 , 236 , 237 , 238 , 239 , 240 , 241 , 242 , 243 , 244 , 245 , 246 , 247 , 248 , 249 , 250 , 251 , 252 , 253 , 254 , 255  } ;
+            int [] red= {  0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 6 , 17 , 28 , 39 , 50 , 61 , 72 , 83 , 94 , 105 , 117 , 126 , 136 , 146 , 156 , 166 , 171 , 178 , 185 , 192 , 198 , 204 , 209 , 215 , 221 , 227 , 232 , 238 , 244 , 249 , 255 , 255 , 255 , 255 , 255 , 255 , 255 , 255 , 255 , 255 , 255 , 255 , 255 , 255 , 255 , 255 , 255 , 255 , 255 , 255 , 255 , 255 , 255 , 255 , 255 , 255 , 255 , 255 , 255 , 255 , 255 , 255 , 255 , 255 , 255 , 255 , 255 , 255 , 255 , 255 , 255 , 255 , 255 , 255 , 255 , 255 , 255 , 255 , 255 , 255 , 255 , 255 , 255 , 255 , 255 , 255 , 255 , 255 , 255 , 255 , 255 , 255 , 255 , 255 , 255 , 255 , 255 , 255 , 255 , 255 , 255 , 255 , 255 , 255 , 255 , 255 , 255 , 255 , 255 , 255 , 255 , 255 , 255 , 255 , 255 , 255 , 255 , 255 , 255 , 255 , 255 , 255 , 255 , 254 , 253 , 252 , 251 , 250 , 249 , 247 , 244 , 241 , 239 , 237 , 234 , 229 , 224 , 220 , 215 , 211 , 207 , 202 , 198 , 193 , 189 , 184 , 180 , 176 , 171 , 167 , 162 , 158 , 154 , 149 , 145 , 140 , 136 , 132 , 127  } ;
+            int [] green= {  0 , 2 , 4 , 6 , 8 , 10 , 12 , 13 , 15 , 17 , 19 , 22 , 25 , 28 , 32 , 35 , 39 , 42 , 48 , 53 , 57 , 62 , 67 , 73 , 78 , 82 , 86 , 90 , 94 , 99 , 103 , 107 , 110 , 113 , 116 , 119 , 122 , 125 , 128 , 131 , 133 , 136 , 139 , 142 , 145 , 148 , 151 , 153 , 156 , 159 , 162 , 164 , 165 , 166 , 168 , 169 , 170 , 171 , 173 , 174 , 175 , 176 , 178 , 179 , 180 , 181 , 183 , 184 , 185 , 186 , 187 , 189 , 190 , 191 , 192 , 194 , 195 , 196 , 197 , 199 , 200 , 201 , 202 , 204 , 205 , 206 , 207 , 209 , 210 , 211 , 212 , 213 , 215 , 216 , 217 , 218 , 220 , 221 , 222 , 223 , 225 , 226 , 227 , 228 , 230 , 231 , 232 , 233 , 234 , 236 , 237 , 238 , 239 , 241 , 242 , 243 , 244 , 246 , 247 , 248 , 249 , 251 , 252 , 253 , 255 , 255 , 255 , 255 , 255 , 255 , 255 , 255 , 255 , 255 , 255 , 255 , 255 , 255 , 255 , 255 , 255 , 255 , 255 , 255 , 255 , 255 , 255 , 255 , 253 , 250 , 248 , 246 , 243 , 241 , 238 , 236 , 234 , 231 , 229 , 227 , 224 , 222 , 220 , 217 , 215 , 213 , 210 , 208 , 205 , 203 , 201 , 198 , 196 , 194 , 191 , 189 , 187 , 184 , 182 , 179 , 177 , 175 , 172 , 170 , 168 , 165 , 163 , 161 , 158 , 156 , 153 , 151 , 149 , 146 , 144 , 142 , 139 , 137 , 135 , 132 , 130 , 128 , 125 , 123 , 120 , 118 , 116 , 113 , 111 , 109 , 106 , 104 , 102 , 99 , 97 , 94 , 92 , 90 , 87 , 85 , 83 , 80 , 78 , 76 , 73 , 71 , 68 , 66 , 64 , 61 , 59 , 57 , 54 , 52 , 50 , 47 , 45 , 43 , 40 , 38 , 35 , 33 , 31 , 28 , 26 , 24 , 21 , 19 , 17 , 14 , 12 , 9 , 7 , 5 , 2 , 0  } ;
+            int [] blue= {  124 , 130 , 137 , 143 , 149 , 155 , 162 , 168 , 174 , 181 , 187 , 193 , 197 , 202 , 206 , 211 , 215 , 220 , 224 , 227 , 231 , 234 , 237 , 241 , 243 , 245 , 248 , 250 , 252 , 255 , 255 , 255 , 255 , 255 , 255 , 255 , 255 , 255 , 255 , 255 , 255 , 255 , 255 , 255 , 255 , 255 , 255 , 255 , 254 , 252 , 251 , 249 , 247 , 245 , 244 , 242 , 240 , 238 , 237 , 235 , 233 , 232 , 230 , 228 , 226 , 225 , 223 , 221 , 220 , 218 , 216 , 214 , 213 , 211 , 209 , 207 , 206 , 204 , 202 , 201 , 199 , 197 , 195 , 194 , 192 , 190 , 188 , 187 , 185 , 183 , 183 , 182 , 182 , 182 , 181 , 181 , 181 , 181 , 180 , 180 , 179 , 180 , 181 , 182 , 182 , 183 , 183 , 185 , 187 , 189 , 190 , 193 , 196 , 199 , 203 , 206 , 210 , 213 , 216 , 220 , 223 , 227 , 230 , 235 , 240 , 245 , 249 , 255 , 255 , 246 , 238 , 230 , 222 , 214 , 206 , 198 , 191 , 183 , 175 , 167 , 159 , 151 , 143 , 135 , 127 , 119 , 111 , 103 , 95 , 87 , 79 , 71 , 64 , 56 , 48 , 40 , 32 , 24 , 16 , 8 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0  } ;
+            colorTable = makeColorTable( index, red, green, blue, size, bottom, top );
+            //colorTable[0] = ( colorTable[0] & 0xFFFFFF00 ) | 1;
+        }
+        
         private void initializeBlueToOrange( int size, int bottom, int top ) {
             // cat | awk '{ print $3 "," }'  | xargs
-            int[] index = {   0, 23, 46, 69, 92, 115, 139, 162, 185, 208, 231, 255 };
-            int[] red =   {   0, 25, 50, 101, 153, 204, 255, 255, 255, 255, 255, 255 };
-            int[] green = {   42, 101, 153, 204, 237, 255, 255, 238, 204, 153, 102, 42 };
+            int[] index = {   0,  23,  46,  69,  92, 115, 139, 162, 185, 208, 231, 255 };
+            int[] red =   {   0,  25,  50, 101, 153, 204, 255, 255, 255, 255, 255, 255 };
+            int[] green = {  42, 101, 153, 204, 237, 255, 255, 238, 204, 153, 102, 42 };
             int[] blue =  { 255, 255, 255, 255, 255, 255, 204, 153, 101, 50, 25, 0 };
             colorTable = makeColorTable( index, red, green, blue, size, bottom, top );
             colorTable[0] = ( colorTable[0] & 0xFFFFFF00 ) | 1;
@@ -966,11 +1017,12 @@ public class DasColorBar extends DasAxis {
             int [] blue=  { 86, 114, 139, 142, 141, 129, 110,  87,  46,  23,  37,};
             colorTable = makeColorTable( index, red, green, blue, size, bottom, top );
         }   
-
+       
         /**
          * from the string, identify the type.
          * @param s string like "apl_rainbow_black0"
          * @return type like Type.APL_RAINBOW_BLACK0.
+         * @throws IllegalArgumentException if the type does not exist.
          */
         public static Type parse(String s) {
         	for (Type t: allTypesInOrder) {
