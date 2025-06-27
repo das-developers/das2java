e94da1d0b82b0ef253beb84a7165a183c0b8f029
Jeremy Faden <faden@cottagesystems.com>
2025-06-27 11:04:39 -0500

    bugfix: isSpec also detects $(x;

--- dasCoreDatum/src/org/das2/datum/TimeParser.java
+++ dasCoreDatum/src/org/das2/datum/TimeParser.java
@@ -1275,6 +1275,7 @@ public class TimeParser {
         if ( spec.contains("$o;")|| spec.contains("$(o;") ) return true;
         if ( spec.contains("$(periodic;")) return true;
         if ( spec.contains("$x") ) return true; // Note, $x alone will mean all time, something like year 1000-9000.
+        if ( spec.contains("$(x;") ) return true; // Note, $x alone will mean all time, something like year 1000-9000.
         return false;
     }
 
