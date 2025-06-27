531e8b3229e1a8fa53718210824504f3bee07a98
Jeremy Faden <faden@cottagesystems.com>
2025-06-27 09:13:10 -0500

    recognize $x as being valid time parser spec, since it is.

--- dasCoreDatum/src/org/das2/datum/TimeParser.java
+++ dasCoreDatum/src/org/das2/datum/TimeParser.java
@@ -1274,6 +1274,7 @@ public class TimeParser {
         if ( spec.contains(";Y=") ) return true;
         if ( spec.contains("$o;")|| spec.contains("$(o;") ) return true;
         if ( spec.contains("$(periodic;")) return true;
+        if ( spec.contains("$x") ) return true; // Note, $x alone will mean all time, something like year 1000-9000.
         return false;
     }
 
