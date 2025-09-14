736905d7221cb106ae73bd4b6a919ac2dd90ab6c
Jeremy Faden <faden@cottagesystems.com>
2025-09-14 11:04:06 -0500

    bugfix https://github.com/das-developers/das2java/issues/144: allow arbitary rotation at center

--- dasCore/src/org/das2/graph/DasAnnotation.java
+++ dasCore/src/org/das2/graph/DasAnnotation.java
@@ -889,22 +889,29 @@ public class DasAnnotation extends DasCanvasComponent {
             
             if ( rot==-90 ) {
                 gtext.translate( bb.x + bb.width - ascent, bb.y );
-                gtext.rotate( -rotate*Math.PI/180. );
+                gtext.rotate( -rot*Math.PI/180. );
 
             } else if ( rot==90  ) {
                 gtext.translate( bb.x + ascent, bb.y + bb.height );
-                gtext.rotate( -rotate*Math.PI/180. );
+                gtext.rotate( -rot*Math.PI/180. );
 
             } else if ( rot==180 ) {
                 gtext.translate( bb.x, bb.y + em + (float) ascent );
                 double midx= bb.width/2;
                 double midy= bb.height/2;
-                gtext.rotate( -rotate*Math.PI/180., midx, midy );
+                gtext.rotate( -rot*Math.PI/180., midx, midy );
                 gtext.translate( 0, bb.height );
                 
             } else if ( rot==0 ) {
                 gtext.translate( bb.x, bb.y + em + (float) ascent );
             
+            } else { 
+                // allow for center without border
+                gtext.translate( bb.x, bb.y + em + (float) ascent );
+                double midx= bb.width/2;
+                double midy= bb.height/2;
+                gtext.rotate( -rot*Math.PI/180., midx, midy );
+                gtext.setClip(null);
             }
     
             // add sunburst pattern while I try to figure out rotation. TODO: remove
