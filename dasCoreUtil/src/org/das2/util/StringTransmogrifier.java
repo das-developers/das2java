3827bb5ea6835d8e92ed4673ce81d4a51d903213
Jeremy Faden <faden@cottagesystems.com>
2026-01-19 08:00:14 -0600

    Update StringTransmogrifier.java

--- dasCoreUtil/src/org/das2/util/StringTransmogrifier.java
+++ dasCoreUtil/src/org/das2/util/StringTransmogrifier.java
@@ -1,106 +1,28 @@
+# See also https://github.com/jbfaden/Utilities/blob/main/src/com/cottagesystems/JGrep.java
 
-package org.das2.util;
+from java.util.regex import Pattern
+from org.das2.util import StringTransmogrifier
 
-import java.util.Arrays;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
+# java -jar autoplot.jar --headless --script=https://github.com/autoplot/dev/blob/master/demos/2021/20210108/transmogrify.jy  'pj(\d+)_(.+)_(n|s).(dat)'  '$1-$2-$3'
+# uptime | java -jar autoplot.jar --headless --script=https://github.com/autoplot/dev/blob/master/demos/2021/20210108/transmogrify.jy  '.+up (\d+) days.+(\d+) user.+load average: ([\d\.]+), ([\d\.]+), ([\d\.]+)'  '$1 $2 $3 $4 $5'
 
-/**
- * Apply an operation to a String to make it a String with similar meaning.  This is needed for
- * new code which indicates the orbit number, but the orbit number context is provided elsewhere 
- * on the plot.
- * 
- * https://calvinandhobbes.fandom.com/wiki/Transmogrifier_Gun?file=CalvinTransmogrifierGunWeb.jpg
- *
- * @author jbf
- */
-public class StringTransmogrifier {
-    
-    Pattern p;
-    String result;
-    int nf;
-    String[] byName;
-    int[] byIndex;
-    
-    /**
-     * Create a regex-based transmografier. The result is the template which is
-     * populated from groups of the regular expression.  The groups can be referenced
-     * by index or by name, with 
-     * <ul>
-     * <li>$&lt;num&gt; by index, where 1 is the first group number
-     * <li>$&lt;letter&gt; by 1-letter named group
-     * <li>$&lt;{name}&gt; by named group
-     * @param regex the regular expression, like "foo([a-z]+).cdf"
-     * @param result the result, like "$1"
-     */
-    public StringTransmogrifier( String regex, String result ) {
-        p= Pattern.compile(regex);
-        String[] ss= result.split("\\$");
-        nf= ss.length-1;
-        byIndex= new int[nf];
-        byName= new String[nf];
-        StringBuilder resultb= new StringBuilder(ss[0]);
-        
-        int fieldNum=0;
-        
-        for ( int i=1; i<ss.length; i++ ) {
-            if ( ss[i].length()==0 ) {
-                i++;
-                resultb.append("$");
-                nf= nf-2;
-            } else {
-                String s= ss[i];
-                if ( Character.isDigit(s.charAt(0)) ) {
-                    resultb.append("%s");
-                    if ( s.length()>1 && Character.isDigit(s.charAt(1)) ) {
-                        byIndex[fieldNum]= ( s.charAt(0) - 48 ) * 10 + ( s.charAt(1) - 48 );
-                        resultb.append(s.substring(2));
-                    } else {
-                        byIndex[fieldNum]= ( s.charAt(0) - 48 );
-                        resultb.append(s.substring(1));
-                    }
-                } else if ( s.charAt(0)=='{' ) {
-                    int i2= s.indexOf("}");
-                    byName[fieldNum]= s.substring(1,i2);
-                    resultb.append("%s");
-                    resultb.append(s.substring(i2+1));
-                } else {
-                    byName[fieldNum]= s.substring(0,1);
-                    resultb.append("%s");
-                    resultb.append(s.substring(1));
-                }       
-                fieldNum++;
-            }
-        }
-        if ( fieldNum<byName.length ) {
-            byName= Arrays.copyOf( byName, fieldNum );
-            byIndex= Arrays.copyOf( byIndex, fieldNum );
-            nf= fieldNum;
-        }
-        this.result= resultb.toString();
-        
-    }
-    
-    private String[] getFields( Matcher m ) {
-        if ( nf==-1 ) throw new IllegalArgumentException("getFields cannot be called until transmogrify is called");
-        String[] result= new String[nf];
-        for ( int i=0; i<nf; i++ ) {
-            if ( byName[i]!=null ) {
-                result[i]= m.group(byName[i]);
-            } else {
-                result[i]= m.group(byIndex[i]);
-            }
-        }
-        return result;
-    }
-    
-    public String transmogrify( String string ) {
-        Matcher m= p.matcher(string);
-        if ( m.matches() ) {
-            String[] args= getFields(m);
-            return String.format( result, (Object[])args );
-        } else {
-            return result;
-        }
-    }
-}
+setScriptLabel('Transmography')
+setScriptDescription("""Transmogriphy command takes a regular expression and a
+format specification and converts each line of the input (on stdin) by matching
+it and then formatting it. <p>java -jar autoplot.jar --script=transmogrify.jy mod_(\d+).txt '$1'
+Note Autoplot/Das2 now has a StringTransmogriphy class which can be used.
+See https://github.com/das-developers/das2java/blob/main/dasCoreUtil/src/org/das2/util/StringTransmogrifier.java
+""")
+inp= getParam( 'arg_1', 'pj(\d+)_(.+)_(n|s).(dat)', 'The regular expression' )
+outp= getParam( 'arg_2', '$1 $2 $3', 'print these to stdout' )
+ins= getParam( 'arg_3', '', 'line to run through, or empty string (the default) for stdin', { 'examples':['pj1_5_n.dat',''] } )
+
+import sys
+
+st= StringTransmogrifier( inp, outp )
+if ins!='':        
+    print st.transmogrify( ins )
+else:
+    for line in sys.stdin:
+        print st.transmogrify(line.strip())
+
