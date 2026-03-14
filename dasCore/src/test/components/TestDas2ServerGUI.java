1acd9d3867a4e636a18a16f89ebbdeae0fe56a2c
Jeremy Faden <faden@cottagesystems.com>
2026-03-14 09:18:18 -0500

    add test for Das2Server params, which can be called from Jenkins

---
+++ dasCore/src/test/components/TestDas2ServerGUI.java
@@ -0,0 +1,126 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package test.components;
+
+import java.awt.Dimension;
+import java.awt.Graphics2D;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import javax.imageio.ImageIO;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.SwingUtilities;
+import org.das2.client.Das2ServerGUI;
+
+/**
+ *
+ * @author jbf
+ */
+public class TestDas2ServerGUI {
+
+    private static void renderGUI( File pngFile, String dsdf, String params ) throws Exception {
+        Das2ServerGUI x = new Das2ServerGUI();
+        x.setSpecification(dsdf);
+        x.setParameters(params);
+        SwingUtilities.invokeAndWait(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    JPanel p= x.getPanel();
+                    renderComponentToPng( p, pngFile );
+                    System.err.println("render to "+pngFile);
+                } catch (Exception e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        });
+    }
+    
+    private static void renderComponentToPng(JPanel panel, File file) throws Exception {
+        Dimension size = panel.getPreferredSize();
+        if (size == null || size.width <= 0 || size.height <= 0) {
+            size = new Dimension(400, 300);
+        }
+
+        panel.setSize(size);
+        panel.doLayout();
+
+        layoutRecursively(panel);
+
+        BufferedImage image = new BufferedImage(
+                size.width,
+                size.height,
+                BufferedImage.TYPE_INT_ARGB
+        );
+
+        Graphics2D g2 = image.createGraphics();
+        try {
+            panel.paint(g2);
+        } finally {
+            g2.dispose();
+        }
+
+        ImageIO.write(image, "png", file);
+    }
+
+    private static void layoutRecursively(java.awt.Container c) {
+        c.doLayout();
+        for (java.awt.Component child : c.getComponents()) {
+            if (child instanceof java.awt.Container) {
+                layoutRecursively((java.awt.Container) child);
+            }
+        }
+    }
+    
+    
+    public static void testCase( int test ) throws Exception {
+        String dsdf;
+        String params0;
+        switch (test) {
+            case 1:
+                dsdf = "param_01 = '1.5V_REF | Simulate +1.8 monitor'\n"
+                        + "param_02 = '1.5V_WvFE'\n"
+                        + "param_03 = '1.5V_Y180'\n"
+                        + "param_04 = '1.8U | Power Supply'\n"
+                        + "param_05 = '1.8V_MEM'";
+                params0 = "1.5V_REF 1.8V_MEM Extra-Unrecognized";
+                break;
+            case 2:
+                dsdf = "param_01 = 'packet_ids|Output data from the following packet IDs (defaults to all)|@|set: , x242 x252'\n";
+                params0 = "";
+                break;
+            case 3:
+                dsdf= "param_01='sensor|Select the sensor to output| sensor=@ | option: Eu Ev Ew Bu Bv Bw'\n"
+                    + "param_02='fftlen|Select the number of points in the DFT | fftlen=@ | range: 16 to 208896'\n"
+                    + "param_03='overlap|Select the percentage overlap for each DFT | overlap=@ | range: 0 to 90'\n"
+                    + "param_04='adj-overlap|Adjacent Overlap | adj-overlap=@ | option: true false'";
+                params0= "sensor=Bu overlap=50 adj-overlap=false";
+                break;
+            default:
+                throw new IllegalArgumentException("bad test number");
+        }
+        
+        File f= new File( String.format( "/tmp/ap/%03d.png", test ) );
+        if ( true ) {
+            renderGUI( f, dsdf, params0 );
+        } else {
+            Das2ServerGUI x = new Das2ServerGUI();
+            x.setSpecification(dsdf);
+            x.setParameters(params0);
+
+            if (JOptionPane.OK_OPTION == JOptionPane.showConfirmDialog(null, x.getPanel(), "Edit params", JOptionPane.OK_CANCEL_OPTION)) {
+                System.err.println(x.getParameters());
+            }
+        }
+        
+    }
+    
+    public static void main(String[] args) throws Exception {
+        testCase(1);
+        testCase(2);
+        testCase(3);
+        
+    }
+}
