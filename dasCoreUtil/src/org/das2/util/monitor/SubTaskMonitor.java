fa6da3c2b6c3ead7dd7812c7b8be1c00a5dacbad
Jeremy Faden <faden@cottagesystems.com>
2026-08-31 08:10:41 -0500

    https://github.com/das-developers/das2java/issues/185: recent changes which call started on the subtaskMonitor's parent breaks contract because no one calls "finished" automatically.

--- dasCoreUtil/src/org/das2/util/monitor/SubTaskMonitor.java
+++ dasCoreUtil/src/org/das2/util/monitor/SubTaskMonitor.java
@@ -72,8 +72,9 @@ public class SubTaskMonitor implements ProgressMonitor {
      * @param cancelChecked true if we should check the parent's cancel status
      */
     private SubTaskMonitor( ProgressMonitor parent, long min, long max, boolean cancelChecked ) {
-        if ( !parent.isStarted() ) parent.started();
-        if ( min!=-1 ) parent.setTaskProgress(min);
+        if ( parent.isStarted() ) {
+            if ( min!=-1 ) parent.setTaskProgress(min);
+        }
         this.parent= parent;
         this.min= min;
         this.max= max;
