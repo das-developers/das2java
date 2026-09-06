5cae381a98058bc72b288a7dcd887814dc78ddc2
faden@cottagesystems.com <faden@cottagesystems.com>
2026-09-06 10:02:09 -0500

    add ELLIPSE to BorderType so that annotations which are circles around data feature can be drawn

--- dasCore/src/org/das2/graph/BorderType.java
+++ dasCore/src/org/das2/graph/BorderType.java
@@ -9,5 +9,5 @@ package org.das2.graph;
  * Border types.
  */
 public enum BorderType {
-    NONE, RECTANGLE, ROUNDED_RECTANGLE, UNDERSCORE
+    NONE, RECTANGLE, ROUNDED_RECTANGLE, UNDERSCORE, ELLIPSE
 }
