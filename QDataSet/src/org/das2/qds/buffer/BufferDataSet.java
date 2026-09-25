c68c18fe0203553dc7383c25533837e973b0e590
Jeremy Faden <faden@cottagesystems.com>
2026-09-25 18:07:16 -0500

    move parseLong to BufferDataSet, since it is useful in this context too.

--- QDataSet/src/org/das2/qds/buffer/BufferDataSet.java
+++ QDataSet/src/org/das2/qds/buffer/BufferDataSet.java
@@ -151,6 +151,49 @@ public abstract class BufferDataSet extends AbstractDataSet implements WritableD
      */
     public final static Object UBYTE= "ubyte";
     
+    /**
+     * Parse simple integer expressions in the string.  This will parse the following:<ul>
+     * <li>42
+     * <li>40+2
+     * <li>23+10*4
+     * <li>0x20  (32 decimal)
+     * <li>0x20+4  (36 decimal)
+     * </ul>
+     * @param sval the integer expression
+     * @return the integer value.
+     */
+    public long parseLong( String sval ) {
+        String[] ssum= sval.split("\\+");
+        if ( ssum.length==1 ) {
+            String[] sprod= sval.split("\\*");
+            if ( sprod.length==1 ) {
+                if ( sval.startsWith("0x") ) {
+                    return Long.parseLong(sval.substring(2),16);
+                } else {
+                    int result = Integer.parseInt(sval);
+                    return result;
+                }
+            } else {
+                long prod= parseLong(sprod[0]);
+                for ( int i=1; i<sprod.length; i++ ) {
+                    prod *= parseLong(sprod[i]);
+                }
+                return prod;
+            }
+        } else {
+            long sum=parseLong(ssum[0]);
+            for ( int i=1; i<ssum.length; i++ ) {
+                sum += parseLong(ssum[i]);
+            }
+            return sum;
+        }
+    }
+    
+    /**
+     * return the number of bits in the type.
+     * @param type NYBBLE, DOUBLE, FLOAT, UBYTE, TIME28, etc.
+     * @return 4, 8, 4, 1, etc.
+     */
     public static int bitCount( Object type ) {
         if ( type.equals(NYBBLE) ) {
             return 4; 
